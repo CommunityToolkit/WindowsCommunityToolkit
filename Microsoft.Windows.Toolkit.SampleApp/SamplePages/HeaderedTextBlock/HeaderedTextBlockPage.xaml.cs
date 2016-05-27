dae6f646c9d111f4ac71c32a0f2d318aba9a058b
James Croft <jamz_c@hotmail.co.uk>
2016-05-27 21:13:36 +0100

    Added HeaderedTextBlock and sample

---
+++ Microsoft.Windows.Toolkit.SampleApp/SamplePages/HeaderedTextBlock/HeaderedTextBlockPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Windows.Toolkit.SampleApp.Models;
+
+using Windows.UI.Xaml.Navigation;
+
+namespace Microsoft.Windows.Toolkit.SampleApp.SamplePages
+{
+    public sealed partial class HeaderedTextBlockPage
+    {
+        public HeaderedTextBlockPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called on navigating to the Pivorama sample page.
+        /// </summary>
+        /// <param name="e">
+        /// The navigation event arguments.
+        /// </param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var propertyDesc = e.Parameter as PropertyDescriptor;
+
+            if (propertyDesc != null)
+            {
+                DataContext = propertyDesc.Expando;
+            }
+        }
+    }
+}
---
+++ Microsoft.Windows.Toolkit.UI.Controls/HeaderedTextBlock/HeaderedTextBlock.cs
@@ -0,0 +1,65 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.Windows.Toolkit.UI.Controls
+{
+    /// <summary>
+    /// Defines a control for providing a header for read-only text.
+    /// </summary>
+    [TemplatePart(Name = "HeaderContent", Type = typeof(TextBlock))]
+    public partial class HeaderedTextBlock : Control
+    {
+        private TextBlock _headerContent;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HeaderedTextBlock"/> class.
+        /// </summary>
+        public HeaderedTextBlock()
+        {
+            DefaultStyleKey = typeof(HeaderedTextBlock);
+        }
+
+        /// <summary>
+        /// Called when applying the control template.
+        /// </summary>
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _headerContent = GetTemplateChild("HeaderContent") as TextBlock;
+
+            UpdateVisibility();
+        }
+
+        private void UpdateHeader()
+        {
+            if (_headerContent != null)
+            {
+                UpdateVisibility();
+            }
+        }
+
+        private void UpdateVisibility()
+        {
+            if (_headerContent != null)
+            {
+                _headerContent.Visibility = string.IsNullOrWhiteSpace(_headerContent.Text)
+                                                     ? Visibility.Collapsed
+                                                     : Visibility.Visible;
+            }
+        }
+
+        private void UpdateForOrientation(Orientation orientationValue)
+        {
+            switch (orientationValue)
+            {
+                case Orientation.Vertical:
+                    VisualStateManager.GoToState(this, "Vertical", true);
+                    break;
+                case Orientation.Horizontal:
+                    VisualStateManager.GoToState(this, "Horizontal", true);
+                    break;
+            }
+        }
+    }
+}
