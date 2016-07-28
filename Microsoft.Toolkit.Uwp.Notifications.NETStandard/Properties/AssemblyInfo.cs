a0c53047bcf92870f4f15ef1af51af93cc0fa952
David Catuhe <david.catuhe@live.fr>
2016-07-28 13:46:48 -0700

    Projects renamed

--- Microsoft.Toolkit.Uwp.Notifications.NETStandard/Properties/AssemblyInfo.cs
+++ Microsoft.Toolkit.Uwp.Notifications.NETStandard/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Windows.Toolkit.Notifications")]
+[assembly: AssemblyTitle("Microsoft.Toolkit.uwp.Notifications")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Windows.Toolkit.Notifications")]
+[assembly: AssemblyProduct("Microsoft.Toolkit.uwp.Notifications")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
