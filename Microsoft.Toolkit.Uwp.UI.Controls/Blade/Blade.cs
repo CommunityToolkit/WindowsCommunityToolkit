97438f5dd7aed3ffb18d5b1e934186bf85e4c9ed
NicoVermeir <nico_vermeir@hotmail.com>
2016-08-18 07:46:16 +0200

    Added BladeControl and Blade classes to the controls project

---
+++ Microsoft.Toolkit.Uwp.UI.Controls/Blade/Blade.cs
@@ -0,0 +1,44 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls.Blade
+{
+    /// <summary>
+    /// The Blade is used as a child in the BladeControl
+    /// </summary>
+    [TemplatePart(Name = "CloseButton", Type = typeof(Button))]
+    public partial class Blade : Control
+    {
+        private Button _closeButton;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Blade"/> class.
+        /// </summary>
+        public Blade()
+        {
+            DefaultStyleKey = typeof(Blade);
+        }
+
+        /// <summary>
+        /// Override default OnApplyTemplate to capture child controls
+        /// </summary>
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _closeButton = GetTemplateChild("CloseButton") as Button;
+
+            if (_closeButton == null)
+            {
+                return;
+            }
+
+            _closeButton.Tapped += CloseButtonOnTap;
+        }
+
+        private void CloseButtonOnTap(object sender, RoutedEventArgs routedEventArgs)
+        {
+            IsOpen = false;
+        }
+    }
+}
