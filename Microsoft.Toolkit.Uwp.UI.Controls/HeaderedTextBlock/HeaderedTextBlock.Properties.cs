ab7c3f590dd9feb503e776e38f5540a8eef8053d
Thomas Nigro <tn@thomasnigro.fr>
2016-08-18 12:35:40 +0200

    HeaderedTextBlock: if Text is empty, hide its TextBlock
    Rationale: we don't always have the Text property set (I'm thinking of scenarios
    involving incomplete translation strings)

--- Microsoft.Toolkit.Uwp.UI.Controls/HeaderedTextBlock/HeaderedTextBlock.Properties.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/HeaderedTextBlock/HeaderedTextBlock.Properties.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             nameof(Text),
             typeof(string),
             typeof(HeaderedTextBlock),
-            new PropertyMetadata(null));
+            new PropertyMetadata(null, (d, e) => { ((HeaderedTextBlock)d).UpdateText(); }));
 
         /// <summary>
         /// Defines the <see cref="Orientation"/> dependency property.
--- Microsoft.Toolkit.Uwp.UI.Controls/HeaderedTextBlock/HeaderedTextBlock.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/HeaderedTextBlock/HeaderedTextBlock.cs
@@ -10,6 +10,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
     public partial class HeaderedTextBlock : Control
     {
         private TextBlock _headerContent;
+        private TextBlock _textContent;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HeaderedTextBlock"/> class.
@@ -27,6 +28,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             base.OnApplyTemplate();
 
             _headerContent = GetTemplateChild("HeaderContent") as TextBlock;
+            _textContent = GetTemplateChild("TextContent") as TextBlock;
 
             UpdateVisibility();
         }
@@ -39,6 +41,14 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             }
         }
 
+        private void UpdateText()
+        {
+            if (_textContent != null)
+            {
+                UpdateVisibility();
+            }
+        }
+
         private void UpdateVisibility()
         {
             if (_headerContent != null)
@@ -46,6 +56,13 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
                 _headerContent.Visibility = string.IsNullOrWhiteSpace(_headerContent.Text)
                                                      ? Visibility.Collapsed
                                                      : Visibility.Visible;
+            }
+
+            if (_textContent != null)
+            {
+                _textContent.Visibility = string.IsNullOrWhiteSpace(_textContent.Text)
+                                                    ? Visibility.Collapsed
+                                                    : Visibility.Visible;
             }
         }
 
