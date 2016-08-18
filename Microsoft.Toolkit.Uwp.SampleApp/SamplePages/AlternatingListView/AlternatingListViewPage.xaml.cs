aec20ada66b796d42174981f567ff50e9a25917d
Depechie <glenn.versweyveld@gmail.com>
2016-08-18 23:43:12 +0200

    Adding AlternatingListView

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/AlternatingListView/AlternatingListViewPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Toolkit.Uwp.SampleApp.Models;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    public sealed partial class AlternatingListViewPage : Page
+    {
+        public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
+
+        public AlternatingListViewPage()
+        {
+            this.InitializeComponent();
+            AddItems();
+        }
+
+        private void AddItems()
+        {
+            Items.Clear();
+            for (int i = 1; i < 11; i++)
+            {
+                Items.Add(new Item { Title = $"Item {i}" });
+            }
+        }
+    }
+}
---
+++ Microsoft.Toolkit.Uwp.UI.Controls/AlternatingListView/AlternatingListView.Properties.cs
@@ -0,0 +1,47 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
+{
+    /// <summary>
+    /// Defines the properties for the <see cref="AlternatingListView"/> control.
+    /// </summary>
+    public partial class AlternatingListView
+    {
+        /// <summary>
+        /// Defines the <see cref="OddRowBackground"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty OddRowBackgroundProperty = DependencyProperty.Register(
+            nameof(OddRowBackground),
+            typeof(Brush),
+            typeof(AlternatingListView),
+            new PropertyMetadata(null));
+
+        /// <summary>
+        /// Defines the <see cref="EvenRowBackground"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty EvenRowBackgroundProperty = DependencyProperty.Register(
+            nameof(EvenRowBackground),
+            typeof(Brush),
+            typeof(AlternatingListView),
+            new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets the odd row background brush.
+        /// </summary>
+        public Brush OddRowBackground
+        {
+            get { return (Brush)GetValue(OddRowBackgroundProperty); }
+            set { SetValue(OddRowBackgroundProperty, (Brush)value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the even row background brush.
+        /// </summary>
+        public Brush EvenRowBackground
+        {
+            get { return (Brush)GetValue(EvenRowBackgroundProperty); }
+            set { SetValue(EvenRowBackgroundProperty, (Brush)value); }
+        }
+    }
+}
