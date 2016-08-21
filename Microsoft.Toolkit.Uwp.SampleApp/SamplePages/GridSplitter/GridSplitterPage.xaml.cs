45f56e93f1d298b0cd2e9193d97ae4991653844e
Ibraheem Osama <ibraheem.osama@gmail.com>
2016-08-21 19:30:13 +0200

    Removing empty template comments

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/GridSplitter/GridSplitterPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/GridSplitter/GridSplitterPage.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class GridSplitterPage : Page
     {
         public GridSplitterPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
