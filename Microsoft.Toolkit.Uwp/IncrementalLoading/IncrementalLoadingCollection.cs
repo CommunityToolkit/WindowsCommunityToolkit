c843665d0677451b1bbcdecf1c4a9dfddb5d21b6
Marco Minerva <marco.minerva@gmail.com>
2016-08-30 20:36:38 +0200

    Add ISupportIncrementalLoading implementation

---
+++ Microsoft.Toolkit.Uwp/IncrementalLoading/IncrementalLoadingCollection.cs
@@ -0,0 +1,104 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Microsoft.Toolkit.Uwp
+{
+    /// <summary>
+    /// This class represents an <see cref="ObservableCollection{IType}"/> which items can be loaded incrementally.
+    /// </summary>
+    /// <typeparam name="TSource">
+    /// The data source that must be loaded incrementally.
+    /// </typeparam>
+    /// <typeparam name="IType">
+    /// The type of collection items.
+    /// </typeparam>
+    /// <seealso cref="IIncrementalSource{TSource}"/>
+    /// <seealso cref="ISupportIncrementalLoading"/>
+    public class IncrementalLoadingCollection<TSource, IType> : ObservableCollection<IType>,
+         ISupportIncrementalLoading
+         where TSource : IIncrementalSource<IType>, new()
+    {
+        private readonly TSource source;
+        private readonly int itemsPerPage;
+        private int currentPageIndex;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IncrementalLoadingCollection{TSource, IType}"/> class, optionally specifying how many items to load for each data page.
+        /// </summary>
+        /// <param name="itemsPerPage">
+        /// The number of items to retrieve for each call. Default is 20.
+        /// </param>
+        public IncrementalLoadingCollection(int itemsPerPage = 20)
+        {
+            this.source = new TSource();
+            this.itemsPerPage = itemsPerPage;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the collection contains more items to retrieve.
+        /// </summary>
+        public bool HasMoreItems => source?.HasMoreItems ?? false;
+
+        /// <summary>
+        /// Initializes incremental loading from the view.
+        /// </summary>
+        /// <param name="count">
+        /// The number of items to load.
+        /// </param>
+        /// <returns>
+        /// An object of the <see cref="LoadMoreItemsAsync(uint)"/> that specifies how many items have been actually retrieved.
+        /// </returns>
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            return AsyncInfo.Run(async (c) =>
+                {
+                    uint resultCount = 0;
+                    var data = await this.LoadDataAsync(count);
+
+                    if (data != null)
+                    {
+                        var dispatcher = Window.Current.Dispatcher;
+                        resultCount = (uint)data.Count();
+
+                        await dispatcher.RunAsync(
+                            CoreDispatcherPriority.Normal,
+                            () =>
+                            {
+                                foreach (var item in data)
+                                {
+                                    this.Add(item);
+                                }
+                            });
+                    }
+
+                    return new LoadMoreItemsResult { Count = resultCount };
+                });
+        }
+
+        private async Task<IEnumerable<IType>> LoadDataAsync(uint count)
+        {
+            var result = await source.GetPagedItemsAsync(currentPageIndex++, itemsPerPage);
+            return result;
+        }
+    }
+}
