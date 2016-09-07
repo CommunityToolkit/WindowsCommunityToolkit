e8a6398c057cc06fc20d211ffa196e5c7d459cc5
cbarker <cbarker@microsoft.com>
2016-09-07 16:22:04 -0700

    added support for easily retrieving users albums and photos

---
+++ Microsoft.Toolkit.Uwp.Services/Services/Facebook/FacebookPlatformImageSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Toolkit.Uwp.Services.Facebook
+{
+    /// <summary>
+    /// Strongly types Facebook PlatformImageSource object. Partial for extending properties.
+    /// </summary>
+    public partial class FacebookPlatformImageSource
+    {
+        /// <summary>
+        /// Gets a string description of the strongly typed properties in this model.
+        /// </summary>
+        public static string Fields => "height, source, width";
+
+        /// <summary>
+        /// Gets or sets height property.
+        /// </summary>
+        public string Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets source property.
+        /// </summary>
+        public string Source { get; set; }
+
+        /// <summary>
+        /// Gets or sets width property.
+        /// </summary>
+        public string Width { get; set; }
+    }
+}
