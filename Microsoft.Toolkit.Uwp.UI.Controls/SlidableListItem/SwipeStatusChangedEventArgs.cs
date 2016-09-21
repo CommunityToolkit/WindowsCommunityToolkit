65ac35dc6cd9190808d64f691f5d2a7b60bae28e
pekspro <msn@pekspro.com>
2016-09-21 20:59:33 +0200

    Adds SwipeStatusChanged event to SlidableListItem.

---
+++ Microsoft.Toolkit.Uwp.UI.Controls/SlidableListItem/SwipeStatusChangedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.Toolkit.Uwp.UI.Controls
+{
+    /// <summary>
+    /// Event args for a SwipeStatus changing event
+    /// </summary>
+    public class SwipeStatusChangedEventArgs
+    {
+        /// <summary>
+        /// Gets the old value.
+        /// </summary>
+        public SwipeStatus OldValue { get; internal set; }
+
+        /// <summary>
+        /// Gets the new value.
+        /// </summary>
+        public SwipeStatus NewValue { get; internal set; }
+    }
+}
