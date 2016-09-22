38a4c82e92d764998f78bab0a78279fa90122cb0
Marco Minerva <marco.minerva@gmail.com>
2016-09-22 09:59:55 +0200

    Remove redundant usings

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/MicrosoftGraph Service/MicrosoftGraphSource.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/MicrosoftGraph Service/MicrosoftGraphSource.cs
@@ -10,11 +10,7 @@
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
 // ******************************************************************
 
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Graph;
