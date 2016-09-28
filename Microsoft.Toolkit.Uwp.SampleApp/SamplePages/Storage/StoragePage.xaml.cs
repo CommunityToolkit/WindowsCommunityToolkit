c5b2742c49a4922ca625060af0f69ef1afc43b7a
David Catuhe <david.catuhe@live.fr>
2016-09-28 14:18:44 -0700

    Small renaming

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Storage/StoragePage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Storage/StoragePage.xaml.cs
@@ -1,22 +1,7 @@
-﻿using Microsoft.Toolkit.Uwp;
-using Microsoft.Toolkit.Uwp.SampleApp.Models;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+﻿using Windows.UI.Xaml;
 
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class StoragePage
     {
         private IStorageService localStorageService = new LocalStorageService();
