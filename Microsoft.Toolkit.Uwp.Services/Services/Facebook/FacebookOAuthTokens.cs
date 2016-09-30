166fbc577ed87821858905a350c7a8cb8cbf093d
Pedro Lamas <pedrolamas@gmail.com>
2016-09-30 21:28:45 +0100

    Update all code files to use UTF-8 with correct header

--- Microsoft.Toolkit.Uwp.Services/Services/Facebook/FacebookOAuthTokens.cs
+++ Microsoft.Toolkit.Uwp.Services/Services/Facebook/FacebookOAuthTokens.cs
@@ -1,5 +1,4 @@
 ﻿// ******************************************************************
-//
 // Copyright (c) Microsoft. All rights reserved.
 // This code is licensed under the MIT License (MIT).
 // THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
@@ -9,8 +8,8 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
-//
 // ******************************************************************
+
 namespace Microsoft.Toolkit.Uwp.Services.Facebook
 {
     /// <summary>
