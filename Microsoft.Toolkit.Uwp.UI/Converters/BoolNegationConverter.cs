9b209a2657b9004986abbfdc3c929cb1b21c693c
Tommaso Scalici <tommaso.scalici@outlook.it>
2016-10-08 16:16:10 +0200

    Added BoolNegationConverter, DateTimeToStringConverter and ResourceNameToResourceStringConverter.

---
+++ Microsoft.Toolkit.Uwp.UI/Converters/BoolNegationConverter.cs
@@ -0,0 +1,49 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Microsoft.Toolkit.Uwp.UI.Converters
+{
+    /// <summary>
+    /// Value converter that applies NOT operator to a bool value.
+    /// </summary>
+    public sealed class BoolNegationConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert a boolean value to its negation.
+        /// </summary>
+        /// <param name="value">The source data being passed to the target.</param>
+        /// <param name="targetType">The type of the target property, as a type reference.</param>
+        /// <param name="parameter">An optional parameter to be used to invert the converter logic.</param>
+        /// <param name="language">The language of the conversion.</param>
+        /// <returns>The value to be passed to the target dependency property.</returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return !(value is bool && (bool)value);
+        }
+
+        /// <summary>
+        /// Convert back a boolean value to its negation.
+        /// </summary>
+        /// <param name="value">The source data being passed to the target.</param>
+        /// <param name="targetType">The type of the target property, as a type reference.</param>
+        /// <param name="parameter">An optional parameter to be used to invert the converter logic.</param>
+        /// <param name="language">The language of the conversion.</param>
+        /// <returns>The value to be passed to the target dependency property.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return !(value is bool && (bool)value);
+        }
+    }
+}
---
+++ Microsoft.Toolkit.Uwp.UI/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,62 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using System;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+
+namespace Microsoft.Toolkit.Uwp.UI.Converters
+{
+    /// <summary>
+    /// Value converter that converts a <see cref="DateTime"/> or a <see cref="DateTimeOffset"/> to a <see cref="string"/>
+    /// that can be also formatted passing a string format as a parameter.
+    /// </summary>
+    public sealed class DateTimeToStringConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert a <see cref="DateTime"/> in a <see cref="DateTimeOffset"/> to a <see cref="string"/> with the
+        /// formatting specified as parameter.
+        /// </summary>
+        /// <param name="value">The source data being passed to the target.</param>
+        /// <param name="targetType">The type of the target property, as a type reference.</param>
+        /// <param name="parameter">An optional parameter to be used to invert the converter logic.</param>
+        /// <param name="language">The language of the conversion.</param>
+        /// <returns>The formatted string.</returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is DateTime)
+            {
+                return ((DateTime)value).ToString(parameter as string, CultureInfo.CurrentCulture);
+            }
+
+            if (value is DateTimeOffset)
+            {
+                return ((DateTimeOffset)value).ToString(parameter as string, CultureInfo.CurrentCulture);
+            }
+
+            return value?.ToString();
+        }
+
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        /// <param name="value">The source data being passed to the target.</param>
+        /// <param name="targetType">The type of the target property, as a type reference.</param>
+        /// <param name="parameter">An optional parameter to be used to invert the converter logic.</param>
+        /// <param name="language">The language of the conversion.</param>
+        /// <returns>The value to be passed to the target dependency property.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
