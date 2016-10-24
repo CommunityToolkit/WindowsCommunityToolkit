45cf5504620e1159ad6d28eb94944ae2bae17af8
Shawn Kendrot <skendrot@hotmail.com>
2016-10-23 22:03:34 -0600

    Add back the Blades property and mark it deprecated

--- Microsoft.Toolkit.Uwp.UI.Controls/BladeControl/BladeControl.Properties.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/BladeControl/BladeControl.Properties.cs
@@ -11,6 +11,9 @@
 // ******************************************************************
 
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Windows.Foundation.Metadata;
 using Windows.UI.Xaml;
 
 namespace Microsoft.Toolkit.Uwp.UI.Controls
@@ -21,6 +24,12 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
     /// </summary>
     public partial class BladeControl
     {
+        /// <summary>
+        /// Identifies the <see cref="Blades"/> dependency property.
+        /// </summary>
+        [Deprecated("This property has been replaced with the Items property of the control. It is no longer required to place content within the Blades property.", DeprecationType.Deprecate, 1)]
+        public static readonly DependencyProperty BladesProperty = DependencyProperty.Register(nameof(Blades), typeof(IList<Blade>), typeof(BladeControl), new PropertyMetadata(null, OnBladesChanged));
+
         /// <summary>
         /// Identifies the <see cref="ActiveBlades"/> dependency property.
         /// </summary>
@@ -33,6 +42,53 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         {
             get { return (IList<BladeItem>)GetValue(ActiveBladesProperty); }
             set { SetValue(ActiveBladesProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a collection of blades
+        /// </summary>
+        [Deprecated("This property has been replaced with the Items property of the control. It is no longer required to place content within the Blades property.", DeprecationType.Deprecate, 1)]
+        public IList<Blade> Blades
+        {
+            get { return (IList<Blade>)GetValue(BladesProperty); }
+            set { SetValue(BladesProperty, value); }
+        }
+
+        private static void OnBladesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var bladeControl = (BladeControl)d;
+            if (bladeControl.Blades != null)
+            {
+                foreach (var blade in bladeControl.Blades)
+                {
+                    bladeControl.Items.Add(blade);
+                }
+
+                var collection = bladeControl.Blades as INotifyCollectionChanged;
+                if (collection != null)
+                {
+                    collection.CollectionChanged += bladeControl.OnBladeCollectionChanged;
+                }
+            }
+        }
+
+        private void OnBladeCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null)
+            {
+                foreach (var blade in e.OldItems.OfType<Blade>())
+                {
+                    Items.Remove(blade);
+                }
+            }
+
+            if (e.NewItems != null)
+            {
+                foreach (var blade in e.NewItems.OfType<Blade>())
+                {
+                    Items.Add(blade);
+                }
+            }
         }
     }
 }
