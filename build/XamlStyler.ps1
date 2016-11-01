343783021d56867660e5e54c50e7f5c23d137c63
Scott Lovegrove <scottisafool@live.co.uk>
2016-11-01 10:33:27 +0000

    Update XamlStyler.ps1

--- build/XamlStyler.ps1
+++ build/XamlStyler.ps1
@@ -2,7 +2,14 @@
 $directorypath = Split-Path $invocation.MyCommand.Path
 
 $slndir = (get-item $directorypath).Parent.FullName
+$stylerfile = $slndir + '\settings.xamlstyler'
+$stylerexe = $slndir + '\build\tools\xamlstyler\xstyler.exe'
 
-$Dir = Get-ChildItem $slndir -recurse
-$List = $Dir | where {$_.Extension -eq ".xaml"}
-$List | Format-Table name
+$Dir = Get-ChildItem -Path $slndir -recurse 
+$List = $Dir | where {$_.Extension -eq ".xaml" -and $_.FullName -notmatch 'obj' }
+
+$List | ForEach-Object {
+    $filePath = $_.FullName
+
+    & $stylerexe -f "$filePath" -c "$stylerfile"
+}
