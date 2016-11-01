0582da8d61a79bbe736bc314466351e3fbe13fa3
Scott Lovegrove <scottisafool@live.co.uk>
2016-11-01 10:13:20 +0000

    Add xamlstyler

---
+++ build/XamlStyler.ps1
@@ -0,0 +1,8 @@
+﻿$invocation = (Get-Variable MyInvocation).Value
+$directorypath = Split-Path $invocation.MyCommand.Path
+
+$slndir = (get-item $directorypath).Parent.FullName
+
+$Dir = Get-ChildItem $slndir -recurse
+$List = $Dir | where {$_.Extension -eq ".xaml"}
+$List | Format-Table name
