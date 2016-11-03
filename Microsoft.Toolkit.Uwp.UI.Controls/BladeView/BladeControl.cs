bf0905b8b5ce4ec3d3d4f51a03ba5474ab3de47c
Shawn Kendrot <skendrot@hotmail.com>
2016-11-02 21:40:31 -0600

    Add a deprecated BladeControl class for backward compat

---
+++ Microsoft.Toolkit.Uwp.UI.Controls/BladeView/BladeControl.cs
@@ -0,0 +1,25 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using Windows.Foundation.Metadata;
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
+{
+    /// <summary>
+    /// A container that hosts <see cref="BladeItem"/> controls in a horizontal scrolling list
+    /// Based on the Azure portal UI
+    /// </summary>
+    [Deprecated("The BladeControl class has been replaced with the BladeView class. Please use that going forward", DeprecationType.Deprecate, 1)]
+    public class BladeControl : BladeView
+    {
+    }
+}
