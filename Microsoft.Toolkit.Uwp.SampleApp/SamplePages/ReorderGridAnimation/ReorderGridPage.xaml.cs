22aad2f37fbfddce44eee00caef000bc33f77c3a
Shawn Kendrot <skendrot@hotmail.com>
2016-11-08 11:34:47 -0700

    Add a way to easily add reorder animation to a gridview

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ReorderGridAnimation/ReorderGridPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.Toolkit.Uwp.SampleApp.Data;
+using Microsoft.Toolkit.Uwp.SampleApp.Models;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ReorderGridPage : Page
+    {
+        public ReorderGridPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var propertyDesc = e.Parameter as PropertyDescriptor;
+
+            if (propertyDesc != null)
+            {
+                DataContext = propertyDesc.Expando;
+            }
+
+            ImagesView.ItemsSource = await new Data.PhotosDataSource().GetItemsAsync();
+        }
+    }
+}
