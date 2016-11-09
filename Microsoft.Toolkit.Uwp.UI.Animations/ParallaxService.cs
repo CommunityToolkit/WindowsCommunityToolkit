49ec772bfb8159ca2692baaaddfc640e1b53c77e
Shawn Kendrot <skendrot@hotmail.com>
2016-11-09 15:54:46 -0700

    Add xml comments

--- Microsoft.Toolkit.Uwp.UI.Animations/ParallaxService.cs
+++ Microsoft.Toolkit.Uwp.UI.Animations/ParallaxService.cs
@@ -17,27 +17,56 @@ using Windows.UI.Xaml.Hosting;
 
 namespace Microsoft.Toolkit.Uwp.UI.Animations
 {
+    /// <summary>
+    /// Provides the ability to create a parallax effect to items within a ScrollViewer or List control
+    /// </summary>
     public class ParallaxService
     {
+        /// <summary>
+        /// Identifies the ScrollingElement attached dependency property.
+        /// </summary>
         public static readonly DependencyProperty ScrollingElementProperty = DependencyProperty.RegisterAttached("ScrollingElement", typeof(FrameworkElement), typeof(ParallaxService), new PropertyMetadata(null, OnScrollingElementChanged));
 
+        /// <summary>
+        /// Identifies the Multiplier attached dependency property.
+        /// </summary>
         public static readonly DependencyProperty MultiplierProperty = DependencyProperty.RegisterAttached("Multiplier", typeof(double), typeof(ParallaxService), new PropertyMetadata(0.3d, OnMultiplierChanged));
 
+        /// <summary>
+        /// Gets an object that is, or contains, a ScrollViewer
+        /// </summary>
+        /// <param name="obj">The object to get the ScrollViewer from.</param>
+        /// <returns>A <see cref="FrameworkElement"/> that is, or contains a ScrollViewer.</returns>
         public static FrameworkElement GetScrollingElement(DependencyObject obj)
         {
             return (FrameworkElement)obj.GetValue(ScrollingElementProperty);
         }
 
+        /// <summary>
+        /// Sets the element that is, or contains, a ScrollerViewer.
+        /// </summary>
+        /// <param name="obj">The object to set the value on.</param>
+        /// <param name="value">The element that is, or contains a ScrollViewer.</param>
         public static void SetScrollingElement(DependencyObject obj, FrameworkElement value)
         {
             obj.SetValue(ScrollingElementProperty, value);
         }
 
+        /// <summary>
+        /// Gets a value for how fast the parallax effect should be.
+        /// </summary>
+        /// <param name="obj">The object to get the value from.</param>
+        /// <returns>A value for how fast the parallax effect should be.</returns>
         public static double GetMultiplier(DependencyObject obj)
         {
             return (double)obj.GetValue(MultiplierProperty);
         }
 
+        /// <summary>
+        /// Sets the value for how fast the parallax effect should be.
+        /// </summary>
+        /// <param name="obj">The object to set the value on.</param>
+        /// <param name="value">The value for how fast the parallax effect should be.</param>
         public static void SetMultiplier(DependencyObject obj, double value)
         {
             obj.SetValue(MultiplierProperty, value);
