8a19f00325f2d8c95ad1885312f10989c6a6edab
Odonno <bottiau.david@laposte.net>
2016-11-17 23:35:45 +0100

    docs(BladeView): add binding to BladeMode enum in .bind file

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/BladeView/BladePage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/BladeView/BladePage.xaml.cs
@@ -10,6 +10,9 @@
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
 // ******************************************************************
 
+using Microsoft.Toolkit.Uwp.SampleApp.Models;
+using Windows.UI.Xaml.Navigation;
+
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
     /// <summary>
@@ -20,6 +23,18 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
         public BladePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var propertyDesc = e.Parameter as PropertyDescriptor;
+
+            if (propertyDesc != null)
+            {
+                DataContext = propertyDesc.Expando;
+            }
         }
     }
 }
