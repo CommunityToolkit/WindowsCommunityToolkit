115fcbb7b28380d8dbdb03b45c3df2763bbc5125
Eric Vernié <eric.vernie@hotmail.fr>
2016-11-22 20:37:25 +0100

    UpdateHeaders

--- Microsoft.Toolkit.Uwp.Services/Services/MicrosoftGraph/MicrosoftGraphEnums.cs
+++ Microsoft.Toolkit.Uwp.Services/Services/MicrosoftGraph/MicrosoftGraphEnums.cs
@@ -1,4 +1,6 @@
-﻿// This code is licensed under the MIT License (MIT).
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
 // THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 // INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -6,8 +8,8 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
-//
 // ******************************************************************
+
 using System;
 
 namespace Microsoft.Toolkit.Uwp.Services.MicrosoftGraph
