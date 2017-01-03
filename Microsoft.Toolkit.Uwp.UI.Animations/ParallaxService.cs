41f5d5f70c1c013d41234a0adef539c9eb1d5b62
Shawn Kendrot <skendrot@hotmail.com>
2017-01-03 15:46:19 -0700

    Add horizontal multiplier

--- Microsoft.Toolkit.Uwp.UI.Animations/ParallaxService.cs
+++ Microsoft.Toolkit.Uwp.UI.Animations/ParallaxService.cs
@@ -33,6 +33,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations
         public static readonly DependencyProperty VerticalMultiplierProperty = DependencyProperty.RegisterAttached("VerticalMultiplier", typeof(double), typeof(ParallaxService), new PropertyMetadata(0d, OnMultiplierChanged));
 
         /// <summary>
+        /// Identifies the HorizontalMultiplier attached dependency property.
+        /// </summary>
+        public static readonly DependencyProperty HorizontalMultiplierProperty = DependencyProperty.RegisterAttached("HorizontalMultiplier", typeof(double), typeof(ParallaxService), new PropertyMetadata(0d, OnMultiplierChanged));
+
+        /// <summary>
         /// Gets an object that is, or contains, a ScrollViewer
         /// </summary>
         /// <param name="obj">The object to get the ScrollViewer from.</param>
@@ -72,14 +77,34 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations
             obj.SetValue(VerticalMultiplierProperty, value);
         }
 
+        /// <summary>
+        /// Gets a value for how fast the parallax effect should scroll vertically.
+        /// </summary>
+        /// <param name="obj">The object to get the value from.</param>
+        /// <returns>A value for how fast the parallax effect should scroll vertically.</returns>
+        public static double GetHorizontalMultiplier(DependencyObject obj)
+        {
+            return (double)obj.GetValue(HorizontalMultiplierProperty);
+        }
+
+        /// <summary>
+        /// Sets the value for how fast the parallax effect should scroll horizontally.
+        /// </summary>
+        /// <param name="obj">The object to set the value on.</param>
+        /// <param name="value">The value for how fast the parallax effect should scroll horizontally.</param>
+        public static void SetHorizontalMultiplier(DependencyObject obj, double value)
+        {
+            obj.SetValue(HorizontalMultiplierProperty, value);
+        }
+
         private static void OnScrollingElementChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            CreateParallax(d as UIElement, GetScrollViewer(d), (double)d.GetValue(VerticalMultiplierProperty));
+            CreateParallax(d as UIElement, GetScrollViewer(d), (double)d.GetValue(HorizontalMultiplierProperty), (double)d.GetValue(VerticalMultiplierProperty));
         }
 
         private static void OnMultiplierChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            CreateParallax(d as UIElement, GetScrollViewer(d), (double)d.GetValue(VerticalMultiplierProperty));
+            CreateParallax(d as UIElement, GetScrollViewer(d), (double)d.GetValue(HorizontalMultiplierProperty), (double)d.GetValue(VerticalMultiplierProperty));
         }
 
         private static ScrollViewer GetScrollViewer(DependencyObject obj)
@@ -89,7 +114,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations
             return scroller ?? element?.FindDescendant<ScrollViewer>();
         }
 
-        private static void CreateParallax(UIElement parallaxElement, ScrollViewer scroller, double verticalMultiplier)
+        private static void CreateParallax(UIElement parallaxElement, ScrollViewer scroller, double horizontalMultiplier, double verticalMultiplier)
         {
             if ((parallaxElement == null) || (scroller == null))
             {
@@ -101,8 +126,9 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations
             Compositor compositor = scrollerViewerManipulation.Compositor;
 
             ExpressionAnimation expression = compositor.CreateExpressionAnimation(
-                "Matrix4x4.CreateFromTranslation(Vector3(0.0f, VerticalMultiplier * scroller.Translation.Y, 0.0f))");
+                "Matrix4x4.CreateFromTranslation(Vector3(HorizontalMultiplier * scroller.Translation.X, VerticalMultiplier * scroller.Translation.Y, 0.0f))");
             expression.SetReferenceParameter("scroller", scrollerViewerManipulation);
+            expression.SetScalarParameter("HorizontalMultiplier", (float)horizontalMultiplier);
             expression.SetScalarParameter("VerticalMultiplier", (float)verticalMultiplier);
 
             Visual visual = ElementCompositionPreview.GetElementVisual(parallaxElement);
