147d71dec0a9e317decfdd0dfcf99fb1c2d17129
Shawn Kendrot <skendrot@hotmail.com>
2017-01-03 15:45:29 -0700

    Change Multiplier property to VerticalMultiplier

--- Microsoft.Toolkit.Uwp.UI.Animations/ParallaxService.cs
+++ Microsoft.Toolkit.Uwp.UI.Animations/ParallaxService.cs
@@ -28,9 +28,9 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations
         public static readonly DependencyProperty ScrollingElementProperty = DependencyProperty.RegisterAttached("ScrollingElement", typeof(FrameworkElement), typeof(ParallaxService), new PropertyMetadata(null, OnScrollingElementChanged));
 
         /// <summary>
-        /// Identifies the Multiplier attached dependency property.
+        /// Identifies the VerticalMultiplier attached dependency property.
         /// </summary>
-        public static readonly DependencyProperty MultiplierProperty = DependencyProperty.RegisterAttached("Multiplier", typeof(double), typeof(ParallaxService), new PropertyMetadata(0.3d, OnMultiplierChanged));
+        public static readonly DependencyProperty VerticalMultiplierProperty = DependencyProperty.RegisterAttached("VerticalMultiplier", typeof(double), typeof(ParallaxService), new PropertyMetadata(0.3d, OnMultiplierChanged));
 
         /// <summary>
         /// Gets an object that is, or contains, a ScrollViewer
@@ -53,33 +53,33 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations
         }
 
         /// <summary>
-        /// Gets a value for how fast the parallax effect should be.
+        /// Gets a value for how fast the parallax effect should scroll vertically.
         /// </summary>
         /// <param name="obj">The object to get the value from.</param>
-        /// <returns>A value for how fast the parallax effect should be.</returns>
-        public static double GetMultiplier(DependencyObject obj)
+        /// <returns>A value for how fast the parallax effect should scroll vertically.</returns>
+        public static double GetVerticalMultiplier(DependencyObject obj)
         {
-            return (double)obj.GetValue(MultiplierProperty);
+            return (double)obj.GetValue(VerticalMultiplierProperty);
         }
 
         /// <summary>
-        /// Sets the value for how fast the parallax effect should be.
+        /// Sets the value for how fast the parallax effect should scroll vertically.
         /// </summary>
         /// <param name="obj">The object to set the value on.</param>
-        /// <param name="value">The value for how fast the parallax effect should be.</param>
-        public static void SetMultiplier(DependencyObject obj, double value)
+        /// <param name="value">The value for how fast the parallax effect should scroll vertically.</param>
+        public static void SetVerticalMultiplier(DependencyObject obj, double value)
         {
-            obj.SetValue(MultiplierProperty, value);
+            obj.SetValue(VerticalMultiplierProperty, value);
         }
 
         private static void OnScrollingElementChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            CreateParallax(d as UIElement, GetScrollViewer(d), (double)d.GetValue(MultiplierProperty));
+            CreateParallax(d as UIElement, GetScrollViewer(d), (double)d.GetValue(VerticalMultiplierProperty));
         }
 
         private static void OnMultiplierChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            CreateParallax(d as UIElement, GetScrollViewer(d), (double)d.GetValue(MultiplierProperty));
+            CreateParallax(d as UIElement, GetScrollViewer(d), (double)d.GetValue(VerticalMultiplierProperty));
         }
 
         private static ScrollViewer GetScrollViewer(DependencyObject obj)
@@ -89,7 +89,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations
             return scroller ?? element?.FindDescendant<ScrollViewer>();
         }
 
-        private static void CreateParallax(UIElement parallaxElement, ScrollViewer scroller, double multiplier)
+        private static void CreateParallax(UIElement parallaxElement, ScrollViewer scroller, double verticalMultiplier)
         {
             if ((parallaxElement == null) || (scroller == null))
             {
@@ -101,9 +101,9 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations
             Compositor compositor = scrollerViewerManipulation.Compositor;
 
             ExpressionAnimation expression = compositor.CreateExpressionAnimation(
-                "Matrix4x4.CreateFromTranslation(Vector3(0.0f, ParallaxMultiplier * scroller.Translation.Y, 0.0f))");
+                "Matrix4x4.CreateFromTranslation(Vector3(0.0f, VerticalMultiplier * scroller.Translation.Y, 0.0f))");
             expression.SetReferenceParameter("scroller", scrollerViewerManipulation);
-            expression.SetScalarParameter("ParallaxMultiplier", (float)multiplier);
+            expression.SetScalarParameter("VerticalMultiplier", (float)verticalMultiplier);
 
             Visual visual = ElementCompositionPreview.GetElementVisual(parallaxElement);
             visual.StartAnimation("TransformMatrix", expression);
