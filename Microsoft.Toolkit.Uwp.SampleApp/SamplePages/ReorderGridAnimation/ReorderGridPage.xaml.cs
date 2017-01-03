12764ee0a1322b2bd9ba4c73df1d35d3e4b4a874
David Catuhe <david.catuhe@live.fr>
2017-01-03 10:42:31 -0800

    Fixed warnings

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ReorderGridAnimation/ReorderGridPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ReorderGridAnimation/ReorderGridPage.xaml.cs
@@ -14,13 +14,8 @@ using Microsoft.Toolkit.Uwp.SampleApp.Models;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ReorderGridPage : Page
     {
         public ReorderGridPage()
