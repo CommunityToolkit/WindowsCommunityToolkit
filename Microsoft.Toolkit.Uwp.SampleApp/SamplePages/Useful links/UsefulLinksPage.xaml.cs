52ba7591607f77ec0d8fa027890797ae7589adf3
David Catuhe <david.catuhe@live.fr>
2017-01-17 15:35:25 -0800

    Fixes #670

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Useful links/UsefulLinksPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using System.Collections.Generic;
+using Microsoft.Toolkit.Uwp.SampleApp.Models;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    /// <summary>
+    /// A page that shows how to use the FadeHeaderBehavior
+    /// </summary>
+    public sealed partial class UsefulLinksPage : Page
+    {
+        public UsefulLinksPage()
+        {
+            InitializeComponent();
+
+            // If you wanted to use C# instead of XAML to attach the behavior, you can do it like this
+            // Interaction.GetBehaviors(MyListView).Add(new FadeHeaderBehavior());
+        }
+    }
+}
