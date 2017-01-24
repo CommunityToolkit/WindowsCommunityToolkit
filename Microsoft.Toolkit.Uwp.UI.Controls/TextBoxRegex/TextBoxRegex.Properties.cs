ac2c96be233bbcd1aad491647d45759d7266aaae
DESKTOP-QJU4N0L\mityh <ibraheem.osama@gmail.com>
2017-01-25 07:25:12 +1100

    updating TextBoxRegex comments

--- Microsoft.Toolkit.Uwp.UI.Controls/TextBoxRegex/TextBoxRegex.Properties.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/TextBoxRegex/TextBoxRegex.Properties.cs
@@ -24,103 +24,103 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
     public partial class TextBoxRegex
     {
         /// <summary>
-        /// The regex expression that will be validated on the textbox
+        /// Identifies the Regex attached dependency property.
         /// </summary>
         public static readonly DependencyProperty RegexProperty = DependencyProperty.RegisterAttached("Regex", typeof(string), typeof(TextBoxRegex), new PropertyMetadata(null, TextBoxRegexPropertyOnChange));
 
         /// <summary>
-        /// Gets the result of the TextBoxRegex validation again the Regex property
+        /// Identifies the IsValid attached dependency property.
         /// </summary>
         public static readonly DependencyProperty IsValidProperty = DependencyProperty.RegisterAttached("IsValid", typeof(bool), typeof(TextBoxRegex), new PropertyMetadata(false));
 
         /// <summary>
-        /// The validation mode of the validation extension (Normal, Forced)
+        /// Identifies the ValidationMode attached dependency property.
         /// </summary>
         public static readonly DependencyProperty ValidationModeProperty = DependencyProperty.RegisterAttached("ValidationMode", typeof(ValidationMode), typeof(TextBoxRegex), new PropertyMetadata(ValidationMode.Normal, TextBoxRegexPropertyOnChange));
 
         /// <summary>
-        /// The validation types are predefined validation that can be used directly
+        /// Identifies the ValidationType attached dependency property.
         /// </summary>
         public static readonly DependencyProperty ValidationTypeProperty = DependencyProperty.RegisterAttached("ValidationType", typeof(ValidationType), typeof(TextBoxRegex), new PropertyMetadata(ValidationType.Custom, TextBoxRegexPropertyOnChange));
 
         /// <summary>
-        /// Gets the Regex property
+        /// Gets the value of the TextBoxRegex.Regex XAML attached property from the specified TextBox.
         /// </summary>
-        /// <param name="obj">TextBox to get Regex property from.</param>
+        /// <param name="textBox">TextBox to get Regex property from.</param>
         /// <returns>The regular expression assigned to the TextBox</returns>
-        public static string GetRegex(DependencyObject obj)
+        public static string GetRegex(DependencyObject textBox)
         {
-            return (string)obj.GetValue(RegexProperty);
+            return (string)textBox.GetValue(RegexProperty);
         }
 
         /// <summary>
-        /// Set Regex property
+        /// Sets the value of the TextBoxRegex.Regex XAML attached property for a target TextBox.
         /// </summary>
-        /// <param name="obj">The TextBox to set the regular expression on</param>
+        /// <param name="textBox">The TextBox to set the regular expression on</param>
         /// <param name="value">Regex value</param>
-        public static void SetRegex(DependencyObject obj, string value)
+        public static void SetRegex(DependencyObject textBox, string value)
         {
-            obj.SetValue(RegexProperty, value);
+            textBox.SetValue(RegexProperty, value);
         }
 
         /// <summary>
-        /// Gets a value indicating if the Text is valid according to the Regex property.
+        /// Gets the value of the TextBoxRegex.IsValid XAML attached property from the specified TextBox.
         /// </summary>
-        /// <param name="obj">TextBox to be validated.</param>
+        /// <param name="textBox">TextBox to be validated.</param>
         /// <returns>TextBox regular expression validation result</returns>
-        public static bool GetIsValid(DependencyObject obj)
+        public static bool GetIsValid(DependencyObject textBox)
         {
-            return (bool)obj.GetValue(IsValidProperty);
+            return (bool)textBox.GetValue(IsValidProperty);
         }
 
         /// <summary>
-        /// Sets the IsValid property.
+        /// Sets the value of the TextBoxRegex.IsValid XAML attached property for a target TextBox.
         /// </summary>
-        /// <param name="obj">TextBox to be assigned the property</param>
+        /// <param name="textBox">TextBox to be assigned the property</param>
         /// <param name="value">A value indicating if the Text is valid according to the Regex property.</param>
-        public static void SetIsValid(DependencyObject obj, bool value)
+        public static void SetIsValid(DependencyObject textBox, bool value)
         {
-            obj.SetValue(ValidationModeProperty, value);
+            textBox.SetValue(ValidationModeProperty, value);
         }
 
         /// <summary>
-        /// Gets <see cref="ValidationMode"/> property
+        /// Gets the value of the TextBoxRegex.ValidationMode XAML attached property from the specified TextBox.
         /// </summary>
-        /// <param name="obj">TextBox to get the <see cref="ValidationMode"/> from</param>
+        /// <param name="textBox">TextBox to get the <see cref="ValidationMode"/> from</param>
         /// <returns>TextBox <see cref="ValidationMode"/> value</returns>
-        public static ValidationMode GetValidationMode(DependencyObject obj)
+        public static ValidationMode GetValidationMode(DependencyObject textBox)
         {
-            return (ValidationMode)obj.GetValue(ValidationModeProperty);
+            return (ValidationMode)textBox.GetValue(ValidationModeProperty);
         }
 
         /// <summary>
-        /// Set <see cref="ValidationMode"/> property
+        /// Sets the value of the TextBoxRegex.ValidationMode XAML attached property for a target TextBox.
         /// </summary>
-        /// <param name="obj">TextBox to set the <see cref="ValidationMode"/> on.</param>
+        /// <param name="textBox">TextBox to set the <see cref="ValidationMode"/> on.</param>
         /// <param name="value">TextBox <see cref="ValidationMode"/> value</param>
-        public static void SetValidationMode(DependencyObject obj, ValidationMode value)
+        public static void SetValidationMode(DependencyObject textBox, ValidationMode value)
         {
-            obj.SetValue(ValidationModeProperty, value);
+            textBox.SetValue(ValidationModeProperty, value);
         }
 
         /// <summary>
-        /// Gets <see cref="ValidationType"/> property
+        /// Gets the value of the TextBoxRegex.ValidationType XAML attached property from the specified TextBox.
         /// </summary>
-        /// <param name="obj">TextBox to get <see cref="ValidationType"/> from.</param>
+        /// <param name="textBox">TextBox to get <see cref="ValidationType"/> from.</param>
         /// <returns>TextBox <see cref="ValidationType"/> Value</returns>
-        public static ValidationType GetValidationType(DependencyObject obj)
+        public static ValidationType GetValidationType(DependencyObject textBox)
         {
-            return (ValidationType)obj.GetValue(ValidationTypeProperty);
+            return (ValidationType)textBox.GetValue(ValidationTypeProperty);
         }
 
         /// <summary>
-        /// Set <see cref="ValidationType"/> property
+        /// Sets the value of the TextBoxRegex.ValidationType XAML attached property for a target TextBox.
         /// </summary>
-        /// <param name="obj">TextBox to set the <see cref="ValidationType"/> on.</param>
+        /// <param name="textBox">TextBox to set the <see cref="ValidationType"/> on.</param>
         /// <param name="value">TextBox <see cref="ValidationType"/> value</param>
-        public static void SetValidationType(DependencyObject obj, ValidationType value)
+        public static void SetValidationType(DependencyObject textBox, ValidationType value)
         {
-            obj.SetValue(ValidationTypeProperty, value);
+            textBox.SetValue(ValidationTypeProperty, value);
         }
     }
 }
