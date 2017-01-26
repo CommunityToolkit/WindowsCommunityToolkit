a5a111fad613b276ae4b3e646e323f2efc2b4af1
David Catuhe <david.catuhe@live.fr>
2017-01-26 11:32:47 -0800

    using MD to display useful inks

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Useful links/UsefulLinksPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Useful links/UsefulLinksPage.xaml.cs
@@ -10,15 +10,50 @@
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
 // ******************************************************************
 
-using Windows.UI.Xaml.Controls;
+using System;
+using System.Net.Http;
+using Windows.System;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
-    public sealed partial class UsefulLinksPage : Page
+    public sealed partial class UsefulLinksPage
     {
         public UsefulLinksPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Shell.Current.DisplayWaitRing = true;
+            try
+            {
+                using (var request = new HttpHelperRequest(new Uri("https://raw.githubusercontent.com/Microsoft/UWPCommunityToolkit/dev/githubresources/content/links.md")))
+                {
+                    using (var response = await HttpHelper.Instance.SendRequestAsync(request))
+                    {
+                        if (response.Success)
+                        {
+                            MarkdownTextBlockTextblock.Text = await response.Content.ReadAsStringAsync();
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MarkdownTextBlockTextblock.Text = "Unable to download content: " + exception.Message;
+            }
+
+            Shell.Current.DisplayWaitRing = false;
+        }
+
+        private async void MarkdownTextBlockTextblock_OnLinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri(e.Link));
         }
     }
 }
