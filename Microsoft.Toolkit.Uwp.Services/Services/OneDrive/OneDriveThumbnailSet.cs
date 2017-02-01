021e3d724766ce0df457d9694ee8302bd74ed2b2
David Catuhe <david.catuhe@live.fr>
2017-02-01 10:11:18 -0800

    Add support for ThumbnailSet in OneDrive SDK

---
+++ Microsoft.Toolkit.Uwp.Services/Services/OneDrive/OneDriveThumbnailSet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.OneDrive.Sdk;
+
+namespace Microsoft.Toolkit.Uwp.Services.OneDrive
+{
+    /// <summary>
+    /// Class used to store urls to a specific set of thumbnails
+    /// </summary>
+    public class OneDriveThumbnailSet
+    {
+        /// <summary>
+        /// Gets the url to the small version of the thumbnail
+        /// </summary>
+        public string Small { get; }
+
+        /// <summary>
+        /// Gets the url to the medium version of the thumbnail
+        /// </summary>
+        public string Medium { get; }
+
+        /// <summary>
+        /// Gets the url to the large version of the thumbnail
+        /// </summary>
+        public string Large { get; }
+
+        /// <summary>
+        /// Gets the url to the original version of the thumbnail
+        /// </summary>
+        public string Source { get; }
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="OneDriveThumbnailSet"/> class.
+        /// </summary>
+        /// <param name="set">Original set from OneDrive SDK</param>
+        internal OneDriveThumbnailSet(ThumbnailSet set)
+        {
+            Small = set.Small.Url;
+            Medium = set.Medium.Url;
+            Large = set.Large.Url;
+            Source = set.Source.Url;
+        }
+
+    }
+}
