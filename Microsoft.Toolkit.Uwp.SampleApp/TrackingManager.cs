6dced597e64246fcbdce93b397188397604f4422
David Catuhe <david.catuhe@live.fr>
2017-02-02 11:45:19 -0800

    Add analytics page

---
+++ Microsoft.Toolkit.Uwp.SampleApp/TrackingManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using GoogleAnalytics;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp
+{
+    public static class TrackingManager
+    {
+        private static readonly Tracker Tracker = AnalyticsManager.Current.CreateTracker("UA-91148254-1");
+
+        static TrackingManager()
+        {
+            try
+            {
+                AnalyticsManager.Current.ReportUncaughtExceptions = true;
+                AnalyticsManager.Current.AutoAppLifetimeMonitoring = true;
+            }
+            catch
+            {
+                // Ignoring error
+            }
+        }
+
+        public static void TrackException(Exception ex)
+        {
+            try
+            {
+                Tracker.Send(HitBuilder.CreateException("Exception: " + ex.Message + "->" + ex.StackTrace, false).Build());
+            }
+            catch
+            {
+                // Ignore error
+            }
+        }
+
+        public static void TrackEvent(string category, string action, string label = "", long value = 0)
+        {
+            try
+            {
+                Tracker.Send(HitBuilder.CreateCustomEvent(category, action, label, value).Build());
+            }
+            catch
+            {
+                // Ignore error
+            }
+        }
+
+        public static void TrackPage(string pageName)
+        {
+            try
+            {
+                Tracker.ScreenName = pageName;
+                Tracker.Send(HitBuilder.CreateScreenView().Build());
+            }
+            catch
+            {
+                // Ignore error
+            }
+        }
+    }
+}
---
+++ githubresources/content/analytics.md
@@ -0,0 +1,20 @@
+# Analytics
+
+
+## Google analytics
+*****
+
+* [Main page](https://analytics.google.com)
+* [NuGet](https://www.nuget.org/packages/UWP.SDKforGoogleAnalytics.Managed/)
+&nbsp;  
+&nbsp;  
+
+
+## HockeyApp
+*****
+
+* [Main page](http://hockeyapp.com/)
+* [NuGet](https://www.nuget.org/packages/HockeySDK.UWP/)
+&nbsp;  
+&nbsp;  
+
