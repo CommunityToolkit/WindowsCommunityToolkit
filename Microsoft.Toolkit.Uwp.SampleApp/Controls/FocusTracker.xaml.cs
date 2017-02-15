f1e14395c3d643ae746b37f50fac726f243d4a17
David Catuhe <david.catuhe@live.fr>
2017-02-15 11:04:49 -0800

    Expander Accessibility check FocusTracker

---
+++ Microsoft.Toolkit.Uwp.SampleApp/Controls/FocusTracker.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp
+{
+    public sealed partial class FocusTracker
+    {
+        private readonly DispatcherTimer updateTimer;
+
+        public FocusTracker()
+        {
+            this.InitializeComponent();
+
+            updateTimer = new DispatcherTimer();
+            updateTimer.Tick += UpdateTimer_Tick;
+        }
+
+        public bool IsRunning
+        {
+            get
+            {
+                return updateTimer.IsEnabled;
+            }
+
+            set
+            {
+                if (value)
+                {
+                    updateTimer.Start();
+                }
+                else
+                {
+                    updateTimer.Stop();
+                }
+            }
+        }
+
+        private void UpdateTimer_Tick(object sender, object e)
+        {
+            var focusedControl = FocusManager.GetFocusedElement() as FrameworkElement;
+
+            if (focusedControl == null)
+            {
+                ControlName.Text = string.Empty;
+                ControlType.Text = string.Empty;
+                ControlFirstParentWithName.Text = string.Empty;
+                return;
+            }
+
+            ControlName.Text = focusedControl.Name;
+            ControlType.Text = focusedControl.GetType().Name;
+
+            var parentWithName = FindVisualAscendantWithName(focusedControl);
+
+            ControlFirstParentWithName.Text = parentWithName?.Name ?? string.Empty;
+        }
+
+        private FrameworkElement FindVisualAscendantWithName(FrameworkElement element)
+        {
+            var parent = VisualTreeHelper.GetParent(element) as FrameworkElement;
+
+            if (parent == null)
+            {
+                return null;
+            }
+
+            if (!string.IsNullOrEmpty(parent.Name))
+            {
+                return parent;
+            }
+
+            return FindVisualAscendantWithName(parent);
+        }
+    }
+}
