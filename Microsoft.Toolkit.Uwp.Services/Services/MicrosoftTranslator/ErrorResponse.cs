d801ef1e481ce0dfbb07e8ecabf839ec6a59816d
Marco Minerva <marco.minerva@gmail.com>
2017-02-16 18:11:42 +0100

    Add TranslatorServiceException and better documentation

---
+++ Microsoft.Toolkit.Uwp.Services/Services/MicrosoftTranslator/ErrorResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Toolkit.Uwp.Services.MicrosoftTranslator
+{
+    /// <summary>
+    /// Holds information about an error occurred while accessing Microsoft Translator Service.
+    /// </summary>
+    internal class ErrorResponse
+    {
+        /// <summary>
+        /// Gets or sets the error message.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets the HTTP status code.
+        /// </summary>
+        public int StatusCode { get; set; }
+    }
+}
---
+++ Microsoft.Toolkit.Uwp.Services/Services/MicrosoftTranslator/TranslatorServiceException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Microsoft.Toolkit.Uwp.Services.MicrosoftTranslator
+{
+    /// <summary>
+    /// The <strong>TranslatorServiceException</strong> class holds information about Exception related to <see cref="TranslatorService"/>.
+    /// </summary>
+    /// <seealso cref="TranslatorService"/>
+    public class TranslatorServiceException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TranslatorServiceException"/> class using the specified error message.
+        /// <param name="message">The message that describes the error.</param>
+        /// </summary>
+        public TranslatorServiceException(string message)
+            : base(message)
+        {
+        }
+    }
+}
