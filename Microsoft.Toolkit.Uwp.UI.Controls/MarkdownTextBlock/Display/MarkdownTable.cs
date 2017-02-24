a0dad6a77b9891fdc881ef38933c3c49b0ee8ed2
David Catuhe <david.catuhe@live.fr>
2017-02-24 10:19:56 -0800

    Fixes #977

--- Microsoft.Toolkit.Uwp.UI.Controls/MarkdownTextBlock/Display/MarkdownTable.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/MarkdownTextBlock/Display/MarkdownTable.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
