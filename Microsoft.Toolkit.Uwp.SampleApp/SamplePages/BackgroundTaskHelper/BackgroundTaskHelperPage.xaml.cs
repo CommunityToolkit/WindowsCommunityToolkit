c8074e56dc850e9662c1ae555355c27272243c61
Odonno <bottiau.david@laposte.net>
2017-02-25 12:06:13 +0100

    docs(backgroundTask): update sample page to use both spm and mpm

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/BackgroundTaskHelper/BackgroundTaskHelperPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/BackgroundTaskHelper/BackgroundTaskHelperPage.xaml.cs
@@ -25,29 +25,59 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             InitializeComponent();
         }
 
-        private async void RegisterButton_Click(object sender, RoutedEventArgs e)
+        private bool IsBackgroundTaskRegistered(string taskName)
         {
-            if (BackgroundTaskHelper.IsBackgroundTaskRegistered(Constants.TestBackgroundTaskName))
+            if (BackgroundTaskHelper.IsBackgroundTaskRegistered(taskName))
             {
                 // Background task already registered.
                 StatusMessage.Text = "Background Task already registered";
+                return true;
+            }
+
+            return false;
+        }
+
+        private async void RegisterMpmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsBackgroundTaskRegistered(nameof(TestBackgroundTask)))
+            {
                 return;
             }
 
             // Check for background access.
             await BackgroundExecutionManager.RequestAccessAsync();
 
-            BackgroundTaskHelper.Register(Constants.TestBackgroundTaskName, new TimeTrigger(15, false));
-
             // If registering Multi-Process Background task
-            // BackgroundTaskHelper.Register(testBackgroundTaskName, "Microsoft.Toolkit.Uwp.SampleApp.SamplePages", new TimeTrigger(15, false), false, true, new SystemCondition(SystemConditionType.InternetAvailable));
-            StatusMessage.Text = "Background Task registered";
+            BackgroundTaskHelper.Register(nameof(TestBackgroundTask), "Microsoft.Toolkit.Uwp.SampleApp.SamplePages", new TimeTrigger(15, false), false, true, new SystemCondition(SystemConditionType.InternetAvailable));
+
+            StatusMessage.Text = "Background Task registered (MPM)";
         }
 
-        private void UnregisterButton_Click(object sender, RoutedEventArgs e)
+        private void UnregisterMpmButton_Click(object sender, RoutedEventArgs e)
+        {
+            BackgroundTaskHelper.Unregister(nameof(TestBackgroundTask));
+            StatusMessage.Text = "Background Task Unregistered";
+        }
+
+        private async void RegisterSpmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsBackgroundTaskRegistered(Constants.TestBackgroundTaskName))
+            {
+                return;
+            }
+
+            // Check for background access.
+            await BackgroundExecutionManager.RequestAccessAsync();
+
+            // Registering Single-Process Background task
+            BackgroundTaskHelper.Register(Constants.TestBackgroundTaskName, new TimeTrigger(15, false));
+
+            StatusMessage.Text = "Background Task registered (SPM)";
+        }
+
+        private void UnregisterSpmButton_Click(object sender, RoutedEventArgs e)
         {
             BackgroundTaskHelper.Unregister(Constants.TestBackgroundTaskName);
-
             StatusMessage.Text = "Background Task Unregistered";
         }
     }
