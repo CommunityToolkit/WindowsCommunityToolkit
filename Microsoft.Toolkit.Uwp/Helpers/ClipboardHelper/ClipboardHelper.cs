da2256556155fb343d2bfb6fad30a202d18e7914
h82258652 <h82258652@hotmail.com>
2017-03-07 14:06:42 +0800

    add clipboard helper.

---
+++ Microsoft.Toolkit.Uwp/Helpers/ClipboardHelper/ClipboardHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace Microsoft.Toolkit.Uwp
+{
+    /// <summary>
+    /// This class can set clipboard format easier.
+    /// </summary>
+    public static class ClipboardHelper
+    {
+        /// <summary>
+        /// Set html into clipboard.
+        /// </summary>
+        /// <param name="html">Html content.</param>
+        public static void SetRawHtml(string html)
+        {
+            if (html == null)
+            {
+                throw new ArgumentNullException(nameof(html));
+            }
+
+            var content = new DataPackage();
+            content.SetHtmlFormat(new HtmlClipboardFormat(html));
+            Clipboard.SetContent(content);
+        }
+    }
+}
---
+++ Microsoft.Toolkit.Uwp/Helpers/ClipboardHelper/HtmlClipboardFormat.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+
+namespace Microsoft.Toolkit.Uwp
+{
+    public sealed class HtmlClipboardFormat
+    {
+        private const string EndFragment = "EndFragment:{0:D10}";
+
+        private const string EndHtml = "EndHTML:{0:D10}";
+
+        private const string StartFragment = "StartFragment:{0:D10}";
+
+        private const string StartHtml = "StartHTML:{0:D10}";
+
+        private const int StringBuilderDefaultCapacity = 256;
+
+        private const string Version = "Version:0.9";
+
+        private static readonly string End = "<!--EndFragment-->" + Environment.NewLine + "</body>" + Environment.NewLine + "</html>";
+
+        private static readonly string Start = "<html>" + Environment.NewLine + "<body>" + Environment.NewLine + "<!--StartFragment-->";
+
+        private readonly string _html;
+
+        public HtmlClipboardFormat(string html)
+        {
+            if (html == null)
+            {
+                throw new ArgumentNullException(nameof(html));
+            }
+
+            _html = html;
+        }
+
+        public static implicit operator string(HtmlClipboardFormat htmlClipboardFormat)
+        {
+            if (htmlClipboardFormat == null)
+            {
+                throw new ArgumentNullException(nameof(htmlClipboardFormat));
+            }
+
+            return htmlClipboardFormat.ToString();
+        }
+
+        public override string ToString()
+        {
+            var indexBuilder = new StringBuilder(StringBuilderDefaultCapacity);
+            indexBuilder.AppendLine(Version);
+            indexBuilder.AppendLine(string.Format(StartHtml, 0));
+            indexBuilder.AppendLine(string.Format(EndHtml, 0));
+            indexBuilder.AppendLine(string.Format(StartFragment, 0));
+            indexBuilder.AppendLine(string.Format(EndFragment, 0));
+            var startHtmlIndex = Encoding.UTF8.GetByteCount(indexBuilder.ToString());
+            indexBuilder.Append(Start);
+            var startFragmentIndex = Encoding.UTF8.GetByteCount(indexBuilder.ToString());
+            indexBuilder.Append(_html);
+            var endFragmentIndex = Encoding.UTF8.GetByteCount(indexBuilder.ToString());
+            indexBuilder.Append(End);
+            var endHtmlIndex = Encoding.UTF8.GetByteCount(indexBuilder.ToString());
+
+            var outputBuilder = new StringBuilder(StringBuilderDefaultCapacity);
+            outputBuilder.AppendLine(Version);
+            outputBuilder.AppendLine(string.Format(StartHtml, startHtmlIndex));
+            outputBuilder.AppendLine(string.Format(EndHtml, endHtmlIndex));
+            outputBuilder.AppendLine(string.Format(StartFragment, startFragmentIndex));
+            outputBuilder.AppendLine(string.Format(EndFragment, endFragmentIndex));
+            outputBuilder.Append(Start);
+            outputBuilder.Append(_html);
+            outputBuilder.Append(End);
+            return outputBuilder.ToString();
+        }
+    }
+}
