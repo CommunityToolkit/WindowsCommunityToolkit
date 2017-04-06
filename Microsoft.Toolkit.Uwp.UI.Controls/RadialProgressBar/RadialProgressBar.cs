93dcfb83de0de4d745e5c9615ae78f936eea2ceb
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-04-06 16:48:34 +0900

    Fix

--- Microsoft.Toolkit.Uwp.UI.Controls/RadialProgressBar/RadialProgressBar.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/RadialProgressBar/RadialProgressBar.cs
@@ -8,7 +8,7 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
-// *****************************************************************
+// ******************************************************************
 
 using System;
 using Windows.Foundation;
