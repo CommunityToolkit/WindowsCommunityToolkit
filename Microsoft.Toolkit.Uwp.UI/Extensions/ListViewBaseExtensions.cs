1ce44cb8a621dc077539bd1f03b40a721c0721e3
Edwin van Driel <edwin@westside.nl>
2017-04-15 20:10:56 +0200

    ListViewBase extended with an alternate rowcolor property

--- Microsoft.Toolkit.Uwp.UI/Extensions/ListViewBaseExtensions.cs
+++ Microsoft.Toolkit.Uwp.UI/Extensions/ListViewBaseExtensions.cs
@@ -13,6 +13,7 @@
 using System.Windows.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Microsoft.Toolkit.Uwp.UI
 {
@@ -82,6 +83,67 @@ namespace Microsoft.Toolkit.Uwp.UI
             if (command != null && command.CanExecute(args.ClickedItem))
             {
                 command.Execute(args.ClickedItem);
+            }
+        }
+
+        /// <summary>
+        /// Attached <see cref="DependencyProperty"/> for binding a <see cref="Brush"/> as an alternate row background color to a <see cref="ListViewBase"/>
+        /// </summary>
+        public static readonly DependencyProperty AlternateRowColorProperty = DependencyProperty.RegisterAttached(
+            "AlternateRowColor",
+            typeof(Brush),
+            typeof(ListViewBaseExtensions),
+            new PropertyMetadata(null, OnAlternateRowColorPropertyChanged));
+
+        /// <summary>
+        /// Gets the alternate <see cref="Brush"/> associated with the specified <see cref="ListViewBase"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="ListViewBase"/> to get the associated <see cref="Brush"/> from</param>
+        /// <returns>The <see cref="Brush"/> associated with the <see cref="ListViewBase"/></returns>
+        public static Brush GetAlternateRowColor(ListViewBase obj)
+        {
+            return (Brush)obj.GetValue(AlternateRowColorProperty);
+        }
+
+        /// <summary>
+        /// Sets the alternate <see cref="Brush"/> associated with the specified <see cref="DependencyObject"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="ListViewBase"/> to associate the <see cref="Brush"/> with</param>
+        /// <param name="value">The <see cref="Brush"/> for binding to the <see cref="ListViewBase"/></param>
+        public static void SetAlternateRowColor(ListViewBase obj, Brush value)
+        {
+            obj.SetValue(AlternateRowColorProperty, value);
+        }
+
+        private static void OnAlternateRowColorPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ListViewBase listViewBase = sender as ListViewBase;
+
+            if (listViewBase == null)
+            {
+                return;
+            }
+
+            listViewBase.ContainerContentChanging -= ColorContainerContentChanging;
+
+            if (AlternateRowColorProperty != null)
+            {
+                listViewBase.ContainerContentChanging += ColorContainerContentChanging;
+            }
+        }
+
+        private static void ColorContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            var itemContainer = args.ItemContainer as ListViewItem;
+            var itemIndex = sender.IndexFromContainer(itemContainer);
+
+            if (itemIndex % 2 == 0)
+            {
+                itemContainer.Background = GetAlternateRowColor(sender);
+            }
+            else
+            {
+                itemContainer.Background = null;
             }
         }
     }
