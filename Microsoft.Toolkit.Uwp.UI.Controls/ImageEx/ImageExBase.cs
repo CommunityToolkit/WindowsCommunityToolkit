d0d4d5d3d3b15d6117d5e1b0d9945b89bc38404e
William Bradley <williamabradley@outlook.com>
2017-04-16 02:02:42 +1200

    -Fixed Typo.

--- Microsoft.Toolkit.Uwp.UI.Controls/ImageEx/ImageExBase.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/ImageEx/ImageExBase.cs
@@ -37,7 +37,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         private const string UnloadedState = "Unloaded";
         private const string FailedState = "Failed";
         private const string ShowStrokeState = "ShowStroke";
-        private const string StrokeUnloaded = "StokeUnloaded";
+        private const string StrokeUnloaded = "StrokeUnloaded";
 
         protected object _image;
         protected ProgressRing _progress;
