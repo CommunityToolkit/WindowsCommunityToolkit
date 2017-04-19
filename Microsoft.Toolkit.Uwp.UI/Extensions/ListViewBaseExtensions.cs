16122032a0f4feca13080b0503d30828e30f837f
Edwin van Driel <edwin@westside.nl>
2017-04-19 15:46:59 +0200

    AlternateRowItemTemplate

--- Microsoft.Toolkit.Uwp.UI/Extensions/ListViewBaseExtensions.cs
+++ Microsoft.Toolkit.Uwp.UI/Extensions/ListViewBaseExtensions.cs
@@ -18,14 +18,14 @@ using Windows.UI.Xaml.Media;
 namespace Microsoft.Toolkit.Uwp.UI
 {
     /// <summary>
-    /// Provides attached dependency properties for the <see cref="ListViewBase"/> that allows attaching an <see cref="ICommand"/>.
-    /// This ICommand is executed when ListViewBase Item receives interaction by means of ItemClick. This requires IsItemClickEnabled to set to true.
-    /// The ICommand is passed the Item that received interaction as a parameter
+    /// Provides attached dependency properties for the <see cref="ListViewBase"/>
     /// </summary>
     public static class ListViewBaseExtensions
     {
         /// <summary>
         /// Attached <see cref="DependencyProperty"/> for binding an <see cref="ICommand"/> instance to a <see cref="ListViewBase"/>
+        /// This ICommand is executed when ListViewBase Item receives interaction by means of ItemClick. This requires IsItemClickEnabled to set to true.
+        /// The ICommand is passed the Item that received interaction as a parameter
         /// </summary>
         public static readonly DependencyProperty CommandProperty = DependencyProperty.RegisterAttached(
             "Command",
@@ -144,6 +144,67 @@ namespace Microsoft.Toolkit.Uwp.UI
             else
             {
                 itemContainer.Background = null;
+            }
+        }
+
+        /// <summary>
+        /// Attached <see cref="DependencyProperty"/> for binding a <see cref="DataTemplate"/> as an alternate row template to a <see cref="ListViewBase"/>
+        /// </summary>
+        public static readonly DependencyProperty AlternateRowItemTemplateProperty = DependencyProperty.RegisterAttached(
+            "AlternateRowItemTemplate",
+            typeof(DataTemplate),
+            typeof(ListViewBaseExtensions),
+            new PropertyMetadata(null, OnAlternateRowItemTemplatePropertyChanged));
+
+        /// <summary>
+        /// Gets the <see cref="DataTemplate"/> associated with the specified <see cref="ListViewBase"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="ListViewBase"/> to get the associated <see cref="DataTemplate"/> from</param>
+        /// <returns>The <see cref="DataTemplate"/> associated with the <see cref="ListViewBase"/></returns>
+        public static DataTemplate GetAlternateRowItemTemplate(ListViewBase obj)
+        {
+            return (DataTemplate)obj.GetValue(AlternateRowItemTemplateProperty);
+        }
+
+        /// <summary>
+        /// Sets the <see cref="DataTemplate"/> associated with the specified <see cref="ListViewBase"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="ListViewBase"/> to associate the <see cref="DataTemplate"/> with</param>
+        /// <param name="value">The <see cref="DataTemplate"/> for binding to the <see cref="ListViewBase"/></param>
+        public static void SetAlternateRowItemTemplate(ListViewBase obj, DataTemplate value)
+        {
+            obj.SetValue(AlternateRowItemTemplateProperty, value);
+        }
+
+        private static void OnAlternateRowItemTemplatePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ListViewBase listViewBase = sender as ListViewBase;
+
+            if (listViewBase == null)
+            {
+                return;
+            }
+
+            listViewBase.ContainerContentChanging -= ItemTemplateContainerContentChanging;
+
+            if (AlternateRowItemTemplateProperty != null)
+            {
+                listViewBase.ContainerContentChanging += ItemTemplateContainerContentChanging;
+            }
+        }
+
+        private static void ItemTemplateContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            var itemContainer = args.ItemContainer as ListViewItem;
+            var itemIndex = sender.IndexFromContainer(itemContainer);
+
+            if (itemIndex % 2 == 0)
+            {
+                itemContainer.ContentTemplate = GetAlternateRowItemTemplate(sender);
+            }
+            else
+            {
+                itemContainer.ContentTemplate = sender.ItemTemplate;
             }
         }
     }
