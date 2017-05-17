4d20d13efe4fb3c2ab8a3d69e8047048500a3d1f
Shen Chauhan <shenchauhan@hotmail.com>
2017-05-17 16:25:38 -0700

    Adding Error string class

--- Microsoft.Toolkit.Uwp/Helpers/BluetoothLEHelper/GattProtocolErrorParser.cs
+++ Microsoft.Toolkit.Uwp/Helpers/BluetoothLEHelper/GattProtocolErrorParser.cs
@@ -14,12 +14,12 @@ namespace Microsoft.Toolkit.Uwp
         /// <summary>
         /// Helper to convert an gatt error value into a string
         /// </summary>
-        /// <param name="errorValue"></param>
+        /// <param name="errorValue"> the byte error value.</param>
         /// <returns>String representation of the error</returns>
         public static string GetErrorString(byte? errorValue)
         {
-            string ret = "Protocol Error";
-            
+            var ret = "Protocol Error";
+
             if (errorValue.HasValue == false)
             {
                 return ret;
@@ -29,67 +29,83 @@ namespace Microsoft.Toolkit.Uwp
             {
                 return "Attribute Not Found";
             }
-            else if (errorValue == GattProtocolError.AttributeNotLong)
+
+            if (errorValue == GattProtocolError.AttributeNotLong)
             {
                 return "Attribute Not Long";
             }
-            else if (errorValue == GattProtocolError.InsufficientAuthentication)
+
+            if (errorValue == GattProtocolError.InsufficientAuthentication)
             {
                 return "Insufficient Authentication";
             }
-            else if (errorValue == GattProtocolError.InsufficientAuthorization)
+
+            if (errorValue == GattProtocolError.InsufficientAuthorization)
             {
                 return "Insufficient Authorization";
             }
-            else if (errorValue == GattProtocolError.InsufficientEncryption)
+
+            if (errorValue == GattProtocolError.InsufficientEncryption)
             {
                 return "Insufficient Encryption";
             }
-            else if (errorValue == GattProtocolError.InsufficientEncryptionKeySize)
+
+            if (errorValue == GattProtocolError.InsufficientEncryptionKeySize)
             {
                 return "Insufficient Encryption Key Size";
             }
-            else if (errorValue == GattProtocolError.InsufficientResources)
+
+            if (errorValue == GattProtocolError.InsufficientResources)
             {
                 return "Insufficient Resources";
             }
-            else if (errorValue == GattProtocolError.InvalidAttributeValueLength)
+
+            if (errorValue == GattProtocolError.InvalidAttributeValueLength)
             {
                 return "Invalid Attribute Value Length";
             }
-            else if (errorValue == GattProtocolError.InvalidHandle)
+
+            if (errorValue == GattProtocolError.InvalidHandle)
             {
                 return "Invalid Handle";
             }
-            else if (errorValue == GattProtocolError.InvalidOffset)
+
+            if (errorValue == GattProtocolError.InvalidOffset)
             {
                 return "Invalid Offset";
             }
-            else if (errorValue == GattProtocolError.InvalidPdu)
+
+            if (errorValue == GattProtocolError.InvalidPdu)
             {
                 return "Invalid Pdu";
             }
-            else if (errorValue == GattProtocolError.PrepareQueueFull)
+
+            if (errorValue == GattProtocolError.PrepareQueueFull)
             {
                 return "Prepare Queue Full";
             }
-            else if (errorValue == GattProtocolError.ReadNotPermitted)
+
+            if (errorValue == GattProtocolError.ReadNotPermitted)
             {
                 return "Read Not Permitted";
             }
-            else if (errorValue == GattProtocolError.RequestNotSupported)
+
+            if (errorValue == GattProtocolError.RequestNotSupported)
             {
                 return "Request Not Supported";
             }
-            else if (errorValue == GattProtocolError.UnlikelyError)
+
+            if (errorValue == GattProtocolError.UnlikelyError)
             {
                 return "UnlikelyError";
             }
-            else if (errorValue == GattProtocolError.UnsupportedGroupType)
+
+            if (errorValue == GattProtocolError.UnsupportedGroupType)
             {
                 return "Unsupported Group Type";
             }
-            else if (errorValue == GattProtocolError.WriteNotPermitted)
+
+            if (errorValue == GattProtocolError.WriteNotPermitted)
             {
                 return "Write Not Permitted";
             }
