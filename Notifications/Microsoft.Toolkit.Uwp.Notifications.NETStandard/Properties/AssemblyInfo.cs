383595ef0fb430cf6bffba754acd05329ca7308d
Nikola Metulev <nikolame@microsoft.com>
2017-06-27 13:29:30 -0700

    test build

--- Notifications/Microsoft.Toolkit.Uwp.Notifications.NETStandard/Properties/AssemblyInfo.cs
+++ Notifications/Microsoft.Toolkit.Uwp.Notifications.NETStandard/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// ******************************************************************
+// ******************************************************************
 // Copyright (c) Microsoft. All rights reserved.
 // This code is licensed under the MIT License (MIT).
 // THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
@@ -37,6 +37,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.5.0-dev.283+Branch.dev.Sha.b519192ce4b22f79f4c8ef9644280433772e5352")]
