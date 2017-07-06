9000fcf6b457771fc66ede715eccd548fc9fd8ab
Edwin van Driel <edwin@westside.nl>
2017-07-06 23:02:24 +0200

    Changed namespaces in samples

--- Microsoft.Toolkit.Uwp.UI/Extensions/VisualTreeExtensions.cs
+++ Microsoft.Toolkit.Uwp.UI/Extensions/VisualTreeExtensions.cs
@@ -14,7 +14,7 @@ using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 
-namespace Microsoft.Toolkit.Uwp.UI.Extensions
+namespace Microsoft.Toolkit.Uwp.UI
 {
     /// <summary>
     /// Defines a collection of extensions methods for UI.
