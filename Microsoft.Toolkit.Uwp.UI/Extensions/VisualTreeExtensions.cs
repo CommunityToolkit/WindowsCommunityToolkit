ba98af5afbc07deb455ae300f5f27f70eec84fcd
mhawker <michael.hawker@outlook.com>
2017-08-16 13:51:23 -0700

    Add LogicalTreeExtensions: FindChildByName, FindChild<t>, FindParentByName, FindParent<T>, GetContentControl Move and Obsolete FindAscendant<T> from VisualTreeExtensions (renamed to FindParent<T>)

--- Microsoft.Toolkit.Uwp.UI/Extensions/VisualTreeExtensions.cs
+++ Microsoft.Toolkit.Uwp.UI/Extensions/VisualTreeExtensions.cs
@@ -86,28 +86,6 @@ namespace Microsoft.Toolkit.Uwp.UI
         }
 
         /// <summary>
-        /// Find first ascendant control of a specified type.
-        /// </summary>
-        /// <typeparam name="T">Type to search for.</typeparam>
-        /// <param name="element">Child element.</param>
-        /// <returns>Ascendant control or null if not found.</returns>
-        public static T FindAscendant<T>(this FrameworkElement element)
-            where T : FrameworkElement
-        {
-            if (element.Parent == null)
-            {
-                return null;
-            }
-
-            if (element.Parent is T)
-            {
-                return element.Parent as T;
-            }
-
-            return (element.Parent as FrameworkElement).FindAscendant<T>();
-        }
-
-        /// <summary>
         /// Find first visual ascendant control of a specified type.
         /// </summary>
         /// <typeparam name="T">Type to search for.</typeparam>
