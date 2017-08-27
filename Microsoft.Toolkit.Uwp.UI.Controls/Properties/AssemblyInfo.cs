6bb3dec012b3c0a86eba4422caa3d218638fbe39
Oren Novotny <oren@novotny.org>
2017-08-27 16:27:14 -0400

    Port Controls

--- Microsoft.Toolkit.Uwp.UI.Controls/Properties/AssemblyInfo.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/Properties/AssemblyInfo.cs
@@ -17,14 +17,5 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Toolkit.Uwp.UI.LayoutControls")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Toolkit.Uwp.UI.LayoutControls")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-[assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("UniversalMarkdownUnitTests")]
