f2a672e893c87215dfc3ac5963b0fa256933f0e2
Morten Nielsen <no@spam.thx>
2017-10-11 21:54:57 -0700

    Fixed incorrect type used for feature attribute defaults

--- Microsoft.Toolkit.Uwp.UI.Controls.Design/RadialProgressBarMetadata.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Design/RadialProgressBarMetadata.cs
@@ -36,7 +36,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls.Design
 			AddCallback(typeof(Microsoft.Toolkit.Uwp.UI.Controls.RadialProgressBar),
 				b =>
 				{
-                    b.AddCustomAttributes(new FeatureAttribute(typeof(RadialProgressBar)));
+                    b.AddCustomAttributes(new FeatureAttribute(typeof(RadialProgressBarDefaults)));
                     b.AddCustomAttributes(nameof(RadialProgressBar.Thickness), new CategoryAttribute(Properties.Resources.CategoryAppearance));
                     b.AddCustomAttributes(nameof(RadialProgressBar.Outline), new CategoryAttribute(Properties.Resources.CategoryBrush));
                     b.AddCustomAttributes(new ToolboxCategoryAttribute(ToolboxCategoryPaths.Toolkit, false));
