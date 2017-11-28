ed5e568d60fbdc563dfd4f863649924b0e894036
mhawker <michael.hawker@outlook.com>
2017-11-27 22:09:11 -0800

    Remove Dead Code related to DataContext in SampleApp

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Carousel/CarouselPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Carousel/CarouselPage.xaml.cs
@@ -40,21 +40,5 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             carouselControl = control.FindDescendantByName("CarouselControl") as Carousel;
             carouselControl.ItemsSource = await new Data.PhotosDataSource().GetItemsAsync();
         }
-
-        /// <summary>
-        /// Invoked when the Page is loaded and becomes the current source of a parent Frame.
-        /// </summary>
-        /// <param name="e">Event data that can be examined by overriding code. The event data is representative of the pending navigation that will load the current Page. Usually the most relevant property to examine is Parameter.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-
-            var propertyDesc = e.Parameter as PropertyDescriptor;
-
-            if (propertyDesc != null)
-            {
-                DataContext = propertyDesc.Expando;
-            }
-        }
     }
 }
