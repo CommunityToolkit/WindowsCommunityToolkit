0a50473549db22714f80e26060b6d45ba2ed3ef9
Shawn Kendrot <skendrot@hotmail.com>
2017-12-01 15:03:11 -0700

    Add StaggeredPanel sample

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/StaggeredPanel/StaggeredPanelPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StaggeredPanelPage : Page
+    {
+        public StaggeredPanelPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var items = await new Data.PhotosDataSource().GetItemsAsync();
+            GridView.ItemsSource = items
+                .Select((p, i) => new
+                {
+                    Item = p,
+                    Index = i + 1
+                });
+        }
+    }
+}
