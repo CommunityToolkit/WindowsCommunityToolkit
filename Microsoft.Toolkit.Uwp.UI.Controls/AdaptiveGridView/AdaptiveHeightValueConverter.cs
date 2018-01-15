8fe828e7743cb766802537a8261a3678c423eb8d
Shawn Kendrot <skendrot@hotmail.com>
2018-01-15 12:01:08 -0700

    Remove unused usings

--- Microsoft.Toolkit.Uwp.UI.Controls/AdaptiveGridView/AdaptiveHeightValueConverter.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/AdaptiveGridView/AdaptiveHeightValueConverter.cs
@@ -11,10 +11,7 @@
 // ******************************************************************
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
