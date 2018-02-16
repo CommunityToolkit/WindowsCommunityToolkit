fb98d17fee010c6185bae08e1fa4270dc26d1d81
michael-hawker <michael.hawker@outlook.com>
2018-02-15 19:46:24 -0800

    Initial Implementation of RadialGradient class which matches WPF behavior for implemented properties. Adds Win2DCanvasBrushBase helper for interop with Win2D and CompositionBrush.

--- Microsoft.Toolkit.Uwp.UI/Brushes/RadialGradientBrush.cs
+++ Microsoft.Toolkit.Uwp.UI/Brushes/RadialGradientBrush.cs
@@ -10,17 +10,14 @@
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
 // ******************************************************************
 
+using System.Numerics;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Brushes;
-using Microsoft.Graphics.Canvas.Effects;
-using Microsoft.Graphics.Canvas.UI.Composition;
 using Microsoft.Toolkit.Uwp.UI.Extensions;
-using System.Numerics;
 using Windows.Foundation;
-using Windows.Graphics.DirectX;
 using Windows.UI;
-using Windows.UI.Composition;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 
 namespace Microsoft.Toolkit.Uwp.UI.Brushes
@@ -28,10 +25,89 @@ namespace Microsoft.Toolkit.Uwp.UI.Brushes
     /// <summary>
     /// Goal: Drop-in replacement for https://msdn.microsoft.com/en-us/library/system.windows.media.radialgradientbrush(v=vs.110).aspx.
     /// </summary>
-    public class RadialGradientBrush : XamlCompositionBrushBase
+    [ContentProperty(Name = nameof(GradientStops))]
+    public class RadialGradientBrush : Win2DCanvasBrushBase
     {
         private CanvasRadialGradientBrush _gradientBrush;
-        private CompositionSurfaceBrush _surfaceBrush;
+
+        /// <summary>
+        /// Gets or sets the brush's gradient stops.
+        /// </summary>
+        public GradientStopCollection GradientStops
+        {
+            get { return (GradientStopCollection)GetValue(GradientStopsProperty); }
+            set { SetValue(GradientStopsProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="GradientStops"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty GradientStopsProperty =
+            DependencyProperty.Register(nameof(GradientStops), typeof(GradientStopCollection), typeof(RadialGradientBrush), new PropertyMetadata(null, new PropertyChangedCallback(OnGradientStopsChanged)));
+
+        /// <summary>
+        /// Gets or sets the center of the outermost circle of the radial gradient.  The defaults is 0.5,0.5.
+        /// </summary>
+        public Point Center
+        {
+            get { return (Point)GetValue(CenterProperty); }
+            set { SetValue(CenterProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="Center"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty CenterProperty =
+            DependencyProperty.Register(nameof(Center), typeof(Point), typeof(RadialGradientBrush), new PropertyMetadata(new Point(0.5, 0.5)));
+
+        /// <summary>
+        /// Gets or sets the location of the two-dimensional focal point that defines the beginning of the gradient.  The default is 0.5,0.5.
+        /// </summary>
+        public Point GradientOrigin
+        {
+            get { return (Point)GetValue(GradientOriginProperty); }
+            set { SetValue(GradientOriginProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="GradientOrigin"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty GradientOriginProperty =
+            DependencyProperty.Register(nameof(GradientOrigin), typeof(Point), typeof(RadialGradientBrush), new PropertyMetadata(new Point(0.5, 0.5)));
+
+        /// <summary>
+        /// Gets or sets the horizontal radius of the outermost circle of the radial gradient.  The default is 0.5.
+        /// </summary>
+        public double RadiusX
+        {
+            get { return (double)GetValue(RadiusXProperty); }
+            set { SetValue(RadiusXProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="RadiusX"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty RadiusXProperty =
+            DependencyProperty.Register(nameof(RadiusX), typeof(double), typeof(RadialGradientBrush), new PropertyMetadata(0.5));
+
+        /// <summary>
+        /// Gets or sets the vertical radius of the outermost circle of the radial gradient.  The default is 0.5.
+        /// </summary>
+        public double RadiusY
+        {
+            get { return (double)GetValue(RadiusYProperty); }
+            set { SetValue(RadiusYProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="RadiusX"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty RadiusYProperty =
+            DependencyProperty.Register(nameof(RadiusY), typeof(double), typeof(RadialGradientBrush), new PropertyMetadata(0.5));
+
+        private static void OnGradientStopsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RadialGradientBrush"/> class.
@@ -39,67 +115,84 @@ namespace Microsoft.Toolkit.Uwp.UI.Brushes
         public RadialGradientBrush()
         {
             this.FallbackColor = Colors.Transparent;
+
+            // For this brush, as long as we're greater than 1x1 it doesn't seem to effect quality of output.
+            this.SURFACE_RESOLUTION_X = 512;
+            this.SURFACE_RESOLUTION_Y = 512;
+
+            GradientStops = new GradientStopCollection();
         }
 
-        /// <summary>
-        /// Initializes the Composition Brush.
-        /// </summary>
-        protected override void OnConnected()
+        public RadialGradientBrush(Color startColor, Color endColor)
+            : this()
         {
-            // Delay creating composition resources until they're required.
-            if (CompositionBrush == null)
-            {
-                // Abort if effects aren't supported.
-                if (!CompositionCapabilities.GetForCurrentView().AreEffectsSupported())
-                {
-                    return;
-                }
-
-                var visual = Window.Current.Compositor.CreateSpriteVisual();
-                visual.Size = new Vector2(64.0f, 64.0f);
-
-                var size = visual?.Parent?.Size;
-
-                if (visual.Size.X == 0 || visual.Size.Y == 0)
-                {
-                    // We have no dimension, so we can't create a surface yet.
-                    return;
-                }
-
-                var device = new CanvasDevice();
-                var graphics = CanvasComposition.CreateCompositionGraphicsDevice(Window.Current.Compositor, device);
-
-                var surface = graphics.CreateDrawingSurface(visual.Size.ToSize(), DirectXPixelFormat.B8G8R8A8UIntNormalized, DirectXAlphaMode.Premultiplied);
-
-                _gradientBrush = new CanvasRadialGradientBrush(device, Colors.Red, Colors.Green);
-                _gradientBrush.RadiusX = visual.Size.X * 0.5f;
-                _gradientBrush.RadiusY = visual.Size.Y * 0.5f;
-                _gradientBrush.Center = visual.Size * new Vector2(0.6f, 0.5f);
-                _gradientBrush.OriginOffset = visual.Size * new Vector2(0.1f, 0.4f);
-
-                using (var session = CanvasComposition.CreateDrawingSession(surface))
-                {
-                    session.FillRectangle(visual.Size.ToRect(), _gradientBrush);
-                }
-
-                _surfaceBrush = Window.Current.Compositor.CreateSurfaceBrush(surface);
-                _surfaceBrush.Stretch = CompositionStretch.Fill;
-
-                CompositionBrush = _surfaceBrush;
-            }
+            GradientStops = new GradientStopCollection();
+            GradientStops.Add(new GradientStop() { Color = startColor, Offset = 0.0 });
+            GradientStops.Add(new GradientStop() { Color = endColor, Offset = 1.0 });
         }
 
-        /// <summary>
-        /// Deconstructs the Composition Brush.
-        /// </summary>
+        public RadialGradientBrush(GradientStopCollection gradientStopCollection)
+            : this()
+        {
+            GradientStops = gradientStopCollection;
+        }
+
+        public override void OnDraw(CanvasDevice device, CanvasDrawingSession session, Vector2 size)
+        {
+            if (_gradientBrush != null)
+            {
+                _gradientBrush.Dispose();
+                _gradientBrush = null;
+            }
+
+            // Create our Brush
+            _gradientBrush = new CanvasRadialGradientBrush(device, this.GradientStops.ToWin2DGradientStops());
+            _gradientBrush.RadiusX = size.X * (float)RadiusX;
+            _gradientBrush.RadiusY = size.Y * (float)RadiusY;
+            _gradientBrush.Center = size * Center.ToVector2();
+            _gradientBrush.OriginOffset = size * (GradientOrigin.ToVector2() - Center.ToVector2());
+            _gradientBrush.Opacity = (float)Opacity;
+
+            // Use brush to draw on our canvas
+            session.FillRectangle(size.ToRect(), _gradientBrush);
+        }
+
         protected override void OnDisconnected()
         {
-            // Dispose of composition resources when no longer in use.
-            if (CompositionBrush != null)
+            base.OnDisconnected();
+
+            if (_gradientBrush != null)
             {
-                CompositionBrush.Dispose();
-                CompositionBrush = null;
+                _gradientBrush.Dispose();
+                _gradientBrush = null;
+
+                // Our GradientStops don't seem to reset between initializations...?
+                GradientStops = null;
             }
+        }
+    }
+
+    /// <summary>
+    /// Helper extensions for <see cref="RadialGradientBrush"/>.
+    /// </summary>
+    public static class RadialGradientBrushHelpers
+    {
+        /// <summary>
+        /// Converts a <see cref="GradientStopCollection"/> to an array of <see cref="CanvasGradientStop"/>.
+        /// </summary>
+        /// <param name="stops"><see cref="GradientStopCollection"/> collection of gradient stops.</param>
+        /// <returns>New array of <see cref="CanvasGradientStop"/> stops.</returns>
+        public static CanvasGradientStop[] ToWin2DGradientStops(this GradientStopCollection stops)
+        {
+            var canvasStops = new CanvasGradientStop[stops.Count];
+
+            int x = 0;
+            foreach (var stop in stops)
+            {
+                canvasStops[x++] = new CanvasGradientStop() { Color = stop.Color, Position = (float)stop.Offset };
+            }
+
+            return canvasStops;
         }
     }
 }
---
+++ Microsoft.Toolkit.Uwp.UI/Brushes/Win2DCanvasBrushBase.cs
@@ -0,0 +1,103 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using System.Numerics;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+using Windows.Graphics.DirectX;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.Toolkit.Uwp.UI.Brushes
+{
+    /// <summary>
+    /// Helper Brush class to interop with Win2D Canvas calls.
+    /// </summary>
+    public abstract class Win2DCanvasBrushBase : XamlCompositionBrushBase
+    {
+        /// <summary>
+        /// Gets or sets the internal surface render width.  Modify during construction.
+        /// </summary>
+        protected float SURFACE_RESOLUTION_X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the internal surface render height.  Modify during construction.
+        /// </summary>
+        protected float SURFACE_RESOLUTION_Y { get; set; }
+
+        private CompositionSurfaceBrush _surfaceBrush;
+
+        /// <summary>
+        /// Implemented by parent class and called when canvas is being constructed for brush.
+        /// </summary>
+        /// <param name="device">Canvas device.</param>
+        /// <param name="session">Canvas drawing session.</param>
+        /// <param name="size">Size of surface to draw on.</param>
+        public abstract void OnDraw(CanvasDevice device, CanvasDrawingSession session, Vector2 size);
+
+        /// <summary>
+        /// Initializes the Composition Brush.
+        /// </summary>
+        protected override void OnConnected()
+        {
+            // Delay creating composition resources until they're required.
+            if (CompositionBrush == null)
+            {
+                // Abort if effects aren't supported.
+                if (!CompositionCapabilities.GetForCurrentView().AreEffectsSupported())
+                {
+                    return;
+                }
+
+                var visual = Window.Current.Compositor.CreateSpriteVisual();
+                visual.Size = new Vector2(SURFACE_RESOLUTION_X, SURFACE_RESOLUTION_Y);
+
+                // TODO: Investigate if these can be shared across brushes?
+                var device = new CanvasDevice();
+                var graphics = CanvasComposition.CreateCompositionGraphicsDevice(Window.Current.Compositor, device);
+
+                var surface = graphics.CreateDrawingSurface(visual.Size.ToSize(), DirectXPixelFormat.B8G8R8A8UIntNormalized, DirectXAlphaMode.Premultiplied);
+
+                using (var session = CanvasComposition.CreateDrawingSession(surface))
+                {
+                    // Call Implementor to draw on session.
+                    this.OnDraw(device, session, visual.Size);
+                }
+
+                _surfaceBrush = Window.Current.Compositor.CreateSurfaceBrush(surface);
+                _surfaceBrush.Stretch = CompositionStretch.Fill;
+
+                CompositionBrush = _surfaceBrush;
+            }
+        }
+
+        /// <summary>
+        /// Deconstructs the Composition Brush.
+        /// </summary>
+        protected override void OnDisconnected()
+        {
+            // Dispose of composition resources when no longer in use.
+            if (CompositionBrush != null)
+            {
+                CompositionBrush.Dispose();
+                CompositionBrush = null;
+            }
+
+            if (_surfaceBrush != null)
+            {
+                _surfaceBrush.Dispose();
+                _surfaceBrush = null;
+            }
+        }
+    }
+}
