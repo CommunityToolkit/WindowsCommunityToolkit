fb010b14d9501893c6bb777650cbc8b072d87feb
mhawker <michael.hawker@outlook.com>
2018-02-17 21:01:20 -0800

    Split out RadialGradientBrush properties to their own partial class.

--- Microsoft.Toolkit.Uwp.UI/Brushes/RadialGradientBrush.cs
+++ Microsoft.Toolkit.Uwp.UI/Brushes/RadialGradientBrush.cs
@@ -10,6 +10,8 @@
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
 // ******************************************************************
 
+//// UWP Replacement for WPF RadialGradientBrush: https://msdn.microsoft.com/en-us/library/system.windows.media.radialgradientbrush(v=vs.110).aspx.
+
 using System.Numerics;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Brushes;
@@ -23,102 +25,13 @@ using Windows.UI.Xaml.Media;
 namespace Microsoft.Toolkit.Uwp.UI.Brushes
 {
     /// <summary>
-    /// Goal: Drop-in replacement for https://msdn.microsoft.com/en-us/library/system.windows.media.radialgradientbrush(v=vs.110).aspx.
+    /// RadialGradientBrush - This GradientBrush defines its Gradient as an interpolation
+    /// within an Ellipse.
     /// </summary>
     [ContentProperty(Name = nameof(GradientStops))]
-    public class RadialGradientBrush : Win2DCanvasBrushBase
+    public partial class RadialGradientBrush : Win2DCanvasBrushBase
     {
         private CanvasRadialGradientBrush _gradientBrush;
-
-        /// <summary>
-        /// Gets or sets the brush's gradient stops.
-        /// </summary>
-        public GradientStopCollection GradientStops
-        {
-            get { return (GradientStopCollection)GetValue(GradientStopsProperty); }
-            set { SetValue(GradientStopsProperty, value); }
-        }
-
-        /// <summary>
-        /// Identifies the <see cref="GradientStops"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty GradientStopsProperty =
-            DependencyProperty.Register(nameof(GradientStops), typeof(GradientStopCollection), typeof(RadialGradientBrush), new PropertyMetadata(null, new PropertyChangedCallback(OnGradientStopsChanged)));
-
-        /// <summary>
-        /// Gets or sets the center of the outermost circle of the radial gradient.  The defaults is 0.5,0.5.
-        /// </summary>
-        public Point Center
-        {
-            get { return (Point)GetValue(CenterProperty); }
-            set { SetValue(CenterProperty, value); }
-        }
-
-        /// <summary>
-        /// Identifies the <see cref="Center"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty CenterProperty =
-            DependencyProperty.Register(nameof(Center), typeof(Point), typeof(RadialGradientBrush), new PropertyMetadata(new Point(0.5, 0.5)));
-
-        /// <summary>
-        /// Gets or sets the location of the two-dimensional focal point that defines the beginning of the gradient.  The default is 0.5,0.5.
-        /// </summary>
-        public Point GradientOrigin
-        {
-            get { return (Point)GetValue(GradientOriginProperty); }
-            set { SetValue(GradientOriginProperty, value); }
-        }
-
-        /// <summary>
-        /// Identifies the <see cref="GradientOrigin"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty GradientOriginProperty =
-            DependencyProperty.Register(nameof(GradientOrigin), typeof(Point), typeof(RadialGradientBrush), new PropertyMetadata(new Point(0.5, 0.5)));
-
-        /// <summary>
-        /// Gets or sets the horizontal radius of the outermost circle of the radial gradient.  The default is 0.5.
-        /// </summary>
-        public double RadiusX
-        {
-            get { return (double)GetValue(RadiusXProperty); }
-            set { SetValue(RadiusXProperty, value); }
-        }
-
-        /// <summary>
-        /// Identifies the <see cref="RadiusX"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty RadiusXProperty =
-            DependencyProperty.Register(nameof(RadiusX), typeof(double), typeof(RadialGradientBrush), new PropertyMetadata(0.5));
-
-        /// <summary>
-        /// Gets or sets the vertical radius of the outermost circle of the radial gradient.  The default is 0.5.
-        /// </summary>
-        public double RadiusY
-        {
-            get { return (double)GetValue(RadiusYProperty); }
-            set { SetValue(RadiusYProperty, value); }
-        }
-
-        /// <summary>
-        /// Identifies the <see cref="RadiusX"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty RadiusYProperty =
-            DependencyProperty.Register(nameof(RadiusY), typeof(double), typeof(RadialGradientBrush), new PropertyMetadata(0.5));
-
-        /// <summary>
-        /// Gets or sets the type of spread method that specifies how to draw a gradient that starts or ends inside the bounds of the object to be painted. 
-        /// </summary>
-        public GradientSpreadMethod SpreadMethod
-        {
-            get { return (GradientSpreadMethod)GetValue(SpreadMethodProperty); }
-            set { SetValue(SpreadMethodProperty, value); }
-        }
-
-        /// <summary>
-        /// Identifies the <see cref="SpreadMethod"/> dependency property.
-        /// </summary>
-        public static readonly DependencyProperty SpreadMethodProperty =
-            DependencyProperty.Register(nameof(SpreadMethod), typeof(GradientSpreadMethod), typeof(RadialGradientBrush), new PropertyMetadata(GradientSpreadMethod.Pad));
 
         private static void OnGradientStopsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -131,13 +44,21 @@ namespace Microsoft.Toolkit.Uwp.UI.Brushes
         {
             this.FallbackColor = Colors.Transparent;
 
-            // For this brush, as long as we're greater than 1x1 it doesn't seem to effect quality of output.
+            // Rendering surface size, if this is too small the gradient will be pixelated.
+            // This seems like a good compromise.
             this.SURFACE_RESOLUTION_X = 512;
             this.SURFACE_RESOLUTION_Y = 512;
 
             GradientStops = new GradientStopCollection();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RadialGradientBrush"/> class
+        /// with with two colors specified for GradientStops at
+        /// offsets 0.0 and 1.0.
+        /// </summary>
+        /// <param name="startColor"> The Color at offset 0.0. </param>
+        /// <param name="endColor"> The Color at offset 1.0. </param>
         public RadialGradientBrush(Color startColor, Color endColor)
             : this()
         {
@@ -146,6 +67,10 @@ namespace Microsoft.Toolkit.Uwp.UI.Brushes
             GradientStops.Add(new GradientStop() { Color = endColor, Offset = 1.0 });
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RadialGradientBrush"/> class with GradientStops set to the passed-in collection.
+        /// </summary>
+        /// <param name="gradientStopCollection"> GradientStopCollection to set on this brush. </param>
         public RadialGradientBrush(GradientStopCollection gradientStopCollection)
             : this()
         {
