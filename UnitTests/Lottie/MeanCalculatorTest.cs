100a322bf0509f173254e2aa9c588dac068a3146
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2018-03-19 16:48:09 -0700

    Fixed Lottie Header files.

--- UnitTests/Lottie/MeanCalculatorTest.cs
+++ UnitTests/Lottie/MeanCalculatorTest.cs
@@ -1,4 +1,16 @@
-﻿using Microsoft.Toolkit.Uwp.UI.Controls.Lottie.Utils;
+// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using Microsoft.Toolkit.Uwp.UI.Controls.Lottie.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests.Lottie
