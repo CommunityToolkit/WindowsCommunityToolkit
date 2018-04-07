58cf58ce413610b1c34b29edf54a1ce5bb78a896
Oren Novotny <oren@novotny.org>
2018-04-07 14:24:59 -0400

    Remove code using the old statusbar

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ViewExtensions/ViewExtensionsPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ViewExtensions/ViewExtensionsPage.xaml.cs
@@ -37,7 +37,6 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             base.OnNavigatedFrom(e);
 
             // Reset app back to normal.
-
             ApplicationViewExtensions.SetTitle(this, string.Empty);
 
             var lightGreyBrush = (Color)Application.Current.Resources["Grey-04"];
