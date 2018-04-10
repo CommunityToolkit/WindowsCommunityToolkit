164ac3bc579206d866661458653f0a1412c5d263
mhawker <michael.hawker@outlook.com>
2018-04-10 12:07:28 -0700

    Add NullableBool MarkupExtension and tests.

---
+++ Microsoft.Toolkit.Uwp.UI/Extensions/Markup/NullableBool.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+
+namespace Microsoft.Toolkit.Uwp.UI.Extensions
+{
+    /// <summary>
+    /// Custom <see cref="MarkupExtension"/> which can provide nullable bool values.
+    /// </summary>
+    [Bindable]
+    [MarkupExtensionReturnType(ReturnType = typeof(bool?))]
+    public class NullableBool : MarkupExtension
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the value of the Boolean is true.  Ignored if <see cref="IsNull"/> is true.
+        /// </summary>
+        public bool Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the value should be null.  Overrides the <see cref="Value"/> property.
+        /// </summary>
+        public bool IsNull { get; set; }
+
+        /// <inheritdoc/>
+        protected override object ProvideValue()
+        {
+            if (IsNull)
+            {
+                return null;
+            }
+
+            return Value;
+        }
+    }
+}
---
+++ UnitTests/Extensions/Test_NullableBoolMarkupExtension.cs
@@ -0,0 +1,121 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using Microsoft.Toolkit.Extensions;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework.AppContainer;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using Assert = Microsoft.VisualStudio.TestPlatform.UnitTestFramework.Assert;
+
+namespace UnitTests.Extensions
+{
+    [TestClass]
+    public class Test_NullableBoolMarkupExtension
+    {
+        [TestCategory("NullableBoolMarkupExtension")]
+        [UITestMethod]
+        public void Test_NullableBool_MarkupExtension_ProvidesTrue()
+        {
+            var treeroot = XamlReader.Load(@"<Page
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+    xmlns:ex=""using:Microsoft.Toolkit.Uwp.UI.Extensions"">
+        <CheckBox x:Name=""Check"" IsChecked=""{ex:NullableBool Value=True}""/>
+</Page>") as FrameworkElement;
+
+            var toggle = treeroot.FindChildByName("Check") as CheckBox;
+
+            Assert.IsNotNull(toggle, "Could not find checkbox control in tree.");
+
+            Assert.AreEqual(true, toggle.IsChecked, "Expected checkbox value to be true.");
+        }
+
+        [TestCategory("NullableBoolMarkupExtension")]
+        [UITestMethod]
+        public void Test_NullableBool_MarkupExtension_ProvidesFalse()
+        {
+            var treeroot = XamlReader.Load(@"<Page
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+    xmlns:ex=""using:Microsoft.Toolkit.Uwp.UI.Extensions"">
+        <CheckBox x:Name=""Check"" IsChecked=""{ex:NullableBool Value=False}""/>
+</Page>") as FrameworkElement;
+
+            var toggle = treeroot.FindChildByName("Check") as CheckBox;
+
+            Assert.IsNotNull(toggle, "Could not find checkbox control in tree.");
+
+            Assert.AreEqual(false, toggle.IsChecked, "Expected checkbox value to be false.");
+        }
+
+        [TestCategory("NullableBoolMarkupExtension")]
+        [UITestMethod]
+        public void Test_NullableBool_MarkupExtension_ProvidesNull()
+        {
+            var treeroot = XamlReader.Load(@"<Page
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+    xmlns:ex=""using:Microsoft.Toolkit.Uwp.UI.Extensions"">
+        <CheckBox x:Name=""Check"" IsChecked=""{ex:NullableBool IsNull=True}""/>
+</Page>") as FrameworkElement;
+
+            var toggle = treeroot.FindChildByName("Check") as CheckBox;
+
+            Assert.IsNotNull(toggle, "Could not find checkbox control in tree.");
+
+            Assert.AreEqual(null, toggle.IsChecked, "Expected checkbox value to be null.");
+        }
+
+        [TestCategory("NullableBoolMarkupExtension")]
+        [UITestMethod]
+        public void Test_NullableBool_MarkupExtension_ProvidesNullStillWithTrueValue()
+        {
+            var treeroot = XamlReader.Load(@"<Page
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+    xmlns:ex=""using:Microsoft.Toolkit.Uwp.UI.Extensions"">
+        <CheckBox x:Name=""Check"" IsChecked=""{ex:NullableBool IsNull=True, Value=True}""/>
+</Page>") as FrameworkElement;
+
+            var toggle = treeroot.FindChildByName("Check") as CheckBox;
+
+            Assert.IsNotNull(toggle, "Could not find checkbox control in tree.");
+
+            Assert.AreEqual(null, toggle.IsChecked, "Expected checkbox value to be null.");
+        }
+
+        [TestCategory("NullableBoolMarkupExtension")]
+        [UITestMethod]
+        public void Test_NullableBool_MarkupExtension_ProvidesNullStillWithFalseValue()
+        {
+            // Should be no-op as Value is false by default.
+            var treeroot = XamlReader.Load(@"<Page
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+    xmlns:ex=""using:Microsoft.Toolkit.Uwp.UI.Extensions"">
+        <CheckBox x:Name=""Check"" IsChecked=""{ex:NullableBool IsNull=True, Value=False}""/>
+</Page>") as FrameworkElement;
+
+            var toggle = treeroot.FindChildByName("Check") as CheckBox;
+
+            Assert.IsNotNull(toggle, "Could not find checkbox control in tree.");
+
+            Assert.AreEqual(null, toggle.IsChecked, "Expected checkbox value to be null.");
+        }
+    }
+}
