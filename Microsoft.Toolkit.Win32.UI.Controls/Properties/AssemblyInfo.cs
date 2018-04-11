84eeb742b0de3d16b1d34580f0e1a641f2bde05d
Richard Murillo <rimuri@microsoft.com>
2018-04-11 13:53:23 -0700

    Update AssemblyInfo.cs
    Add attributes that are missing

--- Microsoft.Toolkit.Win32.UI.Controls/Properties/AssemblyInfo.cs
+++ Microsoft.Toolkit.Win32.UI.Controls/Properties/AssemblyInfo.cs
@@ -11,17 +11,27 @@
 // ******************************************************************
 
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.Toolkit.Win32.UI.Controls.dll")]
 [assembly: AssemblyDescription("Microsoft.Toolkit.Win32.UI.Controls.dll")]
+[assembly: AssemblyProduct("Microsoft.Toolkit.Win32.UI.Controls")]
+[assembly: AssemblyCopyright("\x00a9 Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("392734a6-6ccb-49d5-bca2-44a484008fb8")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
 // Dependencies
 
@@ -66,6 +76,7 @@ using System.Runtime.InteropServices;
 // InternalsVisableTo
 [assembly: InternalsVisibleTo("Microsoft.Toolkit.Win32.UI.Controls.WinForms.Tests")]
 
+[assembly: SecurityRules(SecurityRuleSet.Level2)]
 #if ALLOW_PARTIALLY_TRUSTED_CALLERS
 [assembly: System.Security.AllowPartiallyTrustedCallers]
 #endif
