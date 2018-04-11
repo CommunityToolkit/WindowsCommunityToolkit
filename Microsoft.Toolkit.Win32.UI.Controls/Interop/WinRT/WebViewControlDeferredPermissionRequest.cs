3cca486b395d404e38728048825c56579a7cae2b
Richard Murillo <rimuri@microsoft.com>
2018-04-11 14:11:48 -0700

    Add doc for WebViewControlDeferredPermissionRequest

--- Microsoft.Toolkit.Win32.UI.Controls/Interop/WinRT/WebViewControlDeferredPermissionRequest.cs
+++ Microsoft.Toolkit.Win32.UI.Controls/Interop/WinRT/WebViewControlDeferredPermissionRequest.cs
@@ -15,29 +15,47 @@ using System.Security;
 
 namespace Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT
 {
-  public sealed class WebViewControlDeferredPermissionRequest
-  {
-    [SecurityCritical]
-    private readonly Windows.Web.UI.WebViewControlDeferredPermissionRequest _webViewControlDeferredPermissionRequest;
-
-    internal WebViewControlDeferredPermissionRequest(Windows.Web.UI.WebViewControlDeferredPermissionRequest webViewControlDeferredPermissionRequest)
+    /// <summary>
+    /// A proxy for <see cref="Windows.Web.UI.WebViewControlDeferredPermissionRequest"/> class.
+    /// </summary>
+    public sealed class WebViewControlDeferredPermissionRequest
     {
-      _webViewControlDeferredPermissionRequest = webViewControlDeferredPermissionRequest;
+        [SecurityCritical]
+        private readonly Windows.Web.UI.WebViewControlDeferredPermissionRequest _webViewControlDeferredPermissionRequest;
+
+        internal WebViewControlDeferredPermissionRequest(Windows.Web.UI.WebViewControlDeferredPermissionRequest webViewControlDeferredPermissionRequest)
+        {
+            _webViewControlDeferredPermissionRequest = webViewControlDeferredPermissionRequest;
+        }
+
+        /// <summary>
+        /// Gets the identifier for the permission request.
+        /// </summary>
+        public uint Id => _webViewControlDeferredPermissionRequest.Id;
+        /// <summary>
+        /// Gets a value that indicates the type of permission that's requested.
+        /// </summary>
+        public WebViewControlPermissionType PermissionType => (WebViewControlPermissionType)_webViewControlDeferredPermissionRequest.PermissionType;
+        /// <summary>
+        /// Gets the Uniform Resource Identifier (URI) of the content where the permission request originated.
+        /// </summary>
+        public Uri Uri => _webViewControlDeferredPermissionRequest.Uri;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2225:OperatorOverloadsHaveNamedAlternates")]
+        public static implicit operator WebViewControlDeferredPermissionRequest(
+      Windows.Web.UI.WebViewControlDeferredPermissionRequest webViewControlDeferredPermissionRequest)
+        {
+            return new WebViewControlDeferredPermissionRequest(webViewControlDeferredPermissionRequest);
+        }
+
+        /// <summary>
+        /// Grants the requested permission.
+        /// </summary>
+        public void Allow() => _webViewControlDeferredPermissionRequest.Allow();
+
+        /// <summary>
+        /// Denies the requested permission.
+        /// </summary>
+        public void Deny() => _webViewControlDeferredPermissionRequest.Deny();
     }
-
-    public uint Id => _webViewControlDeferredPermissionRequest.Id;
-    public WebViewControlPermissionType PermissionType => (WebViewControlPermissionType)_webViewControlDeferredPermissionRequest.PermissionType;
-    public Uri Uri => _webViewControlDeferredPermissionRequest.Uri;
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2225:OperatorOverloadsHaveNamedAlternates")]
-    public static implicit operator WebViewControlDeferredPermissionRequest(
-  Windows.Web.UI.WebViewControlDeferredPermissionRequest webViewControlDeferredPermissionRequest)
-    {
-      return new WebViewControlDeferredPermissionRequest(webViewControlDeferredPermissionRequest);
-    }
-
-    public void Allow() => _webViewControlDeferredPermissionRequest.Allow();
-
-    public void Deny() => _webViewControlDeferredPermissionRequest.Deny();
-  }
 }
