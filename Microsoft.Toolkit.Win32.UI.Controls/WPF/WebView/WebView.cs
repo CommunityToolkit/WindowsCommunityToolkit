1f11a169915fe61cbca624b56940405a14d2c906
Richard Murillo <rimuri@microsoft.com>
2018-04-20 15:29:51 -0700

    Enable WPF layout debug messages

--- Microsoft.Toolkit.Win32.UI.Controls/WPF/WebView/WebView.cs
+++ Microsoft.Toolkit.Win32.UI.Controls/WPF/WebView/WebView.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.InteropServices;
