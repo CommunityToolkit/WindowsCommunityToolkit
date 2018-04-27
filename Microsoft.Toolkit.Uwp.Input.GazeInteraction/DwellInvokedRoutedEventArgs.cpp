59b20b09fbc6d9cc64fc729e97491e0e779da9cd
Pete Ansell <petea@microsoft.com>
2018-04-27 14:21:51 -0700

    Trimmed top of .cpp files, or removed them if they were empty.

--- Microsoft.Toolkit.Uwp.Input.GazeInteraction/DwellInvokedRoutedEventArgs.cpp
+++
@@ -1,9 +0,0 @@
-//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
-//See LICENSE in the project root for license information.
-
-#include "pch.h"
-#include "DwellInvokedRoutedEventArgs.h"
-
-BEGIN_NAMESPACE_GAZE_INPUT
-
-END_NAMESPACE_GAZE_INPUT
