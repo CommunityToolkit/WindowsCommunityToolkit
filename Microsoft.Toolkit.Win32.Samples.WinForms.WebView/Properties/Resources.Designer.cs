57932512dacbd4f9738864aed0bdc0a8db099cfd
Richard Murillo <rimuri@microsoft.com>
2018-04-30 13:04:52 -0700

    Change Win32 controls to use 4.6.2
    4.6.2 is the minimum required that supports `DPIChangedEvent` required for WebView high DPI scenarios.

--- Microsoft.Toolkit.Win32.Samples.WinForms.WebView/Properties/Resources.Designer.cs
+++ Microsoft.Toolkit.Win32.Samples.WinForms.WebView/Properties/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace Microsoft.Toolkit.Win32.Samples.WinForms.WebView.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -36,7 +39,7 @@ namespace Microsoft.Toolkit.Win32.Samples.WinForms.WebView.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WebViewTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Toolkit.Win32.Samples.WinForms.WebView.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
