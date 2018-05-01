f8087abd5481b71dc2342ac6026ef0fd4e510237
Richard Murillo <rimuri@microsoft.com>
2018-05-01 13:38:14 -0700

    Remove CS1591 suppression from WebViewControlPermissionType
    Add summary documentation for duplicated type and remove 1591 suppression

--- Microsoft.Toolkit.Win32.UI.Controls/Interop/WinRT/WebViewControlPermissionType.cs
+++ Microsoft.Toolkit.Win32.UI.Controls/Interop/WinRT/WebViewControlPermissionType.cs
@@ -12,9 +12,6 @@
 
 namespace Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT
 {
-    // Type is a copy. Information regarding the origination of the type is in summary comments
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-
     /// <summary>
     /// Defines constants that specify the state of a <see cref="IWebView.PermissionRequested"/> event.
     /// </summary>
@@ -22,12 +19,35 @@ namespace Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT
     /// <seealso cref="Windows.Web.UI.WebViewControlPermissionType"/>
     public enum WebViewControlPermissionType
     {
+        /// <summary>
+        /// Permission is for geolocation.
+        /// </summary>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Geolocation", Justification = "Value from WinRT type")]
         Geolocation,
+
+        /// <summary>
+        /// Permission is for unlimited IndexedDB data storage.
+        /// </summary>
         UnlimitedIndexedDBQuota,
+
+        /// <summary>
+        /// Permission is for media.
+        /// </summary>
         Media,
+
+        /// <summary>
+        /// Permission is for pointer lock.
+        /// </summary>
         PointerLock,
+
+        /// <summary>
+        /// Permission is for web notifications.
+        /// </summary>
         WebNotifications,
+
+        /// <summary>
+        /// Permission is for screen.
+        /// </summary>
         Screen,
     }
 }
