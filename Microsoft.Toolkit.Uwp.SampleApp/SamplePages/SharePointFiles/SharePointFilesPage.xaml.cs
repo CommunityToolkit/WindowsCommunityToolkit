9895f4e47a760fc9ebcf716c342d983ecd6a5d70
Alex Chen <alexbiz@foxmail.com>
2018-05-03 21:30:58 +0800

    added ability to convert SP doc lib URL to drive URL

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/SharePointFiles/SharePointFilesPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/SharePointFiles/SharePointFilesPage.xaml.cs
@@ -10,7 +10,12 @@
 // THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
 // ******************************************************************
 
-using Microsoft.Toolkit.Uwp.SampleApp.Models;
+using Microsoft.Toolkit.Uwp.UI.Controls.Graph;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
+using System;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -19,14 +24,74 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
     /// <summary>
     /// A page that shows how to use the opacity behavior.
     /// </summary>
-    public sealed partial class SharePointFilesPage : Page
+    public sealed partial class SharePointFilesPage : Page, IXamlRenderListener
     {
+        private SharePointFiles _sharePointFilesControl;
+        private StackPanel _convertPanel;
+        private TextBox _tbDocLibURL;
+        private Button _btnConvert;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="AADLoginPage"/> class.
         /// </summary>
         public SharePointFilesPage()
         {
             InitializeComponent();
+        }
+
+        public void OnXamlRendered(FrameworkElement control)
+        {
+            _sharePointFilesControl = control.FindDescendantByName("SharePointFilesControl") as SharePointFiles;
+            _convertPanel = control.FindDescendantByName("ConvertPanel") as StackPanel;
+            _tbDocLibURL = control.FindDescendantByName("tbDocLibURL") as TextBox;
+            _btnConvert = control.FindDescendantByName("btnConvert") as Button;
+
+            if (_sharePointFilesControl != null)
+            {
+                _btnConvert.Click += ConvertButton_Click;
+            }
+        }
+
+        private async void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_sharePointFilesControl != null && _tbDocLibURL != null && _convertPanel != null)
+            {
+                if (string.IsNullOrEmpty(_sharePointFilesControl.GraphAccessToken))
+                {
+                    await new MessageDialog("Please set the GraphAccessToken in the properties panel first.").ShowAsync();
+                    return;
+                }
+
+                string driveURL = await _sharePointFilesControl.GetDriveUrlFromSharePointUrl(_tbDocLibURL.Text);
+                DataPackage copyData = new DataPackage();
+                copyData.SetText(driveURL);
+                Clipboard.SetContent(copyData);
+
+                _sharePointFilesControl.Visibility = Visibility.Visible;
+                _convertPanel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Shell.Current.RegisterNewCommand("Convert Drive URL", async (sender, args) =>
+            {
+                if (_sharePointFilesControl != null && _convertPanel != null)
+                {
+                    if (_sharePointFilesControl.Visibility == Visibility.Collapsed)
+                    {
+                        _sharePointFilesControl.Visibility = Visibility.Visible;
+                        _convertPanel.Visibility = Visibility.Collapsed;
+                    }
+                    else
+                    {
+                        _sharePointFilesControl.Visibility = Visibility.Collapsed;
+                        _convertPanel.Visibility = Visibility.Visible;
+                    }
+                }
+            });
         }
     }
 }
