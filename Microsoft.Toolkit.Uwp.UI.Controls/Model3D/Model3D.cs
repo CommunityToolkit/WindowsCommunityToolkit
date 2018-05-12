c104e3e76a134006cdedcd14c1bd744a05b779cc
David Catuhe <david.catuhe@live.fr>
2018-05-11 17:38:16 -0700

    First update - Attached to #2034

--- Microsoft.Toolkit.Uwp.UI.Controls/Model3D/Model3D.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/Model3D/Model3D.cs
@@ -3,28 +3,44 @@ using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
-
 namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
+    /// <summary>
+    /// Class used to display glTF models
+    /// </summary>
     public sealed class Model3D : Control
     {
         private WebView _view;
 
+        /// <summary>
+        /// Event raised when the assets are loading
+        /// </summary>
         public event EventHandler AssetLoading;
+
+        /// <summary>
+        /// Event raised when assets are loaded
+        /// </summary>
         public event EventHandler AssetLoaded;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Model3D"/> class
+        /// </summary>
         public Model3D()
         {
-            this.DefaultStyleKey = typeof(Model3D);
+            DefaultStyleKey = typeof(Model3D);
         }
 
+        /// <summary>
+        /// Invoked whenever application code or internal processes (such as a rebuilding layout pass) call
+        /// ApplyTemplate. In simplest terms, this means the method is called just before a UI element displays
+        /// in your app. Override this method to influence the default post-template logic of a class.
+        /// </summary>
         protected override void OnApplyTemplate()
         {
             if (_view != null)
             {
-                _view.ScriptNotify -= _view_ScriptNotify;
-                _view.NavigationCompleted -= _view_NavigationCompleted;
+                _view.ScriptNotify -= View_ScriptNotify;
+                _view.NavigationCompleted -= View_NavigationCompleted;
             }
 
             base.OnApplyTemplate();
@@ -32,35 +48,74 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             _view = GetTemplateChild("View") as WebView;
             if (_view != null)
             {
-                _view.ScriptNotify += _view_ScriptNotify;
-                _view.NavigationCompleted += _view_NavigationCompleted;
+                _view.ScriptNotify += View_ScriptNotify;
+                _view.NavigationCompleted += View_NavigationCompleted;
                 _view.Navigate(new Uri("ms-appx-web:///Microsoft.Toolkit.Uwp.UI.Controls/Model3D/BabylonView.html"));
             }
-
         }
 
-        private void _view_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        private static async void OnCameraControlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            SetupSource();
-            UpdateCamera();
+            await (d as Model3D).UpdateCameraControlAsync();
         }
 
-        private void _view_ScriptNotify(object sender, NotifyEventArgs e)
+        private static async void OnRadiusValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            await (d as Model3D).UpdateRadiusAsync();
+        }
+
+        private static async void OnCameraValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            await (d as Model3D).UpdateCameraAsync();
+        }
+
+        private static async void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            await (d as Model3D).SetupSourceAsync();
+        }
+
+        private static async void OnEnvironmentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            await (d as Model3D).UpdateEnvironment();
+        }
+
+        private async void View_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            await SetupSourceAsync();
+            await UpdateCameraAsync();
+            await UpdateRadiusAsync();
+            await UpdateCameraControlAsync();
+            await UpdateEnvironment();
+        }
+
+        private void View_ScriptNotify(object sender, NotifyEventArgs e)
         {
             if (e.Value == "loading")
             {
                 // Loading
                 AssetLoading?.Invoke(this, EventArgs.Empty);
             }
-            else
+            else if (e.Value == "loaded")
             {
                 // ready
                 _view.Opacity = 1;
                 AssetLoaded?.Invoke(this, EventArgs.Empty);
             }
+            else if (e.Value.StartsWith("alpha:"))
+            {
+                AlphaInDegrees = GetDegreeFromRadian(float.Parse(e.Value.Substring(6)));
+            }
+            else if (e.Value.StartsWith("beta:"))
+            {
+                BetaInDegrees = GetDegreeFromRadian(float.Parse(e.Value.Substring(5)));
+            }
+            else if (e.Value.StartsWith("radius:"))
+            {
+                RadiusPercentage = float.Parse(e.Value.Substring(7));
+            }
         }
 
-        private async Task SetupSource()
+        private async Task SetupSourceAsync()
         {
             if (_view == null || string.IsNullOrWhiteSpace(Source))
             {
@@ -77,6 +132,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
                 {
                     slashIndex++;
                 }
+
                 fileName = Source.Substring(slashIndex);
             }
             else if (Uri.TryCreate(Source, UriKind.Absolute, out uri))
@@ -89,6 +145,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
                 {
                     slashIndex++;
                 }
+
                 fileName = uri.LocalPath.Substring(slashIndex);
             }
             else
@@ -99,7 +156,17 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             await _view.InvokeScriptAsync("setupSource", new string[] { root, fileName });
         }
 
-        private async Task UpdateCamera()
+        private async Task UpdateEnvironment()
+        {
+            if (_view == null || string.IsNullOrWhiteSpace(Environment))
+            {
+                return;
+            }
+ 
+            await _view.InvokeScriptAsync("updateEnvironment", new string[] { Environment });
+        }
+
+        private async Task UpdateCameraAsync()
         {
             if (_view == null)
             {
@@ -112,51 +179,131 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             await _view.InvokeScriptAsync("updateCameraPositionValues", new string[] { alpha.ToString(), beta.ToString() });
         }
 
+        private async Task UpdateRadiusAsync()
+        {
+            if (_view == null)
+            {
+                return;
+            }
+
+            await _view.InvokeScriptAsync("updateCameraRadiusValue", new string[] { RadiusPercentage.ToString() });
+        }
+
+        private async Task UpdateCameraControlAsync()
+        {
+            if (_view == null)
+            {
+                return;
+            }
+
+            await _view.InvokeScriptAsync("updateCameraControl", new string[] { CameraControl.ToString() });
+        }
+
         private float GetRadianFromDegree(float degrees)
         {
             return degrees / 180f * (float)Math.PI;
         }
 
-        private static void OnCameraValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private float GetDegreeFromRadian(float radians)
         {
-            (d as Model3D).UpdateCamera();
+            var value = (radians * 180f / (float)Math.PI) % 360f;
+
+            if (value < 0)
+            {
+                value += 360f * (float)Math.Ceiling(Math.Abs(value / 360.0f));
+            }
+
+            return value;
         }
 
-        private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            (d as Model3D).SetupSource();
-        }
-
+        /// <summary>
+        /// Gets or sets a string indicating the source path for the model
+        /// </summary>
         public string Source
         {
             get { return (string)GetValue(SourceProperty); }
             set { SetValue(SourceProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Source.  This enables animation, styling, binding, etc...
+        /// <summary>
+        /// Identifies the <see cref="Source"/> property
+        /// </summary>
         public static readonly DependencyProperty SourceProperty =
             DependencyProperty.Register("Source", typeof(string), typeof(Model3D), new PropertyMetadata(string.Empty, OnSourceChanged));
 
+        /// <summary>
+        /// Gets or sets a string indicating the path for the environment texture (must be a DDS file)
+        /// </summary>
+        public string Environment
+        {
+            get { return (string)GetValue(EnvironmentProperty); }
+            set { SetValue(EnvironmentProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="Environment"/> property
+        /// </summary>
+        public static readonly DependencyProperty EnvironmentProperty =
+            DependencyProperty.Register("Environment", typeof(string), typeof(Model3D), new PropertyMetadata(string.Empty, OnEnvironmentChanged));
+
+        /// <summary>
+        /// Gets or sets a float value indicating the alpha angle in degrees (which defines the rotation on horizontal plane)
+        /// </summary>
         public float AlphaInDegrees
         {
             get { return (float)GetValue(AlphaInDegreesProperty); }
             set { SetValue(AlphaInDegreesProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for AlphaInDegrees.  This enables animation, styling, binding, etc...
+        /// <summary>
+        /// Identifies the <see cref="AlphaInDegrees"/> property
+        /// </summary>
         public static readonly DependencyProperty AlphaInDegreesProperty =
             DependencyProperty.Register("AlphaInDegrees", typeof(float), typeof(Model3D), new PropertyMetadata(90f, OnCameraValueChanged));
 
-
-
+        /// <summary>
+        /// Gets or sets a float value indicating the beta angle in degrees (which defines the rotation on vertical plane)
+        /// </summary>
         public float BetaInDegrees
         {
             get { return (float)GetValue(BetaInDegreesProperty); }
             set { SetValue(BetaInDegreesProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for BetaInDegrees.  This enables animation, styling, binding, etc...
+        /// <summary>
+        /// Identifies the <see cref="BetaInDegrees"/> property
+        /// </summary>
         public static readonly DependencyProperty BetaInDegreesProperty =
             DependencyProperty.Register("BetaInDegrees", typeof(float), typeof(Model3D), new PropertyMetadata(90f, OnCameraValueChanged));
+
+        /// <summary>
+        /// Gets or sets a float value between 0 and 100 indicating the distance from the center of the scene
+        /// </summary>
+        public float RadiusPercentage
+        {
+            get { return (float)GetValue(RadiusPercentageProperty); }
+            set { SetValue(RadiusPercentageProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="RadiusPercentage"/> property
+        /// </summary>
+        public static readonly DependencyProperty RadiusPercentageProperty =
+            DependencyProperty.Register("RadiusPercentage", typeof(float), typeof(Model3D), new PropertyMetadata(5f, OnRadiusValueChanged));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the camera can be controlled with pointers (mouse, touch, pencil) or not
+        /// </summary>
+        public bool CameraControl
+        {
+            get { return (bool)GetValue(CameraControlProperty); }
+            set { SetValue(CameraControlProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="CameraControl"/> property
+        /// </summary>
+        public static readonly DependencyProperty CameraControlProperty =
+            DependencyProperty.Register("CameraControl", typeof(bool), typeof(Model3D), new PropertyMetadata(true, OnCameraControlChanged));
     }
 }
