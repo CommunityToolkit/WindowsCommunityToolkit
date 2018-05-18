84cc17645edcf607d1e61efcd3ad6b8c3e30939a
AVK Naidu <vamshikrishna.naidu@live.com>
2018-05-18 17:49:50 -0400

    Add Control to Sample

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/RemoteDevicePicker/RemoteDevicePickerControlPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using System;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    public sealed partial class RemoteDevicePickerControlPage : Page
+    {
+        public RemoteDevicePickerControlPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            RemoteDevicePicker remoteDevicePicker = new RemoteDevicePicker()
+            {
+                Title = "Pick Remote Device",
+                DeviceListSelectionMode = ListViewSelectionMode.Single,
+                HeaderLineColor = new SolidColorBrush((Color)Application.Current.Resources["Brand-Color"])
+            };
+            remoteDevicePicker.RemoteDevicePickerClosed += RemoteDevicePicker_RemoteDevicePickerClosed;
+            await remoteDevicePicker.ShowAsync();
+        }
+
+        private async void RemoteDevicePicker_RemoteDevicePickerClosed(object sender, RemoteDevicePickerEventArgs e)
+        {
+            await new MessageDialog(e.Devices.Count.ToString()).ShowAsync();
+        }
+    }
+}
