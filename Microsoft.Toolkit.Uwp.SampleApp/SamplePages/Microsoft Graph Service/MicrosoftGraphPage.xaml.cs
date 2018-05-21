39d47a11126b85935944aea94bd49d0dd790416f
Vinoth Rajendran <r.vinoth@live.com>
2018-05-21 21:09:43 +0530

    Merge Conflicting fixing

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Microsoft Graph Service/MicrosoftGraphPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Microsoft Graph Service/MicrosoftGraphPage.xaml.cs
@@ -11,14 +11,19 @@
 // ******************************************************************
 
 using System;
+using System.Collections.ObjectModel;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Toolkit.Services.MicrosoftGraph;
+using Windows.Storage.Streams;
 using Windows.UI.Core;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 using static Microsoft.Toolkit.Services.MicrosoftGraph.MicrosoftGraphEnums;
 
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
@@ -53,18 +58,17 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             var item = VersionEndpointDropdown.SelectedItem as ComboBoxItem;
             var endpointVersion = item.Tag.ToString() == "v2" ? AuthenticationModel.V2 : AuthenticationModel.V1;
 
-            // MicrosoftGraphService.Instance.AuthenticationModel = endpointVersion;
+            MicrosoftGraphService.Instance.AuthenticationModel = endpointVersion;
 
             // Initialize the service
             switch (endpointVersion)
             {
                 case AuthenticationModel.V1:
-                   // MicrosoftGraphService.Instance.Initialize(ClientId.Text);
+                    MicrosoftGraphService.Instance.Initialize(ClientId.Text);
                     break;
                 case AuthenticationModel.V2:
                     var scopes = DelegatedPermissionScopes.Text.Split(' ');
-
-                   // MicrosoftGraphService.Instance.Initialize(ClientId.Text, ServicesToInitialize.Message | ServicesToInitialize.UserProfile | ServicesToInitialize.Event, scopes);
+                    MicrosoftGraphService.Instance.Initialize(ClientId.Text, ServicesToInitialize.Message | ServicesToInitialize.UserProfile | ServicesToInitialize.Event, scopes);
                     break;
                 default:
                     break;
@@ -73,12 +77,12 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             // Login via Azure Active Directory
             try
             {
-                // if (!await MicrosoftGraphService.Instance.LoginAsync())
-                // {
-                //    var error = new MessageDialog("Unable to sign in to Office 365");
-                //    await error.ShowAsync();
-                //    return;
-                // }
+                if (!await MicrosoftGraphService.Instance.LoginAsync(upn))
+                {
+                    var error = new MessageDialog("Unable to sign in to Office 365");
+                    await error.ShowAsync();
+                    return;
+                }
             }
             catch (AdalServiceException ase)
             {
@@ -109,23 +113,23 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             try
             {
                 // Retrieve user's info from Azure Active Directory
-                // var user = await MicrosoftGraphService.Instance.User.GetProfileAsync();
-                // UserPanel.DataContext = user;
+                var user = await MicrosoftGraphService.Instance.User.GetProfileAsync();
+                UserPanel.DataContext = user;
 
-                // using (IRandomAccessStream photoStream = await MicrosoftGraphService.Instance.User.GetPhotoAsync())
-                // {
-                //    BitmapImage photo = new BitmapImage();
-                //    if (photoStream != null)
-                //    {
-                //        await photo.SetSourceAsync(photoStream);
-                //    }
-                //    else
-                //    {
-                //        photo.UriSource = new Uri("ms-appx:///SamplePages/MicrosoftGraph Service/user.png");
-                //    }
+                using (IRandomAccessStream photoStream = await MicrosoftGraphService.Instance.User.GetPhotoAsync())
+                {
+                    BitmapImage photo = new BitmapImage();
+                    if (photoStream != null)
+                    {
+                        await photo.SetSourceAsync(photoStream);
+                    }
+                    else
+                    {
+                        photo.UriSource = new Uri("ms-appx:///SamplePages/MicrosoftGraph Service/user.png");
+                    }
 
-                // Photo.Source = photo;
-                // }
+                    Photo.Source = photo;
+                }
             }
             catch (ServiceException ex)
             {
@@ -285,11 +289,10 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             }
         }
 
-        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        private async void LogOutButton_Click(object sender, RoutedEventArgs e)
         {
             MessagesList.LoadMoreItemsAsync().Cancel();
-
-            // await MicrosoftGraphService.Instance.Logout();
+            await MicrosoftGraphService.Instance.Logout();
             EventsList.Visibility = Visibility.Collapsed;
             EventsBox.Visibility = Visibility.Collapsed;
             MessagesList.Visibility = Visibility.Collapsed;
