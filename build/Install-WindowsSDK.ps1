8a71806f70d224e2fb07b51439f592f56399eaf9
Oren Novotny <oren@novotny.org>
2018-05-29 20:52:09 -0400

    wait for finish

--- build/Install-WindowsSDK.ps1
+++ build/Install-WindowsSDK.ps1
@@ -3,4 +3,4 @@ mkdir c:\winsdktemp
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://go.microsoft.com/fwlink/p/?linkid=870807","c:\winsdktemp\winsdksetup.exe")
 
-c:\winsdktemp\winsdksetup.exe /features OptionId.UWPCpp /q
+Start-Process -Wait "c:\winsdktemp\winsdksetup.exe" "/features OptionId.UWPCpp /q"
