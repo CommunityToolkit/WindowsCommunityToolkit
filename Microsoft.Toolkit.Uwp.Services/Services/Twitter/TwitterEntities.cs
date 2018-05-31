3c500ab747a547c11e428396d001b987d9060ae8
Claire Davis (Strodtbeck) <clstrodt@microsoft.com>
2018-05-31 11:04:27 -0700

    Updated TwitterEntities comments.
    TODO:
    TwitterUserMention

--- Microsoft.Toolkit.Uwp.Services/Services/Twitter/TwitterEntities.cs
+++ Microsoft.Toolkit.Uwp.Services/Services/Twitter/TwitterEntities.cs
@@ -13,18 +13,21 @@ namespace Microsoft.Toolkit.Uwp.Services.Twitter
     {
         /// <summary>
         /// Gets or sets Hashtags array of the tweet.
+        /// This array always exists, even if it's empty (no hashtags).
         /// </summary>
-        [JsonProperty("Hashtags")]
+        [JsonProperty("hashtags")]
         public TwitterHashtag[] Hashtags { get; set; }
 
         /// <summary>
         /// Gets or sets Media array of the tweet.
+        /// This array will not exist if no media is attached (GIF, photo, or video).
         /// </summary>
         [JsonProperty("media")]
         public TwitterMedia[] Media { get; set; }
 
         /// <summary>
         /// Gets or sets Urls array of the tweet.
+        /// This array will always exist, even if it's empty (no URLs).
         /// </summary>
         [JsonProperty("urls")]
         public TwitterUrl[] Urls { get; set; }
