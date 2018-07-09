063e8748f1f3f694f75bfface7dacad259523a72
Shawn Kendrot <skendrot@hotmail.com>
2018-07-09 11:24:02 -0600

    Remove unused using

--- Microsoft.Toolkit.Parsers/Markdown/Inlines/LinkAnchorInline.cs
+++ Microsoft.Toolkit.Parsers/Markdown/Inlines/LinkAnchorInline.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
-using System.Xml;
 using System.Xml.Linq;
 using Microsoft.Toolkit.Parsers.Markdown.Helpers;
 
