6b5eac9f21a6ecd801f42e63d5e19b579d9093ad
AVK Naidu <vamshikrishna.naidu@live.com>
2018-07-12 14:01:16 -0400

    Update Regex to support multiple formats.

--- Microsoft.Toolkit/Extensions/StringExtensions.cs
+++ Microsoft.Toolkit/Extensions/StringExtensions.cs
@@ -13,7 +13,8 @@ namespace Microsoft.Toolkit.Extensions
     /// </summary>
     public static class StringExtensions
     {
-        internal const string PhoneNumberRegex = @"^\s*\+?\s*([0-9][\s-]*){9,}$";
+        // internal const string PhoneNumberRegex = @"^\s*\+?\s*([0-9][\s-]*){9,}$";
+        internal const string PhoneNumberRegex = @"(\([0-9]{3}\) ?)[0-9]{3}-[0-9]{4}|[0-9]{3}-?[0-9]{3}-?[0-9]{4}|([00|+][0-9]{1,2}) ?[0-9]{3}-?[0-9]{3}-?[0-9]{4}";
         internal const string CharactersRegex = "^[A-Za-z]+$";
         internal const string EmailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
