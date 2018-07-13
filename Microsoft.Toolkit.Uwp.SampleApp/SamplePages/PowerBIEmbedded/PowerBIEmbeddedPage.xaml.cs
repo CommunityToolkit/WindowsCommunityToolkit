7940e394d63a5b775c5b431cf143959670d98a53
Theodore Shi <theodore.shi@canviz.com>
2018-07-13 15:33:11 +0800

    Update sample page of PowerBIEmbeded Control.

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/PowerBIEmbedded/PowerBIEmbeddedPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/PowerBIEmbedded/PowerBIEmbeddedPage.xaml.cs
@@ -2,19 +2,65 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
-    public sealed partial class PowerBIEmbeddedPage : IXamlRenderListener
+    public sealed partial class PowerBIEmbeddedPage
     {
         public PowerBIEmbeddedPage()
         {
             this.InitializeComponent();
         }
 
-        public void OnXamlRendered(FrameworkElement control)
+        private void PropertyDropdown_SelectionChanged(object sender, Windows.UI.Xaml.Controls.SelectionChangedEventArgs e)
         {
+            PropertyValue.Text = string.Empty;
+            PowerBIEmbeddedControl.GroupId = string.Empty;
+            PowerBIEmbeddedControl.EmbedUrl = string.Empty;
+
+        }
+
+        private void ClientIdExpandButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetVisibilityStatusPanel(ClientIdBox, (Button)sender);
+        }
+
+        private void SetVisibilityStatusPanel(FrameworkElement box, Button switchButton)
+        {
+            if (box.Visibility == Visibility.Visible)
+            {
+                switchButton.Content = "ª²";
+                box.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                switchButton.Content = "ª±";
+                box.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ClientId.Text.Trim())
+                && !string.IsNullOrEmpty(PropertyValue.Text.Trim()))
+            {
+                switch (PropertyDropdown.SelectedIndex)
+                {
+                    case 0:
+                        PowerBIEmbeddedControl.ClientId = ClientId.Text.Trim();
+                        PowerBIEmbeddedControl.GroupId = PropertyValue.Text.Trim();
+                        break;
+                    case 1:
+                        PowerBIEmbeddedControl.ClientId = ClientId.Text.Trim();
+                        PowerBIEmbeddedControl.EmbedUrl = PropertyValue.Text.Trim();
+                        break;
+                }
+
+                SetVisibilityStatusPanel(ClientIdBox, ClientIdExpandButton);
+            }
         }
     }
 }
