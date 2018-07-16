4152595888195e35fa172d1c44ef1e8836f15934
Jim Wilcox (The Granite State Hacker) <jimw@bluemetal.com>
2018-07-16 13:28:14 -0400

    Ran UpdateHeaders.bat

--- Microsoft.Toolkit.Win32/Microsoft.Toolkit.Win32.UI.Controls/WPF/InkToolbar/InkToolbarInitialControls.cs
+++ Microsoft.Toolkit.Win32/Microsoft.Toolkit.Win32.UI.Controls/WPF/InkToolbar/InkToolbarInitialControls.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.Toolkit.Win32.UI.Controls.WPF
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.Toolkit.Win32.UI.Controls.WPF
 {
     public enum InkToolbarInitialControls
     {
