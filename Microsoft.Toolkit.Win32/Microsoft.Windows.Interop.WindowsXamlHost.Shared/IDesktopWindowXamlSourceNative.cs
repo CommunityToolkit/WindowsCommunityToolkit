ed7ccbd9530f05a54501c3457d8a7bda0349ffbf
Jim Wilcox (The Granite State Hacker) <jimw@bluemetal.com>
2018-07-24 19:44:39 -0400

    Ran UpdateHeaders.bat

--- Microsoft.Toolkit.Win32/Microsoft.Windows.Interop.WindowsXamlHost.Shared/IDesktopWindowXamlSourceNative.cs
+++ Microsoft.Toolkit.Win32/Microsoft.Windows.Interop.WindowsXamlHost.Shared/IDesktopWindowXamlSourceNative.cs
@@ -1,7 +1,6 @@
-﻿// <copyright file="IDesktopWindowsXamlHostNative.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-// <author>Microsoft</author>
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 namespace Microsoft.Toolkit.Win32.UI.Interop
 {
