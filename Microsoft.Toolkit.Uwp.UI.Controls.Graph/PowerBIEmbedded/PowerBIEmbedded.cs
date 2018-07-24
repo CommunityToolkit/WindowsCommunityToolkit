f93f424dade31620fb11cfa0754af46541478408
Theodore Shi <theodore.shi@canviz.com>
2018-07-24 17:22:15 +0800

    fix merge issue

--- Microsoft.Toolkit.Uwp.UI.Controls.Graph/PowerBIEmbedded/PowerBIEmbedded.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Graph/PowerBIEmbedded/PowerBIEmbedded.cs
@@ -147,7 +147,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls.Graph
                                 $"'{token}', " +
                                 $"{JsonConvert.SerializeObject(new Report[] { report })}, " +
                                 $"{JsonConvert.SerializeObject(report)}, " +
-                                (IsWindowsPhone ? $"'{DisplayInformation.CurrentOrientation.ToString()}',)" : "'', " +
+                                (IsWindowsPhone ? $"'{DisplayInformation.CurrentOrientation.ToString()}'," : "'', ") +
                                 $"{ShowFilter.ToString().ToLower()}" +
                                 ")");
 
@@ -192,7 +192,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls.Graph
                         $"'{token}', " +
                         $"{JsonConvert.SerializeObject(reports)}, " +
                         $"{JsonConvert.SerializeObject(selectionReport)}, " +
-                        (IsWindowsPhone ? $"'{DisplayInformation.CurrentOrientation.ToString()}',)" : "'', " +
+                        (IsWindowsPhone ? $"'{DisplayInformation.CurrentOrientation.ToString()}'," : "'', ") +
                         $"{ShowFilter.ToString().ToLower()}" +
                         ")");
                 }
