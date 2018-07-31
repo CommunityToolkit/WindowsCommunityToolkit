350b7735ec72456a50d462ec80fe0bb84b4b806c
Richard Murillo <rimuri@microsoft.com>
2018-07-30 20:11:31 -0700

    Clean up BindingExpression.cs
    - Remove `this`

--- Microsoft.Toolkit.Win32/Microsoft.Toolkit.Win32.UI.Controls.WPF/Interop/WinRT/BindingExpression.cs
+++ Microsoft.Toolkit.Win32/Microsoft.Toolkit.Win32.UI.Controls.WPF/Interop/WinRT/BindingExpression.cs
@@ -17,7 +17,7 @@ namespace Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT
         /// </summary>
         public BindingExpression(Windows.UI.Xaml.Data.BindingExpression instance)
         {
-            this.UwpInstance = instance;
+            UwpInstance = instance;
         }
 
         /// <summary>
