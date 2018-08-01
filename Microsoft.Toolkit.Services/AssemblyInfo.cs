74b17f3161b01b3fdde6dc882ad861ecc357e29a
Oren Novotny <oren@novotny.org>
2018-08-01 09:22:34 -0400

    only UWP

--- Microsoft.Toolkit.Services/AssemblyInfo.cs
+++ Microsoft.Toolkit.Services/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if WINDOWS_UWP
 using System.Runtime.CompilerServices;
 [assembly:InternalsVisibleTo("Microsoft.Toolkit.Uwp.Services")]
 [assembly:InternalsVisibleTo("Microsoft.Toolkit.Uwp.UI.Controls.Graph")]
+#endif
