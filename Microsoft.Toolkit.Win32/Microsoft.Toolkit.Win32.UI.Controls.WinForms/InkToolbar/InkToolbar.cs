69242e3eb04c82958a3b9e87a47c0e444ec0e1e3
Jim Wilcox <jimw@bluemetal.com>
2018-08-06 11:46:01 -0400

    Ran UpdateHeaders.bat.

--- Microsoft.Toolkit.Win32/Microsoft.Toolkit.Win32.UI.Controls.WinForms/InkToolbar/InkToolbar.cs
+++ Microsoft.Toolkit.Win32/Microsoft.Toolkit.Win32.UI.Controls.WinForms/InkToolbar/InkToolbar.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
 using Microsoft.Toolkit.Win32.UI.Interop;
 using Microsoft.Toolkit.Win32.UI.Interop.WinForms;
 
