e46e429386471990385760b7cba395ea72acb4b6
Kevin Bronstein <kevin.bronstein@southworks.com>
2018-08-15 17:13:22 -0300

    Add test app for twitter service login and authorization

---
+++ TwitterLoginExample/TwitterLoginExample/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwitterLoginExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // ref http://stackoverflow.com/a/3978040
+        public static int GetRandomUnusedPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+
+        private HttpListener StartHttpListener(string uri)
+        {
+            var http = new HttpListener();
+            http.Prefixes.Add(uri);
+            http.Start();
+            return http;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            var http = StartHttpListener($"http://localhost:{GetRandomUnusedPort()}/");
+            var oauthToken = "";
+            string authorizationRequest = $"https://api.twitter.com/oauth/authorize?oauth_token={oauthToken}&force_login=true";
+
+            // Opens request in the browser.
+            System.Diagnostics.Process.Start(authorizationRequest);
+
+            // Waits for the OAuth authorization response.
+            var context = await http.GetContextAsync();
+
+            // Brings this app back to the foreground.
+            this.Activate();
+
+            // Sends an HTTP response to the browser.
+            var response = context.Response;
+            string responseString = string.Format("<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Please return to the app.</body></html>");
+            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            var responseOutput = response.OutputStream;
+            Task responseTask = responseOutput.WriteAsync(buffer, 0, buffer.Length).ContinueWith((task) =>
+            {
+                responseOutput.Close();
+                http.Stop();
+                Console.WriteLine("HTTP server stopped.");
+            });
+
+            // Checks for errors.
+            if (context.Request.QueryString.Get("error") != null)
+            {
+                return;
+            }
+            if (context.Request.QueryString.Get("code") == null
+                || context.Request.QueryString.Get("state") == null)
+            {
+                return;
+            }
+
+            // extracts the code
+            var code = context.Request.QueryString.Get("code");
+            var incoming_state = context.Request.QueryString.Get("state");
+
+            Console.WriteLine($"Code: {code}");
+
+        }
+    }
+}
