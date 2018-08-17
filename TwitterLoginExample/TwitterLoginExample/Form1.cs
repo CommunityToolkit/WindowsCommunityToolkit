1169c39ceaf00ea17e78657d865025150ce39c1f
Lucas Rojas <lucas.rojas@southworks.com>
2018-08-17 17:16:21 -0300

    Add Twitter authentication in a WebBrowser in popup

--- TwitterLoginExample/TwitterLoginExample/Form1.cs
+++ TwitterLoginExample/TwitterLoginExample/Form1.cs
@@ -11,16 +11,31 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web;
 
 namespace TwitterLoginExample
 {
     public partial class Form1 : Form
     {
+
+        PopupForm popupForm = new PopupForm();
+        Uri newUrl;
         public Form1()
         {
             InitializeComponent();
+            popupForm.FormClosed += popupIsClosed;
         }
 
+        private void popupIsClosed(object sender, FormClosedEventArgs e)
+        {
+            newUrl = popupForm.actualUrl;
+            if(newUrl != null)
+            {
+                var query = HttpUtility.ParseQueryString(newUrl.Query);
+                var auth_token = query.Get("oauth_token");
+                var auth_verifier = query.Get("oauth_verifier");
+            }
+        }
 
         // ref http://stackoverflow.com/a/3978040
         public static int GetRandomUnusedPort()
@@ -47,44 +62,8 @@ namespace TwitterLoginExample
             var oauthToken = "";
             string authorizationRequest = $"https://api.twitter.com/oauth/authorize?oauth_token={oauthToken}&force_login=true";
 
-            // Opens request in the browser.
-            System.Diagnostics.Process.Start(authorizationRequest);
-
-            // Waits for the OAuth authorization response.
-            var context = await http.GetContextAsync();
-
-            // Brings this app back to the foreground.
-            this.Activate();
-
-            // Sends an HTTP response to the browser.
-            var response = context.Response;
-            string responseString = string.Format("<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Please return to the app.</body></html>");
-            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-            response.ContentLength64 = buffer.Length;
-            var responseOutput = response.OutputStream;
-            Task responseTask = responseOutput.WriteAsync(buffer, 0, buffer.Length).ContinueWith((task) =>
-            {
-                responseOutput.Close();
-                http.Stop();
-                Console.WriteLine("HTTP server stopped.");
-            });
-
-            // Checks for errors.
-            if (context.Request.QueryString.Get("error") != null)
-            {
-                return;
-            }
-            if (context.Request.QueryString.Get("code") == null
-                || context.Request.QueryString.Get("state") == null)
-            {
-                return;
-            }
-
-            // extracts the code
-            var code = context.Request.QueryString.Get("code");
-            var incoming_state = context.Request.QueryString.Get("state");
-
-            Console.WriteLine($"Code: {code}");
+            popupForm.navigateTo(authorizationRequest);
+            popupForm.Show();
 
         }
     }
---
+++ TwitterLoginExample/TwitterLoginExample/PopupForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwitterLoginExample
+{
+    public partial class PopupForm : Form
+    {
+        private Uri navigateToUrl;
+        public Uri actualUrl;
+        public PopupForm()
+        {
+            InitializeComponent();
+            webBrowser1.AllowNavigation = true;
+            webBrowser1.Navigating += webBrowserNavigating;
+
+        }
+
+        private void webBrowserNavigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            if (navigateToUrl.Host != e.Url.Host)
+            {
+                actualUrl = e.Url;
+                this.Close();
+            }
+        }
+
+        public void navigateTo(string url)
+        {
+            navigateToUrl = new Uri(url);
+            webBrowser1.Navigate(url);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
