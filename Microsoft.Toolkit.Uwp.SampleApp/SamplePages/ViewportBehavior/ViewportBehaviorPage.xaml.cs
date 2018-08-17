c48de961e9f4206b6532687c501622756e1fc2a2
h82258652 <h82258652@hotmail.com>
2018-08-17 16:42:45 +0800

    commit codes

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ViewportBehavior/ViewportBehaviorPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.UI.Animations;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    /// <summary>
+    /// A page that shows how to use the viewport behavior.
+    /// </summary>
+    public sealed partial class ViewportBehaviorPage
+    {
+        private readonly ObservableCollection<string> _logs = new ObservableCollection<string>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewportBehaviorPage"/> class.
+        /// </summary>
+        public ViewportBehaviorPage()
+        {
+            InitializeComponent();
+
+            LogsItemsControl.ItemsSource = _logs;
+            EffectElement.Fade(value: 0, duration: 0).Start();
+        }
+
+        private async void AddLog(string log)
+        {
+            _logs.Add(log);
+            await Task.Yield(); // wait for layout updated.
+            LogsScrollViewer.ChangeView(null, LogsScrollViewer.ScrollableHeight, null);
+        }
+
+        private void ClearLogsButton_Click(object sender, RoutedEventArgs e)
+        {
+            _logs.Clear();
+        }
+
+        private async void EffectHost_EnteredViewport(object sender, EventArgs e)
+        {
+            AddLog("Entered viewport");
+
+            await EffectElement.Fade(value: 1, duration: 2000).StartAsync();
+        }
+
+        private async void EffectHost_LeavedViewport(object sender, EventArgs e)
+        {
+            AddLog("Leaved viewport");
+
+            EffectElement.Source = null;
+            await EffectElement.Fade(value: 0, duration: 0).StartAsync();
+        }
+
+        private void EffectHost_EnteringViewport(object sender, EventArgs e)
+        {
+            AddLog("Entering viewport");
+
+            EffectElement.Source = new BitmapImage(new Uri("ms-appx:///Assets/ToolkitLogo.png"));
+        }
+
+        private void EffectHost_LeavingViewport(object sender, EventArgs e)
+        {
+            AddLog("Leaving viewport");
+        }
+    }
+}
---
+++ Microsoft.Toolkit.Uwp.UI.Animations/Behaviors/ViewportBehavior.cs
@@ -0,0 +1,171 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.Toolkit.Uwp.UI.Animations.Behaviors
+{
+    /// <summary>
+    /// A class for listening element enter viewport or leave viewport
+    /// </summary>
+    public class ViewportBehavior : BehaviorBase<FrameworkElement>
+    {
+        /// <summary>
+        /// The IsFullyInViewport value of the associated element
+        /// </summary>
+        public static readonly DependencyProperty IsFullyInViewportProperty =
+            DependencyProperty.Register(nameof(IsFullyInViewport), typeof(bool), typeof(ViewportBehavior), new PropertyMetadata(default(bool), OnIsFullyInViewportChanged));
+
+        /// <summary>
+        /// The IsInViewport value of the associated element
+        /// </summary>
+        public static readonly DependencyProperty IsInViewportProperty =
+            DependencyProperty.Register(nameof(IsInViewport), typeof(bool), typeof(ViewportBehavior), new PropertyMetadata(default(bool), OnIsInViewportChanged));
+
+        /// <summary>
+        /// Associated element fully enter viewport event
+        /// </summary>
+        public event EventHandler EnteredViewport;
+
+        /// <summary>
+        /// Associated element fully leave viewport event
+        /// </summary>
+        public event EventHandler LeavedViewport;
+
+        /// <summary>
+        /// Associated element enter viewport event
+        /// </summary>
+        public event EventHandler EnteringViewport;
+
+        /// <summary>
+        /// Associated element leave viewport event
+        /// </summary>
+        public event EventHandler LeavingViewport;
+
+        public bool IsFullyInViewport
+        {
+            get { return (bool)GetValue(IsFullyInViewportProperty); }
+            private set { SetValue(IsFullyInViewportProperty, value); }
+        }
+
+        /// <summary>
+        ///   Gets a value indicating whether associated element is in viewport
+        /// </summary>
+        public bool IsInViewport
+        {
+            get { return (bool)GetValue(IsInViewportProperty); }
+            private set { SetValue(IsInViewportProperty, value); }
+        }
+
+        /// <summary>
+        /// Called after the behavior is attached to the <see cref="P:Microsoft.Xaml.Interactivity.Behavior.AssociatedObject" />.
+        /// </summary>
+        /// <remarks>
+        /// Override this to hook up functionality to the <see cref="P:Microsoft.Xaml.Interactivity.Behavior.AssociatedObject" />
+        /// </remarks>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.LayoutUpdated += AssociatedObject_LayoutUpdated;
+        }
+
+        /// <summary>
+        /// Called when the behavior is being detached from its <see cref="P:Microsoft.Xaml.Interactivity.Behavior.AssociatedObject" />.
+        /// </summary>
+        /// <remarks>
+        /// Override this to unhook functionality from the <see cref="P:Microsoft.Xaml.Interactivity.Behavior.AssociatedObject" />
+        /// </remarks>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.LayoutUpdated -= AssociatedObject_LayoutUpdated;
+        }
+
+        private static void OnIsFullyInViewportChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = (ViewportBehavior)d;
+            var value = (bool)e.NewValue;
+            if (value)
+            {
+                obj.EnteredViewport?.Invoke(obj.AssociatedObject, EventArgs.Empty);
+            }
+            else
+            {
+                obj.LeavingViewport?.Invoke(obj.AssociatedObject, EventArgs.Empty);
+            }
+        }
+
+        private static void OnIsInViewportChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = (ViewportBehavior)d;
+            var value = (bool)e.NewValue;
+            if (value)
+            {
+                obj.EnteringViewport?.Invoke(obj.AssociatedObject, EventArgs.Empty);
+            }
+            else
+            {
+                obj.LeavedViewport?.Invoke(obj.AssociatedObject, EventArgs.Empty);
+            }
+        }
+
+        private void AssociatedObject_LayoutUpdated(object sender, object e)
+        {
+            FrameworkElement hostElement = AssociatedObject;
+            while (true)
+            {
+                var parent = VisualTreeHelper.GetParent(hostElement) as FrameworkElement;
+                if (parent == null)
+                {
+                    break;
+                }
+
+                if (parent is ScrollViewer)
+                {
+                    hostElement = parent;
+                    break;
+                }
+
+                hostElement = parent;
+            }
+
+            if (hostElement != null)
+            {
+                var associatedControlRect = AssociatedObject.TransformToVisual(hostElement)
+                    .TransformBounds(new Rect(0, 0, AssociatedObject.ActualWidth, AssociatedObject.ActualHeight));
+                var rootContentRect = new Rect(0, 0, hostElement.ActualWidth, hostElement.ActualHeight);
+
+                if (rootContentRect.Contains(new Point(associatedControlRect.Left, associatedControlRect.Top)) ||
+                    rootContentRect.Contains(new Point(associatedControlRect.Right, associatedControlRect.Top)) ||
+                    rootContentRect.Contains(new Point(associatedControlRect.Right, associatedControlRect.Bottom)) ||
+                    rootContentRect.Contains(new Point(associatedControlRect.Left, associatedControlRect.Bottom)))
+                {
+                    IsInViewport = true;
+
+                    if (rootContentRect.Contains(new Point(associatedControlRect.Left, associatedControlRect.Top)) &&
+                        rootContentRect.Contains(new Point(associatedControlRect.Right, associatedControlRect.Top)) &&
+                        rootContentRect.Contains(new Point(associatedControlRect.Right, associatedControlRect.Bottom)) &&
+                        rootContentRect.Contains(new Point(associatedControlRect.Left, associatedControlRect.Bottom)))
+                    {
+                        IsFullyInViewport = true;
+                    }
+                    else
+                    {
+                        IsFullyInViewport = false;
+                    }
+                }
+                else
+                {
+                    IsInViewport = false;
+                }
+            }
+        }
+    }
+}
