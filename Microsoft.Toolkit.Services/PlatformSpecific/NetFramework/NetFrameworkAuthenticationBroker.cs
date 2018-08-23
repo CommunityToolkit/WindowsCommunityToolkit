887a52cab1e1bc845779b410dc77d43cd775463e
Kevin Bronstein <kevin.bronstein@southworks.com>
2018-08-23 16:49:49 -0300

    Add IAuthenticationBroker implementation for net framework

---
+++ Microsoft.Toolkit.Services/PlatformSpecific/NetFramework/NetFrameworkAuthenticationBroker.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Toolkit.Services.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Toolkit.Services.PlatformSpecific.NetFramework
+{
+    internal class NetFrameworkAuthenticationBroker : IAuthenticationBroker
+    {
+#if WINFORMS
+        PopupForm popupForm;
+#endif
+
+        public Task<AuthenticationResult> Authenticate(Uri requestUri, Uri callbackUri)
+        {
+            var taskCompletionSource = new TaskCompletionSource<AuthenticationResult>();
+#if WINFORMS
+            popupForm = new PopupForm(callbackUri);
+            popupForm.FormClosed += (sender, e) => 
+            {
+                var result = new AuthenticationResult();
+                if (popupForm.actualUrl != null)
+                {
+                    var query = System.Web.HttpUtility.ParseQueryString(popupForm.actualUrl.Query);
+                    var auth_token = query.Get("oauth_token");
+                    var auth_verifier = query.Get("oauth_verifier");
+
+                    
+                    result.ResponseData = query.ToString();
+                    result.ResponseStatus = AuthenticationResultStatus.Success;
+                }
+                else
+                {
+                    result.ResponseStatus = AuthenticationResultStatus.ErrorHttp;
+                }
+                taskCompletionSource.SetResult(result);
+            };
+#endif
+
+            return taskCompletionSource.Task;
+        }
+    }
+}
---
+++ Microsoft.Toolkit.Services/PlatformSpecific/NetFramework/PopupForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Microsoft.Toolkit.Services.PlatformSpecific.NetFramework
+{
+    public partial class PopupForm : Form
+    {
+        private Uri navigateToUrl;
+        public Uri actualUrl;
+        private Uri callbackUrl;
+        public PopupForm(Uri callbackUrl)
+        {
+            InitializeComponent();
+            webBrowser1.AllowNavigation = true;
+            webBrowser1.Navigating += webBrowserNavigating;
+            this.callbackUrl = callbackUrl;
+        }
+
+        private void webBrowserNavigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            if (navigateToUrl.Host != e.Url.Host)
+            {
+                if (e.Url.Host == callbackUrl.Host)
+                {
+                    actualUrl = e.Url;
+                }
+
+                this.Close();
+            }
+        }
+
+        public void navigateTo(string url)
+        {
+            navigateToUrl = new Uri(url);
+            webBrowser1.Navigate(url);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
