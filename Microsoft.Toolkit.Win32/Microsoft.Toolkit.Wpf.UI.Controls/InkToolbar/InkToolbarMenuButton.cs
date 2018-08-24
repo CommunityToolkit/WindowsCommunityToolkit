4545eb42a52d9cfc7340b8abc2a9fd1b0e0833cf
Nikola Metulev <nikolame@microsoft.com>
2018-08-23 21:25:43 -0700

    renamed win32 namespaces

---
+++ Microsoft.Toolkit.Win32/Microsoft.Toolkit.Wpf.UI.Controls/InkToolbar/InkToolbarMenuButton.cs
@@ -0,0 +1,88 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Windows;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+
+namespace Microsoft.Toolkit.Wpf.UI.Controls
+{
+    /// <summary>
+    /// Wpf-enabled wrapper for <see cref="Windows.UI.Xaml.Controls.InkToolbarMenuButton"/>
+    /// </summary>
+    public class InkToolbarMenuButton : WindowsXamlHostBaseExt
+    {
+        internal Windows.UI.Xaml.Controls.InkToolbarMenuButton UwpControl => XamlRootInternal as Windows.UI.Xaml.Controls.InkToolbarMenuButton;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InkToolbarMenuButton"/> class, a
+        /// Wpf-enabled wrapper for <see cref="Windows.UI.Xaml.Controls.InkToolbarMenuButton"/>
+        /// </summary>
+        public InkToolbarMenuButton()
+            : this(typeof(Windows.UI.Xaml.Controls.InkToolbarMenuButton).FullName)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InkToolbarMenuButton"/> class, a
+        /// Wpf-enabled wrapper for <see cref="Windows.UI.Xaml.Controls.InkToolbarMenuButton"/>.
+        /// </summary>
+        protected InkToolbarMenuButton(string typeName)
+            : base(typeName)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void SetContent()
+        {
+            // intentionally empty
+        }
+
+        /// <inheritdoc />
+        protected override void OnInitialized(EventArgs e)
+        {
+            // Bind dependency properties across controls
+            // properties of FrameworkElement
+            Bind(nameof(Style), StyleProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.StyleProperty);
+            Bind(nameof(MaxHeight), MaxHeightProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.MaxHeightProperty);
+            Bind(nameof(FlowDirection), FlowDirectionProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.FlowDirectionProperty);
+            Bind(nameof(Margin), MarginProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.MarginProperty);
+            Bind(nameof(HorizontalAlignment), HorizontalAlignmentProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.HorizontalAlignmentProperty);
+            Bind(nameof(VerticalAlignment), VerticalAlignmentProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.VerticalAlignmentProperty);
+            Bind(nameof(MinHeight), MinHeightProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.MinHeightProperty);
+            Bind(nameof(Height), HeightProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.HeightProperty);
+            Bind(nameof(MinWidth), MinWidthProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.MinWidthProperty);
+            Bind(nameof(MaxWidth), MaxWidthProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.MaxWidthProperty);
+            Bind(nameof(UseLayoutRounding), UseLayoutRoundingProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.UseLayoutRoundingProperty);
+            Bind(nameof(Name), NameProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.NameProperty);
+            Bind(nameof(Tag), TagProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.TagProperty);
+            Bind(nameof(DataContext), DataContextProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.DataContextProperty);
+            Bind(nameof(Width), WidthProperty, global::Windows.UI.Xaml.Controls.InkToolbarMenuButton.WidthProperty);
+
+            // InkToolbarMenuButton specific properties
+            Bind(nameof(IsExtensionGlyphShown), IsExtensionGlyphShownProperty, Windows.UI.Xaml.Controls.InkToolbarMenuButton.IsExtensionGlyphShownProperty);
+
+            base.OnInitialized(e);
+        }
+
+        /// <summary>
+        /// Gets <see cref="Windows.UI.Xaml.Controls.InkToolbarMenuButton.IsExtensionGlyphShownProperty"/>
+        /// </summary>
+        public static DependencyProperty IsExtensionGlyphShownProperty { get; } = DependencyProperty.Register(nameof(IsExtensionGlyphShown), typeof(bool), typeof(InkToolbarMenuButton));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether <see cref="Windows.UI.Xaml.Controls.InkToolbarMenuButton.IsExtensionGlyphShown"/>
+        /// </summary>
+        public bool IsExtensionGlyphShown
+        {
+            get => (bool)GetValue(IsExtensionGlyphShownProperty);
+            set => SetValue(IsExtensionGlyphShownProperty, value);
+        }
+
+        /// <summary>
+        /// Gets <see cref="Windows.UI.Xaml.Controls.InkToolbarMenuButton.MenuKind"/>
+        /// </summary>
+        public InkToolbarMenuKind MenuKind => (InkToolbarMenuKind)UwpControl.MenuKind;
+    }
+}
---
+++ Microsoft.Toolkit.Win32/Microsoft.Toolkit.Wpf.UI.Controls/InkToolbar/InkToolbarToggleButton.cs
@@ -0,0 +1,70 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+
+namespace Microsoft.Toolkit.Wpf.UI.Controls
+{
+    /// <summary>
+    /// Wpf-enabled wrapper for <see cref="Windows.UI.Xaml.Controls.InkToolbarToggleButton"/>
+    /// </summary>
+    public class InkToolbarToggleButton : WindowsXamlHostBaseExt
+    {
+        internal Windows.UI.Xaml.Controls.InkToolbarToggleButton UwpControl => XamlRootInternal as Windows.UI.Xaml.Controls.InkToolbarToggleButton;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InkToolbarToggleButton"/> class, a
+        /// Wpf-enabled wrapper for <see cref="Windows.UI.Xaml.Controls.InkToolbarToggleButton"/>
+        /// </summary>
+        public InkToolbarToggleButton()
+            : this(typeof(Windows.UI.Xaml.Controls.InkToolbarToggleButton).FullName)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InkToolbarToggleButton"/> class, a
+        /// Wpf-enabled wrapper for <see cref="Windows.UI.Xaml.Controls.InkToolbarToggleButton"/>.
+        /// </summary>
+        protected InkToolbarToggleButton(string typeName)
+            : base(typeName)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void SetContent()
+        {
+            // intentionally empty
+        }
+
+        /// <inheritdoc />
+        protected override void OnInitialized(EventArgs e)
+        {
+            // Bind dependency properties across controls
+            // properties of FrameworkElement
+            Bind(nameof(Style), StyleProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.StyleProperty);
+            Bind(nameof(MaxHeight), MaxHeightProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.MaxHeightProperty);
+            Bind(nameof(FlowDirection), FlowDirectionProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.FlowDirectionProperty);
+            Bind(nameof(Margin), MarginProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.MarginProperty);
+            Bind(nameof(HorizontalAlignment), HorizontalAlignmentProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.HorizontalAlignmentProperty);
+            Bind(nameof(VerticalAlignment), VerticalAlignmentProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.VerticalAlignmentProperty);
+            Bind(nameof(MinHeight), MinHeightProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.MinHeightProperty);
+            Bind(nameof(Height), HeightProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.HeightProperty);
+            Bind(nameof(MinWidth), MinWidthProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.MinWidthProperty);
+            Bind(nameof(MaxWidth), MaxWidthProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.MaxWidthProperty);
+            Bind(nameof(UseLayoutRounding), UseLayoutRoundingProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.UseLayoutRoundingProperty);
+            Bind(nameof(Name), NameProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.NameProperty);
+            Bind(nameof(Tag), TagProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.TagProperty);
+            Bind(nameof(DataContext), DataContextProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.DataContextProperty);
+            Bind(nameof(Width), WidthProperty, global::Windows.UI.Xaml.Controls.InkToolbarToggleButton.WidthProperty);
+
+            base.OnInitialized(e);
+        }
+
+        /// <summary>
+        /// Gets <see cref="Windows.UI.Xaml.Controls.InkToolbarToggleButton.ToggleKind"/>
+        /// </summary>
+        public InkToolbarToggle ToggleKind => (InkToolbarToggle)UwpControl.ToggleKind;
+    }
+}
