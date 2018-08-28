4510b8d7d1a99e2f063cd43ecf6809dfbcd9437f
Richard Murillo <rimuri@microsoft.com>
2018-08-28 10:46:07 -0700

    Make NativeDefines.cs consistent
    New entries added that should use enum `WM`

--- Microsoft.Toolkit.Win32/Microsoft.Toolkit.Forms.UI.XamlHost/Interop/Win32/NativeDefines.cs
+++ Microsoft.Toolkit.Win32/Microsoft.Toolkit.Forms.UI.XamlHost/Interop/Win32/NativeDefines.cs
@@ -19,22 +19,8 @@ namespace Microsoft.Toolkit.Forms.UI.XamlHost.Interop.Win32
         public const int WM_WINDOWPOSCHANGED = (int)WM.WINDOWPOSCHANGED;
         public const int WM_SETFOCUS = (int)WM.SETFOCUS;
         public const int WM_KILLFOCUS = (int)WM.KILLFOCUS;
-        public const int WM_ACTIVATE = 0x0006;
-        public const int WM_PAINT = 0x000F;
-        public const int WM_ERASEBKGND = 0x14; // 20
-        public const int WM_PARENTNOTIFY = 0x0210;
-        public const int WM_SETREDRAW = 0x000B;
-        public const int WM_NCPAINT = 0x85; // 133
-        public const int WM_POINTERDOWN = 0x0246;
-        public const int WM_LBUTTONDOWN = 0x0201;
-        public const int WM_MOUSEACTIVATE = 0x0021;
-        public const int WM_USER = 0x0400;
-        public const int WM_REFLECT = WM_USER + 0x1C00;
-        public const int WM_KEYDOWN = 0x0100;
-        public const int WM_KEYUP = 0x0101;
-        public const int WM_CHAR = 0x0102;
-        public const int WM_SYSKEYDOWN = 0x0104;
-        public const int WM_SYSKEYUP = 0x0105;
+        public const int WM_KEYDOWN = (int)WM.KEYDOWN;
+        public const int WM_KEYUP = (int)WM.KEYUP;
 
         // Window Styles
         public const uint WS_EX_CONTROLPARENT = 0x00010000;
