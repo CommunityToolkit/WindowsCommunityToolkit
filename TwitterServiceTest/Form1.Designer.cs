db235150757d70a60fdbbfd969db8196d0952862
Lucas Rojas <lucas.rojas@southworks.com>
2018-08-29 11:39:47 -0300

    Fix authentication process with the stored credentials

--- TwitterServiceTest/Form1.Designer.cs
+++ TwitterServiceTest/Form1.Designer.cs
@@ -31,25 +31,29 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tweetText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sendTweet = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(161, 121);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(31, 191);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 71);
+            this.button1.Size = new System.Drawing.Size(111, 89);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Login and Tweet";
+            this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(403, 121);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(166, 191);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 71);
+            this.button2.Size = new System.Drawing.Size(111, 89);
             this.button2.TabIndex = 1;
             this.button2.Text = "Logout";
             this.button2.UseVisualStyleBackColor = true;
@@ -58,22 +62,63 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(425, 72);
+            this.label1.Location = new System.Drawing.Point(118, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(102, 25);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Waiting...";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Status: ";
+            // 
+            // tweetText
+            // 
+            this.tweetText.Location = new System.Drawing.Point(31, 136);
+            this.tweetText.Name = "tweetText";
+            this.tweetText.Size = new System.Drawing.Size(469, 31);
+            this.tweetText.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tweet";
+            // 
+            // sendTweet
+            // 
+            this.sendTweet.Location = new System.Drawing.Point(298, 191);
+            this.sendTweet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sendTweet.Name = "sendTweet";
+            this.sendTweet.Size = new System.Drawing.Size(111, 89);
+            this.sendTweet.TabIndex = 6;
+            this.sendTweet.Text = "Send Tweet";
+            this.sendTweet.UseVisualStyleBackColor = true;
+            this.sendTweet.Click += new System.EventHandler(this.sendTweet_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.ClientSize = new System.Drawing.Size(542, 358);
+            this.Controls.Add(this.sendTweet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tweetText);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -86,6 +131,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tweetText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button sendTweet;
     }
 }
 
