7f0ee7c58c93668d0a1b1ce43d09018df42da897
Valeriu Balan <valeriu.balan@live.com>
2018-09-20 13:55:23 +0300

    Ran UpdateHeaders.bat

--- Microsoft.Toolkit.Uwp.UI.Controls/ImageEx/CachingStrategy.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/ImageEx/CachingStrategy.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.Toolkit.Uwp.UI.Controls
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
     /// <summary>
     /// The type of caching to be applied to <see cref="ImageEx"/>.
