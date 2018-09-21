3d648602c0ea4b8fca8135e4570c1d33c6704e67
AVK Naidu <vamshikrishna.naidu@live.com>
2018-09-21 13:38:18 -0400

    Moved Rome Related Content to Helper Class. Pending Documentation.

---
+++ Microsoft.Toolkit.Uwp/Helpers/RomeHelper/RomeHelper.cs
@@ -0,0 +1,80 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Windows.System.RemoteSystems;
+
+namespace Microsoft.Toolkit.Uwp.Helpers
+{
+    /// <summary>
+    /// Helper to List Remote Devices that are accessible
+    /// </summary>
+    public class RomeHelper
+    {
+        /// <summary>
+        /// Gets or sets List of All Remote Systems based on Selection Filter
+        /// </summary>
+        public ObservableCollection<RemoteSystem> RemoteSystems { get; set; }
+
+        private RemoteSystemWatcher remoteSystemWatcher;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RomeHelper"/> class.
+        /// </summary>
+        public RomeHelper()
+        {
+            RemoteSystems = new ObservableCollection<RemoteSystem>();
+            GenerateSystems();
+        }
+
+        /// <summary>
+        /// Initiate Enumeration
+        /// </summary>
+        public async void GenerateSystems()
+        {
+            RemoteSystemAccessStatus accessStatus = await RemoteSystem.RequestAccessAsync();
+            if (accessStatus == RemoteSystemAccessStatus.Allowed)
+            {
+                remoteSystemWatcher = RemoteSystem.CreateWatcher();
+                remoteSystemWatcher.RemoteSystemAdded += RemoteSystemWatcher_RemoteSystemAdded;
+                remoteSystemWatcher.RemoteSystemRemoved += RemoteSystemWatcher_RemoteSystemRemoved;
+                remoteSystemWatcher.RemoteSystemUpdated += RemoteSystemWatcher_RemoteSystemUpdated;
+                remoteSystemWatcher.EnumerationCompleted += RemoteSystemWatcher_EnumerationCompleted;
+                remoteSystemWatcher.Start();
+            }
+        }
+
+        private void RemoteSystemWatcher_EnumerationCompleted(RemoteSystemWatcher sender, RemoteSystemEnumerationCompletedEventArgs args)
+        {
+            remoteSystemWatcher.Stop();
+        }
+
+        private async void RemoteSystemWatcher_RemoteSystemUpdated(RemoteSystemWatcher sender, RemoteSystemUpdatedEventArgs args)
+        {
+            await DispatcherHelper.ExecuteOnUIThreadAsync(() =>
+            {
+                RemoteSystems.Remove(RemoteSystems.First(a => a.Id == args.RemoteSystem.Id));
+                RemoteSystems.Add(args.RemoteSystem);
+            });
+        }
+
+        private async void RemoteSystemWatcher_RemoteSystemRemoved(RemoteSystemWatcher sender, RemoteSystemRemovedEventArgs args)
+        {
+            await DispatcherHelper.ExecuteOnUIThreadAsync(() =>
+            {
+                RemoteSystems.Remove(RemoteSystems.First(a => a.Id == args.RemoteSystemId));
+            });
+        }
+
+        private async void RemoteSystemWatcher_RemoteSystemAdded(RemoteSystemWatcher sender, RemoteSystemAddedEventArgs args)
+        {
+            await DispatcherHelper.ExecuteOnUIThreadAsync(() =>
+            {
+                RemoteSystems.Add(args.RemoteSystem);
+            });
+        }
+    }
+}
