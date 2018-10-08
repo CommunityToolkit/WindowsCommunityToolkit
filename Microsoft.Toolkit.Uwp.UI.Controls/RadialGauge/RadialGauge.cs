6ed817e0611d7aca7940ad8147f3ecfbe0675a70
Diederik Krols <diederik_krols@hotmail.com>
2018-10-08 15:16:58 +0200

    Reordered using directives

--- Microsoft.Toolkit.Uwp.UI.Controls/RadialGauge/RadialGauge.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/RadialGauge/RadialGauge.cs
@@ -2,12 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.Toolkit.Uwp.UI.Helpers;
 using System;
 using System.Numerics;
+using Microsoft.Toolkit.Uwp.UI.Helpers;
 using Windows.Foundation;
 using Windows.System;
-using Windows.UI;
 using Windows.UI.Composition;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
