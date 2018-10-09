7552549fd11e073b50b2572a23a2e4b6f14f41bf
Jun Ma (WINDOWS) <juma@microsoft.com>
2018-10-10 00:05:35 +0900

    fix warning when building x64. Fix target SDK. Use correct Theme brush.

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/DirectWriteTextBlock/DirectWriteTextBlockPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/DirectWriteTextBlock/DirectWriteTextBlockPage.xaml.cs
@@ -13,8 +13,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
     /// <summary>
