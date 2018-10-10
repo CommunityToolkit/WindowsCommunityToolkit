f27cfcd60f5dc1def15c5d5e25dabdcf241d3091
Diederik Krols <diederik_krols@hotmail.com>
2018-10-10 13:51:18 +0200

    RadialGauge: expose RangeValue properties to Narrator

--- Microsoft.Toolkit.Uwp.UI.Controls/RadialGauge/RadialGaugeAutomationPeer.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/RadialGauge/RadialGaugeAutomationPeer.cs
@@ -3,23 +3,49 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Automation.Provider;
 
 namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
     /// <summary>
     /// Exposes <see cref="RadialGauge"/> to Microsoft UI Automation.
     /// </summary>
-    public class RadialGaugeAutomationPeer : FrameworkElementAutomationPeer
+    public class RadialGaugeAutomationPeer :
+        FrameworkElementAutomationPeer,
+        IRangeValueProvider
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="RadialGaugeAutomationPeer"/> class.
         /// </summary>
         /// <param name="owner">The owner element to create for.</param>
-        public RadialGaugeAutomationPeer(FrameworkElement owner)
+        public RadialGaugeAutomationPeer(RadialGauge owner)
             : base(owner)
         {
+        }
+
+        /// <inheritdoc/>
+        public bool IsReadOnly => !((RadialGauge)Owner).IsInteractive;
+
+        /// <inheritdoc/>
+        public double LargeChange => ((RadialGauge)Owner).StepSize;
+
+        /// <inheritdoc/>
+        public double Maximum => ((RadialGauge)Owner).Maximum;
+
+        /// <inheritdoc/>
+        public double Minimum => ((RadialGauge)Owner).Minimum;
+
+        /// <inheritdoc/>
+        public double SmallChange => ((RadialGauge)Owner).StepSize;
+
+        /// <inheritdoc/>
+        public double Value => ((RadialGauge)Owner).Value;
+
+        /// <inheritdoc/>
+        public void SetValue(double value)
+        {
+            ((RadialGauge)Owner).Value = value;
         }
 
         /// <inheritdoc/>
@@ -32,7 +58,19 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         protected override string GetNameCore()
         {
             var gauge = (RadialGauge)Owner;
-            return gauge.Value + gauge.Unit + "radial gauge";
+            return "radial gauge. " + (string.IsNullOrWhiteSpace(gauge.Unit) ? "no unit specified. " : "unit " + gauge.Unit + ". ");
+        }
+
+        /// <inheritdoc/>
+        protected override object GetPatternCore(PatternInterface patternInterface)
+        {
+            if (patternInterface == PatternInterface.RangeValue)
+            {
+                // Expose RangeValue properties.
+                return this;
+            }
+
+            return base.GetPatternCore(patternInterface);
         }
 
         /// <inheritdoc/>
