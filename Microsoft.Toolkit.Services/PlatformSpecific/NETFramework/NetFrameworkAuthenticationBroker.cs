93d6a2d3aa01d43202f766b9ecceeb67a58ba11e
bmartin2013 <barbara.martinez@southworks.com>
2018-10-11 14:33:58 -0300

    delete duplicated folder

---
+++ Microsoft.Toolkit.Services/PlatformSpecific/NETFramework/NetFrameworkAuthenticationBroker.cs
@@ -0,0 +1,86 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+﻿using System;
+using System.Windows;
+
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Services.Core;
+using ApplicationForm = System.Windows.Forms.Application;
+
+namespace Microsoft.Toolkit.Services.PlatformSpecific.NetFramework
+{
+    internal class NetFrameworkAuthenticationBroker : IAuthenticationBroker
+    {
+
+        public Task<AuthenticationResult> Authenticate(Uri requestUri, Uri callbackUri)
+        {
+            int numberForms = ApplicationForm.OpenForms.Count;
+            if (numberForms > 0)
+            {
+                return AutenticateForm(requestUri, callbackUri);
+            }
+            else if (Application.Current != null)
+            {
+                return AutenticateWindow(requestUri, callbackUri);
+            }
+            else
+            {
+                // Your code shouldn't reach this exception.
+                throw new Exception("Cannot identify the current application. Please review your main app");
+            }
+        }
+
+        public Task<AuthenticationResult> AutenticateWindow(Uri requestUri, Uri callbackUri)
+        {
+            PopupWPF popupWindow;
+            var taskCompletionSource = new TaskCompletionSource<AuthenticationResult>();
+            popupWindow = new PopupWPF(callbackUri);
+            popupWindow.Closed += (sender, e) =>
+            {
+                taskCompletionSource.SetResult(HandleExit(popupWindow.actualUrl));
+            };
+
+            popupWindow.Show();
+            popupWindow.navigateTo(requestUri.AbsoluteUri);
+            return taskCompletionSource.Task;
+        }
+
+        public Task<AuthenticationResult> AutenticateForm(Uri requestUri, Uri callbackUri)
+        {
+            PopupForm popupForm;
+            var taskCompletionSource = new TaskCompletionSource<AuthenticationResult>();
+            popupForm = new PopupForm(callbackUri);
+            popupForm.FormClosed += (sender, e) =>
+            {
+               
+            	taskCompletionSource.SetResult(HandleExit(popupForm.actualUrl));
+            };
+
+            popupForm.Show();
+            popupForm.navigateTo(requestUri.AbsoluteUri);d
+            return taskCompletionSource.Task;
+        }
+
+        public AuthenticationResult HandleExit(Uri actualUrl)
+        {
+            var taskCompletionSource = new TaskCompletionSource<AuthenticationResult>();
+            var result = new AuthenticationResult();
+            if (actualUrl != null)
+            {
+                var query = System.Web.HttpUtility.ParseQueryString(actualUrl.Query);
+
+                result.ResponseData = query.ToString();
+                result.ResponseStatus = AuthenticationResultStatus.Success;
+            }
+            else
+            {
+                result.ResponseStatus = AuthenticationResultStatus.ErrorHttp;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Microsoft.Toolkit.Services/PlatformSpecific/NETFramework/PopupForm.cs
@@ -0,0 +1,101 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Microsoft.Toolkit.Services.PlatformSpecific.NetFramework
+{
+    /// <summary>
+    /// Redirect form for Twitter Service login.
+    /// </summary>
+    public partial class PopupForm : Form
+    {
+        private string initialHost;
+
+        private Uri actualUrl;
+        private string callbackHost;
+        /// <summary>
+        /// Gets or sets the actual url.
+        /// </summary>
+        public Uri ActualUrl { get => actualUrl; set => actualUrl = value; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PopupForm"/> class.
+        /// </summary>
+        /// <param name="callbackUrl">Uri callback url</param>
+        public PopupForm(Uri callbackUrl)
+        {
+            InitializeComponent();
+            webBrowser1.AllowNavigation = true;
+            webBrowser1.Navigating += WebBrowserNavigating;
+            callbackHost = GetTopLevelDomain(callbackUrl);
+        }
+
+        /// <summary>
+        /// Checks if the actual page is the Callback url.
+        /// </summary>
+        /// <param name="sender">sender object</param>
+        /// <param name="e">WebBrowser navigating event arguments</param>
+        private void WebBrowserNavigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            if (initialHost != GetTopLevelDomain(e.Url))
+            {
+                if (GetTopLevelDomain(e.Url) == callbackHost)
+                {
+                    ActualUrl = e.Url;
+                }
+
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// Loads a given url in the WebBrowser
+        /// </summary>
+        /// <param name="url">Url string to navigate to.</param>
+        public void NavigateTo(string url)
+        {
+            initialHost = GetTopLevelDomain(url);
+            webBrowser1.Navigate(url);
+        }
+
+        /// <summary>
+        /// empty
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">WebBrowserDocumentCompleteEventArgs</param>
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Invoke the GetTopLevelDomain method
+        /// </summary>
+        /// <param name="url">URL String</param>
+        /// <returns>Url's top level domain</returns>
+        private string GetTopLevelDomain(string url)
+        {
+            return GetTopLevelDomain(new Uri(url));
+        }
+
+        /// <summary>
+        /// Gets the top level domain from a given url
+        /// </summary>
+        /// <param name="url">URI url</param>
+        /// <returns>The top level domain</returns>
+        private string GetTopLevelDomain(Uri url)
+        {
+            var hostParts = url.Host.Split('.').Select(x => x.ToString());
+            if (hostParts.Count() > 1)
+            {
+                return hostParts.ElementAt(1);
+            }
+
+            return hostParts.ElementAt(0);
+        }
+    }
+}
