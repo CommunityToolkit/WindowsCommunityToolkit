62c0b4c716c70a55c74a04cbab93fdab980029cb
kbrons <kevin.bronstein@southworks.com>
2018-10-12 19:32:17 -0300

    Add headers for PasswordManagerNativeMethods, PopupForm.Designer, PopupForm and PopupWPF.xaml.cs

--- Microsoft.Toolkit.Services/PlatformSpecific/NETFramework/PopupForm.cs
+++ Microsoft.Toolkit.Services/PlatformSpecific/NETFramework/PopupForm.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
