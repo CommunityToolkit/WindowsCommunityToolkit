91990ee4b4e0e8e655a55cb025069d19e4542efa
Shawn Kendrot <skendrot@hotmail.com>
2018-10-12 16:00:11 -0600

    Add license header

--- Microsoft.Toolkit.Uwp.UI.Controls/MasterDetailsView/BackButtonBehavior.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/MasterDetailsView/BackButtonBehavior.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.Toolkit.Uwp.UI.Controls
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
     public enum BackButtonBehavior
     {
