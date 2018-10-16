4ada4722573ea0a6d3d7c9f997ebef50fe9aa7a3
AVK <vamshikrishna.naidu@live.com>
2018-10-16 23:00:36 +0530

    Control Changes

--- Microsoft.Toolkit.Uwp.UI.Controls/RemoteDevicePicker/RemoteDevicePicker.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/RemoteDevicePicker/RemoteDevicePicker.cs
@@ -12,7 +12,6 @@ using Windows.System.RemoteSystems;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 
 namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
@@ -28,6 +27,10 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         private ComboBox _listDeviceTypes;
         private ProgressRing _progressRing;
         private Grid _commandSpace;
+        private ComboBox _deviceDiscoveryItemsControl;
+        private ComboBox _deviceStatusItemsControl;
+        private ComboBox _authorizationTypeItemsControl;
+        private Grid _advancedFiltersGrid;
 
         /// <summary>
         /// Gets or sets List of All Remote Systems based on Selection Filter
@@ -35,29 +38,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         private ObservableCollection<RemoteSystem> RemoteSystems { get; set; }
 
         /// <summary>
-        /// Gets or sets the Line Color on control Header. takes **SystemControlBackgroundAccentBrush** by default
-        /// </summary>
-        public Brush HeaderLineBrush
-        {
-            get { return (Brush)GetValue(HeaderLineBrushProperty); }
-            set { SetValue(HeaderLineBrushProperty, value); }
-        }
-
-        /// <summary>
-        /// Gets the dependency property for <see cref="HeaderLineBrush"/>.
-        /// </summary>
-        public static readonly DependencyProperty HeaderLineBrushProperty = DependencyProperty.Register(
-            nameof(HeaderLineBrush),
-            typeof(Brush),
-            typeof(RemoteDevicePicker),
-            new PropertyMetadata(Application.Current.Resources["SystemControlBackgroundAccentBrush"]));
-
-        /// <summary>
         /// Gets or sets the DeviceList Selection Mode. Defaults to ListViewSelectionMode.Single
         /// </summary>
-        public ListViewSelectionMode SelectionMode
+        public RemoteDeviceSelectionMode SelectionMode
         {
-            get { return (ListViewSelectionMode)GetValue(SelectionModeProperty); }
+            get { return (RemoteDeviceSelectionMode)GetValue(SelectionModeProperty); }
             set { SetValue(SelectionModeProperty, value); }
         }
 
@@ -66,9 +51,27 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         /// </summary>
         public static readonly DependencyProperty SelectionModeProperty = DependencyProperty.Register(
             nameof(SelectionMode),
-            typeof(ListViewSelectionMode),
+            typeof(RemoteDeviceSelectionMode),
             typeof(RemoteDevicePicker),
-            new PropertyMetadata(ListViewSelectionMode.Single));
+            new PropertyMetadata(RemoteDeviceSelectionMode.Single));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Advanced Filters visible or not
+        /// </summary>
+        public bool ShowAdvancedFilters
+        {
+            get { return (bool)GetValue(ShowAdvancedFiltersProperty); }
+            set { SetValue(ShowAdvancedFiltersProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets the dependency property for <see cref="ShowAdvancedFilters"/>.
+        /// </summary>
+        public static readonly DependencyProperty ShowAdvancedFiltersProperty = DependencyProperty.Register(
+            nameof(ShowAdvancedFilters),
+            typeof(bool),
+            typeof(RemoteDevicePicker),
+            new PropertyMetadata(false));
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RemoteDevicePicker"/> class.
@@ -104,32 +107,77 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         /// <inheritdoc />
         protected override void OnApplyTemplate()
         {
+            if (_listDeviceTypes != null)
+            {
+                _listDeviceTypes.SelectionChanged -= ListDeviceTypes_SelectionChanged;
+            }
+
+            if (_listDevices != null)
+            {
+                _listDevices.SelectionChanged -= ListDevices_SelectionChanged;
+                _listDevices.DoubleTapped -= ListDevices_DoubleTapped;
+                _listDevices.ContainerContentChanging -= ListDevices_ContainerContentChanging;
+                _listDeviceTypes.SelectedIndex = -1;
+            }
+
             _listDevices = GetTemplateChild("PART_ListDevices") as ListView;
             _listDeviceTypes = GetTemplateChild("PART_ListDeviceTypes") as ComboBox;
             _progressRing = GetTemplateChild("PART_ProgressRing") as ProgressRing;
             _commandSpace = GetTemplateChild("CommandSpace") as Grid;
+            _deviceDiscoveryItemsControl = GetTemplateChild("DiscoveryType") as ComboBox;
+            _deviceStatusItemsControl = GetTemplateChild("StatusType") as ComboBox;
+            _authorizationTypeItemsControl = GetTemplateChild("AuthorizationType") as ComboBox;
+            _advancedFiltersGrid = GetTemplateChild("AdvancedFiltersGrid") as Grid;
 
             List<string> deviceList = typeof(RemoteSystemKinds).GetProperties().Select(a => a.Name).ToList();
             deviceList.Add("All");
             deviceList.Add("Unknown");
 
-            _listDeviceTypes.ItemsSource = deviceList.OrderBy(a => a.ToString());
-            _listDeviceTypes.SelectionChanged += ListDeviceTypes_SelectionChanged;
-            _listDevices.SelectionChanged += ListDevices_SelectionChanged;
-            _listDevices.DoubleTapped += ListDevices_DoubleTapped;
-            _listDevices.ContainerContentChanging += ListDevices_ContainerContentChanging;
-            _listDeviceTypes.SelectedIndex = 0;
+            if (_listDeviceTypes != null)
+            {
+                _listDeviceTypes.ItemsSource = deviceList.OrderBy(a => a.ToString());
+                _listDeviceTypes.SelectionChanged += ListDeviceTypes_SelectionChanged;
+            }
 
-            _commandSpace.Visibility = SelectionMode == ListViewSelectionMode.Single ? Visibility.Collapsed : Visibility.Visible;
-            _listDevices.IsMultiSelectCheckBoxEnabled = SelectionMode == ListViewSelectionMode.Multiple ? true : false;
+            if (_listDevices != null)
+            {
+                _listDevices.SelectionChanged += ListDevices_SelectionChanged;
+                _listDevices.DoubleTapped += ListDevices_DoubleTapped;
+                _listDevices.ContainerContentChanging += ListDevices_ContainerContentChanging;
 
-            RomeHelper romeHelper = new RomeHelper();
+                _commandSpace.Visibility = Visibility.Visible;
+                _listDevices.SelectionMode = SelectionMode == RemoteDeviceSelectionMode.Single ? ListViewSelectionMode.Single : ListViewSelectionMode.Multiple;
+                _listDevices.IsMultiSelectCheckBoxEnabled = SelectionMode == RemoteDeviceSelectionMode.Multiple ? true : false;
+            }
+
+            if (_advancedFiltersGrid != null)
+            {
+                _advancedFiltersGrid.Visibility = ShowAdvancedFilters ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            RemoteDeviceHelper romeHelper = new RemoteDeviceHelper();
             RemoteSystems = romeHelper.RemoteSystems;
 
             UpdateProgressRing(true);
             UpdateList();
 
+            Focus(FocusState.Programmatic);
+
+            LoadFilters();
+
             base.OnApplyTemplate();
+        }
+
+        private void LoadFilters()
+        {
+            List<string> discoveryType = typeof(RemoteSystemDiscoveryType).GetEnumNames().OrderBy(a => a.ToString()).ToList();
+            _deviceDiscoveryItemsControl.ItemsSource = discoveryType;
+
+            List<string> statusType = typeof(RemoteSystemStatusType).GetEnumNames().OrderBy(a => a.ToString()).ToList();
+            _deviceStatusItemsControl.ItemsSource = statusType;
+
+            List<string> authType = typeof(RemoteSystemAuthorizationKind).GetEnumNames().OrderBy(a => a.ToString()).ToList();
+            _authorizationTypeItemsControl.ItemsSource = authType;
         }
 
         private void ListDevices_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
@@ -166,7 +214,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
 
         private void ListDeviceTypes_SelectionChanged(object sender, SelectionChangedEventArgs e) => UpdateList();
 
-        internal void UpdateList()
+        private void UpdateList()
         {
             ObservableCollection<RemoteSystem> bindingList = new ObservableCollection<RemoteSystem>();
             if (RemoteSystems != null)
@@ -180,7 +228,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             }
         }
 
-        internal void UpdateProgressRing(bool state)
+        private void UpdateProgressRing(bool state)
         {
             if (_progressRing != null)
             {
