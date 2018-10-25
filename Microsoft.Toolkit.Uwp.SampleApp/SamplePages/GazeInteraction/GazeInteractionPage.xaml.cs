1e181e34512fd62f5b13db3dc85bbab3fc98cc93
Pete Ansell <petea@microsoft.com>
2018-10-25 12:09:07 -0700

    Removed calibration test code from sample application.

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/GazeInteraction/GazeInteractionPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/GazeInteraction/GazeInteractionPage.xaml.cs
@@ -140,10 +140,5 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
         {
             ClickCount.Text = $"Number of clicks = {++_targetButtonClickCount}";
         }
-
-        private async void Calibrate_Click(object sender, RoutedEventArgs e)
-        {
-            await GazeInput.GetGazePointer(this).RequestCalibrationAsync();
-        }
     }
 }
