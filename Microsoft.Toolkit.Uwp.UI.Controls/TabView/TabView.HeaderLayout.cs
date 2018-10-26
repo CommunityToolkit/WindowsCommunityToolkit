74050c0d1cb7eb4e7d022a842750e2ffc2faeeb4
michael-hawker <michael.hawker@outlook.com>
2018-10-26 15:42:53 -0700

    Fix Missing Headers

--- Microsoft.Toolkit.Uwp.UI.Controls/TabView/TabView.HeaderLayout.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/TabView/TabView.HeaderLayout.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Toolkit.Uwp.UI.Extensions;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Toolkit.Uwp.UI.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
