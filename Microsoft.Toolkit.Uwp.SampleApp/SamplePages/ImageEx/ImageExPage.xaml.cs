e5d32dd92aa3362ef0bd115655e6716f7c0c2850
h82258652 <h82258652@hotmail.com>
2018-11-27 11:01:33 +0800

    update button text

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ImageEx/ImageExPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ImageEx/ImageExPage.xaml.cs
@@ -69,7 +69,7 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
                 AddImage(false, false, true);
             });
 
-            SampleController.Current.RegisterNewCommand("Lazy loading sample", (sender, args) =>
+            SampleController.Current.RegisterNewCommand("Lazy loading sample (17763 or higher supported)", (sender, args) =>
             {
                 var imageExLazyLoadingControl = new ImageExLazyLoadingControl();
                 imageExLazyLoadingControl.CloseButtonClick += (_, __) =>
