4c97b88caf0a49a56b8032237ba969b1cca2cddd
hhchaos <hhchaos@outlook.com>
2018-12-04 23:57:18 +0800

    UpdateHeaders

--- Microsoft.Toolkit.Uwp.UI.Controls/ImageCropper/ImageCropper.Constants.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/ImageCropper/ImageCropper.Constants.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.Toolkit.Uwp.UI.Controls
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
     /// <summary>
     /// The <see cref="ImageCropper"/> control allows user to crop image freely.
