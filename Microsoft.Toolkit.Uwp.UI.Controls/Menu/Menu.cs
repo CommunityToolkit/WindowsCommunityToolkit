40a060497578e2dbf3e4fe77102f456baba79a41
Lucas Rojas <lucas.rojas@southworks.com>
2019-01-24 11:05:21 -0300

    Move deprecation message from Menu.cs to MenuItem.cs

--- Microsoft.Toolkit.Uwp.UI.Controls/Menu/Menu.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/Menu/Menu.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -13,7 +12,6 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
     /// <summary>
     /// Menu Control defines a menu of choices for users to invoke.
     /// </summary>
-    [Obsolete("This control will be removed in a future major release. Please use the MenuBar control from the WinUI Library instead.")]
     public partial class Menu : ItemsControl
     {
         private WrapPanel _wrapPanel;
