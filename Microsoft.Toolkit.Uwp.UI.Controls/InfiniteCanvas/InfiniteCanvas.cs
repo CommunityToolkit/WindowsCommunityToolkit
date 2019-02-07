1fc1ee65bbad580a98137c750d83b0c3bed4efdb
Steve Nessen <steve.nessen@microsoft.com>
2019-02-07 11:09:28 +0100

    Reordered usings

--- Microsoft.Toolkit.Uwp.UI.Controls/InfiniteCanvas/InfiniteCanvas.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/InfiniteCanvas/InfiniteCanvas.cs
@@ -3,8 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Graphics.Canvas;
 using Windows.Foundation;
 using Windows.Storage.Streams;
