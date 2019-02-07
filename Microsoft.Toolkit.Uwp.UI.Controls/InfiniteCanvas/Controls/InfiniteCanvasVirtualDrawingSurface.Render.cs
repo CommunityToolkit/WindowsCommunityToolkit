3187f525ed6b0796ea5bdea099b90b4d352433f1
Steve Nessen <steve.nessen@microsoft.com>
2019-02-07 10:36:05 +0100

    Resolved merge conflict

--- Microsoft.Toolkit.Uwp.UI.Controls/InfiniteCanvas/Controls/InfiniteCanvasVirtualDrawingSurface.Render.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/InfiniteCanvas/Controls/InfiniteCanvasVirtualDrawingSurface.Render.cs
@@ -28,7 +28,10 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         internal void ReDraw(Rect viewPort, float zoom)
         {
             var toDraw = GetDrawingBoundaries(viewPort);
-            using (var drawingSession = CanvasComposition.CreateDrawingSession(_drawingSurface, toDraw))
+
+            var scale = _screenScale * zoom;
+
+            using (var drawingSession = CanvasComposition.CreateDrawingSession(_drawingSurface, ScaleRect(toDraw, scale), BaseCanvasDPI * (float)scale))
             {
                 drawingSession.Clear(Colors.White);
                 foreach (var drawable in _visibleList)
@@ -36,6 +39,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
                     drawable.Draw(drawingSession, toDraw);
                 }
             }
+        }
+
+        private Rect ScaleRect(Rect rect, double scale)
+        {
+            return new Rect(rect.X * scale, rect.Y * scale, rect.Width * scale, rect.Height * scale);
         }
 
         internal CanvasRenderTarget ExportMaxOffScreenDrawings()
@@ -61,7 +69,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             });
         }
 
-        internal void RenderFromJsonAndDraw(Rect viewPort, string json)
+        internal void RenderFromJsonAndDraw(Rect viewPort, string json, float zoom)
         {
             _visibleList.Clear();
             _drawableList.Clear();
@@ -85,7 +93,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
                 _drawableList.Add(drawable);
             }
 
-            ReDraw(viewPort);
+            ReDraw(viewPort, zoom);
         }
 
         private Rect GetDrawingBoundaries(Rect viewPort)
