bbb20e12aa1362ab0a6ae66b4860f3b7f1f931ec
hhchaos <hhchaos@outlook.com>
2019-03-04 23:06:19 +0800

    Add Eyedropper.

---
+++ Microsoft.Toolkit.Uwp.UI.Controls/Eyedropper/ColorChangedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI;
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
+{
+    /// <summary>
+    /// Provides event data for the ColorChanged event.
+    /// </summary>
+    public sealed class ColorChangedEventArgs
+    {
+        /// <summary>
+        /// Gets the color that is currently selected in the control.
+        /// </summary>
+        public Color NewColor { get; internal set; }
+
+        /// <summary>
+        /// Gets the color that was previously selected in the control.
+        /// </summary>
+        public Color OldColor { get; internal set; }
+    }
+}
---
+++ Microsoft.Toolkit.Uwp.UI.Controls/Eyedropper/Eyedropper.Properties.cs
@@ -0,0 +1,74 @@
+﻿using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
+{
+    /// <summary>
+    /// The <see cref="Eyedropper"/> control can pick up a color from anywhere in your application.
+    /// </summary>
+    public partial class Eyedropper
+    {
+        /// <summary>
+        /// Identifies the <see cref="Color"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(nameof(Color), typeof(Color), typeof(Eyedropper), new PropertyMetadata(default(Color), OnColorChanged));
+
+        /// <summary>
+        /// Identifies the <see cref="Preview"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty PreviewProperty =
+            DependencyProperty.Register(nameof(Preview), typeof(ImageSource), typeof(Eyedropper), new PropertyMetadata(default(ImageSource)));
+
+        /// <summary>
+        /// Identifies the <see cref="WorkArea"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WorkAreaProperty =
+            DependencyProperty.Register(nameof(WorkArea), typeof(Rect), typeof(Eyedropper), new PropertyMetadata(default(Rect), OnWorkAreaChanged));
+
+        /// <summary>
+        /// Gets the current color value.
+        /// </summary>
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            private set => SetValue(ColorProperty, value);
+        }
+
+        /// <summary>
+        /// Gets the current color value.
+        /// </summary>
+        public ImageSource Preview
+        {
+            get => (ImageSource)GetValue(PreviewProperty);
+            private set => SetValue(PreviewProperty, value);
+        }
+
+        /// <summary>
+        /// Gets the working area of the eyedropper.
+        /// </summary>
+        public Rect WorkArea
+        {
+            get => (Rect)GetValue(WorkAreaProperty);
+            private set => SetValue(WorkAreaProperty, value);
+        }
+
+        private static void OnColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Eyedropper eyedropper)
+            {
+                eyedropper.ColorChanged?.Invoke(eyedropper, new ColorChangedEventArgs { OldColor = (Color)e.OldValue, NewColor = (Color)e.NewValue });
+            }
+        }
+
+        private static void OnWorkAreaChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Eyedropper eyedropper)
+            {
+                eyedropper.UpadateWorkArea();
+            }
+        }
+    }
+}
