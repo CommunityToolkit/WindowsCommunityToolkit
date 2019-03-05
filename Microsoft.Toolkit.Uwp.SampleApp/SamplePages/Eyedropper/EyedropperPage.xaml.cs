ddbbb4b324833cb7a0fccb3b624afb03a2387844
hhchaos <HHChaos@outlook.com>
2019-03-05 11:08:02 +0800

    Add Eyedropper Sample.

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Eyedropper/EyedropperPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    /// <summary>
+    /// A page that shows how to use the Eyedropper control.
+    /// </summary>
+    public sealed partial class EyedropperPage : Page, IXamlRenderListener
+    {
+        public EyedropperPage()
+        {
+            this.InitializeComponent();
+            Load();
+        }
+
+        public void OnXamlRendered(FrameworkElement control)
+        {
+        }
+
+        private void Load()
+        {
+            SampleController.Current.RegisterNewCommand("Global Eyedropper", async (sender, args) =>
+            {
+                var eyedropper = new Eyedropper();
+                var color = await eyedropper.Open();
+                InAppNotification.Show($"You get {color}.", 3000);
+            });
+        }
+    }
+}
