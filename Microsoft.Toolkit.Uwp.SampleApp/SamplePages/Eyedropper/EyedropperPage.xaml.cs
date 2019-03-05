cf6ed96f8b55a31b1c0268c0e8dc7fd7330054cd
hhchaos <HHChaos@outlook.com>
2019-03-05 12:16:12 +0800

    Update Headers.

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Eyedropper/EyedropperPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Eyedropper/EyedropperPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Toolkit.Uwp.UI.Controls;
 using Microsoft.Toolkit.Uwp.UI.Extensions;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
