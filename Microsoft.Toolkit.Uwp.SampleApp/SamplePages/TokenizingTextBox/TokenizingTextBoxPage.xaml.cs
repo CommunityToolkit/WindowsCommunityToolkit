4e52dad1259b15848e95792fc3ac374efc45e6d2
Shane Weaver <shweaver@microsoft.com>
2019-09-18 13:14:09 -0700

    Adding sample page

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/TokenizingTextBox/TokenizingTextBoxPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    public sealed partial class TokenizingTextBoxPage : Page, IXamlRenderListener
+    {
+        public TokenizingTextBoxPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnXamlRendered(FrameworkElement control)
+        {
+
+        }
+    }
+}
