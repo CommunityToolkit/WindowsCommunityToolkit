74bb90d3f5293cf9a3836da01be37066787fb680
Steven He <hez2010@outlook.com>
2019-11-18 17:42:11 +0800

    Add Dispatcher.YieldAsync() for UWP

--- Microsoft.Toolkit.Uwp/Helpers/DispatcherHelper.cs
+++ Microsoft.Toolkit.Uwp/Helpers/DispatcherHelper.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
@@ -14,6 +15,71 @@ namespace Microsoft.Toolkit.Uwp.Helpers
     /// </summary>
     public static class DispatcherHelper
     {
+        /// <summary>
+        /// This struct represents an awaitable dispatcher.
+        /// </summary>
+        public struct DispatcherPriorityAwaitable
+        {
+            private readonly CoreDispatcher dispatcher;
+            private readonly CoreDispatcherPriority priority;
+
+            internal DispatcherPriorityAwaitable(CoreDispatcher dispatcher, CoreDispatcherPriority priority)
+            {
+                this.dispatcher = dispatcher;
+                this.priority = priority;
+            }
+
+            /// <summary>
+            /// Get awaiter of DispatcherPriorityAwaiter
+            /// </summary>
+            /// <returns>Awaiter of DispatcherPriorityAwaiter</returns>
+            public DispatcherPriorityAwaiter GetAwaiter() => new DispatcherPriorityAwaiter(this.dispatcher, this.priority);
+        }
+
+        /// <summary>
+        /// This struct represents the awaiter of a dispatcher.
+        /// </summary>
+        public struct DispatcherPriorityAwaiter : INotifyCompletion
+        {
+            private readonly CoreDispatcher dispatcher;
+            private readonly CoreDispatcherPriority priority;
+
+            /// <summary>
+            /// Gets a value indicating whether task has completed
+            /// </summary>
+            public bool IsCompleted => false;
+
+            internal DispatcherPriorityAwaiter(CoreDispatcher dispatcher, CoreDispatcherPriority priority)
+            {
+                this.dispatcher = dispatcher;
+                this.priority = priority;
+            }
+
+            /// <summary>
+            /// Get result for this awaiter
+            /// </summary>
+            public void GetResult()
+            {
+            }
+
+            /// <summary>
+            /// Fired once task has complated for notify completion
+            /// </summary>
+            /// <param name="continuation">Continuation action</param>
+            public async void OnCompleted(Action continuation)
+            {
+                await this.dispatcher.RunAsync(this.priority, new DispatchedHandler(continuation));
+            }
+        }
+
+        /// <summary>
+        /// Yield and allow UI update during tasks.
+        /// </summary>
+        /// <param name="dispatcher">Dispatcher of a thread to yield</param>
+        /// <param name="priority">Dispatcher execution priority, default is low</param>
+        /// <returns>Awaitable dispatcher task</returns>
+        public static DispatcherPriorityAwaitable YieldAsync(this CoreDispatcher dispatcher, CoreDispatcherPriority priority = CoreDispatcherPriority.Low) => new DispatcherPriorityAwaitable(dispatcher, priority);
+
         /// <summary>
         /// Executes the given function asynchronously on UI thread of the main view.
         /// </summary>
