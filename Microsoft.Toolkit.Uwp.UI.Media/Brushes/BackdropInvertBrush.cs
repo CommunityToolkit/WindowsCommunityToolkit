cac2fd15be765231ca5af162be782edfa0f93cd4
Sergio Pedri <sergio0694@live.com>
2020-01-30 17:47:36 +0100

    BackdropInvertBrush switched to pipeline

--- Microsoft.Toolkit.Uwp.UI.Media/Brushes/BackdropInvertBrush.cs
+++ Microsoft.Toolkit.Uwp.UI.Media/Brushes/BackdropInvertBrush.cs
@@ -4,9 +4,8 @@
 
 //// Example brush from https://blogs.windows.com/buildingapps/2017/07/18/working-brushes-content-xaml-visual-layer-interop-part-one/#z70vPv1QMAvZsceo.97
 
-using Microsoft.Graphics.Canvas.Effects;
-using Windows.UI.Composition;
-using Windows.UI.Xaml;
+using Microsoft.Toolkit.Uwp.UI.Media.Brushes.Base;
+using Microsoft.Toolkit.Uwp.UI.Media.Pipelines;
 using Windows.UI.Xaml.Media;
 
 namespace Microsoft.Toolkit.Uwp.UI.Media
@@ -14,58 +13,12 @@ namespace Microsoft.Toolkit.Uwp.UI.Media
     /// <summary>
     /// The <see cref="BackdropInvertBrush"/> is a <see cref="Brush"/> which inverts whatever is behind it in the application.
     /// </summary>
-    public class BackdropInvertBrush : XamlCompositionBrushBase
+    public class BackdropInvertBrush : XamlCompositionEffectBrushBase
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BackdropInvertBrush"/> class.
-        /// </summary>
-        public BackdropInvertBrush()
+        /// <inheritdoc/>
+        protected override PipelineBuilder OnBrushRequested()
         {
-        }
-
-        /// <summary>
-        /// Initializes the Composition Brush.
-        /// </summary>
-        protected override void OnConnected()
-        {
-            // Delay creating composition resources until they're required.
-            if (CompositionBrush == null)
-            {
-                // Abort if effects aren't supported.
-                if (!CompositionCapabilities.GetForCurrentView().AreEffectsSupported())
-                {
-                    return;
-                }
-
-                var backdrop = Window.Current.Compositor.CreateBackdropBrush();
-
-                // Use a Win2D invert affect applied to a CompositionBackdropBrush.
-                var graphicsEffect = new InvertEffect
-                {
-                    Name = "Invert",
-                    Source = new CompositionEffectSourceParameter("backdrop")
-                };
-
-                var effectFactory = Window.Current.Compositor.CreateEffectFactory(graphicsEffect);
-                var effectBrush = effectFactory.CreateBrush();
-
-                effectBrush.SetSourceParameter("backdrop", backdrop);
-
-                CompositionBrush = effectBrush;
-            }
-        }
-
-        /// <summary>
-        /// Deconstructs the Composition Brush.
-        /// </summary>
-        protected override void OnDisconnected()
-        {
-            // Dispose of composition resources when no longer in use.
-            if (CompositionBrush != null)
-            {
-                CompositionBrush.Dispose();
-                CompositionBrush = null;
-            }
+            return PipelineBuilder.FromBackdrop().Invert();
         }
     }
 }
