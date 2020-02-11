0472bd1caecb7abadf52aa09e4a97eb833254e67
Sergio Pedri <sergio0694@live.com>
2020-02-11 15:14:36 +0100

    Minor code refactoring

--- Microsoft.Toolkit.HighPerformance/Extensions/ReadOnlySpanExtensions.Enumerate.cs
+++ Microsoft.Toolkit.HighPerformance/Extensions/ReadOnlySpanExtensions.Enumerate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -11,6 +12,32 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
     /// </summary>
     public static partial class ReadOnlySpanExtensions
     {
+        /// <summary>
+        /// Enumerates the items in the input <see cref="ReadOnlySpan{T}"/> instance, as pairs of value/index values.
+        /// This extension should be used directly within a <see langword="foreach"/> loop:
+        /// <code>
+        /// ReadOnlySpan&lt;string&gt; words = new[] { "Hello", ", ", "world", "!" };
+        ///
+        /// foreach (var item in words.Enumerate())
+        /// {
+        ///     // Access the index and value of each item here...
+        ///     int index = item.Index;
+        ///     string value = item.Value;
+        /// }
+        /// </code>
+        /// The compiler will take care of properly setting up the <see langword="foreach"/> loop with the type returned from this method.
+        /// </summary>
+        /// <typeparam name="T">The type of items to enumerate.</typeparam>
+        /// <param name="span">The source <see cref="ReadOnlySpan{T}"/> to enumerate</param>
+        /// <returns>A wrapper type that will handle the value/index enumeration for <paramref name="span"/></returns>
+        /// <remarks>The returned <see cref="__ReadOnlySpanEnumerator{T}"/> value shouldn't be used directly: use this extension in a <see langword="foreach"/> loop.</remarks>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static __ReadOnlySpanEnumerator<T> Enumerate<T>(this ReadOnlySpan<T> span)
+        {
+            return new __ReadOnlySpanEnumerator<T>(span);
+        }
+
         /// <summary>
         /// A <see langword="ref"/> <see langword="struct"/> that enumerates the items in a given <see cref="ReadOnlySpan{T}"/> instance.
         /// </summary>
--- Microsoft.Toolkit.HighPerformance/Extensions/ReadOnlySpanExtensions.cs
+++ Microsoft.Toolkit.HighPerformance/Extensions/ReadOnlySpanExtensions.cs
@@ -31,32 +31,6 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
         }
 
         /// <summary>
-        /// Enumerates the items in the input <see cref="ReadOnlySpan{T}"/> instance, as pairs of value/index values.
-        /// This extension should be used directly within a <see langword="foreach"/> loop:
-        /// <code>
-        /// ReadOnlySpan&lt;string&gt; words = new[] { "Hello", ", ", "world", "!" };
-        ///
-        /// foreach (var item in words.Enumerate())
-        /// {
-        ///     // Access the index and value of each item here...
-        ///     int index = item.Index;
-        ///     string value = item.Value;
-        /// }
-        /// </code>
-        /// The compiler will take care of properly setting up the <see langword="foreach"/> loop with the type returned from this method.
-        /// </summary>
-        /// <typeparam name="T">The type of items to enumerate.</typeparam>
-        /// <param name="span">The source <see cref="ReadOnlySpan{T}"/> to enumerate</param>
-        /// <returns>A wrapper type that will handle the value/index enumeration for <paramref name="span"/></returns>
-        /// <remarks>The returned <see cref="__ReadOnlySpanEnumerator{T}"/> value shouldn't be used directly: use this extension in a <see langword="foreach"/> loop.</remarks>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static __ReadOnlySpanEnumerator<T> Enumerate<T>(this ReadOnlySpan<T> span)
-        {
-            return new __ReadOnlySpanEnumerator<T>(span);
-        }
-
-        /// <summary>
         /// Gets a content hash from the input <see cref="ReadOnlySpan{T}"/> instance using the Djb2 algorithm.
         /// </summary>
         /// <typeparam name="T">The type of items in the input <see cref="ReadOnlySpan{T}"/> instance.</typeparam>
