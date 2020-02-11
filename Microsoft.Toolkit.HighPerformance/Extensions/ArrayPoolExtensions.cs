98cb8aeb63ec20f868f46617645b5ecdb0ecb8d8
Sergio Pedri <sergio0694@live.com>
2020-02-11 10:55:38 +0100

    Added ArrayPoolExtensions class

---
+++ Microsoft.Toolkit.HighPerformance/Extensions/ArrayPoolExtensions.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Buffers;
+
+#nullable enable
+
+namespace Microsoft.Toolkit.HighPerformance.Extensions
+{
+    /// <summary>
+    /// Helpers for working with the <see cref="ArrayPool{T}"/> type
+    /// </summary>
+    public static class ArrayPoolExtensions
+    {
+        /// <summary>
+        /// Changes the number of elements of a rented one-dimensional array to the specified new size
+        /// </summary>
+        /// <typeparam name="T">The type of items into the target array to resize</typeparam>
+        /// <param name="pool">The target <see cref="ArrayPool{T}"/> instance to use to resize the array</param>
+        /// <param name="array">The rented <typeparamref name="T"/> array to resize, or <see langword="null"/> to create a new array</param>
+        /// <param name="newSize">The size of the new array</param>
+        /// <param name="clearArray">Indicates whether the contents of the array should be cleared before reuse</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="newSize"/> is less than 0</exception>
+        /// <remarks>When this method returns, the caller must not use any references to the old array anymore</remarks>
+        public static void Resize<T>(this ArrayPool<T> pool, ref T[]? array, int newSize, bool clearArray = false)
+        {
+            if (newSize < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(newSize), "The new size can't be less than 0");
+            }
+
+            // If the old array is null, just create a new one with the requested size
+            if (array is null)
+            {
+                array = pool.Rent(newSize);
+
+                return;
+            }
+
+            // If the new size is the same as the current size, do nothing
+            if (array.Length == newSize)
+            {
+                return;
+            }
+
+            /* Rent a new array with the specified size, and copy as many items from the current array
+             * as possible to the new array. This mirrors the behavior of the Array.Resize API from
+             * the BCL: if the new size is greater than the length of the current array, copy all the
+             * items from the original array into the new one. Otherwise, copy as many items as possible,
+             * until the new array is completely filled, and ignore the remaining items in the first array. */
+            T[] newArray = pool.Rent(newSize);
+            int itemsToCopy = Math.Min(array.Length, newSize);
+
+            array.AsSpan(0, itemsToCopy).CopyTo(newArray);
+
+            pool.Return(array, clearArray);
+
+            array = newArray;
+        }
+
+        /// <summary>
+        /// Changes the number of elements of an <see cref="IMemoryOwner{T}"/> instance to the specified new size
+        /// </summary>
+        /// <typeparam name="T">The type of items into the target buffer to resize</typeparam>
+        /// <param name="pool">The target <see cref="MemoryPool{T}"/> instance to use to resize the buffer</param>
+        /// <param name="memoryOwner">The rented <see cref="IMemoryOwner{T}"/> instance to resize, or <see langword="null"/> to create a new one</param>
+        /// <param name="newSize">The size of the new buffer</param>
+        /// <param name="clearBuffer">Indicates whether the contents of the buffer should be cleared before reuse</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="newSize"/> is less than 0</exception>
+        /// <remarks>When this method returns, the caller must not use any references to the old buffer anymore</remarks>
+        public static void Resize<T>(this MemoryPool<T> pool, ref IMemoryOwner<T>? memoryOwner, int newSize, bool clearBuffer = false)
+        {
+            if (newSize < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(newSize), "The new size can't be less than 0");
+            }
+
+            // If the old memory is null, just create a new one with the requested size
+            if (memoryOwner is null)
+            {
+                memoryOwner = pool.Rent(newSize);
+
+                return;
+            }
+
+            // If the new size is the same as the current size, do nothing
+            var memory = memoryOwner.Memory;
+            if (memory.Length == newSize)
+            {
+                return;
+            }
+
+            // Same behavior as the T[] resize extension
+            var newMemoryOwner = pool.Rent(newSize);
+            int itemsToCopy = Math.Min(memory.Length, newSize);
+
+            memory.Slice(0, itemsToCopy).CopyTo(newMemoryOwner.Memory);
+
+            // Clear the original buffer, if needed
+            if (clearBuffer)
+            {
+                memory.Span.Clear();
+            }
+
+            // There isn't a return API for the memory pool, so just invoke Dispose directly
+            memoryOwner.Dispose();
+
+            memoryOwner = newMemoryOwner;
+        }
+    }
+}
