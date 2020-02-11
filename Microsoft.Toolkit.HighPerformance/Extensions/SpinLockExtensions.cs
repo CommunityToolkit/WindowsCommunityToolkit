680736e0a557ba756bb0f170279e49bc4f12dc93
Sergio Pedri <sergio0694@live.com>
2020-02-11 11:07:56 +0100

    Added SpinLockExtensions class

---
+++ Microsoft.Toolkit.HighPerformance/Extensions/SpinLockExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Microsoft.Toolkit.HighPerformance.Extensions
+{
+    /// <summary>
+    /// Helpers for working with the <see cref="SpinLock"/> type.
+    /// </summary>
+    public static class SpinLockExtensions
+    {
+        /// <summary>
+        /// Enters a specified <see cref="SpinLock"/> instance and returns a wrapper to use to release the lock.
+        /// This extension should be used though a <see langword="using"/> block or statement:
+        /// <code>
+        /// SpinLock spinLock = new SpinLock();
+        ///
+        /// using (spinLock.Enter())
+        /// {
+        ///     // Thread-safe code here...
+        /// }
+        /// </code>
+        /// The compiler will take care of releasing the SpinLock when the code goes out of that <see langword="using"/> scope.
+        /// </summary>
+        /// <param name="spinLock">The target <see cref="SpinLock"/> to use</param>
+        /// <returns>A wrapper type that will release <paramref name="spinLock"/> when its <see cref="System.IDisposable.Dispose"/> method is called.</returns>
+        /// <remarks>The returned <see cref="__Lock"/> value shouldn't be used directly: use this extension in a <see langword="using"/> block or statement.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static __Lock Enter(ref this SpinLock spinLock)
+        {
+            return new __Lock(ref spinLock);
+        }
+
+        /// <summary>
+        /// A <see langword="struct"/> that is used to enter and hold a <see cref="SpinLock"/> through a <see langword="using"/> block or statement.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300", Justification = "The type is not meant to be used directly by users")]
+        public ref struct __Lock
+        {
+            /// <summary>
+            /// The <see cref="ByReference{T}"/> instance pointing to the target <see cref="SpinLock"/> value to use.
+            /// </summary>
+            private readonly ByReference<SpinLock> r0;
+
+            /// <summary>
+            /// A value indicating whether or not the lock is taken by this <see cref="__Lock"/> instance.
+            /// </summary>
+            private readonly bool lockTaken;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="__Lock"/> struct.
+            /// </summary>
+            /// <param name="spinLock">The target <see cref="SpinLock"/> to use.</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public __Lock(ref SpinLock spinLock)
+            {
+                r0 = new ByReference<SpinLock>(ref spinLock);
+                lockTaken = false;
+
+                spinLock.Enter(ref lockTaken);
+            }
+
+            /// <summary>
+            /// Implements the duck-typed <see cref="System.IDisposable.Dispose"/> method and releases the current <see cref="SpinLock"/> instance.
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Dispose()
+            {
+                if (lockTaken)
+                {
+                    r0.Value.Exit();
+                }
+            }
+        }
+    }
+}
