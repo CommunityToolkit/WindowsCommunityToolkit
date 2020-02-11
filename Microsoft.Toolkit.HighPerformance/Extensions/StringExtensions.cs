e05f2c73e8732cedef2a7f950c47877322fd8000
Sergio Pedri <sergio0694@live.com>
2020-02-11 16:04:26 +0100

    Added string.DangerousGetReference extensions

--- Microsoft.Toolkit.HighPerformance/Extensions/StringExtensions.cs
+++ Microsoft.Toolkit.HighPerformance/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Contracts;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 
 namespace Microsoft.Toolkit.HighPerformance.Extensions
@@ -8,6 +9,55 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
     /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Returns a reference to the first element within a given <see cref="string"/>, with no bounds checks.
+        /// </summary>
+        /// <param name="text">The input <see cref="string"/> instance.</param>
+        /// <returns>A reference to the first element within <paramref name="text"/>, or the location it would have used, if <paramref name="text"/> is empty.</returns>
+        /// <remarks>This method doesn't do any bounds checks, therefore it is responsibility of the caller to perform checks in case the returned value is dereferenced.</remarks>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref readonly char DangerousGetReference(this string text)
+        {
+            var stringData = Unsafe.As<RawStringData>(text);
+
+            return ref stringData.Data;
+        }
+
+        /// <summary>
+        /// Returns a reference to an element at a specified index within a given <see cref="string"/>, with no bounds checks.
+        /// </summary>
+        /// <param name="text">The input <see cref="string"/> instance.</param>
+        /// <param name="i">The index of the element to retrieve within <paramref name="text"/>.</param>
+        /// <returns>A reference to the element within <paramref name="text"/> at the index specified by <paramref name="i"/>.</returns>
+        /// <remarks>This method doesn't do any bounds checks, therefore it is responsibility of the caller to ensure the <paramref name="i"/> parameter is valid.</remarks>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref readonly char DangerousGetReferenceAt(this string text, int i)
+        {
+            var stringData = Unsafe.As<RawStringData>(text);
+            ref var ri = ref Unsafe.Add(ref stringData.Data, i);
+
+            return ref ri;
+        }
+
+        // Description adapted from CoreCLR: see https://source.dot.net/#System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.CoreCLR.cs,285.
+        // CLR strings are laid out in memory as follows:
+        // [ sync block || pMethodTable || length || string data .. ]
+        //                 ^                         ^
+        //                 |                         \-- ref Unsafe.As<RawStringData>(text).Data
+        //                 \-- string
+        // The reference to RawStringData.Data points to the first character in the
+        // string, skipping over the sync block, method table and string length.
+        private sealed class RawStringData
+        {
+            [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401", Justification = "Definition from CoreCLR source")]
+            public int Length;
+
+            [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401", Justification = "Needs access to this field from parent class")]
+            public char Data;
+        }
+
         /// <summary>
         /// Counts the number of occurrences of a given character into a target <see cref="string"/> instance.
         /// </summary>
