0292f9db88e615b2d400b5e8036119e3321b1a06
Sergio Pedri <sergio0694@live.com>
2020-02-13 13:54:24 +0100

    Added Guard size API overloads for the string type

--- Microsoft.Toolkit/Diagnostics/Guard.Enumerable.cs
+++ Microsoft.Toolkit/Diagnostics/Guard.Enumerable.cs
@@ -17,6 +17,90 @@ namespace Microsoft.Toolkit.Diagnostics
     public static partial class Guard
     {
         /// <summary>
+        /// Asserts that the input <see cref="string"/> instance must have a size of a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the input <see cref="string"/> instance.</typeparam>
+        /// <param name="text">The input <see cref="string"/> instance to check the size for.</param>
+        /// <param name="size">The target size to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentException">Thrown if the size of <paramref name="text"/> is != <paramref name="size"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void HasSizeEqualTo(string text, int size, string name)
+        {
+            HasSizeEqualTo(text.AsSpan(), size, name);
+        }
+
+        /// <summary>
+        /// Asserts that the input <see cref="string"/> instance must have a size not equal to a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the input <see cref="string"/> instance.</typeparam>
+        /// <param name="text">The input <see cref="string"/> instance to check the size for.</param>
+        /// <param name="size">The target size to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentException">Thrown if the size of <paramref name="text"/> is == <paramref name="size"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void HasSizeNotEqualTo(string text, int size, string name)
+        {
+            HasSizeNotEqualTo(text.AsSpan(), size, name);
+        }
+
+        /// <summary>
+        /// Asserts that the input <see cref="string"/> instance must have a size of at least specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the input <see cref="string"/> instance.</typeparam>
+        /// <param name="text">The input <see cref="string"/> instance to check the size for.</param>
+        /// <param name="size">The target size to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentException">Thrown if the size of <paramref name="text"/> is &lt;= <paramref name="size"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void HasSizeAtLeast(string text, int size, string name)
+        {
+            HasSizeAtLeast(text.AsSpan(), size, name);
+        }
+
+        /// <summary>
+        /// Asserts that the input <see cref="string"/> instance must have a size of at least or equal to a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the input <see cref="string"/> instance.</typeparam>
+        /// <param name="text">The input <see cref="string"/> instance to check the size for.</param>
+        /// <param name="size">The target size to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentException">Thrown if the size of <paramref name="text"/> is &lt; <paramref name="size"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void HasSizeAtLeastOrEqualTo(string text, int size, string name)
+        {
+            HasSizeAtLeastOrEqualTo(text.AsSpan(), size, name);
+        }
+
+        /// <summary>
+        /// Asserts that the input <see cref="string"/> instance must have a size of less than a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the input <see cref="string"/> instance.</typeparam>
+        /// <param name="text">The input <see cref="string"/> instance to check the size for.</param>
+        /// <param name="size">The target size to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentException">Thrown if the size of <paramref name="text"/> is >= <paramref name="size"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void HasSizeLessThan(string text, int size, string name)
+        {
+            HasSizeLessThan(text.AsSpan(), size, name);
+        }
+
+        /// <summary>
+        /// Asserts that the input <see cref="string"/> instance must have a size of less than or equal to a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the input <see cref="string"/> instance.</typeparam>
+        /// <param name="text">The input <see cref="string"/> instance to check the size for.</param>
+        /// <param name="size">The target size to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentException">Thrown if the size of <paramref name="text"/> is > <paramref name="size"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void HasSizeLessThanOrEqualTo(string text, int size, string name)
+        {
+            HasSizeLessThanOrEqualTo(text.AsSpan(), size, name);
+        }
+
+        /// <summary>
         /// Asserts that the input <see cref="IEnumerable{T}"/> instance must have a size of a specified value.
         /// </summary>
         /// <typeparam name="T">The type of items in the input <see cref="IEnumerable{T}"/> instance.</typeparam>
