1c791578cde353db078b334a090053a28b9855c5
Sergio Pedri <sergio0694@live.com>
2020-02-13 11:30:00 +0100

    Initial guard APIs added

---
+++ Microsoft.Toolkit/Diagnostics/Guard.cs
@@ -0,0 +1,143 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+#nullable enable
+
+namespace Microsoft.Toolkit.Diagnostics
+{
+    /// <summary>
+    /// Helper methods to verify conditions when running code.
+    /// </summary>
+    [DebuggerStepThrough]
+    public static class Guard
+    {
+        /// <summary>
+        /// Asserts that the input value is not <see langword="null"/>.
+        /// </summary>
+        /// <param name="value">The input value to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <see langword="null"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void MustBeNotNull(object? value, string name)
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(name, $"Parameter {name} must be not null");
+            }
+        }
+
+        /// <summary>
+        /// Asserts that the input value is of a specific type.
+        /// </summary>
+        /// <typeparam name="T">The type of the input value.</typeparam>
+        /// <param name="value">The input <see cref="object"/> to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="value"/> is not of type <typeparamref name="T"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void MustBeOf<T>(object value, string name)
+            where T : class
+        {
+            if (value.GetType() != typeof(T))
+            {
+                throw new ArgumentException($"Parameter {name} must be of type {typeof(T)}, was {value.GetType()}", name);
+            }
+        }
+
+        /// <summary>
+        /// Asserts that the input value can be cast to a specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to check the input value against.</typeparam>
+        /// <param name="value">The input <see cref="object"/> to test.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="value"/> can't be cast to type <typeparamref name="T"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void MustBeAssignableTo<T>(object value, string name)
+        {
+            if (!(value is T))
+            {
+                throw new ArgumentException($"Parameter {name} must be assignable to type {typeof(T)}, was {value.GetType()}", name);
+            }
+        }
+
+        /// <summary>
+        /// Asserts that the input value must be less than a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of input values to compare.</typeparam>
+        /// <param name="value">The input <typeparamref name="T"/> value to test.</param>
+        /// <param name="max">The exclusive maximum <typeparamref name="T"/> value that is accepted.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is >= <paramref name="max"/>.</exception>
+        /// <remarks>The method is generic to avoid boxing the parameters, if they are value types.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void MustBeLessThan<T>(T value, T max, string name)
+            where T : notnull, IComparable<T>
+        {
+            if (value.CompareTo(max) >= 0)
+            {
+                throw new ArgumentOutOfRangeException(name, $"Parameter {name} must be < {max}, was {value}");
+            }
+        }
+
+        /// <summary>
+        /// Asserts that the input value must be less than or equal to a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of input values to compare.</typeparam>
+        /// <param name="value">The input <typeparamref name="T"/> value to test.</param>
+        /// <param name="maximum">The inclusive maximum <typeparamref name="T"/> value that is accepted.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is > <paramref name="maximum"/>.</exception>
+        /// <remarks>The method is generic to avoid boxing the parameters, if they are value types.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void MustBeLessThanOrEqualTo<T>(T value, T maximum, string name)
+            where T : notnull, IComparable<T>
+        {
+            if (value.CompareTo(maximum) > 0)
+            {
+                throw new ArgumentOutOfRangeException(name, $"Parameter {name} must be <= {maximum}, was {value}");
+            }
+        }
+
+        /// <summary>
+        /// Asserts that the input value must be greater than a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of input values to compare.</typeparam>
+        /// <param name="value">The input <typeparamref name="T"/> value to test.</param>
+        /// <param name="minimum">The exclusive minimum <typeparamref name="T"/> value that is accepted.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is &lt;= <paramref name="minimum"/>.</exception>
+        /// <remarks>The method is generic to avoid boxing the parameters, if they are value types.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void MustBeGreaterThan<T>(T value, T minimum, string name)
+            where T : notnull, IComparable<T>
+        {
+            if (value.CompareTo(minimum) <= 0)
+            {
+                throw new ArgumentOutOfRangeException(name, $"Parameter {name} must be > {minimum}, was {value}");
+            }
+        }
+
+        /// <summary>
+        /// Asserts that the input value must be greater than or equal to a specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of input values to compare.</typeparam>
+        /// <param name="value">The input <typeparamref name="T"/> value to test.</param>
+        /// <param name="minimum">The inclusive minimum <typeparamref name="T"/> value that is accepted.</param>
+        /// <param name="name">The name of the input parameter being tested.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is &lt; <paramref name="minimum"/>.</exception>
+        /// <remarks>The method is generic to avoid boxing the parameters, if they are value types.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void MustBeGreaterThanOrEqualTo<T>(T value, T minimum, string name)
+            where T : notnull, IComparable<T>
+        {
+            if (value.CompareTo(minimum) < 0)
+            {
+                throw new ArgumentOutOfRangeException(name, $"Parameter {name} must be >= {minimum}, was {value}");
+            }
+        }
+    }
+}
