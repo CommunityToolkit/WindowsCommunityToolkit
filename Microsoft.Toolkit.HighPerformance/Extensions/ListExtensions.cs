a50d0dd21bc47e91163f443292e98511432b34bf
Sergio Pedri <sergio0694@live.com>
2020-02-15 17:35:28 +0100

    Added missing List<T> extension APIs

--- Microsoft.Toolkit.HighPerformance/Extensions/ListExtensions.cs
+++ Microsoft.Toolkit.HighPerformance/Extensions/ListExtensions.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
+using Microsoft.Toolkit.HighPerformance.Enumerables;
 
 #nullable enable
 
@@ -101,10 +102,68 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
         }
 
         /// <summary>
-        /// Gets a content hash from the input <see cref="List{T}"/>instance using the Djb2 algorithm.
+        /// Enumerates the items in the input <see cref="List{T}"/> instance, as pairs of value/index values.
+        /// This extension should be used directly within a <see langword="foreach"/> loop:
+        /// <code>
+        /// List&lt;string> words = new List&lt;string> { "Hello", ", ", "world", "!" };
+        ///
+        /// foreach (var item in words.Enumerate())
+        /// {
+        ///     // Access the index and value of each item here...
+        ///     int index = item.Index;
+        ///     string value = item.Value;
+        /// }
+        /// </code>
+        /// The compiler will take care of properly setting up the <see langword="foreach"/> loop with the type returned from this method.
+        /// </summary>
+        /// <typeparam name="T">The type of items to enumerate.</typeparam>
+        /// <param name="list">The source <see cref="List{T}"/> to enumerate.</param>
+        /// <returns>A wrapper type that will handle the value/index enumeration for <paramref name="list"/>.</returns>
+        /// <remarks>
+        /// The returned <see cref="ReadOnlySpanEnumerable{T}"/> value shouldn't be used directly: use this extension in a <see langword="foreach"/> loop.
+        /// The input <see cref="List{T}"/> should not be modified while using the returned array, as doing so might cause <paramref name="list"/> to swap it out.
+        /// </remarks>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ReadOnlySpanEnumerable<T> Enumerate<T>(this List<T> list)
+        {
+            return new ReadOnlySpanEnumerable<T>(list.DangerousGetUnderlyingArray());
+        }
+
+        /// <summary>
+        /// Tokenizes the values in the input <see cref="List{T}"/> instance using a specified separator.
+        /// This extension should be used directly within a <see langword="foreach"/> loop:
+        /// <code>
+        /// List&lt;char> text = "Hello, world!".ToList();
+        ///
+        /// foreach (var token in text.Tokenize(','))
+        /// {
+        ///     // Access the tokens here...
+        /// }
+        /// </code>
+        /// The compiler will take care of properly setting up the <see langword="foreach"/> loop with the type returned from this method.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the <see cref="List{T}"/> to tokenize.</typeparam>
+        /// <param name="list">The source <see cref="List{T}"/> to tokenize.</param>
+        /// <param name="separator">The separator <typeparamref name="T"/> item to use.</param>
+        /// <returns>A wrapper type that will handle the tokenization for <paramref name="list"/>.</returns>
+        /// <remarks>
+        /// The returned <see cref="SpanTokenizer{T}"/> value shouldn't be used directly: use this extension in a <see langword="foreach"/> loop.
+        /// The input <see cref="List{T}"/> should not be modified while using the returned array, as doing so might cause <paramref name="list"/> to swap it out.
+        /// </remarks>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static SpanTokenizer<T> Tokenize<T>(this List<T> list, T separator)
+            where T : IEquatable<T>
+        {
+            return new SpanTokenizer<T>(list.DangerousGetUnderlyingArray(), separator);
+        }
+
+        /// <summary>
+        /// Gets a content hash from the input <see cref="List{T}"/> instance using the Djb2 algorithm.
         /// </summary>
         /// <typeparam name="T">The type of items in the input <see cref="List{T}"/> instance.</typeparam>
-        /// <param name="list">The input <see cref="List{T}"/> instance.</param>
+        /// <param name="list">The input<see cref="List{T}"/> instance.</param>
         /// <returns>The Djb2 value for the input <see cref="List{T}"/> instance.</returns>
         /// <remarks>The Djb2 hash is fully deterministic and with no random components.</remarks>
         [Pure]
