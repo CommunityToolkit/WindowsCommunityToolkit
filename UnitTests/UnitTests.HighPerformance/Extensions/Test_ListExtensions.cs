124d0810ae71f9b65da10cb6b8d9be1dfebad3fa
Sergio Pedri <sergio0694@live.com>
2020-02-15 18:53:26 +0100

    Added more unit tests

--- UnitTests/UnitTests.HighPerformance/Extensions/Test_ListExtensions.cs
+++ UnitTests/UnitTests.HighPerformance/Extensions/Test_ListExtensions.cs
@@ -55,6 +55,38 @@ namespace UnitTests.HighPerformance.Extensions
 
         [TestCategory("ListExtensions")]
         [TestMethod]
+        public void Test_ListExtensions_DangerousGetUnderlyingArray()
+        {
+            var data = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var array = data.DangerousGetUnderlyingArray();
+
+            Assert.IsTrue(array.Length >= data.Count);
+
+            array[0] = 99;
+            array[data.Count - 1] = 99;
+
+            Assert.AreEqual(data[0], array[0]);
+            Assert.AreEqual(data[data.Count - 1], array[data.Count - 1]);
+        }
+
+        [TestCategory("ListExtensions")]
+        [TestMethod]
+        public void Test_ListExtensions_DangerousAsSpan()
+        {
+            var data = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var span = data.DangerousAsSpan();
+
+            Assert.AreEqual(data.Count, span.Length);
+
+            span[0] = 99;
+            span[span.Length - 1] = 77;
+
+            Assert.AreEqual(data[0], span[0]);
+            Assert.AreEqual(data[data.Count - 1], span[span.Length - 1]);
+        }
+
+        [TestCategory("ListExtensions")]
+        [TestMethod]
         public void Test_ListExtensions_Count()
         {
             var random = new Random();
