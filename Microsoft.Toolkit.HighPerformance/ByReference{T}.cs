a8163ee9c0d6251b1a3cebddb00ce4a7f02d1875
Sergio Pedri <sergio0694@live.com>
2020-02-17 13:12:12 +0100

    Switched compile tile directives in HighPerformance project

--- Microsoft.Toolkit.HighPerformance/ByReference{T}.cs
+++ Microsoft.Toolkit.HighPerformance/ByReference{T}.cs
@@ -2,14 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if NETSTANDARD2_0
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-#else
+#if NETSTANDARD2_1
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+#else
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 #endif
 
 namespace Microsoft.Toolkit.HighPerformance
@@ -21,7 +21,44 @@ namespace Microsoft.Toolkit.HighPerformance
     [SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1206", Justification = "The type is a ref struct")]
     public readonly ref struct ByReference<T>
     {
-#if NETSTANDARD2_0
+#if NETSTANDARD2_1
+        /// <summary>
+        /// The 1-length <see cref="Span{T}"/> instance used to track the target <typeparamref name="T"/> value.
+        /// </summary>
+        private readonly Span<T> span;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ByReference{T}"/> struct.
+        /// </summary>
+        /// <param name="value">The reference to the target <typeparamref name="T"/> value.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ByReference(ref T value)
+        {
+            span = MemoryMarshal.CreateSpan(ref value, 1);
+        }
+
+        /// <summary>
+        /// Gets the <typeparamref name="T"/> reference represented by the current <see cref="ByReference{T}"/> instance.
+        /// </summary>
+        public ref T Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ref MemoryMarshal.GetReference(span);
+        }
+
+        /// <summary>
+        /// Implicitly creates a new <see cref="ByReference{T}"/> instance from the specified readonly reference.
+        /// </summary>
+        /// <param name="value">The readonly reference to the target <typeparamref name="T"/> value.</param>
+        /// <remarks>This operator converts a readonly reference in a mutable one, so make sure that's the intended behavior.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator ByReference<T>(in T value)
+        {
+            ref T r0 = ref Unsafe.AsRef(value);
+
+            return new ByReference<T>(ref r0);
+        }
+#else
         /// <summary>
         /// The owner <see cref="object"/> the current instance belongs to
         /// </summary>
@@ -81,43 +118,6 @@ namespace Microsoft.Toolkit.HighPerformance
             public byte Data;
 #pragma warning restore CS0649
 #pragma warning restore SA1401
-        }
-#else
-        /// <summary>
-        /// The 1-length <see cref="Span{T}"/> instance used to track the target <typeparamref name="T"/> value.
-        /// </summary>
-        private readonly Span<T> span;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ByReference{T}"/> struct.
-        /// </summary>
-        /// <param name="value">The reference to the target <typeparamref name="T"/> value.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public ByReference(ref T value)
-        {
-            span = MemoryMarshal.CreateSpan(ref value, 1);
-        }
-
-        /// <summary>
-        /// Gets the <typeparamref name="T"/> reference represented by the current <see cref="ByReference{T}"/> instance.
-        /// </summary>
-        public ref T Value
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => ref MemoryMarshal.GetReference(span);
-        }
-
-        /// <summary>
-        /// Implicitly creates a new <see cref="ByReference{T}"/> instance from the specified readonly reference.
-        /// </summary>
-        /// <param name="value">The readonly reference to the target <typeparamref name="T"/> value.</param>
-        /// <remarks>This operator converts a readonly reference in a mutable one, so make sure that's the intended behavior.</remarks>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator ByReference<T>(in T value)
-        {
-            ref T r0 = ref Unsafe.AsRef(value);
-
-            return new ByReference<T>(ref r0);
         }
 #endif
 
--- Microsoft.Toolkit.HighPerformance/ReadOnlyByReference{T}.cs
+++ Microsoft.Toolkit.HighPerformance/ReadOnlyByReference{T}.cs
@@ -2,14 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if NETSTANDARD2_0
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-#else
+#if NETSTANDARD2_1
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+#else
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 #endif
 
 namespace Microsoft.Toolkit.HighPerformance
@@ -21,7 +21,53 @@ namespace Microsoft.Toolkit.HighPerformance
     [SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1206", Justification = "The type is a ref struct")]
     public readonly ref struct ReadOnlyByReference<T>
     {
-#if NETSTANDARD2_0
+#if NETSTANDARD2_1
+        /// <summary>
+        /// The 1-length <see cref="Span{T}"/> instance used to track the target <typeparamref name="T"/> value.
+        /// </summary>
+        private readonly Span<T> span;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadOnlyByReference{T}"/> struct.
+        /// </summary>
+        /// <param name="value">The readonly reference to the target <typeparamref name="T"/> value.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlyByReference(in T value)
+        {
+            ref T r0 = ref Unsafe.AsRef(value);
+
+            span = MemoryMarshal.CreateSpan(ref r0, 1);
+        }
+
+        /// <summary>
+        /// Gets the readonly <typeparamref name="T"/> reference represented by the current <see cref="ByReference{T}"/> instance.
+        /// </summary>
+        public ref readonly T Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ref MemoryMarshal.GetReference(span);
+        }
+
+        /// <summary>
+        /// Implicitly creates a new <see cref="ReadOnlyByReference{T}"/> instance from the specified readonly reference.
+        /// </summary>
+        /// <param name="value">The readonly reference to the target <typeparamref name="T"/> value.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator ReadOnlyByReference<T>(in T value)
+        {
+            return new ReadOnlyByReference<T>(value);
+        }
+
+        /// <summary>
+        /// Implicitly converts a <see cref="ByReference{T}"/> instance into a <see cref="ReadOnlyByReference{T}"/> one.
+        /// </summary>
+        /// <param name="reference">The input <see cref="ByReference{T}"/> instance.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator ReadOnlyByReference<T>(ByReference<T> reference)
+        {
+            return new ReadOnlyByReference<T>(reference.Value);
+        }
+#else
         /// <summary>
         /// The owner <see cref="object"/> the current instance belongs to
         /// </summary>
@@ -88,52 +134,6 @@ namespace Microsoft.Toolkit.HighPerformance
         public static implicit operator ReadOnlyByReference<T>(ByReference<T> reference)
         {
             return new ReadOnlyByReference<T>(reference.Owner, reference.Offset);
-        }
-#else
-        /// <summary>
-        /// The 1-length <see cref="Span{T}"/> instance used to track the target <typeparamref name="T"/> value.
-        /// </summary>
-        private readonly Span<T> span;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ReadOnlyByReference{T}"/> struct.
-        /// </summary>
-        /// <param name="value">The readonly reference to the target <typeparamref name="T"/> value.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public ReadOnlyByReference(in T value)
-        {
-            ref T r0 = ref Unsafe.AsRef(value);
-
-            span = MemoryMarshal.CreateSpan(ref r0, 1);
-        }
-
-        /// <summary>
-        /// Gets the readonly <typeparamref name="T"/> reference represented by the current <see cref="ByReference{T}"/> instance.
-        /// </summary>
-        public ref readonly T Value
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => ref MemoryMarshal.GetReference(span);
-        }
-
-        /// <summary>
-        /// Implicitly creates a new <see cref="ReadOnlyByReference{T}"/> instance from the specified readonly reference.
-        /// </summary>
-        /// <param name="value">The readonly reference to the target <typeparamref name="T"/> value.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator ReadOnlyByReference<T>(in T value)
-        {
-            return new ReadOnlyByReference<T>(value);
-        }
-
-        /// <summary>
-        /// Implicitly converts a <see cref="ByReference{T}"/> instance into a <see cref="ReadOnlyByReference{T}"/> one.
-        /// </summary>
-        /// <param name="reference">The input <see cref="ByReference{T}"/> instance.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator ReadOnlyByReference<T>(ByReference<T> reference)
-        {
-            return new ReadOnlyByReference<T>(reference.Value);
         }
 #endif
 
