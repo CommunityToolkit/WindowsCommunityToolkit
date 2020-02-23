61a96e194aaa1bbe1101afa6bb4ab8258bbe902f
Sergio Pedri <sergio0694@live.com>
2020-02-23 12:56:45 +0100

    Added MemoryOwner<T> tests

---
+++ UnitTests/UnitTests.HighPerformance.Shared/Buffers/Test_MemoryOwner{T}.cs
@@ -0,0 +1,88 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+using Microsoft.Toolkit.HighPerformance.Buffers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.HighPerformance.Buffers
+{
+    [TestClass]
+    public class Test_MemoryOwnerOfT
+    {
+        [TestCategory("MemoryOwnerOfT")]
+        [TestMethod]
+        public void Test_MemoryOwnerOfT_AllocateAndGetMemoryAndSpan()
+        {
+            using var buffer = MemoryOwner<int>.Allocate(127);
+
+            Assert.IsTrue(buffer.Length == 127);
+            Assert.IsTrue(buffer.Memory.Length == 127);
+            Assert.IsTrue(buffer.Span.Length == 127);
+
+            buffer.Span.Fill(42);
+
+            Assert.IsTrue(buffer.Memory.Span.ToArray().All(i => i == 42));
+            Assert.IsTrue(buffer.Span.ToArray().All(i => i == 42));
+        }
+
+        [TestCategory("MemoryOwnerOfT")]
+        [TestMethod]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void Test_MemoryOwnerOfT_DisposedMemory()
+        {
+            var buffer = MemoryOwner<int>.Allocate(127);
+
+            buffer.Dispose();
+
+            _ = buffer.Memory;
+        }
+
+        [TestCategory("MemoryOwnerOfT")]
+        [TestMethod]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void Test_MemoryOwnerOfT_DisposedSpan()
+        {
+            var buffer = MemoryOwner<int>.Allocate(127);
+
+            buffer.Dispose();
+
+            _ = buffer.Span;
+        }
+
+        [TestCategory("MemoryOwnerOfT")]
+        [TestMethod]
+        public void Test_MemoryOwnerOfT_MultipleDispose()
+        {
+            var buffer = MemoryOwner<int>.Allocate(127);
+
+            buffer.Dispose();
+            buffer.Dispose();
+            buffer.Dispose();
+            buffer.Dispose();
+        }
+
+        [TestCategory("HashCodeOfT")]
+        [TestMethod]
+        public void Test_MemoryOwnerOfT_PooledBuffersAndClear()
+        {
+            var buffer = MemoryOwner<int>.Allocate(127);
+
+            buffer.Span.Fill(42);
+
+            buffer.Dispose();
+
+            buffer = MemoryOwner<int>.Allocate(127);
+
+            Assert.IsTrue(buffer.Span.ToArray().All(i => i == 42));
+
+            buffer.Dispose();
+
+            buffer = MemoryOwner<int>.Allocate(127, true);
+
+            Assert.IsTrue(buffer.Span.ToArray().All(i => i == 0));
+        }
+    }
+}
