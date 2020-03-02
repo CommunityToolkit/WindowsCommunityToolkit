203c77014616ea6be7a12b1967dd6239368c128e
Vincent Gromfeld <vigromfe@microsoft.com>
2020-03-02 13:34:33 +0100

    add enum to value converter

---
+++ Microsoft.Toolkit.Uwp.UI/Converters/EnumerationValueConverter.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Microsoft.Toolkit.Uwp.UI.Converters
+{
+    /// <summary>
+    /// A one-way convert to transform an enumeration value to <see cref="TrueValue"/> if it matches the converter parameter
+    /// or <see cref="FalseValue"/> otherwise.
+    /// </summary>
+    /// <example>
+    /// In the code behind:
+    ///     - enum MyEnum { Value1, Value2, }
+    /// In the resources:
+    ///     - &lt;namespace:MyEnum x:Key="MyEnumValue1"&gt;Value1&lt;/namespace:MyEnum&gt;.
+    /// In a control:
+    ///     - IsEnabled="{x:Bind ViewModel.MyEnum, Converter={StaticResource EnumerationValueConverter}, ConverterParameter={StaticResource MyEnumValue1}}".
+    /// </example>
+    public sealed class EnumerationValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Gets or sets the value to return if the value to convert matches the converter parameter.
+        /// </summary>
+        public object TrueValue
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the value to return if the value to convert does not match the converter parameter.
+        /// </summary>
+        public object FalseValue
+        {
+            get; set;
+        }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || parameter == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!(value.GetType().IsEnum || value.GetType() == typeof(int)) ||
+                !(parameter.GetType().Equals(value.GetType()) || parameter.GetType().Equals(typeof(int))))
+            {
+                throw new ArgumentException("value is not an enum type or parameter does not match value type nor is convertible to value type.");
+            }
+
+            // we convert the enum values to 'int' because the XAML runtime is converting the enum parameters to 'int'.
+            return (int)value == (int)parameter ? TrueValue : FalseValue;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+    }
+}
---
+++ UnitTests/Converters/EnumerationToValueConverterTests.cs
@@ -0,0 +1,133 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Toolkit.Uwp.UI.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests.Converters
+{
+    [TestClass]
+    public class EnumerationToValueConverterTests
+    {
+        [TestCategory("Converters")]
+        [DataTestMethod]
+        [DataRow(TestEnum.Value1, TestEnum.Value1, 10)]
+        [DataRow(TestEnum.Value2, TestEnum.Value1, -23)]
+        [DataRow(TestEnum.Value2, TestEnum.Value2, 10)]
+        [DataRow(TestEnum.Value1, TestEnum.Value2, -23)]
+        public void Convert_WithValidEnumInputs_ShouldReturnExpectedResult(TestEnum valueToConvert, TestEnum converterParameter, int expectedResult)
+        {
+            var converter = new EnumerationValueConverter
+            {
+                TrueValue = 10,
+                FalseValue = -23,
+            };
+
+            var result = converter.Convert(valueToConvert, typeof(int), converterParameter, "language");
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCategory("Converters")]
+        [DataTestMethod]
+        [DataRow(TestEnum.Value1, (int)TestEnum.Value1, 10)]
+        [DataRow(TestEnum.Value2, (int)TestEnum.Value1, -23)]
+        [DataRow(TestEnum.Value2, (int)TestEnum.Value2, 10)]
+        [DataRow(TestEnum.Value1, (int)TestEnum.Value2, -23)]
+        public void Convert_WithValidIntegerInputs_ShouldReturnExpectedResult(TestEnum valueToConvert, int converterParameter, int expectedResult)
+        {
+            var converter = new EnumerationValueConverter
+            {
+                TrueValue = 10,
+                FalseValue = -23,
+            };
+
+            var result = converter.Convert(valueToConvert, typeof(int), converterParameter, "language");
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCategory("Converters")]
+        [DataTestMethod]
+        [DataRow((int)TestEnum.Value1, (int)TestEnum.Value1, 10)]
+        [DataRow((int)TestEnum.Value2, (int)TestEnum.Value1, -23)]
+        [DataRow((int)TestEnum.Value2, (int)TestEnum.Value2, 10)]
+        [DataRow((int)TestEnum.Value1, (int)TestEnum.Value2, -23)]
+        public void Convert_WithValidIntegerValueAndParameter_ShouldReturnExpectedResult(int valueToConvert, int converterParameter, int expectedResult)
+        {
+            var converter = new EnumerationValueConverter
+            {
+                TrueValue = 10,
+                FalseValue = -23,
+            };
+
+            var result = converter.Convert(valueToConvert, typeof(int), converterParameter, "language");
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCategory("Converters")]
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow("not-an-enum-value")]
+        public void Convert_WithInvalidConverterParameter_ShouldAssert(object converterParameter)
+        {
+            var converter = new EnumerationValueConverter
+            {
+                TrueValue = 10,
+                FalseValue = -23,
+            };
+
+            Action action = () => converter.Convert(TestEnum.Value1, typeof(int), converterParameter, language: "language");
+
+            Assert.ThrowsException<ArgumentException>(action);
+        }
+
+        [TestCategory("Converters")]
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow("not-an-enum-value")]
+        public void Convert_WithInvalidInput_ShouldThrow(object valueToConvert)
+        {
+            var converter = new EnumerationValueConverter
+            {
+                TrueValue = 10,
+                FalseValue = -23,
+            };
+
+            Action action = () => converter.Convert(valueToConvert, typeof(int), parameter: TestEnum.Value1, language: "language");
+
+            Assert.ThrowsException<ArgumentException>(action);
+        }
+
+        [TestCategory("Converters")]
+        [TestMethod]
+        public void ConvertBack_ShouldThrowNotImplementedException()
+        {
+            var converter = new EnumerationValueConverter
+            {
+                TrueValue = 10,
+                FalseValue = -23,
+            };
+
+            Action action = () => converter.ConvertBack(10, typeof(TestEnum), parameter: null, language: null);
+
+            Assert.ThrowsException<NotImplementedException>(action);
+        }
+    }
+
+    public enum TestEnum
+    {
+        /// <summary>
+        /// The first value of the enum.
+        /// </summary>
+        Value1,
+
+        /// <summary>
+        /// The second value of the enum.
+        /// </summary>
+        Value2,
+    }
+}
