0338d70667975c8c604228e4de6e924a4ca28146
Shawn Kendrot <skendrot@hotmail.com>
2020-03-10 12:06:29 -0600

    Add header to file

--- Microsoft.Toolkit.Uwp.UI.Controls.Layout/WrapLayout/UvMeasure.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Layout/WrapLayout/UvMeasure.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.UI.Xaml.Controls;
 
 namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
@@ -33,6 +37,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             }
 
             return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
