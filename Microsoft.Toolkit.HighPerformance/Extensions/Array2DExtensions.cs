10d3d1d161974f7a23800555fe7e4a0f0c5c55d5
Sergio Pedri <sergio0694@live.com>
2020-03-25 18:04:34 +0100

    Added T[,].AsSpan extension

--- Microsoft.Toolkit.HighPerformance/Extensions/Array2DExtensions.cs
+++ Microsoft.Toolkit.HighPerformance/Extensions/Array2DExtensions.cs
@@ -81,5 +81,34 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
 #pragma warning restore CS0649
 #pragma warning restore SA1401
         }
+
+#if NETSTANDARD2_1
+        /// <summary>
+        /// Cretes a new <see cref="Span{T}"/> over an input 2D <typeparamref name="T"/> array.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the input 2D <typeparamref name="T"/> array instance.</typeparam>
+        /// <param name="array">The input 2D <typeparamref name="T"/> array instance.</param>
+        /// <returns>A <see cref="Span{T}"/> instance with the values of <paramref name="array"/>.</returns>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Span<T> AsSpan<T>(this T[,] array)
+        {
+            var arrayData = Unsafe.As<RawArray2DData>(array);
+
+            /* On x64, the length is padded to x64, but it is represented in memory
+             * as two sequential uint fields (one of which is padding).
+             * So we can just reinterpret a reference to the IntPtr as one of type
+             * uint, to access the first 4 bytes of that field, regardless of whether
+             * we're running in a 32 or 64 bit process. This will work when on little
+             * endian systems as well, as the memory layout for fields is the same,
+             * the only difference is the order of bytes within each field of a given type.
+             * We use checked here to follow suit with the CoreCLR source, where an
+             * invalid value here should fail to perform the cast and throw an exception. */
+            int length = checked((int)Unsafe.As<IntPtr, uint>(ref arrayData.Length));
+            ref T r0 = ref Unsafe.As<byte, T>(ref arrayData.Data);
+
+            return MemoryMarshal.CreateSpan(ref r0, length);
+        }
+#endif
     }
 }
