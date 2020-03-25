a3bea61a8454cd345d8d1b983fe1cf209e19580e
hhchaos <HHChaos@outlook.com>
2020-03-25 14:41:09 +0800

    Update Microsoft.Toolkit.Uwp.UI.Controls/ImageCropper/ImageCropperCroppedRegionChangedEventArgs.cs
    Co-Authored-By: Michael Hawker MSFT (XAML Llama) <michael.hawker@outlook.com>

--- Microsoft.Toolkit.Uwp.UI.Controls/ImageCropper/ImageCropperCroppedRegionChangedEventArgs.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/ImageCropper/ImageCropperCroppedRegionChangedEventArgs.cs
@@ -7,7 +7,7 @@ using Windows.Foundation;
 namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
     /// <summary>
-    /// Provides event data for the CropChanged event.
+    ///为< 请参阅cref = “ ImageCropper.CroppedRegionChanged ” />事件提供事件数据。 
     /// </summary>
     public sealed class ImageCropperCroppedRegionChangedEventArgs
     {
