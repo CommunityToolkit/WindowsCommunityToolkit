a18b783bc55295e52d17e32f3fb1430953796bdd
Sergio Pedri <sergio0694@live.com>
2020-03-27 14:58:43 +0100

    Minor style tweaks

--- Microsoft.Toolkit.HighPerformance/NullableReadOnlyByReference{T}.cs
+++ Microsoft.Toolkit.HighPerformance/NullableReadOnlyByReference{T}.cs
@@ -2,12 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NETSTANDARD2_1
+
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-#if NETSTANDARD2_1
 
 namespace Microsoft.Toolkit.HighPerformance
 {
