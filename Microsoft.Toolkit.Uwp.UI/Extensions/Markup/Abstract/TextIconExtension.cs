1a524b6c02a51315a7deb0d5a0c94ac5e0ae25b7
Sergio Pedri <sergio0694@live.com>
2020-04-04 13:23:51 +0200

    Code refactoring

---
+++ Microsoft.Toolkit.Uwp.UI/Extensions/Markup/Abstract/TextIconExtension.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.UI.Text;
+using Windows.UI.Xaml.Markup;
+
+namespace Microsoft.Toolkit.Uwp.UI.Extensions
+{
+    /// <summary>
+    /// An abstract <see cref="MarkupExtension"/> which to produce text-based icons.
+    /// </summary>
+    /// <typeparam name="T">The type representing the glyph for the current icon.</typeparam>
+    public abstract class TextIconExtension<T> : MarkupExtension
+    {
+        /// <summary>
+        /// Gets or sets the <typeparamref name="T"/> value representing the icon to display.
+        /// </summary>
+        public T Glyph { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the icon to display.
+        /// </summary>
+        public double FontSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the thickness of the icon glyph.
+        /// </summary>
+        public FontWeight FontWeight { get; set; } = FontWeights.Normal;
+
+        /// <summary>
+        /// Gets or sets the font style for the icon glyph.
+        /// </summary>
+        public FontStyle FontStyle { get; set; } = FontStyle.Normal;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether automatic text enlargement, to reflect the system text size setting, is enabled.
+        /// </summary>
+        public bool IsTextScaleFactorEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the icon is mirrored when the flow direction is right to left.
+        /// </summary>
+        public bool MirroredWhenRightToLeft { get; set; }
+    }
+}
--- Microsoft.Toolkit.Uwp.UI/Extensions/Markup/FontIconSourceExtension.cs
+++ Microsoft.Toolkit.Uwp.UI/Extensions/Markup/FontIconSourceExtension.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Windows.UI.Text;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
@@ -13,42 +12,12 @@ namespace Microsoft.Toolkit.Uwp.UI.Extensions
     /// Custom <see cref="MarkupExtension"/> which can provide <see cref="FontIconSource"/> values.
     /// </summary>
     [MarkupExtensionReturnType(ReturnType = typeof(FontIconSource))]
-    public class FontIconSourceExtension : MarkupExtension
+    public class FontIconSourceExtension : TextIconExtension<string>
     {
-        /// <summary>
-        /// Gets or sets the <see cref="string"/> representing the icon to display.
-        /// </summary>
-        public string Glyph { get; set; }
-
-        /// <summary>
-        /// Gets or sets the size of the icon to display.
-        /// </summary>
-        public double FontSize { get; set; }
-
         /// <summary>
         /// Gets or sets the font family to use to display the icon. If <see langword="null"/>, "Segoe MDL2 Assets" will be used.
         /// </summary>
         public FontFamily FontFamily { get; set; }
-
-        /// <summary>
-        /// Gets or sets the thickness of the icon glyph.
-        /// </summary>
-        public FontWeight FontWeight { get; set; } = FontWeights.Normal;
-
-        /// <summary>
-        /// Gets or sets the font style for the icon glyph.
-        /// </summary>
-        public FontStyle FontStyle { get; set; } = FontStyle.Normal;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether automatic text enlargement, to reflect the system text size setting, is enabled.
-        /// </summary>
-        public bool IsTextScaleFactorEnabled { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether the icon is mirrored when the flow direction is right to left.
-        /// </summary>
-        public bool MirroredWhenRightToLeft { get; set; }
 
         /// <inheritdoc/>
         protected override object ProvideValue()
