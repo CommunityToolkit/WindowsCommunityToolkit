7752b114fe2672c7419b784c293c86afe7290ece
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2020-04-09 10:27:45 -0700

    Improved docs for StringExtensions' UIContext param.

--- Microsoft.Toolkit.Uwp/Extensions/StringExtensions.cs
+++ Microsoft.Toolkit.Uwp/Extensions/StringExtensions.cs
@@ -4,6 +4,8 @@
 
 using Windows.ApplicationModel.Resources;
 using Windows.UI;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
 
 namespace Microsoft.Toolkit.Uwp.Extensions
 {
@@ -18,7 +20,8 @@ namespace Microsoft.Toolkit.Uwp.Extensions
         /// Retrieves the provided resource for the current view context.
         /// </summary>
         /// <param name="resourceKey">Resource key to retrieve.</param>
-        /// <param name="uiContext">UIContext to use </param>
+        /// <param name="uiContext"><see cref="UIContext"/> to be used to get the <paramref name="resourceKey"/> from.
+        /// You can retrieve this from a <see cref="UIElement.UIContext"/>, <see cref="XamlRoot.UIContext"/> (XamlIslands), or <see cref="Window.UIContext"/>.</param>
         /// <returns>string value for given resource or empty string if not found.</returns>
         public static string GetViewLocalized(this string resourceKey, UIContext uiContext = null)
         {
@@ -37,7 +40,8 @@ namespace Microsoft.Toolkit.Uwp.Extensions
         /// Retrieves the provided resource for the given key for use independent of the UI thread.
         /// </summary>
         /// <param name="resourceKey">Resource key to retrieve.</param>
-        /// <param name="uiContext">UIContext to use </param>
+        /// <param name="uiContext"><see cref="UIContext"/> to be used to get the <paramref name="resourceKey"/> from.
+        /// You can retrieve this from a <see cref="UIElement.UIContext"/>, <see cref="XamlRoot.UIContext"/> (XamlIslands), or <see cref="Window.UIContext"/>.</param>
         /// <returns>string value for given resource or empty string if not found.</returns>
         public static string GetLocalized(this string resourceKey, UIContext uiContext = null)
         {
