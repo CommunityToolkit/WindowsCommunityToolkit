f93ae4f60845d2ab0d5eb36401181a5395f0f9cb
Sergio Pedri <sergio0694@live.com>
2020-04-13 19:38:54 +0200

    Added the RelayCommand<T> type

---
+++ Microsoft.Toolkit.Mvvm/RelayCommand{T}.cs
@@ -0,0 +1,118 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma warning disable SA1512
+
+// <copyright file="RelayCommand.cs" company="GalaSoft Laurent Bugnion">
+// Copyright © GalaSoft Laurent Bugnion 2009-2016
+// </copyright>
+// ****************************************************************************
+// <author>Laurent Bugnion</author>
+// <email>laurent@galasoft.ch</email>
+// <date>22.4.2009</date>
+// <project>GalaSoft.MvvmLight</project>
+// <web>http://www.mvvmlight.net</web>
+// <license>
+// See license.txt in this project or http://www.galasoft.ch/license_MIT.txt
+// </license>
+// ****************************************************************************
+// <credits>This class was developed by Josh Smith (http://joshsmithonwpf.wordpress.com) and
+// slightly modified with his permission.</credits>
+// ****************************************************************************
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Microsoft.Toolkit.Mvvm
+{
+    /// <summary>
+    /// A generic command whose sole purpose is to relay its functionality to other
+    /// objects by invoking delegates. The default return value for the CanExecute
+    /// method is <see langword="true"/>. This class allows you to accept command parameters
+    /// in the <see cref="Execute(T)"/> and <see cref="CanExecute(T)"/> callback methods.
+    /// </summary>
+    /// <typeparam name="T">The type of parameter being passed as input to the callbacks.</typeparam>
+    public sealed class RelayCommand<T> : ICommand
+    {
+        /// <summary>
+        /// The <see cref="Action"/> to invoke when <see cref="Execute(T)"/> is used.
+        /// </summary>
+        private readonly Action<T> execute;
+
+        /// <summary>
+        /// The optional action to invoke when <see cref="CanExecute(T)"/> is used.
+        /// </summary>
+        private readonly Func<T, bool>? canExecute;
+
+        /// <inheritdoc/>
+        public event EventHandler? CanExecuteChanged;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand{T}"/> class that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand{T}"/> class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Func<T, bool>? canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="CanExecuteChanged" /> event.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Provides a strongly-typed variant of <see cref="CanExecute(object)"/>.
+        /// </summary>
+        /// <param name="parameter">The input parameter.</param>
+        /// <returns>Whether or not the current command can be executed.</returns>
+        /// <remarks>Use this overload to avoid boxing, if <typeparamref name="T"/> is a value type.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool CanExecute(T parameter)
+        {
+            return this.canExecute?.Invoke(parameter) != false;
+        }
+
+        /// <inheritdoc/>
+        public bool CanExecute(object parameter)
+        {
+            return CanExecute((T)parameter);
+        }
+
+        /// <summary>
+        /// Provides a strongly-typed variant of <see cref="Execute(object)"/>.
+        /// </summary>
+        /// <param name="parameter">The input parameter.</param>
+        /// <remarks>Use this overload to avoid boxing, if <typeparamref name="T"/> is a value type.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Execute(T parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                this.execute(parameter);
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Execute(object parameter)
+        {
+            Execute((T)parameter);
+        }
+    }
+}
