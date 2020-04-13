47f1373ee4017e334af390ffd3adf603d98fe44a
Sergio Pedri <sergio0694@live.com>
2020-04-13 18:38:24 +0200

    Added ObservableObject class

---
+++ Microsoft.Toolkit.Mvvm/ObservableObject.cs
@@ -0,0 +1,86 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// Original file header:
+// ****************************************************************************
+// <copyright file="ObservableObject.cs" company="GalaSoft Laurent Bugnion">
+// Copyright © GalaSoft Laurent Bugnion 2011-2016
+// </copyright>
+// ****************************************************************************
+// <author>Laurent Bugnion</author>
+// <email>laurent@galasoft.ch</email>
+// <date>10.4.2011</date>
+// <project>GalaSoft.MvvmLight.Messaging</project>
+// <web>http://www.mvvmlight.net</web>
+// <license>
+// See license.txt in this project or http://www.galasoft.ch/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Toolkit.Mvvm
+{
+    /// <summary>
+    /// A base class for objects of which the properties must be observable.
+    /// </summary>
+    public abstract class ObservableObject : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        /// <inheritdoc cref="INotifyPropertyChanged.PropertyChanged"/>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <inheritdoc cref="INotifyPropertyChanging.PropertyChanging"/>
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged"/> event if needed.
+        /// </summary>
+        /// <param name="propertyName">(optional) The name of the property that changed.</param>
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChanging"/> event if needed.
+        /// </summary>
+        /// <param name="propertyName">(optional) The name of the property that changed.</param>
+        protected virtual void RaisePropertyChanging([CallerMemberName] string propertyName = null!)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Compares the current and new values for a given property. If the value has changed,
+        /// raises the <see cref="PropertyChanging"/> event, updates the property with the new
+        /// value, then raises the <see cref="PropertyChanged"/> event.
+        /// </summary>
+        /// <typeparam name="T">The type of the property that changed.</typeparam>
+        /// <param name="field">The field storing the property's value.</param>
+        /// <param name="newValue">The property's value after the change occurred.</param>
+        /// <param name="propertyName">(optional) The name of the property that changed.</param>
+        /// <returns><see langword="true"/> if the property was changed, <see langword="false"/> otherwise.</returns>
+        /// <remarks>
+        /// The <see cref="PropertyChanging"/> and <see cref="PropertyChanged"/> events are not raised
+        /// if the current and new value for the target property are the same.
+        /// </remarks>
+        protected bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null!)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            this.RaisePropertyChanging(propertyName);
+
+            field = newValue;
+
+            this.RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+    }
+}
