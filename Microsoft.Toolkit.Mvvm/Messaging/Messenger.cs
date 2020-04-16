22aa752726a50296d31ff550cf6a2fd2d0da3ee2
Sergio Pedri <sergio0694@live.com>
2020-04-16 15:39:03 +0200

    Fixed a comment

--- Microsoft.Toolkit.Mvvm/Messaging/Messenger.cs
+++ Microsoft.Toolkit.Mvvm/Messaging/Messenger.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Toolkit.Mvvm.Messaging
         //                    |                          \____/_____[*]IDictionarySlim<Recipient, Token>
         //                    |  ____________________________/             \____
         //                    | /                                               \
-        // DictionarySlim<Recipient, DictionarySlim<TToken, Action<TMessage>>> Values
+        // DictionarySlim<Recipient, DictionarySlim<TToken, Action<TMessage>>> mapping
         //                                            /               /          /
         //                       __(T1)______________/_______________/          /
         //                      /     ____(T2)______/                          /
