53982ad32fe8d16f7912586d6688e00b4fd22050
Sergio Pedri <sergio0694@live.com>
2020-04-16 15:57:48 +0200

    Removed unnecessary using directives

--- Microsoft.Toolkit.Mvvm/Messaging/IMessenger.cs
+++ Microsoft.Toolkit.Mvvm/Messaging/IMessenger.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Buffers;
-using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 
 namespace Microsoft.Toolkit.Mvvm.Messaging
