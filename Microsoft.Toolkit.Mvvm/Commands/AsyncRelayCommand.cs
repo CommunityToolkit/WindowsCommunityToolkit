58f3c6472a4da9427165803fa17215788b202c00
Sergio Pedri <sergio0694@live.com>
2020-04-16 18:49:06 +0200

    Added AsyncRelayCommand type

---
+++ Microsoft.Toolkit.Mvvm/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,90 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Microsoft.Toolkit.Mvvm.Commands
+{
+    /// <summary>
+    /// A command that mirrors the functionality of <see cref="RelayCommand"/>, with the addition of
+    /// accepting a <see cref="Func{TResult}"/> returning a <see cref="Task{TResult}"/> as the execute
+    /// action, and providing a <see cref="ExecutionTask"/> property that notifies changes when
+    /// <see cref="Execute"/> is invoked and when the returned <see cref="Task{TResult}"/> completes.
+    /// </summary>
+    public sealed class AsyncRelayCommand : ObservableObject, ICommand
+    {
+        /// <summary>
+        /// The <see cref="Func{TResult}"/> to invoke when <see cref="Execute"/> is used.
+        /// </summary>
+        private readonly Func<Task> execute;
+
+        /// <summary>
+        /// The optional action to invoke when <see cref="CanExecute"/> is used.
+        /// </summary>
+        private readonly Func<bool>? canExecute;
+
+        /// <inheritdoc/>
+        public event EventHandler? CanExecuteChanged;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncRelayCommand"/> class that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public AsyncRelayCommand(Func<Task> execute)
+        {
+            this.execute = execute;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncRelayCommand"/> class that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public AsyncRelayCommand(Func<Task> execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        private Task? executionTask;
+
+        /// <summary>
+        /// Gets the last scheduled <see cref="Task"/>, if available.
+        /// This property notifies a change when the <see cref="Task{TResult}"/> completes.
+        /// </summary>
+        public Task? ExecutionTask
+        {
+            get => this.executionTask;
+            private set => SetAndNotifyOnCompletion(() => this.executionTask, value);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="CanExecuteChanged" /> event.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute?.Invoke() != false;
+        }
+
+        /// <inheritdoc/>
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                ExecutionTask = this.execute();
+            }
+        }
+    }
+}
