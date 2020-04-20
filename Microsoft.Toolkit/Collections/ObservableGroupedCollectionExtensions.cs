23fee05dd28e327ea962f420c8398b9dbacc523f
Vincent <zeGroumf@hotmail.com>
2020-04-21 00:07:06 +0200

    add add/remove extension methods

---
+++ Microsoft.Toolkit/Collections/ObservableGroupedCollectionExtensions.cs
@@ -0,0 +1,190 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Toolkit.Collections
+{
+    /// <summary>
+    /// The extensions methods to simplify the usage of <see cref="ObservableGroupedCollection{TKey, TValue}"/>.
+    /// </summary>
+    public static class ObservableGroupedCollectionExtensions
+    {
+        /// <summary>
+        /// Adds a key-value <see cref="ObservableGroup{TKey, TValue}"/> item into a target <see cref="ObservableGroupedCollection{TKey, TValue}"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the group key.</typeparam>
+        /// <typeparam name="TValue">The type of the items in the collection.</typeparam>
+        /// <param name="source">The source <see cref="ObservableGroupedCollection{TKey, TValue}"/> instance.</param>
+        /// <param name="key">The key to add.</param>
+        /// <param name="value">The value to add.</param>
+        /// <returns>The added <see cref="ObservableGroup{TKey, TValue}"/>.</returns>
+        public static ObservableGroup<TKey, TValue> AddGroup<TKey, TValue>(
+            this ObservableGroupedCollection<TKey, TValue> source,
+            TKey key,
+            TValue value)
+            => AddGroup(source, key, new[] { value });
+
+        /// <summary>
+        /// Adds a key-collection <see cref="ObservableGroup{TKey, TValue}"/> item into a target <see cref="ObservableGroupedCollection{TKey, TValue}"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the group key.</typeparam>
+        /// <typeparam name="TValue">The type of the items in the collection.</typeparam>
+        /// <param name="source">The source <see cref="ObservableGroupedCollection{TKey, TValue}"/> instance.</param>
+        /// <param name="key">The key to add.</param>
+        /// <param name="collection">The collection to add.</param>
+        /// <returns>The added <see cref="ObservableGroup{TKey, TValue}"/>.</returns>
+        public static ObservableGroup<TKey, TValue> AddGroup<TKey, TValue>(
+            this ObservableGroupedCollection<TKey, TValue> source,
+            TKey key,
+            params TValue[] collection)
+            => source.AddGroup(key, (IEnumerable<TValue>)collection);
+
+        /// <summary>
+        /// Adds a key-collection <see cref="ObservableGroup{TKey, TValue}"/> item into a target <see cref="ObservableGroupedCollection{TKey, TValue}"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the group key.</typeparam>
+        /// <typeparam name="TValue">The type of the items in the collection.</typeparam>
+        /// <param name="source">The source <see cref="ObservableGroupedCollection{TKey, TValue}"/> instance.</param>
+        /// <param name="key">The key to add.</param>
+        /// <param name="collection">The collection to add.</param>
+        /// <returns>The added <see cref="ObservableGroup{TKey, TValue}"/>.</returns>
+        public static ObservableGroup<TKey, TValue> AddGroup<TKey, TValue>(
+            this ObservableGroupedCollection<TKey, TValue> source,
+            TKey key,
+            IEnumerable<TValue> collection)
+        {
+            var group = new ObservableGroup<TKey, TValue>(key, collection);
+            source.Add(group);
+
+            return group;
+        }
+
+        /// <summary>
+        /// Add <paramref name="item"/> into the group with <paramref name="key"/> key.
+        /// If the group does not exist, it will be added.
+        /// If several groups exist with the same key, <paramref name="item"/> will be added to the first group.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the group key.</typeparam>
+        /// <typeparam name="TValue">The type of the items in the collection.</typeparam>
+        /// <param name="source">The source <see cref="ObservableGroupedCollection{TKey, TValue}"/> instance.</param>
+        /// <param name="key">The key to add.</param>
+        /// <param name="item">The item to add.</param>
+        /// <returns>The instance of the <see cref="ObservableGroup{TKey, TValue}"/> which will receive the value. It will either be an existing group or a new group.</returns>
+        public static ObservableGroup<TKey, TValue> AddItem<TKey, TValue>(
+            this ObservableGroupedCollection<TKey, TValue> source,
+            TKey key,
+            TValue item)
+        {
+            var existingGroup = source.FirstOrDefault(group => GroupKeyPredicate(group, key));
+            if (existingGroup is null)
+            {
+                existingGroup = new ObservableGroup<TKey, TValue>(key);
+                source.Add(existingGroup);
+            }
+
+            existingGroup.Add(item);
+            return existingGroup;
+        }
+
+        /// <summary>
+        /// Remove the first occurrence of the group with <paramref name="key"/> from the <paramref name="source"/> grouped collection.
+        /// It will not do anything if the group does not exist.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the group key.</typeparam>
+        /// <typeparam name="TValue">The type of the items in the collection.</typeparam>
+        /// <param name="source">The source <see cref="ObservableGroupedCollection{TKey, TValue}"/> instance.</param>
+        /// <param name="key">The key of the item to remove.</param>
+        public static void RemoveGroup<TKey, TValue>(
+            this ObservableGroupedCollection<TKey, TValue> source,
+            TKey key)
+        {
+            var index = 0;
+            foreach (var group in source)
+            {
+                if (GroupKeyPredicate(group, key))
+                {
+                    source.RemoveAt(index);
+                    return;
+                }
+
+                index++;
+            }
+        }
+
+        /// <summary>
+        /// Remove the first <paramref name="item"/> from the first group with <paramref name="key"/> from the <paramref name="source"/> grouped collection.
+        /// It will not do anything if the group or the item does not exist.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the group key.</typeparam>
+        /// <typeparam name="TValue">The type of the items in the collection.</typeparam>
+        /// <param name="source">The source <see cref="ObservableGroupedCollection{TKey, TValue}"/> instance.</param>
+        /// <param name="key">The key of the item to remove.</param>
+        /// <param name="item">The item to remove.</param>
+        /// <param name="removeGroupIfEmpty">If true (default value), the group will be removed once it becomes empty.</param>
+        public static void RemoveItem<TKey, TValue>(
+            this ObservableGroupedCollection<TKey, TValue> source,
+            TKey key,
+            TValue item,
+            bool removeGroupIfEmpty = true)
+        {
+            var index = 0;
+            foreach (var group in source)
+            {
+                if (GroupKeyPredicate(group, key))
+                {
+                    group.Remove(item);
+
+                    if (removeGroupIfEmpty && group.Count == 0)
+                    {
+                        source.RemoveAt(index);
+                    }
+
+                    return;
+                }
+
+                index++;
+            }
+        }
+
+        /// <summary>
+        /// Remove the item at <paramref name="index"/> from the first group with <paramref name="key"/> from the <paramref name="source"/> grouped collection.
+        /// It will not do anything if the group or the item does not exist.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the group key.</typeparam>
+        /// <typeparam name="TValue">The type of the items in the collection.</typeparam>
+        /// <param name="source">The source <see cref="ObservableGroupedCollection{TKey, TValue}"/> instance.</param>
+        /// <param name="key">The key of the item to remove.</param>
+        /// <param name="index">The index of the item to remove in the group.</param>
+        /// <param name="removeGroupIfEmpty">If true (default value), the group will be removed once it becomes empty.</param>
+        public static void RemoveItemAt<TKey, TValue>(
+            this ObservableGroupedCollection<TKey, TValue> source,
+            TKey key,
+            int index,
+            bool removeGroupIfEmpty = true)
+        {
+            var groupIndex = 0;
+            foreach (var group in source)
+            {
+                if (GroupKeyPredicate(group, key))
+                {
+                    group.RemoveAt(index);
+
+                    if (removeGroupIfEmpty && group.Count == 0)
+                    {
+                        source.RemoveAt(groupIndex);
+                    }
+
+                    return;
+                }
+
+                groupIndex++;
+            }
+        }
+
+        private static bool GroupKeyPredicate<TKey, TValue>(ObservableGroup<TKey, TValue> group, TKey expectedKey)
+            => EqualityComparer<TKey>.Default.Equals(group.Key, expectedKey);
+    }
+}
---
+++ UnitTests/Collections/ObservableGroupedCollectionExtensionsTests.cs
@@ -0,0 +1,290 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using FluentAssertions;
+using Microsoft.Toolkit.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace UnitTests.Collections
+{
+    [TestClass]
+    public class ObservableGroupedCollectionExtensionsTests
+    {
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void AddGroup_WithItem_ShouldAddGroup()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+
+            var addedGroup = groupedCollection.AddGroup("new key", 23);
+
+            addedGroup.Should().NotBeNull();
+            addedGroup.Key.Should().Be("new key");
+            addedGroup.Should().ContainSingle();
+            addedGroup.Should().ContainInOrder(23);
+
+            groupedCollection.Should().ContainSingle();
+            groupedCollection.Should().HaveElementAt(0, addedGroup);
+        }
+
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void AddGroup_WithCollection_ShouldAddGroup()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+
+            var addedGroup = groupedCollection.AddGroup("new key", new[] { 23, 10, 42 });
+
+            addedGroup.Should().NotBeNull();
+            addedGroup.Key.Should().Be("new key");
+            addedGroup.Should().HaveCount(3);
+            addedGroup.Should().ContainInOrder(23, 10, 42);
+
+            groupedCollection.Should().ContainSingle();
+            groupedCollection.Should().HaveElementAt(0, addedGroup);
+        }
+
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void AddGroup_WithParamsCollection_ShouldAddGroup()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+
+            var addedGroup = groupedCollection.AddGroup("new key", 23, 10, 42);
+
+            addedGroup.Should().NotBeNull();
+            addedGroup.Key.Should().Be("new key");
+            addedGroup.Should().HaveCount(3);
+            addedGroup.Should().ContainInOrder(23, 10, 42);
+
+            groupedCollection.Should().ContainSingle();
+            groupedCollection.Should().HaveElementAt(0, addedGroup);
+        }
+
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void AddItem_WhenTargetGroupDoesNotExists_ShouldCreateAndAddNewGroup()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+
+            var addedGroup = groupedCollection.AddItem("new key", 23);
+
+            addedGroup.Should().NotBeNull();
+            addedGroup.Key.Should().Be("new key");
+            addedGroup.Should().ContainSingle();
+            addedGroup.Should().ContainInOrder(23);
+
+            groupedCollection.Should().ContainSingle();
+            groupedCollection.Should().HaveElementAt(0, addedGroup);
+        }
+
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void AddItem_WhenSingleTargetGroupAlreadyExists_ShouldAddItemToExistingGroup()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+            var targetGroup = groupedCollection.AddGroup("B", 4, 5, 6);
+            groupedCollection.AddGroup("C", 7, 8);
+
+            var addedGroup = groupedCollection.AddItem("B", 23);
+
+            addedGroup.Should().BeSameAs(targetGroup);
+            addedGroup.Key.Should().Be("B");
+            addedGroup.Should().HaveCount(4);
+            addedGroup.Should().ContainInOrder(4, 5, 6, 23);
+
+            groupedCollection.Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+
+            groupedCollection.ElementAt(1).Key.Should().Be("B");
+            groupedCollection.ElementAt(1).Should().HaveCount(4);
+            groupedCollection.ElementAt(1).Should().ContainInOrder(4, 5, 6, 23);
+
+            groupedCollection.ElementAt(2).Key.Should().Be("C");
+            groupedCollection.ElementAt(2).Should().HaveCount(2);
+            groupedCollection.ElementAt(2).Should().ContainInOrder(7, 8);
+        }
+
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void AddItem_WhenSeveralTargetGroupsAlreadyExist_ShouldAddItemToFirstExistingGroup()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+            var targetGroup = groupedCollection.AddGroup("B", 4, 5, 6);
+            groupedCollection.AddGroup("B", 7, 8, 9);
+            groupedCollection.AddGroup("C", 10, 11);
+
+            var addedGroup = groupedCollection.AddItem("B", 23);
+
+            addedGroup.Should().BeSameAs(targetGroup);
+            addedGroup.Key.Should().Be("B");
+            addedGroup.Should().HaveCount(4);
+            addedGroup.Should().ContainInOrder(4, 5, 6, 23);
+
+            groupedCollection.Should().HaveCount(4);
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+
+            groupedCollection.ElementAt(1).Key.Should().Be("B");
+            groupedCollection.ElementAt(1).Should().HaveCount(4);
+            groupedCollection.ElementAt(1).Should().ContainInOrder(4, 5, 6, 23);
+
+            groupedCollection.ElementAt(2).Key.Should().Be("B");
+            groupedCollection.ElementAt(2).Should().HaveCount(3);
+            groupedCollection.ElementAt(2).Should().ContainInOrder(7, 8, 9);
+
+            groupedCollection.ElementAt(3).Key.Should().Be("C");
+            groupedCollection.ElementAt(3).Should().HaveCount(2);
+            groupedCollection.ElementAt(3).Should().ContainInOrder(10, 11);
+        }
+
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void RemoveGroup_WhenGroupDoesNotExists_ShouldDoNothing()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+
+            groupedCollection.RemoveGroup("I do not exist");
+
+            groupedCollection.Should().ContainSingle();
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+        }
+
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void RemoveGroup_WhenSingleGroupExists_ShouldRemoveGroup()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+            groupedCollection.AddGroup("B", 4, 5, 6);
+
+            groupedCollection.RemoveGroup("B");
+
+            groupedCollection.Should().ContainSingle();
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+        }
+
+        [TestCategory("Collections")]
+        [TestMethod]
+        public void RemoveGroup_WhenSeveralGroupsExist_ShouldRemoveFirstGroup()
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+            groupedCollection.AddGroup("B", 4, 5, 6);
+            groupedCollection.AddGroup("B", 7, 8);
+
+            groupedCollection.RemoveGroup("B");
+
+            groupedCollection.Should().HaveCount(2);
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+
+            groupedCollection.ElementAt(1).Key.Should().Be("B");
+            groupedCollection.ElementAt(1).Should().HaveCount(2);
+            groupedCollection.ElementAt(1).Should().ContainInOrder(7, 8);
+        }
+
+        [TestCategory("Collections")]
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void RemoveItem_WhenGroupDoesNotExist_ShouldDoNothing(bool removeGroupIfEmpty)
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+            groupedCollection.AddGroup("B", 4, 5, 6);
+
+            groupedCollection.RemoveItem("I do not exist", 8, removeGroupIfEmpty);
+
+            groupedCollection.Should().HaveCount(2);
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+
+            groupedCollection.ElementAt(1).Key.Should().Be("B");
+            groupedCollection.ElementAt(1).Should().HaveCount(3);
+            groupedCollection.ElementAt(1).Should().ContainInOrder(4, 5, 6);
+        }
+
+        [TestCategory("Collections")]
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void RemoveItem_WhenGroupExistsAndItemDoesNotExist_ShouldDoNothing(bool removeGroupIfEmpty)
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+            groupedCollection.AddGroup("B", 4, 5, 6);
+
+            groupedCollection.RemoveItem("B", 8, removeGroupIfEmpty);
+
+            groupedCollection.Should().HaveCount(2);
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+
+            groupedCollection.ElementAt(1).Key.Should().Be("B");
+            groupedCollection.ElementAt(1).Should().HaveCount(3);
+            groupedCollection.ElementAt(1).Should().ContainInOrder(4, 5, 6);
+        }
+
+        [TestCategory("Collections")]
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void RemoveItem_WhenGroupAndItemExist_ShouldRemoveItemFromGroup(bool removeGroupIfEmpty)
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+            groupedCollection.AddGroup("B", 4, 5, 6);
+
+            groupedCollection.RemoveItem("B", 5, removeGroupIfEmpty);
+
+            groupedCollection.Should().HaveCount(2);
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+
+            groupedCollection.ElementAt(1).Key.Should().Be("B");
+            groupedCollection.ElementAt(1).Should().HaveCount(2);
+            groupedCollection.ElementAt(1).Should().ContainInOrder(4, 6);
+        }
+
+        [TestCategory("Collections")]
+        [DataTestMethod]
+        [DataRow(true, true)]
+        [DataRow(false, false)]
+        public void RemoveItem_WhenRemovingLastItem_ShouldRemoveGroupIfRequired(bool removeGroupIfEmpty, bool expectGroupRemoved)
+        {
+            var groupedCollection = new ObservableGroupedCollection<string, int>();
+            groupedCollection.AddGroup("A", 1, 2, 3);
+            groupedCollection.AddGroup("B", 4);
+
+            groupedCollection.RemoveItem("B", 4, removeGroupIfEmpty);
+
+            groupedCollection.Should().HaveCount(expectGroupRemoved ? 1 : 2);
+            groupedCollection.ElementAt(0).Key.Should().Be("A");
+            groupedCollection.ElementAt(0).Should().HaveCount(3);
+            groupedCollection.ElementAt(0).Should().ContainInOrder(1, 2, 3);
+
+            if (!expectGroupRemoved)
+            {
+                groupedCollection.ElementAt(1).Key.Should().Be("B");
+                groupedCollection.ElementAt(1).Should().BeEmpty();
+            }
+        }
+    }
+}
