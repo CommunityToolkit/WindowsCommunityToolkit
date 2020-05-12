a633fb4bada4cd65ab0f9c2804001373660f9acf
Sergio Pedri <sergio0694@live.com>
2020-05-12 13:30:23 +0200

    Added example to ScrollViewerExtensions sample page

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ScrollViewerExtensions/ScrollViewerExtensionsPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ScrollViewerExtensions/ScrollViewerExtensionsPage.xaml.cs
@@ -4,11 +4,10 @@
 
 using System.Collections.ObjectModel;
 using Microsoft.Toolkit.Uwp.SampleApp.Models;
-using Microsoft.Toolkit.Uwp.UI.Controls;
 using Microsoft.Toolkit.Uwp.UI.Extensions;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.UI.Enums;
 
 namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
@@ -35,6 +34,14 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             if (listView != null)
             {
                 listView.ItemsSource = _items;
+
+                var shapesPanel = control.FindChildByName("shapesPanel") as StackPanel;
+                if (shapesPanel != null)
+                {
+                    var listScrollViewer = listView.FindDescendant<ScrollViewer>();
+
+                    listScrollViewer?.StartExpressionAnimation(shapesPanel, Axis.Y);
+                }
             }
         }
     }
