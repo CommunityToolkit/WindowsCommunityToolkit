a86d1c5b4a136eb2beaeb5c8179d362c904d37bb
Sergio Pedri <sergio0694@live.com>
2020-05-14 13:55:06 +0200

    Fixed file headers

--- Microsoft.Toolkit.Mvvm/ComponentModel/ObservableObject.cs
+++ Microsoft.Toolkit.Mvvm/ComponentModel/ObservableObject.cs
@@ -1,6 +1,9 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
+
+#pragma warning disable SA1512
+
 // This file is inspired from the MvvmLight libray (lbugnion/mvvmlight),
 // more info in ThirdPartyNotices.txt in the root of the project.
 
