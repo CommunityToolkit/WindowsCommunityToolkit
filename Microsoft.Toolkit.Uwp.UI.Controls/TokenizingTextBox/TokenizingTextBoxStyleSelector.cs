bfe9c817eeee6cf5dc8556d88609d32a6d63ab1e
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2020-05-15 00:33:23 -0700

    Add comments to StyleSelector class
    Remove unused DataTemplate Selector

--- Microsoft.Toolkit.Uwp.UI.Controls/TokenizingTextBox/TokenizingTextBoxStyleSelector.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/TokenizingTextBox/TokenizingTextBoxStyleSelector.cs
@@ -7,16 +7,29 @@ using Windows.UI.Xaml.Controls;
 
 namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
+    /// <summary>
+    /// <see cref="StyleSelector"/> used by <see cref="TokenizingTextBox"/> to choose the proper <see cref="TokenizingTextBoxItem"/> container style (text entry or token).
+    /// </summary>
     public class TokenizingTextBoxStyleSelector : StyleSelector
     {
+        /// <summary>
+        /// Gets or sets the <see cref="Style"/> of a token item.
+        /// </summary>
         public Style TokenStyle { get; set; }
 
+        /// <summary>
+        /// Gets or sets the <see cref="Style"/> of a text entry item.
+        /// </summary>
         public Style TextStyle { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TokenizingTextBoxStyleSelector"/> class.
+        /// </summary>
         public TokenizingTextBoxStyleSelector()
         {
         }
 
+        /// <inheritdoc/>
         protected override Style SelectStyleCore(object item, DependencyObject container)
         {
             if (item is PretokenStringContainer)
@@ -25,8 +38,6 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             }
 
             return TokenStyle;
-
-            ////return base.SelectStyleCore(item, container);
         }
     }
 }
