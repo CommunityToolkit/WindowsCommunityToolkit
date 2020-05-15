d5bfc9f7f391056bc1695316aa1be539381f42e8
Morten Nielsen <1378165+dotMorten@users.noreply.github.com>
2020-05-15 13:20:44 -0700

    Initial port of WindowsStateTriggers to the UWP Toolkit. (#3030)
    * Initial port of WindowsStateTriggers to the UWP Toolkit.
    
    * Weak listener cleanup
    
    * Undo line
    
    * Added IsNullOrEmpty State Trigger sample
    
    * Fix inconsistent naming of triggers
    
    * Added EqualsStateTrigger and CompareStateTrigger samples
    
    * Added remaining samples
    
    * Fix regex expression so it won't pick up the wrong expressions (RegexStateTrigger sample was affected by this)
    
    * Fix full-screen toggle code
    
    * Remove InputTypeStateTrigger
    
    * Update based on code-review comments
    
    * Fix comparison to handle => and =< better
    
    * Add `s` to name to match `EqualsStateTrigger`
    
    * Add sample for <= and >= and fixes for comparison
    
    * Renamed equal triggers to Is...
    
    * Clean-up StyleCop Issues and fixed sample with Trigger PR
    
    Code was accidently removed from all samples including the original!
    Add comment about E-mail RegEx helper in Toolkit
    
    Co-authored-by: Alexandre Zollinger Chohfi <alzollin@microsoft.com>
    Co-authored-by: Kyaa-dost <35208324+Kyaa-dost@users.noreply.github.com>
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <michael.hawker@outlook.com>
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>

---
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Triggers/IsNullOrEmptyStateTriggerPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Toolkit.Uwp.UI.Extensions;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class IsNullOrEmptyStateTriggerPage : Page, IXamlRenderListener
+    {
+        private Button _addButton;
+        private Button _removeButton;
+        private ListBox _listBox;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IsNullOrEmptyStateTriggerPage"/> class.
+        /// </summary>
+        public IsNullOrEmptyStateTriggerPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnXamlRendered(FrameworkElement control)
+        {
+            if (_addButton != null)
+            {
+                _addButton.Click -= this.AddButton_Click;
+            }
+
+            if (control.FindDescendantByName("AddButton") is Button btn)
+            {
+                _addButton = btn;
+
+                _addButton.Click += this.AddButton_Click;
+            }
+
+            if (_removeButton != null)
+            {
+                _removeButton.Click -= this.RemoveButton_Click;
+            }
+
+            if (control.FindDescendantByName("RemoveButton") is Button btn2)
+            {
+                _removeButton = btn2;
+
+                _removeButton.Click += this.RemoveButton_Click;
+            }
+
+            _listBox = control.FindDescendantByName("OurList") as ListBox;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_listBox != null)
+            {
+                _listBox.Items.Add("Item");
+            }
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_listBox != null)
+            {
+                _listBox.Items.RemoveAt(0);
+            }
+        }
+    }
+}
