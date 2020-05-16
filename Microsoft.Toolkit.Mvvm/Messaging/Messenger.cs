93c653e602f80e77313ce57a2d81a690bccf8784
Sergio Pedri <sergio0694@live.com>
2020-05-16 23:48:40 +0200

    Removed original Unit type

--- Microsoft.Toolkit.Mvvm/Messaging/Messenger.cs
+++ Microsoft.Toolkit.Mvvm/Messaging/Messenger.cs
@@ -599,32 +599,6 @@ namespace Microsoft.Toolkit.Mvvm.Messaging
         }
 
         /// <summary>
-        /// An empty type representing a generic token with no specific value.
-        /// </summary>
-        internal struct Unit : IEquatable<Unit>
-        {
-            /// <inheritdoc/>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public bool Equals(Unit other)
-            {
-                return true;
-            }
-
-            /// <inheritdoc/>
-            public override bool Equals(object? obj)
-            {
-                return obj is Unit;
-            }
-
-            /// <inheritdoc/>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public override int GetHashCode()
-            {
-                return 0;
-            }
-        }
-
-        /// <summary>
         /// Throws an <see cref="InvalidOperationException"/> when trying to add a duplicate handler.
         /// </summary>
         [MethodImpl(MethodImplOptions.NoInlining)]
