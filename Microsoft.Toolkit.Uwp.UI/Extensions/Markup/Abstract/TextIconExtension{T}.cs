c51e301993f9350ff9df6f6a79f73773f89bf578
Sergio Pedri <sergio0694@live.com>
2020-05-16 11:29:25 +0200

    FontIcon markup extension (#3110)
    * Added FontIcon markup extension
    
    * Removed unnecessary using directives
    
    * Renamed the FontIcon markup extension
    
    * Added FontIcon tests
    
    * Fixed a typo in the test XAML
    
    * Update UnitTests/Extensions/Test_FontIconExtensionMarkupExtension.cs
    
    Co-Authored-By: Michael Hawker MSFT (XAML Llama) <michael.hawker@outlook.com>
    
    * Update UnitTests/Extensions/Test_FontIconExtensionMarkupExtension.cs
    
    Co-Authored-By: Michael Hawker MSFT (XAML Llama) <michael.hawker@outlook.com>
    
    * Update UnitTests/Extensions/Test_FontIconExtensionMarkupExtension.cs
    
    Co-Authored-By: Michael Hawker MSFT (XAML Llama) <michael.hawker@outlook.com>
    
    * Added new FontIcon properties
    
    * Added more tests for the new properties
    
    * Added FontIconSourceExtension
    
    * Added BitmapIconExtension
    
    * Added tests for the FontIconSource extension
    
    * Fixed namespace for BitmapIconExtension type
    
    * Added tests for BitmapIconExtension type
    
    * Removed [Bindable] attribute from markup extensions
    
    See #3191 for more info about this
    
    * Added a sample page for the FontIconExtension type
    
    * Added sample for the BitmapIcon extension
    
    * Added FontIconSource sample page (not loading)
    
    * Minor code style tweaks, also tests work now ¯\_(ツ)_/¯
    
    * Added shallow copy for icon markup extensions
    
    * Fixed a missing resource in the icon markup sample page
    
    * Code refactoring
    
    * Updated samples page
    
    * Code refactoring, minor optimizations
    
    * Added SymbolIconSourceExtension type
    
    * Added SymbolIconSourceExtension sample
    
    * Added foreground property to icon extensions
    
    * Fixed a typo in a unit test
    
    * Fixed remaining unit tests
    
    * Removed unnecessary using directives
    
    * Fixed BitmapIcon and FontIcon extension tests
    
    * Fixed FontIconSource extension tests
    
    * Fixed build errors after merge
    
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <michael.hawker@outlook.com>
    Co-authored-by: Alexandre Zollinger Chohfi <alzollin@microsoft.com>

---
+++ Microsoft.Toolkit.Uwp.UI/Extensions/Markup/Abstract/TextIconExtension{T}.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.UI.Xaml.Markup;
+
+namespace Microsoft.Toolkit.Uwp.UI.Extensions
+{
+    /// <summary>
+    /// An abstract <see cref="MarkupExtension"/> which to produce text-based icons.
+    /// </summary>
+    /// <typeparam name="T">The type representing the glyph for the current icon.</typeparam>
+    public abstract class TextIconExtension<T> : TextIconExtension
+    {
+        /// <summary>
+        /// Gets or sets the <typeparamref name="T"/> value representing the icon to display.
+        /// </summary>
+        public T Glyph { get; set; }
+    }
+}
