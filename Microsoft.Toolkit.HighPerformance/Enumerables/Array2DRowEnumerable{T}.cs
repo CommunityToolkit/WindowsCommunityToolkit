a4cd9494db147be0aca5f4ca88e0fb570760dfc0
Sergio Pedri <sergio0694@live.com>
2020-06-11 02:15:43 +0200

    Reverted changes for 2D array enumerables
    - The one for rows is not needed from .NET Core 2.1 and above
    - The other has the extra ToArray API which needs the initial values to remain available, and the type to be readonly

--- Microsoft.Toolkit.HighPerformance/Enumerables/Array2DRowEnumerable{T}.cs
+++ Microsoft.Toolkit.HighPerformance/Enumerables/Array2DRowEnumerable{T}.cs
@@ -18,10 +18,10 @@ namespace Microsoft.Toolkit.HighPerformance.Enumerables
     /// </summary>
     /// <typeparam name="T">The type of items to enumerate.</typeparam>
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public ref struct Array2DRowEnumerable<T>
+    public readonly ref struct Array2DRowEnumerable<T>
     {
         /// <summary>
-        /// The source 2D array instance.
+        /// The source 2D <typeparamref name="T"/> array instance.
         /// </summary>
         private readonly T[,] array;
 
@@ -31,74 +31,23 @@ namespace Microsoft.Toolkit.HighPerformance.Enumerables
         private readonly int row;
 
         /// <summary>
-        /// The width of a row in <see cref="array"/>.
-        /// </summary>
-        private readonly int width;
-
-        /// <summary>
-        /// The current column.
-        /// </summary>
-        private int column;
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="Array2DRowEnumerable{T}"/> struct.
         /// </summary>
-        /// <param name="array">The source 2D array instance.</param>
+        /// <param name="array">The source 2D <typeparamref name="T"/> array instance.</param>
         /// <param name="row">The target row to iterate within <paramref name="array"/>.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Array2DRowEnumerable(T[,] array, int row)
         {
-            if ((uint)row >= (uint)array.GetLength(0))
-            {
-                ThrowArgumentOutOfRangeExceptionForInvalidRow();
-            }
-
             this.array = array;
             this.row = row;
-            this.width = array.GetLength(1);
-            this.column = -1;
         }
 
         /// <summary>
         /// Implements the duck-typed <see cref="IEnumerable{T}.GetEnumerator"/> method.
         /// </summary>
-        /// <returns>An <see cref="Array2DRowEnumerable{T}"/> instance targeting the current 2D <typeparamref name="T"/> array instance.</returns>
+        /// <returns>An <see cref="Enumerator"/> instance targeting the current 2D <typeparamref name="T"/> array instance.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public Array2DRowEnumerable<T> GetEnumerator() => this;
-
-        /// <summary>
-        /// Implements the duck-typed <see cref="System.Collections.IEnumerator.MoveNext"/> method.
-        /// </summary>
-        /// <returns><see langword="true"/> whether a new element is available, <see langword="false"/> otherwise</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool MoveNext()
-        {
-            int column = this.column + 1;
-
-            if (column < this.width)
-            {
-                this.column = column;
-
-                return true;
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// Gets the duck-typed <see cref="IEnumerator{T}.Current"/> property.
-        /// </summary>
-        public ref T Current
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get
-            {
-                // This type is never used on .NET Core runtimes, where
-                // the fast indexer is available. Therefore, we can just
-                // use the built-in indexer for 2D arrays to access the value.
-                return ref this.array[this.row, this.column];
-            }
-        }
+        public Enumerator GetEnumerator() => new Enumerator(this.array, this.row);
 
         /// <summary>
         /// Returns a <typeparamref name="T"/> array with the values in the target row.
@@ -126,6 +75,86 @@ namespace Microsoft.Toolkit.HighPerformance.Enumerables
             }
 
             return array;
+        }
+
+        /// <summary>
+        /// An enumerator for a source 2D array instance.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public ref struct Enumerator
+        {
+            /// <summary>
+            /// The source 2D array instance.
+            /// </summary>
+            private readonly T[,] array;
+
+            /// <summary>
+            /// The target row to iterate within <see cref="array"/>.
+            /// </summary>
+            private readonly int row;
+
+            /// <summary>
+            /// The width of a row in <see cref="array"/>.
+            /// </summary>
+            private readonly int width;
+
+            /// <summary>
+            /// The current column.
+            /// </summary>
+            private int column;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Enumerator"/> struct.
+            /// </summary>
+            /// <param name="array">The source 2D array instance.</param>
+            /// <param name="row">The target row to iterate within <paramref name="array"/>.</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Enumerator(T[,] array, int row)
+            {
+                if ((uint)row >= (uint)array.GetLength(0))
+                {
+                    ThrowArgumentOutOfRangeExceptionForInvalidRow();
+                }
+
+                this.array = array;
+                this.row = row;
+                this.width = array.GetLength(1);
+                this.column = -1;
+            }
+
+            /// <summary>
+            /// Implements the duck-typed <see cref="System.Collections.IEnumerator.MoveNext"/> method.
+            /// </summary>
+            /// <returns><see langword="true"/> whether a new element is available, <see langword="false"/> otherwise</returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                int column = this.column + 1;
+
+                if (column < this.width)
+                {
+                    this.column = column;
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            /// <summary>
+            /// Gets the duck-typed <see cref="IEnumerator{T}.Current"/> property.
+            /// </summary>
+            public ref T Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get
+                {
+                    // This type is never used on .NET Core runtimes, where
+                    // the fast indexer is available. Therefore, we can just
+                    // use the built-in indexer for 2D arrays to access the value.
+                    return ref this.array[this.row, this.column];
+                }
+            }
         }
 
         /// <summary>
