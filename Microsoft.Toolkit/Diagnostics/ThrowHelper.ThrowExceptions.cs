33b83ab677eb114fe11880dc09dd7598fb9fc965
Sergio Pedri <sergio0694@live.com>
2020-06-14 22:07:28 +0200

    Added more throw helper APIs

--- Microsoft.Toolkit/Diagnostics/ThrowHelper.ThrowExceptions.cs
+++ Microsoft.Toolkit/Diagnostics/ThrowHelper.ThrowExceptions.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
+using System.Threading;
 
 #nullable enable
 
@@ -15,22 +16,6 @@ namespace Microsoft.Toolkit.Diagnostics
     /// </summary>
     public static partial class ThrowHelper
     {
-        /// <summary>
-        /// Returns a formatted representation of the input value.
-        /// </summary>
-        /// <param name="obj">The input <see cref="object"/> to format.</param>
-        /// <returns>A formatted representation of <paramref name="obj"/> to display in error messages.</returns>
-        [Pure]
-        private static string ToAssertString(this object? obj)
-        {
-            return obj switch
-            {
-                string _ => $"\"{obj}\"",
-                null => "null",
-                _ => $"<{obj}>"
-            };
-        }
-
         /// <summary>
         /// Throws a new <see cref="AccessViolationException"/>.
         /// </summary>
@@ -122,6 +107,258 @@ namespace Microsoft.Toolkit.Diagnostics
         public static void ThrowArgumentOutOfRangeException(string name, object value, string message)
         {
             throw new ArgumentOutOfRangeException(name, value, message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="FormatException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="FormatException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowFormatException(string message)
+        {
+            throw new FormatException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="IndexOutOfRangeException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowIndexOutOfRangeException(string message)
+        {
+            throw new IndexOutOfRangeException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="InsufficientMemoryException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="InsufficientMemoryException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowInsufficientMemoryException(string message)
+        {
+            throw new InsufficientMemoryException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="InvalidOperationException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="InvalidOperationException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowInvalidOperationException(string message)
+        {
+            throw new InvalidOperationException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="MissingFieldException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="MissingFieldException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowMissingFieldException(string message)
+        {
+            throw new MissingFieldException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="MissingFieldException"/>.
+        /// </summary>
+        /// <param name="className">The target class being inspected.</param>
+        /// <param name="fieldName">The target field being retrieved.</param>
+        /// <exception cref="MissingFieldException">Thrown with <paramref name="className"/> and <paramref name="fieldName"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowMissingFieldException(string className, string fieldName)
+        {
+            throw new MissingFieldException(className, fieldName);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="MissingMemberException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="MissingMemberException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowMissingMemberException(string message)
+        {
+            throw new MissingMemberException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="MissingMemberException"/>.
+        /// </summary>
+        /// <param name="className">The target class being inspected.</param>
+        /// <param name="memberName">The target member being retrieved.</param>
+        /// <exception cref="MissingMemberException">Thrown with <paramref name="className"/> and <paramref name="memberName"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowMissingMemberException(string className, string memberName)
+        {
+            throw new MissingMemberException(className, memberName);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="MissingMethodException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="MissingMethodException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowMissingMethodException(string message)
+        {
+            throw new MissingMethodException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="MissingMethodException"/>.
+        /// </summary>
+        /// <param name="className">The target class being inspected.</param>
+        /// <param name="methodName">The target method being retrieved.</param>
+        /// <exception cref="MissingMethodException">Thrown with <paramref name="className"/> and <paramref name="methodName"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowMissingMethodException(string className, string methodName)
+        {
+            throw new MissingMethodException(className, methodName);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="NotSupportedException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="NotSupportedException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowNotSupportedException(string message)
+        {
+            throw new NotSupportedException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="NullReferenceException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="NullReferenceException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowNullReferenceException(string message)
+        {
+            throw new NullReferenceException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="ObjectDisposedException"/>.
+        /// </summary>
+        /// <param name="objectName">The name of the disposed object.</param>
+        /// <exception cref="ObjectDisposedException">Thrown with <paramref name="objectName"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowObjectDisposedException(string objectName)
+        {
+            throw new ObjectDisposedException(objectName);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="ObjectDisposedException"/>.
+        /// </summary>
+        /// <param name="objectName">The name of the disposed object.</param>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="ObjectDisposedException">Thrown with <paramref name="objectName"/> and <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowObjectDisposedException(string objectName, string message)
+        {
+            throw new ObjectDisposedException(objectName, message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="OperationCanceledException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="OperationCanceledException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowOperationCanceledException(string message)
+        {
+            throw new OperationCanceledException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="OperationCanceledException"/>.
+        /// </summary>
+        /// <param name="token">The <see cref="CancellationToken"/> in use.</param>
+        /// <exception cref="OperationCanceledException">Thrown with <paramref name="token"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowOperationCanceledException(CancellationToken token)
+        {
+            throw new OperationCanceledException(token);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="ObjectDisposedException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <param name="token">The <see cref="CancellationToken"/> in use.</param>
+        /// <exception cref="ObjectDisposedException">Thrown with <paramref name="message"/> and <paramref name="token"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowThrowOperationCanceledException(string message, CancellationToken token)
+        {
+            throw new OperationCanceledException(message, token);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="OutOfMemoryException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="OutOfMemoryException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowOutOfMemoryException(string message)
+        {
+            throw new OutOfMemoryException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="PlatformNotSupportedException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="PlatformNotSupportedException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowPlatformNotSupportedException(string message)
+        {
+            throw new PlatformNotSupportedException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="TimeoutException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="TimeoutException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowTimeoutException(string message)
+        {
+            throw new TimeoutException(message);
+        }
+
+        /// <summary>
+        /// Throws a new <see cref="UnauthorizedAccessException"/>.
+        /// </summary>
+        /// <param name="message">The message to include in the exception.</param>
+        /// <exception cref="UnauthorizedAccessException">Thrown with <paramref name="message"/>.</exception>
+        [DoesNotReturn]
+        public static void ThrowUnauthorizedAccessException(string message)
+        {
+            throw new UnauthorizedAccessException(message);
+        }
+
+        /// <summary>
+        /// Returns a formatted representation of the input value.
+        /// </summary>
+        /// <param name="obj">The input <see cref="object"/> to format.</param>
+        /// <returns>A formatted representation of <paramref name="obj"/> to display in error messages.</returns>
+        [Pure]
+        private static string ToAssertString(this object? obj)
+        {
+            return obj switch
+            {
+                string _ => $"\"{obj}\"",
+                null => "null",
+                _ => $"<{obj}>"
+            };
         }
     }
 }
