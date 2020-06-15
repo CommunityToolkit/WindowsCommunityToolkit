c6d95bdbea3126aea11d3d4e9ffca87bee3d3e93
Sergio Pedri <sergio0694@live.com>
2020-06-15 14:07:52 +0200

    Initial .NET 5 support to HighPerformance package

--- Microsoft.Toolkit.HighPerformance/Extensions/ArrayExtensions.2D.cs
+++ Microsoft.Toolkit.HighPerformance/Extensions/ArrayExtensions.2D.cs
@@ -32,7 +32,7 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref T DangerousGetReference<T>(this T[,] array)
         {
-#if NETCORE_RUNTIME
+#if NETCORE_RUNTIME || NETCOREAPP5_0
             var arrayData = Unsafe.As<RawArray2DData>(array);
             ref T r0 = ref Unsafe.As<byte, T>(ref arrayData.Data);
 
@@ -62,7 +62,7 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref T DangerousGetReferenceAt<T>(this T[,] array, int i, int j)
         {
-#if NETCORE_RUNTIME
+#if NETCORE_RUNTIME || NETCOREAPP5_0
             var arrayData = Unsafe.As<RawArray2DData>(array);
             nint offset = ((nint)(uint)i * (nint)(uint)arrayData.Width) + (nint)(uint)j;
             ref T r0 = ref Unsafe.As<byte, T>(ref arrayData.Data);
@@ -80,7 +80,7 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
 #endif
         }
 
-#if NETCORE_RUNTIME
+#if NETCORE_RUNTIME || NETCOREAPP5_0
         // Description adapted from CoreCLR: see https://source.dot.net/#System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.CoreCLR.cs,285.
         // CLR 2D arrays are laid out in memory as follows:
         // [ sync block || pMethodTable || Length (padded to IntPtr) || HxW || HxW bounds || array data .. ]
