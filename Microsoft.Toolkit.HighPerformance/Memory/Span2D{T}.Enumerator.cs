09c0fc4acf8eeb31671255ab13ec6f5f3d4f4dbb
Sergio Pedri <sergio0694@live.com>
2020-06-18 13:30:04 +0200

    Minor code refactoring

---
+++ Microsoft.Toolkit.HighPerformance/Memory/Span2D{T}.Enumerator.cs
@@ -0,0 +1,148 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Toolkit.HighPerformance.Memory
+{
+    /// <inheritdoc cref="Span2D{T}"/>
+    public readonly ref partial struct Span2D<T>
+    {
+        /// <summary>
+        /// Returns an enumerator for the current <see cref="Span2D{T}"/> instance.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to traverse the items in the current <see cref="Span2D{T}"/> instance
+        /// </returns>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Enumerator GetEnumerator() => new Enumerator(this);
+
+        /// <summary>
+        /// Provides an enumerator for the elements of a <see cref="Span2D{T}"/> instance.
+        /// </summary>
+        public ref struct Enumerator
+        {
+#if SPAN_RUNTIME_SUPPORT
+            /// <summary>
+            /// The <see cref="Span{T}"/> instance pointing to the first item in the target memory area.
+            /// </summary>
+            /// <remarks>Just like in <see cref="Span2D{T}"/>, the length is the height of the 2D region.</remarks>
+            private readonly Span<T> span;
+#else
+            /// <summary>
+            /// The target <see cref="object"/> instance, if present.
+            /// </summary>
+            private readonly object? instance;
+
+            /// <summary>
+            /// The initial offset within <see cref="instance"/>.
+            /// </summary>
+            private readonly IntPtr offset;
+
+            /// <summary>
+            /// The height of the specified 2D region.
+            /// </summary>
+            private readonly int height;
+#endif
+
+            /// <summary>
+            /// The width of the specified 2D region.
+            /// </summary>
+            private readonly int width;
+
+            /// <summary>
+            /// The pitch of the specified 2D region.
+            /// </summary>
+            private readonly int pitch;
+
+            /// <summary>
+            /// The current horizontal offset.
+            /// </summary>
+            private int x;
+
+            /// <summary>
+            /// The current vertical offset.
+            /// </summary>
+            private int y;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Enumerator"/> struct.
+            /// </summary>
+            /// <param name="span">The target <see cref="Span2D{T}"/> instance to enumerate.</param>
+            internal Enumerator(Span2D<T> span)
+            {
+#if SPAN_RUNTIME_SUPPORT
+                this.span = span.span;
+#else
+                this.instance = span.instance;
+                this.offset = span.offset;
+                this.height = span.height;
+#endif
+                this.width = span.width;
+                this.pitch = span.pitch;
+                this.x = -1;
+                this.y = 0;
+            }
+
+            /// <summary>
+            /// Implements the duck-typed <see cref="System.Collections.IEnumerator.MoveNext"/> method.
+            /// </summary>
+            /// <returns><see langword="true"/> whether a new element is available, <see langword="false"/> otherwise</returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                int x = this.x + 1;
+
+                // Horizontal move, within range
+                if (x < this.width)
+                {
+                    this.x = x;
+
+                    return true;
+                }
+
+                // We reached the end of a row and there is at least
+                // another row available: wrap to a new line and continue.
+                if (
+#if SPAN_RUNTIME_SUPPORT
+                    this.y < (this.span.Length - 1)
+#else
+                    this.y < this.height - 1
+#endif
+                )
+                {
+                    this.x = 0;
+                    this.y++;
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            /// <summary>
+            /// Gets the duck-typed <see cref="System.Collections.Generic.IEnumerator{T}.Current"/> property.
+            /// </summary>
+            public ref T Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get
+                {
+#if SPAN_RUNTIME_SUPPORT
+                    ref T r0 = ref MemoryMarshal.GetReference(this.span);
+#else
+                    ref T r0 = ref this.instance!.DangerousGetObjectDataReferenceAt<T>(this.offset);
+#endif
+                    int index = (this.y * (this.width + this.pitch)) + this.x;
+
+                    return ref Unsafe.Add(ref r0, index);
+                }
+            }
+        }
+    }
+}
--- Microsoft.Toolkit.HighPerformance/Memory/Span2D{T}.cs
+++ Microsoft.Toolkit.HighPerformance/Memory/Span2D{T}.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Toolkit.HighPerformance.Memory
     /// type and it is transparent to the user, but note that working over discontiguous buffers has a performance impact.
     /// </summary>
     /// <typeparam name="T">The type of items in the current <see cref="Span2D{T}"/> instance.</typeparam>
-    public readonly ref struct Span2D<T>
+    public readonly ref partial struct Span2D<T>
     {
         // Let's consider a representation of a discontiguous 2D memory
         // region within an existing array. The data is represented in
@@ -480,16 +480,6 @@ namespace Microsoft.Toolkit.HighPerformance.Memory
         }
 
         /// <summary>
-        /// Returns an enumerator for the current <see cref="Span2D{T}"/> instance.
-        /// </summary>
-        /// <returns>
-        /// An enumerator that can be used to traverse the items in the current <see cref="Span2D{T}"/> instance
-        /// </returns>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public Enumerator GetEnumerator() => new Enumerator(this);
-
-        /// <summary>
         /// Returns a reference to the 0th element of the <see cref="Span2D{T}"/> instance. If the current
         /// instance is empty, returns a <see langword="null"/> reference. It can be used for pinning
         /// and is required to support the use of span within a fixed statement.
@@ -698,128 +688,5 @@ namespace Microsoft.Toolkit.HighPerformance.Memory
         /// </summary>
         /// <param name="array">The input 2D array to convert.</param>
         public static implicit operator Span2D<T>(T[,]? array) => new Span2D<T>(array);
-
-        /// <summary>
-        /// Provides an enumerator for the elements of a <see cref="Span2D{T}"/> instance.
-        /// </summary>
-        public ref struct Enumerator
-        {
-#if SPAN_RUNTIME_SUPPORT
-            /// <summary>
-            /// The <see cref="Span{T}"/> instance pointing to the first item in the target memory area.
-            /// </summary>
-            /// <remarks>Just like in <see cref="Span2D{T}"/>, the length is the height of the 2D region.</remarks>
-            private readonly Span<T> span;
-#else
-            /// <summary>
-            /// The target <see cref="object"/> instance, if present.
-            /// </summary>
-            private readonly object? instance;
-
-            /// <summary>
-            /// The initial offset within <see cref="instance"/>.
-            /// </summary>
-            private readonly IntPtr offset;
-
-            /// <summary>
-            /// The height of the specified 2D region.
-            /// </summary>
-            private readonly int height;
-#endif
-
-            /// <summary>
-            /// The width of the specified 2D region.
-            /// </summary>
-            private readonly int width;
-
-            /// <summary>
-            /// The pitch of the specified 2D region.
-            /// </summary>
-            private readonly int pitch;
-
-            /// <summary>
-            /// The current horizontal offset.
-            /// </summary>
-            private int x;
-
-            /// <summary>
-            /// The current vertical offset.
-            /// </summary>
-            private int y;
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Enumerator"/> struct.
-            /// </summary>
-            /// <param name="span">The target <see cref="Span2D{T}"/> instance to enumerate.</param>
-            internal Enumerator(Span2D<T> span)
-            {
-#if SPAN_RUNTIME_SUPPORT
-                this.span = span.span;
-#else
-                this.instance = span.instance;
-                this.offset = span.offset;
-                this.height = span.height;
-#endif
-                this.width = span.width;
-                this.pitch = span.pitch;
-                this.x = -1;
-                this.y = 0;
-            }
-
-            /// <summary>
-            /// Implements the duck-typed <see cref="System.Collections.IEnumerator.MoveNext"/> method.
-            /// </summary>
-            /// <returns><see langword="true"/> whether a new element is available, <see langword="false"/> otherwise</returns>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public bool MoveNext()
-            {
-                int x = this.x + 1;
-
-                // Horizontal move, within range
-                if (x < this.width)
-                {
-                    this.x = x;
-
-                    return true;
-                }
-
-                // We reached the end of a row and there is at least
-                // another row available: wrap to a new line and continue.
-                if (
-#if SPAN_RUNTIME_SUPPORT
-                    this.y < (this.span.Length - 1)
-#else
-                    this.y < this.height - 1
-#endif
-                )
-                {
-                    this.x = 0;
-                    this.y++;
-
-                    return true;
-                }
-
-                return false;
-            }
-
-            /// <summary>
-            /// Gets the duck-typed <see cref="System.Collections.Generic.IEnumerator{T}.Current"/> property.
-            /// </summary>
-            public ref T Current
-            {
-                [MethodImpl(MethodImplOptions.AggressiveInlining)]
-                get
-                {
-#if SPAN_RUNTIME_SUPPORT
-                    ref T r0 = ref MemoryMarshal.GetReference(this.span);
-#else
-                    ref T r0 = ref this.instance!.DangerousGetObjectDataReferenceAt<T>(this.offset);
-#endif
-                    int index = (this.y * (this.width + this.pitch)) + this.x;
-
-                    return ref Unsafe.Add(ref r0, index);
-                }
-            }
-        }
     }
 }
