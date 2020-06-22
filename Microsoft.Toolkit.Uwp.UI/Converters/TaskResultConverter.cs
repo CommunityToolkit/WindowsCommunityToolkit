8a7cd06518542bdf8c6d1c4174408c2d8b843d2e
Sergio Pedri <sergio0694@live.com>
2020-06-22 18:06:23 +0200

    Added missing XML note about return type

--- Microsoft.Toolkit.Uwp.UI/Converters/TaskResultConverter.cs
+++ Microsoft.Toolkit.Uwp.UI/Converters/TaskResultConverter.cs
@@ -16,6 +16,8 @@ namespace Microsoft.Toolkit.Uwp.UI.Converters
     /// This is needed because accessing <see cref="Task{TResult}.Result"/> when the task has not
     /// completed yet will block the current thread and might cause a deadlock (eg. if the task was
     /// scheduled on the same synchronization context where the result is being retrieved from).
+    /// The methods in this converter will safely return <see langword="default"/> if the input
+    /// task is still running, or if it has faulted or has been canceled.
     /// </summary>
     public sealed class TaskResultConverter : IValueConverter
     {
