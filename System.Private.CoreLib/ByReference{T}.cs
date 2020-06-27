17eb20f2e9dcc031dd40833d5333d287ba472900
Sergio Pedri <sergio0694@live.com>
2020-06-27 15:32:20 +0200

    Tweaks to ByReference<T>, added XML comments

--- System.Private.CoreLib/ByReference{T}.cs
+++ System.Private.CoreLib/ByReference{T}.cs
@@ -2,14 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.Private.CoreLib
+namespace System
 {
-    // ByReference<T> is meant to be used to represent "ref T" fields. It is working
-    // around lack of first class support for byref fields in C# and IL. The JIT and
-    // type loader has special handling for it that turns it into a thin wrapper around ref T.
-    // From CoreCLR: https://github.com/dotnet/runtime/blob/master/src/libraries/System.Private.CoreLib/src/System/ByReference.cs
-    internal readonly ref struct ByReference<T>
+    /// <summary>
+    /// This type is meant to be used to represent "ref T" fields. It is working around lack of first class
+    /// support for byref fields in C# and IL. The JIT and type loader has special handling for it that turns
+    /// it into a thin wrapper around ref T. This type is ported from CoreCLR, see original source
+    /// <see href="https://github.com/dotnet/runtime/blob/master/src/libraries/System.Private.CoreLib/src/System/ByReference.cs">here</see>.
+    /// </summary>
+    /// <typeparam name="T">The type of reference being stored.</typeparam>
+    public readonly ref struct ByReference<T>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ByReference{T}"/> struct.
+        /// </summary>
+        /// <param name="value">The reference to the target <typeparamref name="T"/> value.</param>
         public ByReference(ref T value)
         {
             // Implemented as a JIT intrinsic - This default implementation is for
@@ -18,6 +25,9 @@ namespace System.Private.CoreLib
             throw new PlatformNotSupportedException();
         }
 
+        /// <summary>
+        /// Gets the <typeparamref name="T"/> reference represented by the current <see cref="ByReference{T}"/> instance.
+        /// </summary>
         public ref T Value
         {
             // Implemented as a JIT intrinsic - This default implementation is for
