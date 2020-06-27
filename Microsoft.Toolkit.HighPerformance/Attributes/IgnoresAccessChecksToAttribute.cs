8cb7f187534f372df2d45c4243384ddb64b9e736
Sergio Pedri <sergio0694@live.com>
2020-06-27 15:27:27 +0200

    Enabled hack to use ByReference<T> from the BCL
    Huge thanks to @ltrzesniewski, see comment here: https://github.com/ltrzesniewski/InlineIL.Fody/issues/20#issuecomment-650423640

---
+++ Microsoft.Toolkit.HighPerformance/Attributes/IgnoresAccessChecksToAttribute.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if NETCORE_RUNTIME
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// A special attribute recognized by CoreCLR that allows to ignore visibility checks for internal APIs.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    internal sealed class IgnoresAccessChecksToAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IgnoresAccessChecksToAttribute"/> class.
+        /// </summary>
+        /// <param name="assemblyName">The assembly name to use.</param>
+        public IgnoresAccessChecksToAttribute(string assemblyName)
+        {
+        }
+    }
+}
+
+#endif
---
+++ System.Private.CoreLib/ByReference{T}.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Private.CoreLib
+{
+    // ByReference<T> is meant to be used to represent "ref T" fields. It is working
+    // around lack of first class support for byref fields in C# and IL. The JIT and
+    // type loader has special handling for it that turns it into a thin wrapper around ref T.
+    // From CoreCLR: https://github.com/dotnet/runtime/blob/master/src/libraries/System.Private.CoreLib/src/System/ByReference.cs
+    internal readonly ref struct ByReference<T>
+    {
+        public ByReference(ref T value)
+        {
+            // Implemented as a JIT intrinsic - This default implementation is for
+            // completeness and to provide a concrete error if called via reflection
+            // or if intrinsic is missed.
+            throw new PlatformNotSupportedException();
+        }
+
+        public ref T Value
+        {
+            // Implemented as a JIT intrinsic - This default implementation is for
+            // completeness and to provide a concrete error if called via reflection
+            // or if the intrinsic is missed.
+            get => throw new PlatformNotSupportedException();
+        }
+    }
+}
