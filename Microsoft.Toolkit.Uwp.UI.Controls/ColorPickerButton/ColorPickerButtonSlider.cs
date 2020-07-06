58108c93863835599cce9cc247fa216fdaf02801
robloo <robert.looyengoed@gmail.com>
2020-07-05 20:17:11 -0400

    Add XML comment

--- Microsoft.Toolkit.Uwp.UI.Controls/ColorPickerButton/ColorPickerButtonSlider.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/ColorPickerButton/ColorPickerButtonSlider.cs
@@ -11,22 +11,31 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
     /// A <see cref="Slider"/> implementation for use in the <see cref="ColorPickerButton"/>.
     /// </summary>
     /// <remarks>
-    /// 
+    ///
     /// ColorPickerSlider is currently the same as Slider except it fixes a critical bug.
     /// For details see:
-    /// 
+    ///
     ///  * https://github.com/microsoft/microsoft-ui-xaml/issues/477
     ///  * https://social.msdn.microsoft.com/Forums/sqlserver/en-US/0d3a2e64-d192-4250-b583-508a02bd75e1/uwp-bug-crash-layoutcycleexception-because-of-slider-under-certain-circumstances?forum=wpdevelop
-    /// 
+    ///
     /// An added benefit of being a separate, derived class is more logic can be added
     /// here in the future just like ColorPicker.
-    /// 
+    ///
     /// </remarks>
     public class ColorPickerButtonSlider : Slider
     {
         private Size oldSize;
         private Size measuredSize;
 
+        /// <summary>
+        /// Measures the size in layout required for child elements and determines a size for the
+        /// FrameworkElement-derived class.
+        /// </summary>
+        /// <param name="availableSize">The available size that this element can give to child elements.
+        /// Infinity can be specified as a value to indicate that the element will size to whatever content
+        /// is available.</param>
+        /// <returns>The size that this element determines it needs during layout,
+        /// based on its calculations of child element sizes.</returns>
         protected override Size MeasureOverride(Size availableSize)
         {
             if (!Size.Equals(oldSize, availableSize))
