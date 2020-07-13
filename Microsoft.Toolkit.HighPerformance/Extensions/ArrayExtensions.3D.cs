9d9ec18f36d01b8a2b737bcd0031a023465eeb71
Sergio Pedri <sergio0694@live.com>
2020-07-13 22:48:39 +0200

    Improved codegen in refactored array extensions

--- Microsoft.Toolkit.HighPerformance/Extensions/ArrayExtensions.3D.cs
+++ Microsoft.Toolkit.HighPerformance/Extensions/ArrayExtensions.3D.cs
@@ -65,13 +65,13 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
         /// </remarks>
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ref T DangerousGetReferenceAt<T>(this T[,,] array, int i, int j, int k)
+        public static unsafe ref T DangerousGetReferenceAt<T>(this T[,,] array, int i, int j, int k)
         {
 #if NETCORE_RUNTIME
             var arrayData = Unsafe.As<RawArray3DData>(array);
             int offset = (i * arrayData.Height * arrayData.Width) + (j * arrayData.Width) + k;
             ref T r0 = ref Unsafe.As<byte, T>(ref arrayData.Data);
-            ref T ri = ref Unsafe.Add(ref r0, offset);
+            ref T ri = ref Unsafe.Add(ref r0, (IntPtr)(void*)(uint)offset);
 
             return ref ri;
 #else
@@ -82,10 +82,7 @@ namespace Microsoft.Toolkit.HighPerformance.Extensions
                 return ref array[i, j, k];
             }
 
-            unsafe
-            {
-                return ref Unsafe.AsRef<T>(null);
-            }
+            return ref Unsafe.AsRef<T>(null);
 #endif
         }
 
