89e2e83defe67f6db629b250419e9b76609dbd5c
Sergio Pedri <sergio0694@live.com>
2020-07-31 14:37:18 +0200

    Added more comments for System.Private.CoreLib

--- Microsoft.Toolkit.HighPerformance/Properties/AssemblyInfo.cs
+++ Microsoft.Toolkit.HighPerformance/Properties/AssemblyInfo.cs
@@ -6,6 +6,15 @@
 
 using System.Runtime.CompilerServices;
 
+// This is needed so that the runtime will actually let us access the internal
+// ByReference<T> type exposed through the fake System.Private.CoreLib assembly
+// that is referenced by this project. The reason for this is that while the proxy
+// type we declared belongs to a project we have internals access to, the real
+// ByReference<T> from the actual CoreLib assembly does not, so the runtime would
+// normally fail to load the type if this attribute wasn't present.
+// Note that while this attribute is internally recognized by the runtime, we still
+// need the fake CoreLib assembly to be able to use the type when building from source,
+// as Roslyn itself does not actually recognize and/or respect it.
 [assembly: IgnoresAccessChecksTo("System.Private.CoreLib")]
 
 #endif
--- System.Private.CoreLib/Properties/AssemblyInfo.cs
+++ System.Private.CoreLib/Properties/AssemblyInfo.cs
@@ -4,4 +4,11 @@
 
 using System.Runtime.CompilerServices;
 
+// The ByReference<T> type included in this project is kept internal, so
+// that users referencing the Microsoft.Toolkit.HighPerformance package will
+// need to go through the (overhead free) abstraction of the Ref<T> types
+// present in that assembly. This makes codebases using this package more
+// resilient to changes in the runtime, and the code itself more portable.
+// We need to provide the full public key here, as this assembly is signed.
+// See the .csproj file for more info on why this is needed.
 [assembly: InternalsVisibleTo("Microsoft.Toolkit.HighPerformance, PublicKey=002400000480000094000000060200000024000052534131000400000100010041753af735ae6140c9508567666c51c6ab929806adb0d210694b30ab142a060237bc741f9682e7d8d4310364b4bba4ee89cc9d3d5ce7e5583587e8ea44dca09977996582875e71fb54fa7b170798d853d5d8010b07219633bdb761d01ac924da44576d6180cdceae537973982bb461c541541d58417a3794e34f45e6f2d129e2")]
