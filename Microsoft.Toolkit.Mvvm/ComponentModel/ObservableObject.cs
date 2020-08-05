3fc26f01ae5da251d40d202f6820b241f8734151
Sergio Pedri <sergio0694@live.com>
2020-08-05 19:11:45 +0200

    Minor performance improvement for Set(ref T) overload

--- Microsoft.Toolkit.Mvvm/ComponentModel/ObservableObject.cs
+++ Microsoft.Toolkit.Mvvm/ComponentModel/ObservableObject.cs
@@ -66,7 +66,26 @@ namespace Microsoft.Toolkit.Mvvm.ComponentModel
         /// </remarks>
         protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
         {
-            return SetProperty(ref field, newValue, EqualityComparer<T>.Default, propertyName);
+            // We duplicate the code here instead of calling the overload because we can't
+            // guarantee that the invoked SetProperty<T> will be inlined, and we need the JIT
+            // to be able to see the full EqualityComparer<T>.Default.Equals call, so that
+            // it'll use the intrinsics version of it and just replace the whole invocation
+            // with a direct comparison when possible (eg. for primitive numeric types).
+            // This is the fastest SetProperty<T> overload so we particularly care about
+            // the codegen quality here, and the code is small and simple enough so that
+            // duplicating it still doesn't make the whole class harder to maintain.
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            OnPropertyChanging(propertyName);
+
+            field = newValue;
+
+            OnPropertyChanged(propertyName);
+
+            return true;
         }
 
         /// <summary>
