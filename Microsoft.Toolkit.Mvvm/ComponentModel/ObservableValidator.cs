f429a78ad89f078e9d5dfc4b1dab90209312853f
Sergio Pedri <sergio0694@live.com>
2020-08-18 13:19:12 +0200

    Removed unnecessary using directive

--- Microsoft.Toolkit.Mvvm/ComponentModel/ObservableValidator.cs
+++ Microsoft.Toolkit.Mvvm/ComponentModel/ObservableValidator.cs
@@ -7,7 +7,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 namespace Microsoft.Toolkit.Mvvm.ComponentModel
