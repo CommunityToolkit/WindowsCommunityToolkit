02a46f5e5d3573dd743cf01a25cf44d60356c0d4
Sergio Pedri <sergio0694@live.com>
2020-08-21 23:52:54 +0200

    Removed deprecated extensions

--- Microsoft.Toolkit/Extensions/ArrayExtensions.cs
+++ Microsoft.Toolkit/Extensions/ArrayExtensions.cs
@@ -15,74 +15,6 @@ namespace Microsoft.Toolkit.Extensions
     public static class ArrayExtensions
     {
         /// <summary>
-        /// Fills elements of a rectangular array at the given position and size to a specific value.
-        /// Ranges given will fill in as many elements as possible, ignoring positions outside the bounds of the array.
-        /// </summary>
-        /// <typeparam name="T">The element type of the array.</typeparam>
-        /// <param name="array">The source array.</param>
-        /// <param name="value">Value to fill with.</param>
-        /// <param name="row">Row to start on (inclusive, zero-index).</param>
-        /// <param name="col">Column to start on (inclusive, zero-index).</param>
-        /// <param name="width">Positive width of area to fill.</param>
-        /// <param name="height">Positive height of area to fill.</param>
-        [Obsolete("This helper will be removed in a future release, use the APIs from Microsoft.Toolkit.HighPerformance to replace it")]
-        public static void Fill<T>(this T[,] array, T value, int row, int col, int width, int height)
-        {
-            for (int r = row; r < row + height; r++)
-            {
-                for (int c = col; c < col + width; c++)
-                {
-                    if (r >= 0 && c >= 0 && r < array.GetLength(0) && c < array.GetLength(1))
-                    {
-                        array[r, c] = value;
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Yields a row from a rectangular array.
-        /// </summary>
-        /// <typeparam name="T">The element type of the array.</typeparam>
-        /// <param name="rectarray">The source array.</param>
-        /// <param name="row">Row record to retrieve, 0-based index.</param>
-        /// <returns>Yielded row.</returns>
-        [Obsolete("This helper will be removed in a future release, use the APIs from Microsoft.Toolkit.HighPerformance to replace it")]
-        public static IEnumerable<T> GetRow<T>(this T[,] rectarray, int row)
-        {
-            if (row < 0 || row >= rectarray.GetLength(0))
-            {
-                throw new ArgumentOutOfRangeException(nameof(row));
-            }
-
-            for (int c = 0; c < rectarray.GetLength(1); c++)
-            {
-                yield return rectarray[row, c];
-            }
-        }
-
-        /// <summary>
-        /// Yields a column from a rectangular array.
-        /// </summary>
-        /// <typeparam name="T">The element type of the array.</typeparam>
-        /// <param name="rectarray">The source array.</param>
-        /// <param name="column">Column record to retrieve, 0-based index.</param>
-        /// <returns>Yielded column.</returns>
-        [Obsolete("This helper will be removed in a future release, use the APIs from Microsoft.Toolkit.HighPerformance to replace it")]
-        public static IEnumerable<T> GetColumn<T>(this T[,] rectarray, int column)
-        {
-            if (column < 0 || column >= rectarray.GetLength(1))
-            {
-                throw new ArgumentOutOfRangeException(nameof(column));
-            }
-
-            for (int r = 0; r < rectarray.GetLength(0); r++)
-            {
-                yield return rectarray[r, column];
-            }
-        }
-
-        /// <summary>
         /// Yields a column from a jagged array.
         /// An exception will be thrown if the column is out of bounds, and return default in places where there are no elements from inner arrays.
         /// Note: There is no equivalent GetRow method, as you can use array[row] to retrieve.
@@ -102,7 +34,7 @@ namespace Microsoft.Toolkit.Extensions
             {
                 if (column >= rectarray[r].Length)
                 {
-                    yield return default(T);
+                    yield return default;
 
                     continue;
                 }
