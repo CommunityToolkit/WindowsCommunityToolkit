1f0bc0014ba2975e4c48592654392a0a30a854de
Andrew Leader <aleader@microsoft.com>
2020-08-28 17:57:23 -0700

    Sample app update

--- Microsoft.Toolkit.Win32.WpfCore.SampleApp/App.xaml.cs
+++ Microsoft.Toolkit.Win32.WpfCore.SampleApp/App.xaml.cs
@@ -27,10 +27,27 @@ namespace Microsoft.Toolkit.Win32.WpfCore.SampleApp
         {
             Dispatcher.Invoke(() =>
             {
+                OpenWindowIfNeeded();
+
                 var args = e.Argument;
                 var userInputCount = e.UserInput.Count;
                 MessageBox.Show("Activated!");
             });
+        }
+
+        private void OpenWindowIfNeeded()
+        {
+            // Make sure we have a window open (in case user clicked toast while app closed)
+            if (App.Current.Windows.Count == 0)
+            {
+                new MainWindow().Show();
+            }
+
+            // Activate the window, bringing it to focus
+            App.Current.Windows[0].Activate();
+
+            // And make sure to maximize the window too, in case it was currently minimized
+            App.Current.Windows[0].WindowState = WindowState.Normal;
         }
     }
 }
