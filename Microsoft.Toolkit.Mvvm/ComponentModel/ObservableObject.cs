97ab1d8ba90652fa7c338a646fab8e3cda79d22c
Sergio Pedri <sergio0694@live.com>
2020-09-04 12:07:04 +0200

    Removed unnecessary using directives

--- Microsoft.Toolkit.Mvvm/ComponentModel/ObservableObject.cs
+++ Microsoft.Toolkit.Mvvm/ComponentModel/ObservableObject.cs
@@ -10,8 +10,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq.Expressions;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
