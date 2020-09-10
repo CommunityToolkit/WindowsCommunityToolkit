05c7d48dd3c21bf75cb7e623f52826a45ed76d70
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2020-09-10 12:59:59 -0700

    Added headers.

--- UITests/UITests.Tests.Shared/TestAssembly.cs
+++ UITests/UITests.Tests.Shared/TestAssembly.cs
@@ -1,4 +1,8 @@
-﻿#if USING_TAEF
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if USING_TAEF
 using WEX.TestExecution.Markup;
 #else
 using Microsoft.VisualStudio.TestTools.UnitTesting;
