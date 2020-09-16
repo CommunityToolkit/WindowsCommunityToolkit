5aa2dfa93bb4fd6e03e0eef181e1b14fbae4955e
Sergio Pedri <sergio0694@live.com>
2020-09-16 23:19:33 +0200

    Added missing XML comments

--- Microsoft.Toolkit.HighPerformance/Streams/Sources/Interfaces/ISpanOwner.cs
+++ Microsoft.Toolkit.HighPerformance/Streams/Sources/Interfaces/ISpanOwner.cs
@@ -6,10 +6,19 @@ using System;
 
 namespace Microsoft.Toolkit.HighPerformance.Streams
 {
+    /// <summary>
+    /// An interface for types acting as sources for <see cref="Span{T}"/> instances.
+    /// </summary>
     internal interface ISpanOwner
     {
+        /// <summary>
+        /// Gets the length of the underlying memory area.
+        /// </summary>
         int Length { get; }
 
+        /// <summary>
+        /// Gets a <see cref="Span{T}"/> instance wrapping the underlying memory area.
+        /// </summary>
         Span<byte> Span { get; }
     }
 }
