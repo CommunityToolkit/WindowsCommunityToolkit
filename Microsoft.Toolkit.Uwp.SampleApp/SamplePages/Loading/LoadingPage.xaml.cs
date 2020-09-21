232e5dabde8861dd17f1efedcd1cd1f04695d073
Sergio Pedri <sergio0694@live.com>
2020-09-21 16:45:44 +0200

    Fixed incorrect blur in loading sample

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Loading/LoadingPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/Loading/LoadingPage.xaml.cs
@@ -13,6 +13,7 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 {
     public sealed partial class LoadingPage : IXamlRenderListener
     {
+        private ScrollViewer scrollViewerControl;
         private AdaptiveGridView adaptiveGridViewControl;
         private Loading loadingControl;
         private ContentControl loadingContentControl;
@@ -26,6 +27,7 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
 
         public async void OnXamlRendered(FrameworkElement control)
         {
+            scrollViewerControl = control.FindChildByName("ScrollViewerControl") as ScrollViewer;
             adaptiveGridViewControl = control.FindChildByName("AdaptiveGridViewControl") as AdaptiveGridView;
             loadingControl = control.FindDescendantByName("LoadingControl") as Loading;
             loadingContentControl = control.FindChildByName("LoadingContentControl") as ContentControl;
@@ -62,9 +64,13 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
                 if (loadingContentControl != null)
                 {
                     loadingContentControl.ContentTemplate = resources["LogoTemplate"] as DataTemplate;
-                    await loadingContentControl.Blur(2, 100).StartAsync();
+
+                    // We need to blur the ScrollViewer hosting the main content of the page and not the
+                    // root control, otherwise the popup with the icon would be blurred as well, as it
+                    // would be in the same visual tree as the control whose visual is being blurred.
+                    await scrollViewerControl.Blur(2, 100).StartAsync();
                     await ShowLoadingDialogAsync();
-                    await loadingContentControl.Blur(0, 0).StartAsync();
+                    await scrollViewerControl.Blur(0, 0).StartAsync();
                 }
             });
         }
