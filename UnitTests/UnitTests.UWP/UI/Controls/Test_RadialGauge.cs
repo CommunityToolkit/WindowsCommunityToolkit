361c553f480400cc190fb184a9cef0d66715e5fa
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-10-23 13:36:46 +0200

    Add license header

--- UnitTests/UnitTests.UWP/UI/Controls/Test_RadialGauge.cs
+++ UnitTests/UnitTests.UWP/UI/Controls/Test_RadialGauge.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using Microsoft.Toolkit.Uwp.UI.Controls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
