d29381fdd663085725f877a652c5c4f0b6b397e0
Sergio Pedri <sergio0694@live.com>
2020-10-24 12:56:50 +0200

    Fixed visibility of local SkipLocalsInitAttribute copy

--- Microsoft.Toolkit.HighPerformance/Attributes/SkipLocalsInitAttribute.cs
+++ Microsoft.Toolkit.HighPerformance/Attributes/SkipLocalsInitAttribute.cs
@@ -20,7 +20,7 @@ namespace System.Runtime.CompilerServices
         AttributeTargets.Property |
         AttributeTargets.Event,
         Inherited = false)]
-    public sealed class SkipLocalsInitAttribute : Attribute
+    internal sealed class SkipLocalsInitAttribute : Attribute
     {
     }
 }
