ea75c8a11ff24c9c78b0d4ef0d29964e6d77d44b
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2020-10-27 15:38:44 -0700

    Add in ColorPickerButton again to Codebase

---
+++ Microsoft.Toolkit.Uwp.UI.Controls/ColorPicker/ColorPickerButton.cs
@@ -0,0 +1,130 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls
+{
+    /// <summary>
+    /// A <see cref="DropDownButton"/> which displays a color as its <c>Content</c> and it's <c>Flyout</c> is a <see cref="ColorPicker"/>.
+    /// </summary>
+    [TemplatePart(Name = nameof(ColorPicker), Type = typeof(ColorPicker))]
+    public class ColorPickerButton : DropDownButton
+    {
+        /// <summary>
+        /// Gets the <see cref="Controls.ColorPicker"/> instances contained by the <see cref="DropDownButton"/>.
+        /// </summary>
+        public ColorPicker ColorPicker { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Style"/> for the <see cref="Controls.ColorPicker"/> control used in the button.
+        /// </summary>
+        public Style ColorPickerStyle
+        {
+            get
+            {
+                return (Style)GetValue(ColorPickerStyleProperty);
+            }
+
+            set
+            {
+                SetValue(ColorPickerStyleProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="ColorPickerStyle"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ColorPickerStyleProperty = DependencyProperty.Register("ColorPickerStyle", typeof(Style), typeof(ColorPickerButton), new PropertyMetadata(default(Style)));
+
+        /// <summary>
+        /// Gets or sets the <see cref="Style"/> for the <see cref="FlyoutPresenter"/> used within the <see cref="Flyout"/> of the <see cref="DropDownButton"/>.
+        /// </summary>
+        public Style FlyoutPresenterStyle
+        {
+            get
+            {
+                return (Style)GetValue(FlyoutPresenterStyleProperty);
+            }
+
+            set
+            {
+                SetValue(FlyoutPresenterStyleProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="FlyoutPresenterStyle"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty FlyoutPresenterStyleProperty = DependencyProperty.Register("FlyoutPresenterStyle", typeof(Style), typeof(ColorPickerButton), new PropertyMetadata(default(Style)));
+
+        /// <summary>
+        /// Gets or sets the selected <see cref="Windows.UI.Color"/> the user has picked from the <see cref="ColorPicker"/>.
+        /// </summary>
+        public Color SelectedColor
+        {
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="SelectedColor"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty SelectedColorProperty =
+            DependencyProperty.Register(nameof(SelectedColor), typeof(Color), typeof(ColorPickerButton), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorPickerButton"/> class.
+        /// </summary>
+        public ColorPickerButton()
+        {
+            this.DefaultStyleKey = typeof(ColorPickerButton);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnApplyTemplate()
+        {
+            if (ColorPicker != null)
+            {
+                // TODO: Unregister
+            }
+
+            base.OnApplyTemplate();
+
+            if (ColorPickerStyle != null)
+            {
+                ColorPicker = new ColorPicker() { Style = ColorPickerStyle };
+            }
+            else
+            {
+                ColorPicker = new ColorPicker();
+            }
+
+            ColorPicker.Color = SelectedColor;
+
+            this.ColorPicker.ColorChanged += (sender, args) =>
+            {
+                SelectedColor = args.NewColor;
+            };
+
+            if (Flyout == null)
+            {
+                Flyout = new Flyout()
+                {
+                    // TODO: Expose Placement
+                    Placement = Windows.UI.Xaml.Controls.Primitives.FlyoutPlacementMode.BottomEdgeAlignedLeft,
+                    FlyoutPresenterStyle = FlyoutPresenterStyle,
+                    Content = ColorPicker
+                };
+            }
+        }
+    }
+}
