959e732aaca60d5e26b5d9273829aaaf25d98276
Vincent Gromfeld <vigromfe@microsoft.com>
2020-10-28 09:14:49 +0100

    fix stylecop issue

--- Microsoft.Toolkit.Uwp.UI.Controls/WrapPanel/WrapPanel.Data.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/WrapPanel/WrapPanel.Data.cs
@@ -2,9 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.Toolkit.Diagnostics;
 using System;
 using System.Collections.Generic;
+using Microsoft.Toolkit.Diagnostics;
 using Windows.Foundation;
 using Windows.UI.Xaml.Controls;
 
