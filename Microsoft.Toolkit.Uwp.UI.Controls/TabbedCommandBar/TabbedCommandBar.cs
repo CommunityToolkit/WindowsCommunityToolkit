4d8e9bf6b1fc10b12a791f47887998bf2b68b1a5
YoshiAsk <jjask7@gmail.com>
2020-11-06 13:05:15 -0600

    Switch TabbedCommandBar to be a NavigationView

--- Microsoft.Toolkit.Uwp.UI.Controls/TabbedCommandBar/TabbedCommandBar.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/TabbedCommandBar/TabbedCommandBar.cs
@@ -14,27 +14,15 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
     /// <summary>
     /// A basic ribbon control that houses <see cref="TabbedCommandBarItem"/>s
     /// </summary>
-    [ContentProperty(Name = nameof(Items))]
+    [ContentProperty(Name = nameof(MenuItems))]
     [TemplatePart(Name = "PART_RibbonNavigationView", Type = typeof(NavigationView))]
     [TemplatePart(Name = "PART_RibbonContent", Type = typeof(ContentControl))]
     [TemplatePart(Name = "PART_TabChangedStoryboard", Type = typeof(Storyboard))]
-    public class TabbedCommandBar : Control
+    public class TabbedCommandBar : NavigationView
     {
         private NavigationView _ribbonNavigationView = null;
         private ContentControl _ribbonContent = null;
         private Storyboard _tabChangedStoryboard = null;
-
-        // This should probably be made public at some point
-        private TabbedCommandBarItem SelectedTab { get; set; }
-
-        /// <summary>
-        /// Identifies the <see cref="Items"/> property.
-        /// </summary>
-        public static readonly DependencyProperty ItemsProperty = DependencyProperty.Register(
-            nameof(Items),
-            typeof(IList<object>),
-            typeof(TabbedCommandBar),
-            new PropertyMetadata(new List<object>()));
 
         /// <summary>
         /// Identifies the <see cref="Footer"/> property.
@@ -44,16 +32,6 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             typeof(UIElement),
             typeof(TabbedCommandBar),
             new PropertyMetadata(new Border()));
-
-        // This should be an IList<TabbedCommandBarItem>, but Intellisense really doesn't like that.
-        /// <summary>
-        /// Gets or sets A list of <see cref="TabbedCommandBarItem"/>s to display in this <see cref="TabbedCommandBar"/>.
-        /// </summary>
-        public IList<object> Items
-        {
-            get { return (IList<object>)GetValue(ItemsProperty); }
-            set { SetValue(ItemsProperty, value); }
-        }
 
         /// <summary>
         /// Gets or sets the <see cref="UIElement"/> to be displayed in the footer of the ribbon tab strip.
@@ -70,6 +48,8 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         public TabbedCommandBar()
         {
             DefaultStyleKey = typeof(TabbedCommandBar);
+
+            SelectionChanged += RibbonNavigationView_SelectionChanged;
         }
 
         /// <inheritdoc/>
@@ -77,24 +57,15 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         {
             base.OnApplyTemplate();
 
+            if (_ribbonContent != null)
+            {
+                _ribbonContent.Content = null;
+            }
+
             // Get RibbonContent first, since setting SelectedItem requires it
             _ribbonContent = GetTemplateChild("PART_RibbonContent") as ContentControl;
 
-            _ribbonNavigationView = GetTemplateChild("PART_RibbonNavigationView") as NavigationView;
-            if (_ribbonNavigationView != null)
-            {
-                // Populate the NavigationView with items
-                // TODO: Get binding working, necessary for contextual tabs
-                _ribbonNavigationView.MenuItems.Clear();
-                foreach (TabbedCommandBarItem item in Items)
-                {
-                    _ribbonNavigationView.MenuItems.Add(item);
-                }
-                _ribbonNavigationView.PaneFooter = Footer;
-
-                _ribbonNavigationView.SelectionChanged += RibbonNavigationView_SelectionChanged;
-                _ribbonNavigationView.SelectedItem = _ribbonNavigationView.MenuItems.FirstOrDefault();
-            }
+            SelectedItem = MenuItems.FirstOrDefault();
 
             _tabChangedStoryboard = GetTemplateChild(nameof(_tabChangedStoryboard)) as Storyboard;
         }
@@ -110,7 +81,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             {
                 // This code is a hack, but it's necessary if binding doesn't work.
                 // RibbonContent might be null here, there should be a check
-                _ribbonContent.Content = Items[System.Math.Min(Items.Count - 1, _ribbonNavigationView.MenuItems.IndexOf(navItem))];
+                _ribbonContent.Content = MenuItems[System.Math.Min(MenuItems.Count - 1, MenuItems.IndexOf(navItem))];
             }
         }
     }
