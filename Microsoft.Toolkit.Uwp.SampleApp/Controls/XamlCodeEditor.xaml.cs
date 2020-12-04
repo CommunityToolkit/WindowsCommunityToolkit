033cd45fe2a4cb24bf165b08f35abc64744b0bbe
null-val <59544401+null-val@users.noreply.github.com>
2020-12-04 13:33:57 -0800

    Updated dark theme color to #660000

--- Microsoft.Toolkit.Uwp.SampleApp/Controls/XamlCodeEditor.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/Controls/XamlCodeEditor.xaml.cs
@@ -125,9 +125,9 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.Controls
 
         private CssLineStyle ErrorStyle
         {
-            get => _themeListener.CurrentTheme.Equals(ApplicationTheme.Light) ? 
-                new CssLineStyle() { BackgroundColor = new SolidColorBrush(Color.FromArgb(0x00, 0xFF, 0xD6, 0xD6)) } : 
-                new CssLineStyle() { BackgroundColor = new SolidColorBrush(Color.FromArgb(0x00, 0x6E, 0x00, 0x00)) };
+            get => _themeListener.CurrentTheme.Equals(ApplicationTheme.Light) ?
+                new CssLineStyle() { BackgroundColor = new SolidColorBrush(Color.FromArgb(0x00, 0xFF, 0xD6, 0xD6)) } :
+                new CssLineStyle() { BackgroundColor = new SolidColorBrush(Color.FromArgb(0x00, 0x66, 0x00, 0x00)) };
         }
 
         private CssGlyphStyle _errorIconStyle = new CssGlyphStyle()
