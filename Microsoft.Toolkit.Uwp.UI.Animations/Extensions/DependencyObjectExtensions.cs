406a298f1571ad461c419e2740a56357aca306a6
Sergio Pedri <sergio0694@live.com>
2020-12-21 13:46:39 +0100

    Added missing XAML animation creation extensions

--- Microsoft.Toolkit.Uwp.UI.Animations/Extensions/DependencyObjectExtensions.cs
+++ Microsoft.Toolkit.Uwp.UI.Animations/Extensions/DependencyObjectExtensions.cs
@@ -6,6 +6,8 @@
 
 using System;
 using System.Diagnostics.Contracts;
+using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media.Animation;
 
@@ -41,12 +43,87 @@ namespace Microsoft.Toolkit.Uwp.UI.Animations.Extensions
         {
             DoubleAnimation animation = new()
             {
-                From = from,
                 To = to,
+                From = from,
                 BeginTime = delay,
                 Duration = duration,
                 EasingFunction = easing,
-                EnableDependentAnimation = enableDependecyAnimations,
+                EnableDependentAnimation = enableDependecyAnimations
+            };
+
+            Storyboard.SetTarget(animation, target);
+            Storyboard.SetTargetProperty(animation, property);
+
+            return animation;
+        }
+
+        /// <summary>
+        /// Prepares a <see cref="PointAnimation"/> with the given info.
+        /// </summary>
+        /// <param name="target">The target <see cref="DependencyObject"/> to animate.</param>
+        /// <param name="property">The property to animate inside the target <see cref="DependencyObject"/>.</param>
+        /// <param name="to">The final property value.</param>
+        /// <param name="from">The optional initial property value.</param>
+        /// <param name="delay">The optional delay for the animation.</param>
+        /// <param name="duration">The duration of the <see cref="PointAnimation"/>.</param>
+        /// <param name="easing">The easing function to use inside the <see cref="PointAnimation"/>.</param>
+        /// <param name="enableDependecyAnimations">Indicates whether or not to apply this animation to elements that need the visual tree to be rearranged.</param>
+        /// <returns>A <see cref="PointAnimation"/> instance with the specified parameters.</returns>
+        [Pure]
+        public static PointAnimation CreatePointAnimation(
+            this DependencyObject target,
+            string property,
+            Point to,
+            Point? from,
+            TimeSpan? delay,
+            TimeSpan duration,
+            EasingFunctionBase? easing = null,
+            bool enableDependecyAnimations = false)
+        {
+            PointAnimation animation = new()
+            {
+                To = to,
+                From = from,
+                BeginTime = delay,
+                Duration = duration,
+                EasingFunction = easing,
+                EnableDependentAnimation = enableDependecyAnimations
+            };
+
+            Storyboard.SetTarget(animation, target);
+            Storyboard.SetTargetProperty(animation, property);
+
+            return animation;
+        }
+
+        /// <summary>
+        /// Prepares a <see cref="ColorAnimation"/> with the given info.
+        /// </summary>
+        /// <param name="target">The target <see cref="DependencyObject"/> to animate.</param>
+        /// <param name="property">The property to animate inside the target <see cref="DependencyObject"/>.</param>
+        /// <param name="to">The final property value.</param>
+        /// <param name="from">The optional initial property value.</param>
+        /// <param name="delay">The optional delay for the animation.</param>
+        /// <param name="duration">The duration of the <see cref="ColorAnimation"/>.</param>
+        /// <param name="easing">The easing function to use inside the <see cref="ColorAnimation"/>.</param>
+        /// <returns>A <see cref="ColorAnimation"/> instance with the specified parameters.</returns>
+        [Pure]
+        public static ColorAnimation CreateColorAnimation(
+            this DependencyObject target,
+            string property,
+            Color to,
+            Color? from,
+            TimeSpan? delay,
+            TimeSpan duration,
+            EasingFunctionBase? easing = null)
+        {
+            ColorAnimation animation = new()
+            {
+                To = to,
+                From = from,
+                BeginTime = delay,
+                Duration = duration,
+                EasingFunction = easing
             };
 
             Storyboard.SetTarget(animation, target);
