0c89f54a4eef3dca93333254ea502ca09299fa8b
Sergio Pedri <sergio0694@live.com>
2020-12-23 13:59:46 +0100

    Added OrientationAnimation type

---
+++ Microsoft.Toolkit.Uwp.UI.Animations/Xaml/Default/OrientationAnimation.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Numerics;
+using Windows.UI.Xaml.Media.Animation;
+using static Microsoft.Toolkit.Uwp.UI.Animations.Extensions.AnimationExtensions;
+
+namespace Microsoft.Toolkit.Uwp.UI.Animations.Xaml
+{
+    /// <summary>
+    /// An orientation animation working on the composition layer.
+    /// </summary>
+    public class OrientationAnimation : Animation<Quaternion?>
+    {
+        /// <inheritdoc/>
+        public override AnimationBuilder AppendToBuilder(AnimationBuilder builder, TimeSpan? delayHint, TimeSpan? durationHint, EasingType? easingTypeHint, EasingMode? easingModeHint)
+        {
+            return builder.Orientation(
+                To!.Value,
+                From,
+                Delay ?? delayHint,
+                Duration ?? durationHint,
+                EasingType ?? easingTypeHint ?? DefaultEasingType,
+                EasingMode ?? easingModeHint ?? DefaultEasingMode);
+        }
+    }
+}
