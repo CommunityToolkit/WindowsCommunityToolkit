2ed00ab00702d7c152b5a6442d6946c04f9fc65c
Rosario Pulella <Rosariopulella@gmail.com>
2021-01-05 16:53:37 -0500

    Update Microsoft.Toolkit.Uwp/Helpers/ObjectStorage/LocalObjectStorageHelper.cs
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>

--- Microsoft.Toolkit.Uwp/Helpers/ObjectStorage/LocalObjectStorageHelper.cs
+++ Microsoft.Toolkit.Uwp/Helpers/ObjectStorage/LocalObjectStorageHelper.cs
@@ -16,8 +16,8 @@ namespace Microsoft.Toolkit.Uwp.Helpers
         /// Initializes a new instance of the <see cref="LocalObjectStorageHelper"/> class,
         /// which can read and write data using the provided <see cref="IObjectSerializer"/>;
         /// if none is provided, a default Json serializer will be used (based on <see cref="DataContractSerializer"/>).
-        /// In 6.1 and older the defualt Serizlizer was based on Newtownsoft Json and the new default Serizlizer may behave diffrenly.
-        /// To implement a <see cref="IObjectSerializer"/> based on Newtonsoft Json or System.Text.Json see https://aka.ms/wct/storagehelper-migration
+        /// In 6.1 and older the default Serializer was based on Newtonsoft.Json and the new default Serializer may behave differently.
+        /// To implement a <see cref="IObjectSerializer"/> based on Newtonsoft.Json or System.Text.Json see https://aka.ms/wct/storagehelper-migration
         /// </summary>
         /// <param name="objectSerializer">The serializer to use.</param>
         public LocalObjectStorageHelper(IObjectSerializer objectSerializer = null)
