b469849bdb50e159363ccbd330e22abcbcd11b26
sonnemaf <fons.sonnemans@reflectionit.nl>
2021-01-21 16:09:20 +0100

    Updated headers

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/VoiceCommands/ClickAction.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/VoiceCommands/ClickAction.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Xaml.Interactivity;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Xaml.Interactivity;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Automation.Peers;
 using Windows.UI.Xaml.Automation.Provider;
