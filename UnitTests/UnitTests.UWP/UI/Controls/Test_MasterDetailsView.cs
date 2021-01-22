a96150deac1d5264a4e588d0cb736c2a3be11efb
Rosario Pulella <RosarioPulella@gmail.com>
2021-01-22 11:18:42 -0500

    UnitTest: Use ListDetailsView where posible

--- UnitTests/UnitTests.UWP/UI/Controls/Test_MasterDetailsView.cs
+++ UnitTests/UnitTests.UWP/UI/Controls/Test_MasterDetailsView.cs
@@ -10,72 +10,72 @@ using System.Linq;
 namespace UnitTests.UI.Controls
 {
     [TestClass]
-    public class Test_MasterDetailsView
+    public class Test_ListDetailsView
     {
-        [TestCategory("MasterDetailsView")]
+        [TestCategory("ListDetailsView")]
         [UITestMethod]
         public void Test_SelectedIndex_Default()
         {
             var items = Enumerable.Range(1, 10).ToArray();
-            var masterDetailsView = new ListDetailsView();
-            masterDetailsView.ItemsSource = items;
-            Assert.AreEqual(-1, masterDetailsView.SelectedIndex);
+            var listDetailsView = new ListDetailsView();
+            listDetailsView.ItemsSource = items;
+            Assert.AreEqual(-1, listDetailsView.SelectedIndex);
         }
 
-        [TestCategory("MasterDetailsView")]
+        [TestCategory("ListDetailsView")]
         [UITestMethod]
         public void Test_SelectedItem_Default()
         {
             var items = Enumerable.Range(1, 10).ToArray();
-            var masterDetailsView = new ListDetailsView();
-            masterDetailsView.ItemsSource = items;
-            Assert.IsNull(masterDetailsView.SelectedItem);
+            var listDetailsView = new ListDetailsView();
+            listDetailsView.ItemsSource = items;
+            Assert.IsNull(listDetailsView.SelectedItem);
         }
 
-        [TestCategory("MasterDetailsView")]
+        [TestCategory("ListDetailsView")]
         [UITestMethod]
         public void Test_SelectedIndex_Syncs_SelectedItem()
         {
             var items = Enumerable.Range(1, 10).ToArray();
-            var masterDetailsView = new ListDetailsView();
-            masterDetailsView.ItemsSource = items;
-            masterDetailsView.SelectedIndex = 6;
-            Assert.AreEqual(items[6], masterDetailsView.SelectedItem);
+            var listDetailsView = new ListDetailsView();
+            listDetailsView.ItemsSource = items;
+            listDetailsView.SelectedIndex = 6;
+            Assert.AreEqual(items[6], listDetailsView.SelectedItem);
         }
 
-        [TestCategory("MasterDetailsView")]
+        [TestCategory("ListDetailsView")]
         [UITestMethod]
         public void Test_UnselectUsingIndex()
         {
             var items = Enumerable.Range(1, 10).ToArray();
-            var masterDetailsView = new ListDetailsView();
-            masterDetailsView.ItemsSource = items;
-            masterDetailsView.SelectedIndex = 5;            
-            masterDetailsView.SelectedIndex = -1;
-            Assert.IsNull(masterDetailsView.SelectedItem);
+            var listDetailsView = new ListDetailsView();
+            listDetailsView.ItemsSource = items;
+            listDetailsView.SelectedIndex = 5;            
+            listDetailsView.SelectedIndex = -1;
+            Assert.IsNull(listDetailsView.SelectedItem);
         }
 
-        [TestCategory("MasterDetailsView")]
+        [TestCategory("ListDetailsView")]
         [UITestMethod]
         public void Test_UnselectUsingItem()
         {
             var items = Enumerable.Range(1, 10).ToArray();
-            var masterDetailsView = new ListDetailsView();
-            masterDetailsView.ItemsSource = items;
-            masterDetailsView.SelectedItem = items[5];
-            masterDetailsView.SelectedItem = null;
-            Assert.AreEqual(-1, masterDetailsView.SelectedIndex);
+            var listDetailsView = new ListDetailsView();
+            listDetailsView.ItemsSource = items;
+            listDetailsView.SelectedItem = items[5];
+            listDetailsView.SelectedItem = null;
+            Assert.AreEqual(-1, listDetailsView.SelectedIndex);
         }
 
-        [TestCategory("MasterDetailsView")]
+        [TestCategory("ListDetailsView")]
         [UITestMethod]
         public void Test_SelectedItem_Syncs_SelectedIndex()
         {
             var items = Enumerable.Range(0, 10).ToArray();
-            var masterDetailsView = new ListDetailsView();
-            masterDetailsView.ItemsSource = items;
-            masterDetailsView.SelectedItem = items[3];
-            Assert.AreEqual(3, masterDetailsView.SelectedIndex);
+            var listDetailsView = new ListDetailsView();
+            listDetailsView.ItemsSource = items;
+            listDetailsView.SelectedItem = items[3];
+            Assert.AreEqual(3, listDetailsView.SelectedIndex);
         }
     }
 }
