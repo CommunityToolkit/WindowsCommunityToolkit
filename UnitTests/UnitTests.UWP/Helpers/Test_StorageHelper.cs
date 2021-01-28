a34ce96b0f6a2a18cc6099e1526aaebcd38fcf1e
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-01-28 01:47:03 -0800

    Fix unresolved comment link.

--- UnitTests/UnitTests.UWP/Helpers/Test_StorageHelper.cs
+++ UnitTests/UnitTests.UWP/Helpers/Test_StorageHelper.cs
@@ -2,12 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Microsoft.Toolkit.Uwp.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using Newtonsoft.Json;
 using UnitTests.UWP.Helpers;
-using System.Diagnostics.Contracts;
+using Windows.Storage;
 
 namespace UnitTests.Helpers
 {
