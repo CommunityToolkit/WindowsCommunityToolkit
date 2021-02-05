9060e8ac17584c975c669a0282c956d9e99defc0
Joshua Askharoun <jjask7@gmail.com>
2021-02-05 00:16:05 -0600

    Fixed a bug where the area behind CommandBars during animations would not match the CommandBar's background

--- Microsoft.Toolkit.Uwp.UI.Controls/TabbedCommandBar/TabbedCommandBar.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/TabbedCommandBar/TabbedCommandBar.cs
@@ -17,26 +17,28 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
     [ContentProperty(Name = nameof(MenuItems))]
     [TemplatePart(Name = "PART_RibbonNavigationView", Type = typeof(NavigationView))]
     [TemplatePart(Name = "PART_RibbonContent", Type = typeof(ContentControl))]
+    [TemplatePart(Name = "PART_RibbonContentBorder", Type = typeof(Border))]
     [TemplatePart(Name = "PART_TabChangedStoryboard", Type = typeof(Storyboard))]
     public class TabbedCommandBar : NavigationView
     {
         private ContentControl _ribbonContent = null;
+        private Border _ribbonContentBorder = null;
         private Storyboard _tabChangedStoryboard = null;
 
         /// <summary>
-        /// Gets or sets the brush to use as the background for content
+        /// Gets or sets the brush to use as the background for <see cref="TabbedCommandBarItem"/>s in MenuItems.
         /// </summary>
-        public Brush ContentBackground
+        public Brush ItemBackground
         {
-            get => (Brush)GetValue(ContentBackgroundProperty);
-            set => SetValue(ContentBackgroundProperty, value);
+            get => (Brush)GetValue(ItemBackgroundProperty);
+            set => SetValue(ItemBackgroundProperty, value);
         }
 
         /// <summary>
-        /// Identifies the <see cref="ContentBackground"/> property.
+        /// Identifies the <see cref="ItemBackground"/> property.
         /// </summary>
-        public static readonly DependencyProperty ContentBackgroundProperty =
-            DependencyProperty.Register(nameof(ContentBackground), typeof(Brush), typeof(TabbedCommandBar), new PropertyMetadata(null));
+        public static readonly DependencyProperty ItemBackgroundProperty =
+            DependencyProperty.Register(nameof(ItemBackground), typeof(Brush), typeof(TabbedCommandBar), new PropertyMetadata(null));
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TabbedCommandBar"/> class.
@@ -45,7 +47,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         {
             DefaultStyleKey = typeof(TabbedCommandBar);
 
-            SelectionChanged += RibbonNavigationView_SelectionChanged;
+            SelectionChanged += (NavigationView sender, NavigationViewSelectionChangedEventArgs args) =>
+            {
+                _ribbonContentBorder.Background = (sender.SelectedItem as Control).Background;
+                _tabChangedStoryboard?.Begin();
+            };
         }
 
         /// <inheritdoc/>
@@ -60,24 +66,10 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
 
             // Get RibbonContent first, since setting SelectedItem requires it
             _ribbonContent = GetTemplateChild("PART_RibbonContent") as ContentControl;
+            _ribbonContentBorder = GetTemplateChild("PART_RibbonContentBorder") as Border;
             _tabChangedStoryboard = GetTemplateChild("TabChangedStoryboard") as Storyboard;
 
             SelectedItem = MenuItems.FirstOrDefault();
-        }
-
-        private void RibbonNavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            if (args.SelectedItem is TabbedCommandBarItem item)
-            {
-                _ribbonContent.Content = item;
-                _tabChangedStoryboard?.Begin();
-            }
-            else if (args.SelectedItem is NavigationViewItem navItem)
-            {
-                // This code is a hack, but it's necessary if binding doesn't work.
-                // RibbonContent might be null here, there should be a check
-                _ribbonContent.Content = MenuItems[System.Math.Min(MenuItems.Count - 1, MenuItems.IndexOf(navItem))];
-            }
         }
     }
 }
