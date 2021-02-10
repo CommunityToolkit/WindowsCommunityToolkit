e6a0257eb75a084003a7ae4092594f13cbdaeef2
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-02-10 00:24:20 -0800

    Add Missing Header

--- UnitTests/UnitTests.UWP/Extensions/Test_LogicalTreeExtensions.cs
+++ UnitTests/UnitTests.UWP/Extensions/Test_LogicalTreeExtensions.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.Toolkit.Uwp.Extensions;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Extensions;
 using Microsoft.Toolkit.Uwp.UI.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Markup;
