00e5965bf7655bf4e747bce14d3ecff6d284fcf8
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-02-09 16:15:15 -0800

    Add an initial Logical Tree Extension FindParent method test
    Required some new Test Infrastructure adapted from WinUI setup, but simplified.
    However, running into deadlock with TaskCompletionSource after test execution (test itself is succeeding)
    This may help? https://devblogs.microsoft.com/premier-developer/the-danger-of-taskcompletionsourcet-class/

---
+++ UnitTests/UnitTests.UWP/Extensions/Test_LogicalTreeExtensions.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Toolkit.Uwp.Extensions;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace UnitTests.Extensions
+{
+    [TestClass]
+    public class Test_LogicalTreeExtensions : VisualUITestBase
+    {
+        [TestCategory("LogicalTree")]
+        [TestMethod]
+        public async Task Test_LogicalTree_FindParent_Exists()
+        {
+            await App.DispatcherQueue.EnqueueAsync(async () =>
+            {
+                var treeRoot = XamlReader.Load(@"<Page
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+    <Grid>
+        <Grid> <!-- Target -->
+            <Border/>
+            <Border>
+                <TextBlock/> <!-- Starting Point -->
+            </Border>
+        </Grid>
+    </Grid>
+</Page>") as Page;
+
+                // Test Setup
+                Assert.IsNotNull(treeRoot, "XAML Failed to Load");
+
+                // Initialize Visual Tree
+                await SetTestContentAsync(treeRoot);
+
+                var outerGrid = treeRoot.Content as Grid;
+
+                Assert.IsNotNull(outerGrid, "Couldn't find Page content.");
+
+                var targetGrid = outerGrid.Children.FirstOrDefault() as Grid;
+                Assert.IsNotNull(targetGrid, "Couldn't find Target Grid");
+                Assert.AreEqual(2, targetGrid.Children.Count, "Grid doesn't have right number of children.");
+
+                var secondBorder = targetGrid.Children[1] as Border;
+                Assert.IsNotNull(secondBorder, "Border not found.");
+
+                var startingPoint = secondBorder.Child as FrameworkElement;
+                Assert.IsNotNull(startingPoint, "Could not find starting element.");
+
+                // Main Test
+                var grid = startingPoint.FindParent<Grid>();
+
+                Assert.IsNotNull(grid, "Expected to find Grid");
+                Assert.AreEqual(targetGrid, grid, "Grid didn't match expected.");
+            });
+        }
+    }
+}
