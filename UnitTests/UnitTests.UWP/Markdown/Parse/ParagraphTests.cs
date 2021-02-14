13bfbf21c7e22556e40431722d638bd119ef0c87
Nirmal Guru <Nirmal4G@gmail.com>
2021-02-14 14:02:07 +0530

    Fix Markdown tests with trailing whitespace
    As we save files with trailing white-space removed,
    the markdown parser tests that depends on the text
    with trailing white-space fails.
    
    So, to prevent any future formatting changes affecting the test result,
    we'll replace the actual trailing space(s) with an interpolated variable
    containing those trailing space(s).

--- UnitTests/UnitTests.UWP/Markdown/Parse/ParagraphTests.cs
+++ UnitTests/UnitTests.UWP/Markdown/Parse/ParagraphTests.cs
@@ -36,8 +36,9 @@ namespace UnitTests.Markdown.Parse
         public void Paragraph_NoLineBreak_OneSpace()
         {
             // A line break in the markup does not translate to a line break in the resulting formatted text.
-            AssertEqual(CollapseWhitespace(@"
-                line 1 
+            var space = " ";
+            AssertEqual(CollapseWhitespace($@"
+                line 1{space}
                 line 2"),
                 new ParagraphBlock().AddChildren(
                     new TextRunInline { Text = "line 1  line 2" }));
@@ -48,9 +49,10 @@ namespace UnitTests.Markdown.Parse
         public void Paragraph_LineBreak()
         {
             // Two spaces at the end of the line results in a line break.
-            AssertEqual(CollapseWhitespace(@"
-                line 1  
-                line 2 with *italic  
+            var spaces = "  ";
+            AssertEqual(CollapseWhitespace($@"
+                line 1{spaces}
+                line 2 with *italic{spaces}
                 formatting*"),
                 new ParagraphBlock().AddChildren(
                     new TextRunInline { Text = "line 1\r\nline 2 with " },
@@ -63,8 +65,9 @@ namespace UnitTests.Markdown.Parse
         public void Paragraph_LineBreak_ThreeSpaces()
         {
             // Three spaces at the end of the line also results in a line break.
-            AssertEqual(CollapseWhitespace(@"
-                line 1   
+            var spaces = "   ";
+            AssertEqual(CollapseWhitespace($@"
+                line 1{spaces}
                 line 2"),
                 new ParagraphBlock().AddChildren(
                     new TextRunInline { Text = "line 1 \r\nline 2" }));
@@ -90,9 +93,10 @@ namespace UnitTests.Markdown.Parse
         public void Paragraph_NewParagraph_Whitespace()
         {
             // A line that contains only whitespace starts a new paragraph.
-            AssertEqual(CollapseWhitespace(@"
+            var spaces = "      ";
+            AssertEqual(CollapseWhitespace($@"
                 line 1
-                      
+                {spaces}
                 line 2"),
                 new ParagraphBlock().AddChildren(
                     new TextRunInline { Text = "line 1" }),
