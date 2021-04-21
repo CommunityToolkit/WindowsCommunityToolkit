319b7354a0b0ffcf685047c343c7556ede5fc928
Sergio Pedri <sergio0694@live.com>
2021-04-21 22:52:51 +0200

    Added unit test for VisualExtensions.Translation property

---
+++ UnitTests/UnitTests.UWP/UI/Extensions/Test_VisualExtensions.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Toolkit.Uwp;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Toolkit.Uwp.UI;
+using System.Numerics;
+
+namespace UnitTests.UWP.UI
+{
+    [TestClass]
+    [TestCategory("Test_VisualExtensions")]
+    public class Test_VisualExtensions : VisualUITestBase
+    {
+        [TestMethod]
+        public async Task SetAndGetTranslation()
+        {
+            await App.DispatcherQueue.EnqueueAsync(async () =>
+            {
+                var button = new Button();
+                var grid = new Grid() { Children = { button } };
+
+                VisualExtensions.SetTranslation(button, "80, 20, 0");
+
+                await SetTestContentAsync(grid);
+
+                Assert.AreEqual(button.GetVisual().TransformMatrix.Translation, new Vector3(80, 20, 0));
+
+                string translation = VisualExtensions.GetTranslation(button);
+
+                Assert.AreEqual(translation, new Vector3(80, 20, 0).ToString());
+            });
+        }
+    }
+}
