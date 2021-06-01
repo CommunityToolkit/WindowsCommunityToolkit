463cd67ecf8c7e9bb4c9526b1c8e799de4678d2f
Rosario Pulella <RosarioPulella@gmail.com>
2021-06-01 12:42:17 -0400

    Set should restart app instead of dispose helper

--- UITests/UITests.Tests.Shared/UITestBase.cs
+++ UITests/UITests.Tests.Shared/UITestBase.cs
@@ -178,7 +178,7 @@ namespace UITests.Tests
         [TestCleanup]
         public async Task TestCleanup()
         {
-            helper.Dispose();
+            TestEnvironment.ShouldRestartApplication = true;
             TestCleanupHelper.Cleanup();
         }
 
