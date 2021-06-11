5a6bf626c093c52b23f90980f801eb610e9b545c
DaZombieKiller <bennyboy.private@hotmail.com.au>
2021-06-11 20:20:16 +1000

    Add RefEnumerable indexer exception tests

--- UnitTests/UnitTests.HighPerformance.Shared/Enumerables/Test_ReadOnlyRefEnumerable{T}.cs
+++ UnitTests/UnitTests.HighPerformance.Shared/Enumerables/Test_ReadOnlyRefEnumerable{T}.cs
@@ -50,11 +50,11 @@ namespace UnitTests.HighPerformance.Enumerables
 
         [TestCategory("ReadOnlyRefEnumerable")]
         [TestMethod]
-        [DataRow(1, 1, new[] { 1 })]
-        [DataRow(4, 1, new[] { 1, 2, 3, 4 })]
-        [DataRow(4, 4, new[] { 1, 5, 9, 13 })]
-        [DataRow(4, 5, new[] { 1, 6, 11, 16 })]
-        public void Test_ReadOnlyRefEnumerable_Indexer(int length, int step, int[] values)
+        [DataRow(1, new[] { 1 })]
+        [DataRow(1, new[] { 1, 2, 3, 4 })]
+        [DataRow(4, new[] { 1, 5, 9, 13 })]
+        [DataRow(5, new[] { 1, 6, 11, 16 })]
+        public void Test_ReadOnlyRefEnumerable_Indexer(int step, int[] values)
         {
             Span<int> data = new[]
             {
@@ -64,7 +64,7 @@ namespace UnitTests.HighPerformance.Enumerables
                 13, 14, 15, 16
             };
 
-            ReadOnlyRefEnumerable<int> enumerable = ReadOnlyRefEnumerable<int>.DangerousCreate(in data[0], length, step);
+            ReadOnlyRefEnumerable<int> enumerable = ReadOnlyRefEnumerable<int>.DangerousCreate(in data[0], values.Length, step);
 
             for (int i = 0; i < enumerable.Length; i++)
             {
@@ -72,14 +72,27 @@ namespace UnitTests.HighPerformance.Enumerables
             }
         }
 
+        [TestCategory("ReadOnlyRefEnumerable")]
+        [TestMethod]
+        public void Test_ReadOnlyRefEnumerable_Indexer_ThrowsIndexOutOfRange()
+        {
+            int[] array = new[]
+            {
+                0, 0, 0, 0
+            };
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => ReadOnlyRefEnumerable<int>.DangerousCreate(in array[0], array.Length, 1)[-1]);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => ReadOnlyRefEnumerable<int>.DangerousCreate(in array[0], array.Length, 1)[array.Length]);
+        }
+
 #if NETCOREAPP3_1_OR_GREATER
         [TestCategory("ReadOnlyRefEnumerable")]
         [TestMethod]
-        [DataRow(1, 1, new[] { 1 })]
-        [DataRow(4, 1, new[] { 1, 2, 3, 4 })]
-        [DataRow(4, 4, new[] { 1, 5, 9, 13 })]
-        [DataRow(4, 5, new[] { 1, 6, 11, 16 })]
-        public void Test_ReadOnlyRefEnumerable_Index_Indexer(int length, int step, int[] values)
+        [DataRow(1, new[] { 1 })]
+        [DataRow(1, new[] { 1, 2, 3, 4 })]
+        [DataRow(4, new[] { 1, 5, 9, 13 })]
+        [DataRow(5, new[] { 1, 6, 11, 16 })]
+        public void Test_ReadOnlyRefEnumerable_Index_Indexer(int step, int[] values)
         {
             Span<int> data = new[]
             {
@@ -89,9 +102,25 @@ namespace UnitTests.HighPerformance.Enumerables
                 13, 14, 15, 16
             };
 
-            ReadOnlyRefEnumerable<int> enumerable = ReadOnlyRefEnumerable<int>.DangerousCreate(in data[0], length, step);
+            ReadOnlyRefEnumerable<int> enumerable = ReadOnlyRefEnumerable<int>.DangerousCreate(in data[0], values.Length, step);
 
-            Assert.AreEqual(values[^1], enumerable[^1]);
+            for (int i = 1; i <= enumerable.Length; i++)
+            {
+                Assert.AreEqual(values[^i], enumerable[^i]);
+            }
+        }
+
+        [TestCategory("ReadOnlyRefEnumerable")]
+        [TestMethod]
+        public void Test_ReadOnlyRefEnumerable_Index_Indexer_ThrowsIndexOutOfRange()
+        {
+            int[] array = new[]
+            {
+                0, 0, 0, 0
+            };
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => ReadOnlyRefEnumerable<int>.DangerousCreate(in array[0], array.Length, 1)[new Index(array.Length)]);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => ReadOnlyRefEnumerable<int>.DangerousCreate(in array[0], array.Length, 1)[^0]);
         }
 #endif
     }
--- UnitTests/UnitTests.HighPerformance.Shared/Enumerables/Test_RefEnumerable{T}.cs
+++ UnitTests/UnitTests.HighPerformance.Shared/Enumerables/Test_RefEnumerable{T}.cs
@@ -50,11 +50,11 @@ namespace UnitTests.HighPerformance.Enumerables
 
         [TestCategory("RefEnumerable")]
         [TestMethod]
-        [DataRow(1, 1, new[] { 1 })]
-        [DataRow(4, 1, new[] { 1, 2, 3, 4 })]
-        [DataRow(4, 4, new[] { 1, 5, 9, 13 })]
-        [DataRow(4, 5, new[] { 1, 6, 11, 16 })]
-        public void Test_RefEnumerable_Indexer(int length, int step, int[] values)
+        [DataRow(1, new[] { 1 })]
+        [DataRow(1, new[] { 1, 2, 3, 4 })]
+        [DataRow(4, new[] { 1, 5, 9, 13 })]
+        [DataRow(5, new[] { 1, 6, 11, 16 })]
+        public void Test_RefEnumerable_Indexer(int step, int[] values)
         {
             Span<int> data = new[]
             {
@@ -64,7 +64,7 @@ namespace UnitTests.HighPerformance.Enumerables
                 13, 14, 15, 16
             };
 
-            RefEnumerable<int> enumerable = RefEnumerable<int>.DangerousCreate(ref data[0], length, step);
+            RefEnumerable<int> enumerable = RefEnumerable<int>.DangerousCreate(ref data[0], values.Length, step);
 
             for (int i = 0; i < enumerable.Length; i++)
             {
@@ -72,13 +72,26 @@ namespace UnitTests.HighPerformance.Enumerables
             }
         }
 
+        [TestCategory("RefEnumerable")]
+        [TestMethod]
+        public void Test_RefEnumerable_Indexer_ThrowsIndexOutOfRange()
+        {
+            int[] array = new[]
+            {
+                0, 0, 0, 0
+            };
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => RefEnumerable<int>.DangerousCreate(ref array[0], array.Length, 1)[-1]);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => RefEnumerable<int>.DangerousCreate(ref array[0], array.Length, 1)[array.Length]);
+        }
+
 #if NETCOREAPP3_1_OR_GREATER
         [TestCategory("RefEnumerable")]
         [TestMethod]
-        [DataRow(1, 1, new[] { 1 })]
-        [DataRow(4, 1, new[] { 1, 2, 3, 4 })]
-        [DataRow(4, 4, new[] { 1, 5, 9, 13 })]
-        [DataRow(4, 5, new[] { 1, 6, 11, 16 })]
+        [DataRow(1, new[] { 1 })]
+        [DataRow(1, new[] { 1, 2, 3, 4 })]
+        [DataRow(4, new[] { 1, 5, 9, 13 })]
+        [DataRow(5, new[] { 1, 6, 11, 16 })]
         public void Test_RefEnumerable_Index_Indexer(int length, int step, int[] values)
         {
             Span<int> data = new[]
@@ -89,9 +102,25 @@ namespace UnitTests.HighPerformance.Enumerables
                 13, 14, 15, 16
             };
 
-            RefEnumerable<int> enumerable = RefEnumerable<int>.DangerousCreate(ref data[0], length, step);
+            RefEnumerable<int> enumerable = RefEnumerable<int>.DangerousCreate(ref data[0], values.Length, step);
 
-            Assert.AreEqual(values[^1], enumerable[^1]);
+            for (int i = 1; i <= enumerable.Length; i++)
+            {
+                Assert.AreEqual(values[^i], enumerable[^i]);
+            }
+        }
+
+        [TestCategory("RefEnumerable")]
+        [TestMethod]
+        public void Test_RefEnumerable_Index_Indexer_ThrowsIndexOutOfRange()
+        {
+            int[] array = new[]
+            {
+                0, 0, 0, 0
+            };
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => RefEnumerable<int>.DangerousCreate(ref array[0], array.Length, 1)[new Index(array.Length)]);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => RefEnumerable<int>.DangerousCreate(ref array[0], array.Length, 1)[^0]);
         }
 #endif
     }
