12981371f438c06a9aaf14fe6c7f93b0526e6d50
Arcadio Garcia Salvadores <arcadiog@microsoft.com>
2021-07-20 15:04:27 -0700

    Address PR feedback

--- Microsoft.Toolkit.Uwp.UI.Controls.Core/DropShadowPanel/IAlphaMaskProvider.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Core/DropShadowPanel/IAlphaMaskProvider.cs
@@ -4,14 +4,17 @@
 
 using Windows.UI.Composition;
 
-/// <summary>
-/// Any user control can implement this interface to provide a custom alpha mask to it's parent <see cref="DropShadowPanel"/>
-/// </summary>
-public interface IAlphaMaskProvider
+namespace Microsoft.Toolkit.Uwp.UI
 {
     /// <summary>
-    /// This method should return the appropiate alpha mask to be used in the shadow of this control
+    /// Any user control can implement this interface to provide a custom alpha mask to it's parent DropShadowPanel
     /// </summary>
-    /// <returns>The alpha mask as a composition brush</returns>
-    CompositionBrush GetAlphaMask();
+    public interface IAlphaMaskProvider
+    {
+        /// <summary>
+        /// This method should return the appropiate alpha mask to be used in the shadow of this control
+        /// </summary>
+        /// <returns>The alpha mask as a composition brush</returns>
+        CompositionBrush GetAlphaMask();
+    }
 }
