3b1518a6cf94dcd26f678e99f8a4266c9c599866
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-07-22 12:20:07 -0700

    Rename ConstrainedPresenter to ConstrainedBox

--- Microsoft.Toolkit.Uwp.UI.Controls.Primitives/ConstrainedBox/AspectRatio.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Primitives/ConstrainedBox/AspectRatio.cs
@@ -7,7 +7,7 @@ using System;
 namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
     /// <summary>
-    /// The <see cref="AspectRatio"/> structure is used by the <see cref="ConstrainedPresenter"/> control to
+    /// The <see cref="AspectRatio"/> structure is used by the <see cref="ConstrainedBox"/> control to
     /// define a specific ratio to restrict its content.
     /// </summary>
     [Windows.Foundation.Metadata.CreateFromString(MethodName = "Microsoft.Toolkit.Uwp.UI.Controls.AspectRatio.ConvertToAspectRatio")]
--- Microsoft.Toolkit.Uwp.UI.Controls.Primitives/ConstrainedBox/ConstrainedBox.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Primitives/ConstrainedBox/ConstrainedBox.cs
@@ -14,10 +14,18 @@ using Windows.UI.Xaml.Controls;
 namespace Microsoft.Toolkit.Uwp.UI.Controls
 {
     /// <summary>
-    /// The <see cref="ConstrainedPresenter"/> is a <see cref="ContentPresenter"/> control which can restrict the
+    /// The <see cref="ConstrainedBox"/> is a <see cref="FrameworkElement"/> control akin to <see cref="Viewbox"/>
+    /// which can modify the behavior of it's child element's layout. <see cref="ConstrainedBox"/> restricts the
     /// available size for its content based on a scale factor and/or a specific <see cref="AspectRatio"/>.
+    /// This is performed as a layout calculation modification.
     /// </summary>
-    public class ConstrainedPresenter : ContentPresenter // TODO: Think it should be a Border? But it's sealed; hopefully can change in WinUI 3.
+    /// <remarks>
+    /// Note that this class being implemented as a <see cref="ContentPresenter"/> is an implementation detail, and
+    /// is not meant to be used as one with a template. It is recommended to avoid styling the frame of the control
+    /// with borders and not using <see cref="ContentPresenter.ContentTemplate"/> for future compatibility of your
+    /// code if moving to WinUI 3 in the future.
+    /// </remarks>
+    public class ConstrainedBox : ContentPresenter // TODO: Should be FrameworkElement directly, see https://github.com/microsoft/microsoft-ui-xaml/issues/5530
     {
         /// <summary>
         /// Gets or sets aspect Ratio to use for the contents of the Panel (after scaling).
@@ -32,11 +40,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         /// Identifies the <see cref="AspectRatio"/> property.
         /// </summary>
         public static readonly DependencyProperty AspectRatioProperty =
-            DependencyProperty.Register(nameof(AspectRatio), typeof(AspectRatio), typeof(ConstrainedPresenter), new PropertyMetadata(null, AspectRatioPropertyChanged));
+            DependencyProperty.Register(nameof(AspectRatio), typeof(AspectRatio), typeof(ConstrainedBox), new PropertyMetadata(null, AspectRatioPropertyChanged));
 
         private static void AspectRatioPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d is ConstrainedPresenter panel)
+            if (d is ConstrainedBox panel)
             {
                 panel.InvalidateMeasure();
             }
@@ -55,7 +63,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         /// Identifies the <see cref="ScaleX"/> property.
         /// </summary>
         public static readonly DependencyProperty ScaleXProperty =
-            DependencyProperty.Register(nameof(ScaleX), typeof(double), typeof(ConstrainedPresenter), new PropertyMetadata(1.0, ScalePropertyChanged));
+            DependencyProperty.Register(nameof(ScaleX), typeof(double), typeof(ConstrainedBox), new PropertyMetadata(1.0, ScalePropertyChanged));
 
         /// <summary>
         /// Gets or sets the scale for the height of the panel. Should be a value between 0-1.0. Default is 1.0.
@@ -70,11 +78,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         /// Identifies the <see cref="ScaleY"/> property.
         /// </summary>
         public static readonly DependencyProperty ScaleYProperty =
-            DependencyProperty.Register(nameof(ScaleY), typeof(double), typeof(ConstrainedPresenter), new PropertyMetadata(1.0, ScalePropertyChanged));
+            DependencyProperty.Register(nameof(ScaleY), typeof(double), typeof(ConstrainedBox), new PropertyMetadata(1.0, ScalePropertyChanged));
 
         private static void ScalePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d is ConstrainedPresenter panel)
+            if (d is ConstrainedBox panel)
             {
                 panel.InvalidateMeasure();
             }
