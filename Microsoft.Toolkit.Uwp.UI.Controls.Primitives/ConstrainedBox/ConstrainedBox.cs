0fd2a3706ef2a6ad3b58540a379f2b9b07c37d49
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-07-28 19:51:39 -0700

    Merge implementations of ConstrainedBox to handle all the scenarios
    1px Rounding was caused by multiple unnecessary passes of Arrange, so only re-calculated if needed (when parent Panel doesn't respect Measure request i.e. Grid Stretch)
    Also adds in handling for Infinity scenarios like StackPanel/ScrollViewer and can try and respect child measurements
    Adds initial basic tests as a starting point for more tests

--- Microsoft.Toolkit.Uwp.UI.Controls.Primitives/ConstrainedBox/ConstrainedBox.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Primitives/ConstrainedBox/ConstrainedBox.cs
@@ -88,39 +88,102 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             }
         }
 
+        private bool IsPositiveRealNumber(double value) => !double.IsNaN(value) && !double.IsInfinity(value) && value > 0;
+
+        private Size _lastMeasuredSize;
+
         /// <inheritdoc/>
         protected override Size MeasureOverride(Size availableSize)
         {
-            return base.MeasureOverride(CalculateConstrainedSize(availableSize));
+            CalculateConstrainedSize(ref availableSize);
+
+            _lastMeasuredSize = availableSize;
+
+            // Call base.MeasureOverride so any child elements know what room there is to work with.
+            // Don't return this though. An image that hasn't loaded yet for example will request very little space.
+            base.MeasureOverride(_lastMeasuredSize);
+            return _lastMeasuredSize;
         }
+
+        //// Our Arrange pass should just use the value we calculated in Measure, so we don't have extra work to do (at least the ContentPresenter we use presently does it for us.)
 
         /// <inheritdoc/>
         protected override Size ArrangeOverride(Size finalSize)
         {
-            return base.ArrangeOverride(CalculateConstrainedSize(finalSize));
+            // Even though we requested in measure to be a specific size, that doesn't mean our parent
+            // panel respected that request. Grid for instance can by default Stretch and if you don't
+            // set Horizontal/VerticalAlignment on the control it won't constrain as we expect.
+            // We could also be in a StackPanel/ScrollViewer where it wants to provide as much space as possible.
+            // However, if we always re-calculate even if we are provided the proper finalSize, this can trigger
+            // multiple arrange passes and cause a rounding error in layout. Therefore, we only want to
+            // re-calculate if we think we will have a significant impact.
+            //// TODO: Not sure what good tolerance is here
+            if (Math.Abs(finalSize.Width - _lastMeasuredSize.Width) > 1.5 ||
+                Math.Abs(finalSize.Height - _lastMeasuredSize.Height) > 1.5)
+            {
+                CalculateConstrainedSize(ref finalSize);
+
+                // Copy again so if Arrange is re-triggered we won't re-calculate.
+                _lastMeasuredSize = finalSize;
+            }
+
+            return base.ArrangeOverride(finalSize);
         }
 
-        private Size CalculateConstrainedSize(Size initialSize)
+        private void CalculateConstrainedSize(ref Size availableSize)
         {
-            var availableSize = new Size(initialSize.Width * ScaleX, initialSize.Height * ScaleY);
+            var hasWidth = IsPositiveRealNumber(availableSize.Width);
+            var hasHeight = IsPositiveRealNumber(availableSize.Height);
+
+            if (!hasWidth && !hasHeight)
+            {
+                // We have infinite space, like a ScrollViewer with both scrolling directions
+                // Ask child how big they want to be first.
+                availableSize = base.MeasureOverride(availableSize);
+
+                hasWidth = IsPositiveRealNumber(availableSize.Width);
+                hasHeight = IsPositiveRealNumber(availableSize.Height);
+
+                if (!hasWidth && !hasHeight)
+                {
+                    // At this point we have no way to determine a constraint, the Panel won't do anything
+                    // This should be rare? We don't really have a way to provide a warning here.
+                    return;
+                }
+            }
+
+            // Scale size first before we constrain aspect ratio
+            availableSize.Width *= ScaleX;
+            availableSize.Height *= ScaleY;
 
             // If we don't have an Aspect Ratio, just return the scaled value.
             if (ReadLocalValue(AspectRatioProperty) == DependencyProperty.UnsetValue)
             {
-                return availableSize;
+                return;
             }
 
             // Calculate the Aspect Ratio constraint based on the newly scaled size.
             var currentAspect = availableSize.Width / availableSize.Height;
-            var desiredAspect = AspectRatio.Value;
 
-            if (currentAspect >= desiredAspect)
+            if (!hasWidth)
             {
-                return new Size(availableSize.Height * desiredAspect, availableSize.Height);
+                // If available width is infinite, set width based on height
+                availableSize.Width = availableSize.Height * AspectRatio;
+            }
+            else if (!hasHeight)
+            {
+                // If avalable height is infinite, set height based on width
+                availableSize.Height = availableSize.Width / AspectRatio;
+            }
+            else if (currentAspect > AspectRatio)
+            {
+                // If the container aspect ratio is wider than our aspect ratio, set width based on height
+                availableSize.Width = availableSize.Height * AspectRatio;
             }
             else
             {
-                return new Size(availableSize.Width, availableSize.Width / desiredAspect);
+                // If the container aspect ratio is taller than our aspect ratio, set height based on width
+                availableSize.Height = availableSize.Width / AspectRatio;
             }
         }
     }
