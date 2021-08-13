75e53c9116e7ff001dc25e663b7abed396bf65fc
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-08-12 17:06:01 -0700

    Rename Shadows.Attached to Effects.Shadow

--- Microsoft.Toolkit.Uwp.UI/Shadows/Effects.cs
+++ Microsoft.Toolkit.Uwp.UI/Shadows/Effects.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -9,33 +9,33 @@ namespace Microsoft.Toolkit.Uwp.UI
     /// <summary>
     /// Helper class for attaching <see cref="AttachedShadowBase"/> shadows to <see cref="FrameworkElement"/>s.
     /// </summary>
-    public static class Shadows
+    public static class Effects
     {
         /// <summary>
-        /// Gets the shadow attached to a <see cref="FrameworkElement"/> by getting the value of the <see cref="AttachedProperty"/> property.
+        /// Gets the shadow attached to a <see cref="FrameworkElement"/> by getting the value of the <see cref="ShadowProperty"/> property.
         /// </summary>
         /// <param name="obj">The <see cref="FrameworkElement"/> the <see cref="AttachedShadowBase"/> is attached to.</param>
         /// <returns>The <see cref="AttachedShadowBase"/> that is attached to the <paramref name="obj">FrameworkElement.</paramref></returns>
-        public static AttachedShadowBase GetAttached(FrameworkElement obj)
+        public static AttachedShadowBase GetShadow(FrameworkElement obj)
         {
-            return (AttachedShadowBase)obj.GetValue(AttachedProperty);
+            return (AttachedShadowBase)obj.GetValue(ShadowProperty);
         }
 
         /// <summary>
-        /// Attaches a shadow to an element by setting the <see cref="AttachedProperty"/> property.
+        /// Attaches a shadow to an element by setting the <see cref="ShadowProperty"/> property.
         /// </summary>
         /// <param name="obj">The <see cref="FrameworkElement"/> to attach the shadow to.</param>
         /// <param name="value">The <see cref="AttachedShadowBase"/> that will be attached to the element</param>
-        public static void SetAttached(FrameworkElement obj, AttachedShadowBase value)
+        public static void SetShadow(FrameworkElement obj, AttachedShadowBase value)
         {
-            obj.SetValue(AttachedProperty, value);
+            obj.SetValue(ShadowProperty, value);
         }
 
         /// <summary>
         /// Attached <see cref="DependencyProperty"/> for setting an <see cref="AttachedShadowBase"/> to a <see cref="FrameworkElement"/>.
         /// </summary>
-        public static readonly DependencyProperty AttachedProperty =
-            DependencyProperty.RegisterAttached("Attached", typeof(AttachedShadowBase), typeof(Shadows), new PropertyMetadata(null, OnShadowChanged));
+        public static readonly DependencyProperty ShadowProperty =
+            DependencyProperty.RegisterAttached("Shadow", typeof(AttachedShadowBase), typeof(Effects), new PropertyMetadata(null, OnShadowChanged));
 
         private static void OnShadowChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
