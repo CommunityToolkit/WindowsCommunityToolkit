e40e461986fea63791afebaf71adb8bdb02f493b
Tung Huynh <tung75605@gmail.com>
2021-08-15 12:58:24 -0700

    add richsuggestbox UI test

---
+++ UITests/UITests.Tests.Shared/Controls/RichSuggestBoxTestPage.xaml.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.App.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RichSuggestBoxTestPage : Page
+    {
+        private static readonly List<string> _suggestions = new List<string> { "Token1", "Token2", "Token3" };
+
+        public RichSuggestBoxTestPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RichSuggestBox_OnSuggestionsRequested(RichSuggestBox sender, SuggestionsRequestedEventArgs args)
+        {
+            sender.ItemsSource = _suggestions.Select(x => args.Query + x);
+        }
+
+        private void RichSuggestBox_OnSuggestionsChosen(RichSuggestBox sender, SuggestionChosenEventArgs args)
+        {
+            args.Text = (string)args.SelectedItem;
+        }
+    }
+}
