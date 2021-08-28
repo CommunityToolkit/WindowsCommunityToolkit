a37860dc3c8d2350f8e3a3bd5d1510cbf35300d1
Tung Huynh <tung75605@gmail.com>
2021-08-28 01:37:02 -0700

    adjust category  attributes

--- Microsoft.Toolkit.Uwp.UI.Controls.Input.Design/Controls/RichSuggestBox.Metadata.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Input.Design/Controls/RichSuggestBox.Metadata.cs
@@ -24,18 +24,27 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls.Design
                     b.AddCustomAttributes(nameof(RichSuggestBox.Description), new CategoryAttribute(Resources.CategoryCommon));
                     b.AddCustomAttributes(nameof(RichSuggestBox.DisabledFormattingAccelerators), new CategoryAttribute(Resources.CategoryCommon));
                     b.AddCustomAttributes(nameof(RichSuggestBox.Header), new CategoryAttribute(Resources.CategoryCommon));
-                    b.AddCustomAttributes(nameof(RichSuggestBox.HeaderTemplate), new CategoryAttribute(Resources.CategoryCommon));
+                    b.AddCustomAttributes(nameof(RichSuggestBox.HeaderTemplate),
+                        new CategoryAttribute(Resources.CategoryAppearance),
+                        new EditorBrowsableAttribute(EditorBrowsableState.Advanced)
+                    );
                     b.AddCustomAttributes(nameof(RichSuggestBox.PlaceholderText), new CategoryAttribute(Resources.CategoryCommon));
-                    b.AddCustomAttributes(nameof(RichSuggestBox.PopupCornerRadius), new CategoryAttribute(Resources.CategoryCommon));
+                    b.AddCustomAttributes(nameof(RichSuggestBox.PopupCornerRadius), new CategoryAttribute(Resources.CategoryAppearance));
                     b.AddCustomAttributes(nameof(RichSuggestBox.PopupFooter), new CategoryAttribute(Resources.CategoryCommon));
-                    b.AddCustomAttributes(nameof(RichSuggestBox.PopupFooterTemplate), new CategoryAttribute(Resources.CategoryCommon));
+                    b.AddCustomAttributes(nameof(RichSuggestBox.PopupFooterTemplate),
+                        new CategoryAttribute(Resources.CategoryAppearance),
+                        new EditorBrowsableAttribute(EditorBrowsableState.Advanced)
+                    );
                     b.AddCustomAttributes(nameof(RichSuggestBox.PopupHeader), new CategoryAttribute(Resources.CategoryCommon));
-                    b.AddCustomAttributes(nameof(RichSuggestBox.PopupHeaderTemplate), new CategoryAttribute(Resources.CategoryCommon));
-                    b.AddCustomAttributes(nameof(RichSuggestBox.PopupPlacement), new CategoryAttribute(Resources.CategoryCommon));
+                    b.AddCustomAttributes(nameof(RichSuggestBox.PopupHeaderTemplate),
+                        new CategoryAttribute(Resources.CategoryAppearance),
+                        new EditorBrowsableAttribute(EditorBrowsableState.Advanced)
+                    );
+                    b.AddCustomAttributes(nameof(RichSuggestBox.PopupPlacement), new CategoryAttribute(Resources.CategoryAppearance));
                     b.AddCustomAttributes(nameof(RichSuggestBox.Prefixes), new CategoryAttribute(Resources.CategoryCommon));
-                    b.AddCustomAttributes(nameof(RichSuggestBox.RichEditBoxStyle), new CategoryAttribute(Resources.CategoryCommon));
-                    b.AddCustomAttributes(nameof(RichSuggestBox.TokenBackground), new CategoryAttribute(Resources.CategoryCommon));
-                    b.AddCustomAttributes(nameof(RichSuggestBox.TokenForeground), new CategoryAttribute(Resources.CategoryCommon));
+                    b.AddCustomAttributes(nameof(RichSuggestBox.RichEditBoxStyle), new CategoryAttribute(Resources.CategoryAppearance));
+                    b.AddCustomAttributes(nameof(RichSuggestBox.TokenBackground), new CategoryAttribute(Resources.CategoryBrush));
+                    b.AddCustomAttributes(nameof(RichSuggestBox.TokenForeground), new CategoryAttribute(Resources.CategoryBrush));
                     b.AddCustomAttributes(new ToolboxCategoryAttribute(ToolboxCategoryPaths.Toolkit, false));
                 }
             );
