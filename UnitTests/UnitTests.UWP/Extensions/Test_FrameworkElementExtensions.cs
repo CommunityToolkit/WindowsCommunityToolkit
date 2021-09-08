762818cf7ef94d14f8ec000e8f2fb2489226ea98
Darren Batchelor <v-dbatchelor@microsoft.com>
2021-09-07 17:30:00 -0700

    Add test for grid unload

--- UnitTests/UnitTests.UWP/Extensions/Test_FrameworkElementExtensions.cs
+++ UnitTests/UnitTests.UWP/Extensions/Test_FrameworkElementExtensions.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Markup;
 
 namespace UnitTests.Extensions
 {
+    [TestClass]
     public class Test_FrameworkElementExtensions : VisualUITestBase
     {
         [TestCategory("FrameworkElementExtensions")]
@@ -48,6 +49,46 @@ namespace UnitTests.Extensions
                 btn.SetValue(FrameworkElementExtensions.AncestorProperty, new object());
 
                 Assert.IsNull(btn.Parent);
+            });
+        }
+
+        [TestCategory("FrameworkElementExtensions")]
+        [TestMethod]
+        public async Task Test_Ancestor_WeakRef_UnloadGrid()
+        {
+            // Need to sim loading the control, and not just load the XAML via XamlReader
+            var view = App.DispatcherQueue.EnqueueAsync(async () =>
+            {
+                var treeRoot = XamlReader.Load(
+           @"<Page
+                    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+                    xmlns:controls=""using:Microsoft.Toolkit.Uwp.UI.Controls""
+                    xmlns:ui=""using:Microsoft.Toolkit.Uwp.UI"">
+
+                    <Grid x:Name=""OuterGrid"" Width=""200"" Background=""Khaki"">
+                        <Button x:Name=""InnerButton"" ui:FrameworkElementExtensions.AncestorType=""Grid"" />
+                    </Grid>
+
+                </Page>") as Page;
+
+                Assert.IsNotNull(treeRoot, "Could not load XAML tree.");
+                await SetTestContentAsync(treeRoot);
+
+                // Need to simulate loading the control (and not just rely upon XamlReader.Load)
+                var frame = new Frame();
+                frame.Navigate(treeRoot.GetType());
+                Assert.IsInstanceOfType(frame.Content, typeof(Page));
+
+                var grid = treeRoot.FindChild("OuterGrid") as Grid;
+                var button = treeRoot.FindChild("InnerButton") as Button;
+
+                button.SetValue(FrameworkElementExtensions.AncestorProperty, new object());
+
+                treeRoot.Unloaded += (sender, e) =>
+                {
+                    Assert.AreEqual(grid, null);
+                };
             });
         }
     }
