6954c0556c4887d14703cfc49274617b13c1bffd
Darren Batchelor <v-dbatchelor@microsoft.com>
2021-10-13 14:08:32 -0700

    Added ConstrainedBox

---
+++ UITests/UITests.Tests.Shared/Controls/ConstrainedBoxTest.cs
@@ -0,0 +1,52 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Windows.Apps.Test.Automation;
+using Microsoft.Windows.Apps.Test.Foundation;
+using Microsoft.Windows.Apps.Test.Foundation.Controls;
+using Windows.UI.Xaml.Tests.MUXControls.InteractionTests.Common;
+using Windows.UI.Xaml.Tests.MUXControls.InteractionTests.Infra;
+
+#if USING_TAEF
+using WEX.Logging.Interop;
+using WEX.TestExecution;
+using WEX.TestExecution.Markup;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+namespace UITests.Tests
+{
+    [TestClass]
+    public class ConstrainedBoxTest : UITestBase
+    {
+        [ClassInitialize]
+        [TestProperty("RunAs", "User")]
+        [TestProperty("Classification", "ScenarioTestSuite")]
+        [TestProperty("Platform", "Any")]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            TestEnvironment.Initialize(testContext, WinUICsUWPSampleApp);
+        }
+
+        [TestMethod]
+        [TestPage("ConstrainedBoxTestPage")]
+        public void SimpleTestMethod()
+        {
+            //ConstrainedBox cb;
+            //var button = new Button(FindElement.ByName("Click Me"));
+            //var textBlock = new TextBlock(FindElement.ById("textBlock"));
+
+            //Verify.IsNotNull(button);
+
+            //Verify.AreEqual(string.Empty, textBlock.GetText());
+
+            //button.Click();
+
+            //Wait.ForIdle();
+
+            //Verify.AreEqual("Clicked", textBlock.GetText());
+        }
+    }
+}
---
+++ UITests/UITests.Tests.Shared/Controls/ConstrainedBoxTestPage.xaml.cs
@@ -0,0 +1,18 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.App.Pages
+{
+    public sealed partial class ConstrainedBoxTestPage : Page
+    {
+        public ConstrainedBoxTestPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
