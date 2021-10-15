3c2123ae23de670bc0907f64924a6d36cd2cb584
Herrick Spencer <herrick.spencer@microsoft.com>
2021-10-15 13:29:06 -0700

    Updating Microsoft.Toolkit.Uwp.SampleApp/SamplePages/ResourceString/ResourceStringXaml.bind Microsoft.Toolkit.Uwp.UI/Extensions/Markup/ResourceStringExtension.cs Per comments in PR

--- Microsoft.Toolkit.Uwp.UI/Extensions/Markup/ResourceStringExtension.cs
+++ Microsoft.Toolkit.Uwp.UI/Extensions/Markup/ResourceStringExtension.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Windows.ApplicationModel.Resources;
+using Windows.ApplicationModel.Resources.Core;
 using Windows.UI.Xaml.Markup;
 
 namespace Microsoft.Toolkit.Uwp.UI
@@ -16,13 +17,13 @@ namespace Microsoft.Toolkit.Uwp.UI
         private static ResourceLoader resourceLoader = ResourceLoader.GetForViewIndependentUse();
 
         /// <summary>
-        /// Gets or sets associated ID from resource strings
+        /// Gets or sets associated ID from resource strings.
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// Gets or sets the associated language resource to use (ie: "es-ES")
-        /// default is the OS language of current view
+        /// Gets or sets the associated language resource to use (ie: "es-ES").
+        /// Defaults to the OS language of current view.
         /// </summary>
         public string Language { get; set; }
 
@@ -33,26 +34,24 @@ namespace Microsoft.Toolkit.Uwp.UI
         }
 
         /// <summary>
-        /// Gets a string value from resource file associated with a resource key
+        /// Gets a string value from resource file associated with a resource key.
         /// </summary>
-        /// <param name="name">Resource key name</param>
-        /// <param name="language">optional language of the associated resource to use (ie: "es-ES")
-        /// default is the OS language of current view</param>
-        /// <returns>a string value from resource file associated with a resource key</returns>
+        /// <param name="name">Resource key name.</param>
+        /// <param name="language">optional language of the associated resource to use (ie: "es-ES").
+        /// default is the OS language of current view.</param>
+        /// <returns>a string value from resource file associated with a resource key.</returns>
         public static string GetValue(string name, string language = "")
         {
             if (string.IsNullOrEmpty(language))
             {
                 return resourceLoader.GetString(name);
             }
-            else
-            {
-                // Not using ResourceContext.GetForCurrentView
-                ResourceContext resourceContext = new() { Languages = new[] { language } };
-                var resourceMap = Windows.ApplicationModel.Resources.Core.ResourceManager.Current.MainResourceMap.GetSubtree("Resources");
 
-                return resourceMap.GetValue(name, resourceContext).ValueAsString;
-            }
+            // Not using ResourceContext.GetForCurrentView nor GetForViewIndependentUse
+            ResourceContext resourceContext = new() { Languages = new[] { language } };
+            var resourceMap = ResourceManager.Current.MainResourceMap.GetSubtree("Resources");
+
+            return resourceMap.GetValue(name, resourceContext).ValueAsString;
         }
     }
 }
