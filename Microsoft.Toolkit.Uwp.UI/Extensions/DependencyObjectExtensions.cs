a174c20e83286fa602bdab69692bec0965483040
Sergio Pedri <sergio0694@live.com>
2021-10-21 15:31:13 +0200

    Added FindAscendantsOrSelf API

--- Microsoft.Toolkit.Uwp.UI/Extensions/DependencyObjectExtensions.cs
+++ Microsoft.Toolkit.Uwp.UI/Extensions/DependencyObjectExtensions.cs
@@ -840,14 +840,45 @@ namespace Microsoft.Toolkit.Uwp.UI
         /// <para>
         /// This method is meant to provide extra flexibility in specific scenarios and it should not
         /// be used when only the first item is being looked for. In those cases, use one of the
-        /// available <see cref="FindAscendant{T}(DependencyObject)"/> overloads instead, which will
-        /// offer a more compact syntax as well as better performance in those cases.
+        /// available <see cref="FindAscendant{T}(DependencyObject)"/> overloads instead,
+        /// which will offer a more compact syntax as well as better performance in those cases.
         /// </para>
         /// </summary>
         /// <param name="element">The root element.</param>
         /// <returns>All the descendant <see cref="DependencyObject"/> instance from <paramref name="element"/>.</returns>
         public static IEnumerable<DependencyObject> FindAscendants(this DependencyObject element)
         {
+            while (true)
+            {
+                DependencyObject? parent = VisualTreeHelper.GetParent(element);
+
+                if (parent is null)
+                {
+                    yield break;
+                }
+
+                yield return parent;
+
+                element = parent;
+            }
+        }
+
+        /// <summary>
+        /// Find all ascendant elements of the specified element (or self). This method can be chained
+        /// with LINQ calls to add additional filters or projections on top of the returned results.
+        /// <para>
+        /// This method is meant to provide extra flexibility in specific scenarios and it should not
+        /// be used when only the first item is being looked for. In those cases, use one of the
+        /// available <see cref="FindAscendantOrSelf{T}(DependencyObject)"/> overloads instead,
+        /// which will offer a more compact syntax as well as better performance in those cases.
+        /// </para>
+        /// </summary>
+        /// <param name="element">The root element.</param>
+        /// <returns>All the descendant <see cref="DependencyObject"/> instance from <paramref name="element"/>.</returns>
+        public static IEnumerable<DependencyObject> FindAscendantsOrSelf(this DependencyObject element)
+        {
+            yield return element;
+
             while (true)
             {
                 DependencyObject? parent = VisualTreeHelper.GetParent(element);
