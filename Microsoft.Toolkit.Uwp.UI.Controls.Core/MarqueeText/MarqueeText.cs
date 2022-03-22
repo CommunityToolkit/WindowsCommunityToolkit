10b3139709fa5a8becf5d91b9210f39b573a404e
Adam Dernis <avid29@live.com>
2022-03-22 12:13:07 -0400

    Bound MarqueeStoryboard to VisualState

--- Microsoft.Toolkit.Uwp.UI.Controls.Core/MarqueeText/MarqueeText.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Core/MarqueeText/MarqueeText.cs
@@ -1,8 +1,8 @@
 using System;
-using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 
 namespace Microsoft.Toolkit.Uwp.UI.Controls
@@ -12,16 +12,27 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
     /// </summary>
     [TemplatePart(Name = CanvasPartName, Type = typeof(Canvas))]
     [TemplatePart(Name = TextBlock1PartName, Type = typeof(TextBlock))]
+    [TemplatePart(Name = MarqueeStoryboardPartName, Type = typeof(Storyboard))]
+    [TemplatePart(Name = MarqueeTransformPartName, Type = typeof(TranslateTransform))]
+    [TemplateVisualState(GroupName = MarqueeStateGroup, Name = MarqueeActiveState)]
+    [TemplateVisualState(GroupName = MarqueeStateGroup, Name = MarqueeStoppedState)]
     [ContentProperty(Name = nameof(Text))]
     public partial class MarqueeText : Control
     {
         private const string CanvasPartName = "Canvas";
         private const string TextBlock1PartName = "TextBlock1";
+        private const string MarqueeStoryboardPartName = "MarqueeStoryboard";
+        private const string MarqueeTransformPartName = "MarqueeTransform";
+
+        private const string MarqueeStateGroup = "MarqueeStateGroup";
+        private const string MarqueeActiveState = "MarqueeActive";
+        private const string MarqueeStoppedState = "MarqueeStopped";
 
         private Canvas _canvas;
         private TextBlock _textBlock1;
-
         private Storyboard _marqueeStoryboad;
+        private VisualState _activeState;
+        private TranslateTransform _marqueeTranform;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MarqueeText"/> class.
@@ -38,6 +49,8 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
 
             _canvas = (Canvas)GetTemplateChild(CanvasPartName);
             _textBlock1 = (TextBlock)GetTemplateChild(TextBlock1PartName);
+            _activeState = (VisualState)GetTemplateChild(MarqueeActiveState);
+            _marqueeTranform = (TranslateTransform)GetTemplateChild(MarqueeTransformPartName);
         }
 
         private void StartAnimation()
@@ -81,10 +94,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
             animation.KeyFrames.Add(frame1);
             animation.KeyFrames.Add(frame2);
             _marqueeStoryboad.Children.Add(animation);
-            Storyboard.SetTarget(animation, _textBlock1.RenderTransform);
+            Storyboard.SetTarget(animation, _marqueeTranform);
             Storyboard.SetTargetProperty(animation, "(TranslateTransform.X)");
 
             _marqueeStoryboad.Begin();
+            _activeState.Storyboard = _marqueeStoryboad;
         }
 
         private void StoryBoard_Completed(object sender, object e)
