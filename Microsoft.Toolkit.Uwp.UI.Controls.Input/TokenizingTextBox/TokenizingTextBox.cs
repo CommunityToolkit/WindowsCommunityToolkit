cd1afd006212a44994d42392aa82ee4ba516b1d8
Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>
2022-10-18 09:27:32 -0700

    fix comment in typo based on PR

--- Microsoft.Toolkit.Uwp.UI.Controls.Input/TokenizingTextBox/TokenizingTextBox.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Input/TokenizingTextBox/TokenizingTextBox.cs
@@ -89,7 +89,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
                     }
                 }
 
-                // Add our text box at the end of items and set it's default value to our initial text, fix for #4749
+                // Add our text box at the end of items and set its default value to our initial text, fix for #4749
                 _currentTextEdit = _lastTextEdit = new PretokenStringContainer(true) { Text = Text };
                 _innerItemsSource.Insert(_innerItemsSource.Count, _currentTextEdit);
                 ItemsSource = _innerItemsSource;
